58e5dc4c56cec6a002b8ef145b94787fa6feb8ab
linas <>
1997-01-11 07:40:40 +0000

    bug fix

--- lang/top.h
+++ lang/top.h
@@ -1,23 +1,48 @@
-
 //
+// FILE:
 // top.h
 //
-// Some utility defines
+// FUNCTION:
+// Utility macro to keep a top-ten list
 //
+// HISTORY:
 // Linas Vepstas January 1997
+
+#ifndef __LAG_TOP_H__
+#define __LAG_TOP_H__
 
 #define UPDATE_TOP_TEN(count,node) {				\
    if (count > top_ten_count[LAG_TOP_TEN-1]) {			\
-      for (i=0; i<LAG_TOP_TEN; i++) {				\
+      for (int i=0; i<LAG_TOP_TEN; i++) {			\
          if (count > top_ten_count[i]) {			\
-            for (j=LAG_TOP_TEN-1; j>i; j--) {			\
+								\
+            /* if slot is empty, use it */			\
+            if (!topten[i]) {					\
+               top_ten_count [i] = count;			\
+               topten[i] = node;				\
+               break;						\
+            }							\
+								\
+            /* search for matching id */			\
+            int j=0;						\
+            int last = LAG_TOP_TEN -1;				\
+            for (j=0; j<LAG_TOP_TEN; j++) {			\
+               if (topten[j]) {					\
+                  if (topten[j]->id == node->id) last = j;	\
+               }						\
+            }							\
+                 						\
+            /* this avoids duplicates in the list */		\
+            for (j=last; j>i; j--) {				\
                topten[j] = topten[j-1];				\
                top_ten_count[j] = top_ten_count[j-1];		\
             }							\
             top_ten_count[i] = count;				\
             topten[i] = node;					\
             break;						\
-          }							\
+         }							\
       }								\
    }								\
 }
+
+#endif // __LAG_TOP_H__
