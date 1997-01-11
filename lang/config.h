fb2f6c1677f88cf8844899a53a23bb9a2b0b2364
linas <>
1997-01-11 07:42:56 +0000

    Initial revision

---
+++ lang/config.h
@@ -0,0 +1,26 @@
+//
+// FILE:
+// config.h
+//
+// FUNCTION:
+// configuration parameters
+//
+// HISTORY:
+// January 1997 Linas Vepstas
+
+#ifndef __LAG_CONFIG_H__
+#define __LAG_CONFIG_H__
+
+#define LAG_HASH_TABLE_SIZE 65536
+#define LAG_WORD_TABLE_SIZE 65536
+
+#define LAG_PAIR_HASH_TABLE_SIZE 65536
+#define LAG_PAIR_TABLE_SIZE 323123
+
+#define LAG_TOP_TEN 30
+   
+#endif // __LAG_CONFIG_H__
+
+// ================= END OF FILE ==================
+
+
