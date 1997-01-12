5791de75c87059ac85c6487cb599db56683577f9
linas <>
1997-01-12 09:00:37 +0000

    changed over to correct cross-referencing scheme:wq

--- lang/analyze.C
+++ lang/analyze.C
@@ -15,7 +15,7 @@
 #include "multihash.h"
 #include "wordhash.h"
 
-#define LAG_USED_SIZE 130
+#define LAG_USED_SIZE 50
 
 class lagTextAnalysis {
    public:
@@ -23,20 +23,23 @@ class lagTextAnalysis {
       void Analyze (FILE *fh);
       void Dump (void);
       void Construct (int);
+   protected:
+      void PrintPhrase (int);
+
    private:
-      lagWordTable *wt;
-      // lagWordQuadTable *pt;
-      lagConcordQuadTable *pt;
+      lagWordTable *word_table;
+      lagWordQuadTable *phrase_table;
+      lagConcordPairTable *xref_table;
       int was_used [LAG_USED_SIZE];
 };
 
 // =====================================================
 
 lagTextAnalysis :: lagTextAnalysis (void) {
-   wt = new lagWordTable;
-   // pt = new lagWordTripleTable;
-   // pt = new lagWordQuadTable;
-   pt = new lagConcordQuadTable;
+   word_table = new lagWordTable;
+   // phrase_table = new lagWordTripleTable;
+   phrase_table = new lagWordQuadTable;
+   xref_table = new lagConcordPairTable;
 
    int i=0;
    for (i=0; i<LAG_USED_SIZE; i++) {
@@ -56,9 +59,21 @@ void lagTextAnalysis :: Analyze (FILE *fh) {
    int clast_word_id = 0;
    int dlast_word_id = 0;
    int elast_word_id = 0;
+
+   int this_phrase_id = 0;
+   int last_phrase_id = 0;
+   int blast_phrase_id = 0;
+   int clast_phrase_id = 0;
+   int dlast_phrase_id = 0;
+   int elast_phrase_id = 0;
+
+   int lineno = 0;
    while (!feof(fh)) {
       fgets (buff, 5000, fh);
       buff[4999] = 0x0;
+
+      lineno ++;
+      if (0 == lineno%1000) { printf ("."); fflush (stdout); }
 
       int i = 0;
       char * word = buff;
@@ -74,9 +89,18 @@ void lagTextAnalysis :: Analyze (FILE *fh) {
                blast_word_id = last_word_id;
                last_word_id = this_word_id;
             }
-            this_word_id = wt -> GetWordID (word); 
-            // pt -> GetID (blast_word_id, last_word_id, this_word_id);
-            pt -> GetID (clast_word_id, blast_word_id, last_word_id, this_word_id);
+            this_word_id = word_table -> GetWordID (word); 
+
+            if (this_phrase_id) {
+               elast_phrase_id = dlast_phrase_id;
+               dlast_phrase_id = clast_phrase_id;
+               clast_phrase_id = blast_phrase_id;
+               blast_phrase_id = last_phrase_id;
+               last_phrase_id = this_phrase_id;
+            }
+
+            this_phrase_id = phrase_table -> GetID (clast_word_id, blast_word_id, last_word_id, this_word_id);
+            xref_table -> GetID (dlast_phrase_id, this_phrase_id);
             word = &buff[i+1];
          } else {
             buff[i] = tolower (buff[i]);
@@ -85,28 +109,58 @@ void lagTextAnalysis :: Analyze (FILE *fh) {
       }
    }
 
+   printf ("\n");
+}
+
+// =====================================================
+
+void lagTextAnalysis :: PrintPhrase (int phrase_id) {
+   if (!phrase_id) return;
+
+   for (int j=0; j<4; j++) {
+      int word_id = phrase_table -> GetElt (phrase_id, j);
+      char * word = word_table -> GetWordFromID (word_id);
+      printf (" %s", word);
+   }
 }
 
 // =====================================================
 
 void lagTextAnalysis :: Dump (void) {
-   wt -> Dump();
-   pt -> Dump();
+   word_table -> Dump();
+   phrase_table -> Dump();
+   xref_table -> Dump();
+
+   printf ("\n");
+   printf ("Info: lagTextAnalysis::Dump(): \n");
+   printf ("top %d pharses: \n", LAG_TOP_TEN);
 
    int i = 0;
    for (i=0; i<LAG_TOP_TEN; i++) {
-      int top = pt -> GetTopTen (i);
+      int top = phrase_table -> GetTopTen (i);
 
-      int fi = pt -> GetElt (top, 0);
-      int se = pt -> GetElt (top, 1);
-      int th = pt -> GetElt (top, 2);
-      int fo = pt -> GetElt (top, 3);
-      int cnt = pt -> GetCount (top);
-      char * wfi = wt -> GetWordFromID (fi);
-      char * wse = wt -> GetWordFromID (se);
-      char * wth = wt -> GetWordFromID (th);
-      char * wfo = wt -> GetWordFromID (fo);
-      printf (" its id=%d cnt=%d %s %s %s %s \n", top, cnt, wfi, wse, wth, wfo);
+      int cnt = phrase_table -> GetCount (top);
+      printf ("top phrase %d cnt=%d ", top, cnt);
+
+      PrintPhrase (top);
+      printf ("\n");
+   }
+
+   printf ("\n");
+   printf ("Info: lagTextAnalysis::Dump(): \n");
+   printf ("top %d xrefs: \n", LAG_TOP_TEN);
+
+   for (i=0; i<LAG_TOP_TEN; i++) {
+      int top = xref_table -> GetTopTen (i);
+
+      int fi = xref_table -> GetElt (top, 0);
+      int se = xref_table -> GetElt (top, 1);
+      int cnt = xref_table -> GetCount (top);
+      printf ("top xref %d cnt=%d \n", top, cnt);
+
+      PrintPhrase (fi);
+      PrintPhrase (se);
+      printf ("\n");
    }
 }
 
@@ -120,41 +174,33 @@ void lagTextAnalysis :: Construct (int top) {
    }
 
    // construct string
-   int pair = pt -> GetTopTen (top);
-   int fi = pt -> GetElt (pair, 0);
-   char * wfi = wt -> GetWordFromID (fi);
-   printf ("%s ", wfi);
-
+   int phrase = phrase_table -> GetTopTen (top);
+   int xref = 0;
    int j=0;
-   while (pair) {
+   while (phrase) {
 
       // avoid infinite loops
       for (i=0; i<LAG_USED_SIZE; i++) {
-         if (pair == was_used [i]) {
+         if (phrase == was_used [i]) {
             return;
          }
          if (!was_used[i]) {
-            was_used[i] = pair;
+            was_used[i] = phrase;
             break;
          }
+         if (LAG_USED_SIZE-1 == i) return;
       }
       
-      int se = pt -> GetElt (pair, 1);
-      int th = pt -> GetElt (pair, 2);
-      int fo = pt -> GetElt (pair, 3);
-      if (!se) break;
-      if (!th) break;
-      if (!fo) break;
-      char * wse = wt -> GetWordFromID (se);
-      char * wth = wt -> GetWordFromID (th);
-      char * wfo = wt -> GetWordFromID (fo);
-      printf ("%s %s %s ", wse, wth, wfo);
+      PrintPhrase (phrase);
       j++;
       if (3 == j) {
          j = 0;
          printf ("\n");
       }
-      pair = pt -> GetTopTupleContainingWord (fo);
+ 
+      xref = xref_table -> GetTopTupleContainingWord (phrase);
+      if (!xref) break;
+      phrase = xref_table -> GetElt (xref, 1); 
    }
 }
 
@@ -171,8 +217,6 @@ main () {
       printf ("\n\n =========== sentance %d =========== \n", i);
       texan -> Construct (i);
    }
-
-
 }
 
 // ========================== END OF FILE ==================
