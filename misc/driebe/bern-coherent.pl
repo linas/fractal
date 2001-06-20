9542e4607a1f8eec76414fa75399f4b54764a3b7
linas <>
2001-06-20 04:39:24 +0000

    updates

--- misc/driebe/bern-coherent.pl
+++ misc/driebe/bern-coherent.pl
@@ -1,12 +1,13 @@
 #! /usr/bin/perl
 
-# coherent eigenstates of bernoulli map
+# coherent eigenstates of frobenius-perron operator of bernoulli map
+# as per dean driebe
 
-$nsteps = 50;
+$nsteps = 5350;
 $l = 0;
 
 $nmax = 100;
-$z = 0.75;
+$z = 0.995;
 
 $twopi= 2.0 * 3.14159265358979;
 $tl = 2*$l +1;
@@ -20,13 +21,13 @@ for ($i=0; $i<=$nsteps; $i ++)
    $zn = 1;
    for ($n=0; $n < $nmax; $n++)
    {
-      $c = cos ($twopi * $tl * $tpn * $x);
-      $e += $zn *$c;
+      $c = $zn * cos ($twopi * $tl * $tpn * $x);
+      $e += $c;
       # print "          $tpn    $c    $e \n";
 
       $tpn *= 2;
       $zn *= $z;
    }
-   print "$x     $e\n";
+   print "$x	$e\n";
 
 }
