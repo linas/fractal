d0a6060999ef9d20114d61f0989a4d948a325535
linas <>
2001-07-09 15:45:46 +0000

    better binnning

--- misc/stern-gerlach/bin.pl
+++ misc/stern-gerlach/bin.pl
@@ -1,19 +1,27 @@
 #! /usr/bin/env perl
 
-$binsz = 0.2;
+$intervals = 10;
+
+$binsz = 3.141592653 / $intervals;
+
+for ($i=0;$i<$intervals; $i++) {
+   $bin[$i] = 0;
+}
 
 while (<>) {
    chop;
    ($nprec, $i, $j, $init, $theta) = split /	/;
-   
+
+   if (($j < 50000000) && ($j > 0)) { next; }   
+
    $n = $theta / $binsz;
    $n = int ($n);
 
    $bin[$n] ++;
 }
 
-for ($i=0;$i<3.14/$binsz; $i++) {
-   $theta = $i * $binsz;
+for ($i=0;$i<$intervals; $i++) {
+   $theta = ($i+0.5) * $binsz;
    print "$theta	$bin[$i]\n";
 }
 
