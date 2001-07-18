f9dc7c0f220ae755b744104d8b5ab6794b57a8c7
linas <>
2001-07-18 03:56:55 +0000

    compute deans legendre's

---
+++ misc/driebe/legendre.scm
@@ -0,0 +1,12 @@
+
+;; compute driebes rescale legendre's
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+   (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count )))
