81d5562fbbaf78a49785a57654e671f9fb44a3df
linas <>
2001-07-18 04:49:21 +0000

    finishing touches

--- misc/driebe/legendre.scm
+++ misc/driebe/legendre.scm
@@ -1,6 +1,9 @@
 
-;; legendre.scm
-;; compute driebes rescale legendre's
+;; (load-module  'legendre.scm)
+;;
+;; compute driebes eqn (A.33) integral of rescaled legendre's
+;;
+;; 
 
 
 ;; define factorial function
@@ -14,16 +17,47 @@
                     max-count )))
      (iter 1 1 n))
 
+;; x raised to power y
 (define (pow x y)
     (exp (* y (log x))))
 
+;; x raised to integer power n
+(define (ipow x n)
+   (define (iter product x counter max-count)
+      (if (>= counter max-count)
+         product
+         (iter (* x product) x
+                    (+ counter 1)
+                    max-count )))
+    (iter 1 x 0 n))
+      
+
+;; #t if n is odd
+(define (odd n)  
+    (= n (* 2 (floor (/ n 2)))) )
+
+;; -1 if n odd, +1 if n even
+(define (sgn n)  
+    (if (odd n) 1 -1) )
+
+
 (define (I m n)
-   (define (sgn l)  1)
    (define (term m n l)
-           (/ (* (sgn l) (pow 0.5 l) (factorial (+ m n l)) )
-            ( * (factorial (- (- n m) l)) 
+           (/ (* (sgn l) (factorial (+ m n l)) )
+            ( * (ipow 2 l)
+                (factorial (- (- n m) l)) 
                 (factorial (+ (* 2 m) l 1)) 
                 (factorial l) ) 
             )
            )
-   (term m n 0))
+   (define (sum m n)
+      (define (iter sm m n l max-count)
+         (if (> l max-count)
+            sm
+            (iter (+ (term m n l) sm)  
+                       m n 
+                       (+ l 1)
+                       max-count )))
+        (iter 0 m n 0 (- n m)))
+   ( / (* (sgn (+ m n)) (sqrt (* (+ (* 2 m) 1) (+ (* 2 n) 1))) (sum m n))
+       (ipow 2 m)))
