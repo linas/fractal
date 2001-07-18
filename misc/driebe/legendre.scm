a7c561e549de35ace9a749d9f67af3869c8eee35
linas <>
2001-07-18 04:20:12 +0000

    more

--- misc/driebe/legendre.scm
+++ misc/driebe/legendre.scm
@@ -1,12 +1,29 @@
 
+;; legendre.scm
 ;; compute driebes rescale legendre's
 
-(define (factorial n)
-  (fact-iter 1 1 n))
 
-(define (fact-iter product counter max-count)
-   (if (> counter max-count)
-      product
-      (fact-iter (* counter product)
-                 (+ counter 1)
-                 max-count )))
+;; define factorial function
+
+(define (factorial n)
+   (define (iter product counter max-count)
+      (if (> counter max-count)
+         product
+         (iter (* counter product)
+                    (+ counter 1)
+                    max-count )))
+     (iter 1 1 n))
+
+(define (pow x y)
+    (exp (* y (log x))))
+
+(define (I m n)
+   (define (sgn l)  1)
+   (define (term m n l)
+           (/ (* (sgn l) (pow 0.5 l) (factorial (+ m n l)) )
+            ( * (factorial (- (- n m) l)) 
+                (factorial (+ (* 2 m) l 1)) 
+                (factorial l) ) 
+            )
+           )
+   (term m n 0))
