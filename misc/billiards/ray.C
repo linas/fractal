7fffdcdcfadcfb22606fff68607f2db6862d1320
linas <>
2001-11-18 12:57:15 +0000

    updates

--- misc/billiards/ray.C
+++ misc/billiards/ray.C
@@ -8,13 +8,22 @@
 // sphere has radius < 1.0
 
 
+#include "vvector.h"
 
 class Ray
 {
    public:
+      void Set (double x[3], double t[3]);
+   public:
       double position[3];   
       double direction[3];
 };
+
+void Ray::Set (double x[3], double t[3])
+{
+   VEC_COPY(position, x);
+   VEC_COPY(direction,  t);
+}
 
 class SinaiRay
   : public Ray
@@ -32,6 +41,8 @@ class SinaiView
       int nx;  // dimension of pixel grid
       int ny;
 
+      double eye[3]; // position of eye
+
    private:
       SinaiRay **sr;
 
@@ -42,9 +53,15 @@ SinaiView::SinaiView (int px, int py)
    nx = px;
    ny = py;
 
+
+   eye[0] = 0.0;
+   eye[1] = 0.0;
+   eye[2] = -5.0;
+
    sr = new (SinaiRay *) [nx];
    for (int i = 0; i<nx; i++) sr[i] = new SinaiRay[ny];
-   
+
+
 
 }
 
@@ -52,6 +69,8 @@ main ()
 {
     SinaiView v (400,400);
 
+
+    
 
 }
 
