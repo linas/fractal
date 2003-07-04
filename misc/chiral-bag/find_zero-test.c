60db8af8123d2fb04e9df9570409211203f5683f
linas <>
2003-07-04 19:09:33 +0000

    add

---
+++ misc/chiral-bag/find_zero-test.c
@@ -0,0 +1,18 @@
+
+/* quick and dirty test */
+
+#include <math.h>
+#include <stdio.h>
+
+#include "find_zero.h"
+
+int main ()
+{
+	double p;
+
+	p = FindZero (sin, 0.0, 15, 0.1, 6.1, 100);
+
+	printf ("found root w/ error=%g\n", p-M_PI);
+
+	return 0;
+}
