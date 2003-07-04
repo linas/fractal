94721a4311a8d60c932792d91ba72382e85e8130
linas <>
2003-07-04 16:37:16 +0000

    start converting to C

--- misc/chiral-bag/bessel.c
+++ misc/chiral-bag/bessel.c
@@ -1,63 +1,144 @@
-C       ******************************************************************
-c	1983 VAX/VMS 11/780 at SUNYSBNP
-C	1987- Prime os 19.1  Sacaly, France.  The Goddamn Compiler
-C		doesn't like for-- end for structures.  Revert to 
-c		using line numbers.
-C	1987- IBM PC -- MSDOS (Saclay, France; Krakow, Poland)
-c	1988 vax/vmx 11/780 (visitng StonyBrook)
-C	1989- IBM RT/ AIX 2.2.1   -- F77 doesn't like underscores
+/*******************************************************************
+ * FILE: bessel.c
+ * FUNCTION: Compute spherical bessel functions
+ * HISTORY: Created by Linas Vepstas <linas@linas.org> in 1983
+ * c	1983 VAX/VMS 11/780 at SUNYSBNP
+ * C	1987- Prime os 19.1  Sacaly, France.  The Goddamn Compiler
+ * C		doesn't like for-- end for structures.  Revert to 
+ * c		using line numbers.
+ * C	1987- IBM PC -- MSDOS (Saclay, France; Krakow, Poland)
+ * c	1988 vax/vmx 11/780 (visitng StonyBrook)
+ * C	1989- IBM RT/ AIX 2.2.1   -- F77 doesn't like underscores
+******************************************************************/
 
-        SUBROUTINE BESSEL (N, X, BESS)
+#include <math.h>
 
+/* ================================================================== */
+/*
+        SUBROUTINE BesselRecurrenceStartRegion (Z, NORDER, NSTART)
+C       GIVEN AN ARGUMENT X AND ORDER NORDER, THE PARENT ROUTINE USES A
+C       BACKWARDS RECURRENCE RELATION TO COMPUTE BESSEL FUNCTIONS.
+C       IN ORDER TO OBTAIN AND ACCURATE ANSWER, THE RECURRENCE MUST BE
+C       STARTED AT A HIGH ENOUGH ORDER SUCH THAT SUCESSIVE TERMS BECOME
+C       BETTER AND BETTER APPROXIMATIONS TO THE TRUE RECURRENCE RELATION
+C       J(N+1)(Z) + J(N-1)(Z) = ((2N+1)/Z) * JN(Z)
+C       I.E. ONE NEEDS TO START IN THE "ASYMPTOTIC REGION" OF SMALL X
+C       AND LARGE ORDER, WHERE THE LARGE ORDER BESSEL FUN. HAS NOT YET
+C       ENCOUNTERED ITS FIRST ZERO. OUR VERY CRUDE ESTIMATE FOR THIS
+C       REGION IS NSTART = MAX (2*X, N+10)
+C       (ABROMOWIZ + STEGUN, PAGE 452FF)
+
+C       THIS ROUTINE ACCEPTS THE VALUES X AND NORDER, AND RETURNS THE
+C       DESIRED STARTING POINT NSTART.
+
+C       NOTES:
+C       1) THE BESSEL ROUTINE WILL NOT WORK RELIABLY FOR ARGUMENTS
+C               LESS THAN 2.0E-6 OR SO
+C       2) THE IRREGULAR BESSEL FUNCTIONS WILL OVERFLOW (1.0E-30 OR SO)
+C               FOR EXCESSIVELY SMALL ARGUMENTS (LIKEWISE, THE REGULAR
+C               FUNCTIONS WILL UNDERFLOW FOR THE SAME VALUES).  THEREFORE,
+C               A LOWER BOUND ON THE ARGUMENT HAS BEEN FIXED, DETERMINED
+C               IN PART BY THE SMALL ARGUMENT EXPANSIONS FOR THE BESSEL
+C               FUNCTIONS (REF: ABROMOWITZ + STEGUN). THE ARGUMENT RETURNED
+C               BY THIS ROUTINE HAS BEEN BOUNDED BELOW BY THIS "SMALL"
+C               BOUNDING ARGUMENT (WHICH MAY BE QUITE LARGE, FOR LARGE ORDERS)
+*/
+
+#define fmax(x,y) ((x)>(y))?(x):(y)
+
+int 
+BesselRecurrenceStartRegion (double z, int norder)
+{
+	int nstart;
+	double az, an, zmin;
+
+	az = fabs(z);
+	an = (double)(norder + 1);
+	zmin = (-60.0 + an * (log(an)-1.0)) / an;
+	zmin = 2.0 * exp (zmin + 12.5 / (an*an)) + 1.0e-6;
+	az = fmax (zmin, az);
+	z = copysign (az, z);
+
+	peritr = 2.0 * an / az;
+	if (3.0 < peritr)
+	{
+		double extra = 20.0 / log (peritr);
+		nstart = norder + 8 + (int) (extra);
+	}
+	else 
+	{
+		iaz = (int) az;
+		if (33 <= iaz) { nstart = 2 * iaz; }
+		else if (13 <= iaz) { nstart = 3 * iaz; }
+		else if (7 <= iaz) { nstart = 4 * iaz; }
+		else { nstart = 6 * iaz +20; }
+	}
+	return nstart;
+}
+
+/* ================================================================== */
+/*
 C       THIS SUBROUTINE CALCULATES THE SPHERICAL BESSEL FUNCTION FOR
 C       ALL ORDERS .LE. N FOR (POSITIVE) ARGUMENT Z
 C       THE RESULTS ARE RETURNED IN THE ARRAY BESS (0:N)
+*/
+void 
+bessel (int n, double x, double *bess)
+{
+#define ARRSZ 650
+	double tmp [ARRSZ];
+	int i, m, mon[ARRSZ];
+	double c, z, oneoz, scale;
 
-        IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-        DOUBLE PRECISION BESS (0:N)
-        DOUBLE PRECISION TMP (0:650)
-        INTEGER * 4 MON (0:650)
-
+/*
 C       WE USE BACKWARD RECURENCE TO FIND THE BESSEL FUNCTIONS
 C       J(N+1)(Z) + J(N-1)(Z) = ((2N+1)/Z) * JN(Z)
 C       (ABROMOWIZ + STEGUN, PAGE 452FF)
 C       THE RECURRENCE RELATION MUST BE STARTED AT A HIGH ORDER.
 C       SEE COMMENT CARDS IN THE SUBROUTINE "BesselRecurrenceStartRegion"
-
-        Z = X
-        CALL BesselRecurrenceStartRegion (Z, N, NSTART)
-        IF (NSTART .GT. 650) STOP 'BESSEL TEMPORARY ARRAY OUT OF BOUNDS'
-        TMP (NSTART) = 0.0
-        TMP (NSTART-1) = 1.0D-30
-        MON (NSTART-1) = 0
-        ONEOZ = 1.0D0 / Z
-        DO 1001 I = NSTART-1, 1, -1
-                TMP (I-1) = ONEOZ * DBLE (2*I+1) * TMP(I) - TMP (I+1)
-                MON (I-1) = MON (I)
-                IF (DABS (TMP (I-1)) .GT. 1.0D+20) THEN
-                        TMP (I) = TMP (I) * 1.0D-30
-                        TMP (I-1) = TMP (I-1) * 1.0D-30
-                        MON (I) = MON (I+1) + 30
-                        MON (I-1) = MON (I)
-                END IF
-1001    CONTINUE
-        C = DSIN (Z) * ONEOZ
-        C = C/ TMP(0)
-        M = MON (0)
+*/
+	z = x;
+	nstart = BesselRecurrenceStartRegion (z,n);
+	if (ARRSZ <= nstart)
+	{
+		printf ("BESSEL TEMPORARY ARRAY OUT OF BOUNDS\n");
+		return;
+	} 
+	tmp[nstart] = 0.0;
+	tmp[nstart-1] = 1.0E-30;
+	mon[nstart-1] = 0;
+	oneoz = 1.0/z;
+	for (i=nstart-1; i>0; i--)
+	{
+		tmp [i-1] = oneoz * ((double) (2*i+1) * tmp[i] - tmp [i+1];
+		mon [i-1] = mon [i];
+		if (fabs (tmp [i-1]) > 1.0E+20)
+		{
+			tmp [i] = tmp [i] * 1.0D-30;
+			tmp [i-1] = tmp [i-1] * 1.0D-30;
+			mon [i] = mon [i+1] + 30;
+			mon [i-1] = mon [i];
+		}
+	}
+	c = sin (z) * oneoz;
+	c = c/ tmp[0];
+   m = mon [0];
+/*
 C       RENORMALIZE THE VALUES.  SINCE EXPONENTIATION TAKES ABOUT A FACTOR
 C       OF 20 MORE CPU TIME THAN IF STATEMENTS, OR MULTIPLICATION,
 C       I'VE RIGGED A LITTLE SCHEME HERE WHEN ALL I REALLY WANT TO DO IS
 C       BESS (I) = C * TMP(I) * (10.0D0 ** (MON(I)-M))
-        SCALE = C
-        BESS (0) = SCALE * TMP(0)
-        DO 1002 I = 1, N
-                IF (MON (I) .NE. MON (I-1))
-     +           SCALE = C * (10.0D0 ** (MON(I)-M))
-                BESS (I) = SCALE * TMP(I)
-1002    CONTINUE
-        RETURN
-        END
+*/
+	scale = c;
+	bess [0] = scale * tmp[0];
+	for (i=1; i<=n; i++)
+	{
+		if (mon[i] != mon[i-1]) { scale = c * pow (10.0, (double) (mon[i]-m)); }
+		bess[i] = scale * tmp[i];
+	}
+}
 
+#if 0
 C       ******************************************************************
 
         SUBROUTINE BESSNEU (N, X, BREGULAR, BIRREG)
@@ -294,59 +375,4 @@ C       FOR N=0, THEN NEU (-1) IS SET TO ZERO
         END
 
 C       ************************************************************
-
-        SUBROUTINE BesselRecurrenceStartRegion (Z, NORDER, NSTART)
-        IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-
-C       GIVEN AN ARGUMENT X AND ORDER NORDER, THE PARENT ROUTINE USES A
-C       BACKWARDS RECURRENCE RELATION TO COMPUTE BESSEL FUNCTIONS.
-C       IN ORDER TO OBTAIN AND ACCURATE ANSWER, THE RECURRENCE MUST BE
-C       STARTED AT A HIGH ENOUGH ORDER SUCH THAT SUCESSIVE TERMS BECOME
-C       BETTER AND BETTER APPROXIMATIONS TO THE TRUE RECURRENCE RELATION
-C       J(N+1)(Z) + J(N-1)(Z) = ((2N+1)/Z) * JN(Z)
-C       I.E. ONE NEEDS TO START IN THE "ASYMPTOTIC REGION" OF SMALL X
-C       AND LARGE ORDER, WHERE THE LARGE ORDER BESSEL FUN. HAS NOT YET
-C       ENCOUNTERED ITS FIRST ZERO. OUR VERY CRUDE ESTIMATE FOR THIS
-C       REGION IS NSTART = MAX (2*X, N+10)
-C       (ABROMOWIZ + STEGUN, PAGE 452FF)
-
-C       THIS ROUTINE ACCEPTS THE VALUES X AND NORDER, AND RETURNS THE
-C       DESIRED STARTING POINT NSTART.
-
-C       NOTES:
-C       1) THE BESSEL ROUTINE WILL NOT WORK RELIABLY FOR ARGUMENTS
-C               LESS THAN 2.0E-6 OR SO
-C       2) THE IRREGULAR BESSEL FUNCTIONS WILL OVERFLOW (1.0E-30 OR SO)
-C               FOR EXCESSIVELY SMALL ARGUMENTS (LIKEWISE, THE REGULAR
-C               FUNCTIONS WILL UNDERFLOW FOR THE SAME VALUES).  THEREFORE,
-C               A LOWER BOUND ON THE ARGUMENT HAS BEEN FIXED, DETERMINED
-C               IN PART BY THE SMALL ARGUMENT EXPANSIONS FOR THE BESSEL
-C               FUNCTIONS (REF: ABROMOWITZ + STEGUN). THE ARGUMENT RETURNED
-C               BY THIS ROUTINE HAS BEEN BOUNDED BELOW BY THIS "SMALL"
-C               BOUNDING ARGUMENT (WHICH MAY BE QUITE LARGE, FOR LARGE ORDERS)
-
-        AZ = DABS (Z)
-        AN = DBLE (NORDER + 1)
-        ZMIN = (-60.0D0 + AN * (DLOG(AN)-1.0D0)) / AN
-        ZMIN = 2.0D0 * DEXP (ZMIN + 12.5D0 / (AN*AN)) + 1.0D-6
-        AZ = DMAX1 (ZMIN, AZ)
-        Z = DSIGN (AZ, Z)
-
-        PERITR = 2.0D0 * AN / AZ
-        IF (PERITR .GT. 3.0D0) THEN
-                EXTRA = 20.0D0 / DLOG (PERITR)
-                NSTART = NORDER + 8 + IDNINT (EXTRA)
-        ELSE
-                IAZ = INT (AZ)
-                IF (IAZ .GE. 33) THEN
-                        NSTART = 2 * IAZ
-                ELSE IF (IAZ .GE. 13) THEN
-                        NSTART = 3 * IAZ
-                ELSE IF (IAZ .GE. 7) THEN
-                        NSTART = 4 * IAZ
-                ELSE
-                        NSTART = 6 * IAZ + 20
-                END IF
-        END IF
-        RETURN
-        END
+#endif
