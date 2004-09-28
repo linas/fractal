9eb048c3b84609695202ec137253021877fcf373
linas <>
2004-09-28 14:43:52 +0000

    initial checkin

---
+++ misc/continued-frobenius/matrix/eigen.c
@@ -0,0 +1,215 @@
+
+/*
+ * Eigenvalue finding
+ * Call lapack from C code.
+ *
+ * Get Schur factorization,
+ * G = OTO^ where O is orthogonal (O^ == O transpose)
+ * T is triangular.
+ *
+ * Use LAPACK xGEEV routine
+ * specifically, real double precision:
+ * DGEEV
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+#include "ache.h"
+
+extern void dgeev_ (char *jobvl, // compute left eigenvecs
+                   char *jobvr,  // compute right eigenvecs
+						 int *n,       // order of the matrix
+						 double *a,    // the matrix
+						 int *lda,     // leading dimension ??
+						 double *wr,   // dim n real part of eigenvalues
+						 double *wi,   // dim n imag part of eigenvalues
+						 double *vl,   // left eigenvectors 
+						 int *ldvl,    // dim of left eignevecs
+						 double *vr,   // right eignevecs
+						 int * ldvr,   // dim of right eigenvec array
+						 double *work, // workspace
+						 int * lwork,  // size of workspace, >= 4*N
+						 int * info);  // returned success code
+
+
+int 
+getworkdim (int dim, double *matrix,
+        double *eigenvalues_re, double *eigenvalues_im, 
+		  double *left_eigen, double *right_eigen,
+		  double *workspace)
+{
+
+	char jobvl = 'V';
+	char jobvr = 'V';
+	int workdim;
+	int info;
+
+	workdim = -1;
+
+	dgeev_ (&jobvl, &jobvr, &dim, matrix, &dim, 
+	       eigenvalues_re, eigenvalues_im, 
+			 left_eigen, &dim, right_eigen, &dim, 
+			 workspace, &workdim, &info);
+
+	return (int) (workspace[0]+0.5);
+}
+
+int 
+geteigen (int dim, double *matrix, 
+        double *eigenvalues_re, double *eigenvalues_im, 
+		  double *left_eigen, double *right_eigen,
+		  int workdim, double *workspace)
+{
+
+	char jobvl = 'V';
+	char jobvr = 'V';
+	int info;
+
+	dgeev_ (&jobvl, &jobvr, &dim, matrix, &dim, 
+	       eigenvalues_re, eigenvalues_im, 
+			 left_eigen, &dim, right_eigen, &dim, 
+			 workspace, &workdim, &info);
+
+}
+
+double 
+kino (int m, int n)
+{
+	if (m != n-2) return 0.0;
+	return (double) (n*(n+1));
+}
+
+main () 
+{
+	double *mat;
+	double *ere;
+	double *eim;
+	double *lev;
+	double *rev;
+	double *work;
+	int dim;
+	int workdim;
+	int i,j, k;
+	
+	dim = 30;
+
+	printf ("#\n#\n");
+	printf ("# Eigenvectors of the GKW (Gauss Kuz'min Wirsing) Operator\n");
+	printf ("# Numerically solved to rank=%d\n", dim);
+	printf ("#\n#\n");
+
+	mat = (double *) malloc (dim*dim*sizeof (double));
+	ere = (double *) malloc (dim*sizeof (double));
+	eim = (double *) malloc (dim*sizeof (double));
+	lev = (double *) malloc (dim*dim*sizeof (double));
+	rev = (double *) malloc (dim*dim*sizeof (double));
+	workdim = 4*dim*dim;
+	work = (double *) malloc (workdim*sizeof (double));
+
+	/* Insert values for the GKW operator at x=1 (y=1-x) */
+	for (i=0; i<dim; i++)
+	{
+		for (j=0; j<dim; j++)
+		{
+			/* Note transposed matrix'ing for FORTRAN */
+			mat[i+j*dim] = ache_mp(i,j);
+		}
+	}
+
+#if POORLY_CONDITIONED_BOMB_OUT_KINETIC
+	/* Add in the kinetic term ! */
+	for (i=0; i<dim; i++)
+	{
+		for (j=0; j<dim; j++)
+		{
+			/* Note transposed matrix'ing for FORTRAN */
+			mat[i+j*dim] -= kino(i,j);
+		}
+	}
+#endif
+	
+	int wd = getworkdim (dim, mat, ere, eim, lev, rev, work);
+	printf ("# recommended dim=%d actual dim=%d\n#\n", wd, workdim);
+	workdim = wd;
+	
+	work = (double *) realloc (work, workdim*sizeof (double));
+	geteigen (dim, mat, ere, eim, lev, rev, workdim, work);
+
+	/* ---------------------------------------------- */
+	/* print the eigenvalues */
+	for (i=0; i<dim; i++)
+	{
+		printf ("# eigen[%d]=%20.15g +i %g\n", i, ere[i], eim[i]);
+	}
+	printf ("\n\n");
+	
+	int prtdim = 6;
+	for (i=0; i<prtdim; i++)
+	{
+		for (j=0; j<prtdim; j++)
+		{
+			printf ("# right %d'th eigenvector[%d]=%g (normalized=%g)\n", 
+			            i,j, rev[j+i*dim], rev[j+i*dim]/rev[i*dim]);
+		}
+		printf ("#\n");
+	}
+	
+	for (i=0; i<prtdim; i++)
+	{
+		for (j=0; j<prtdim; j++)
+		{
+			printf ("# left %d'th eigenvector[%d]=%g (normalized=%g)\n", 
+			            i,j, lev[j+i*dim], lev[j+i*dim]/lev[i*dim]);
+		}
+		printf ("#\n");
+	}
+	
+	/* ---------------------------------------------- */
+	/* Verify i'th eigenvector */
+	for (i=0; i<prtdim; i++)
+	{
+		/* The j'th element of the i'th eigenvector */
+		for (j=0; j<prtdim; j++)
+		{
+			double sum = 0.0;
+			for (k=0; k<dim; k++)
+			{
+				sum += ache_mp(j,k) * rev[k+i*dim];
+			}
+			sum /= rev[j+i*dim];
+			printf ("# %d'th eigenvec validation [%d]=%g\n", i, j, sum);
+		}
+		printf ("#\n");
+	}
+
+	/* ---------------------------------------------- */
+	/* Print graphable data */
+	double y;
+	for (y=1.0; y>=0.0; y-=0.02)
+	{
+		double x = 1.0-y;
+		printf ("%g", x);
+		// validate that the zeroth eignevec is 1/(1+x)
+		// printf ("\t%g", 1.73205/(1.0+x));
+		for (i=0; i<prtdim; i++)
+		{
+			double yn = 1.0;
+			double sum = 0.0;
+			double fact = 1.0;
+			/* The j'th element of the i'th eigenvector */
+			for (j=0; j<dim; j++)
+			{
+				// Factorial not needed, ache already has the factorial folded in.
+				// See, for example, the derives of seroth eigenvec. 
+				// sum += yn * rev[j+i*dim] / fact;
+				sum += yn * rev[j+i*dim];
+				// printf ("duuude j=%d fact=%g yn=%g\n", j, fact, yn);
+				yn *= y;
+				fact *= j+1;
+			}
+			printf("\t%g", -sum);
+		}
+		printf ("\n");
+	}
+}
