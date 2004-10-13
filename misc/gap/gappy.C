e3ea3a794b5fb99eb8100925f1bac24e3ec75a68
linas <>
2004-10-13 03:37:57 +0000

    whatever fixes

--- misc/gap/gappy.C
+++ misc/gap/gappy.C
@@ -12,22 +12,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NV 6
+
 main (int argc, char *argv[])
 {
 	ContinuedFraction f;
-	double w, t, xp, xm;
-	int n,d;
+	double w, t, gap[NV];
+	int n,d,iw;
 
-	n = atoi (argv[1]);
-	d = atoi (argv[2]);
+   if (argc <2) {
+      printf ("Usage: %s <denom> \n", argv[0]);
+      exit (1);
+   }
+
+	d = atoi (argv[1]);
+	printf ("#\n# denom=%d\n#\n", d);
   
-	t = ((double) n) / ((double) d);
-   f.SetRatio (n,d);
-
-	for (w=0.0; w<2.0; w++)
+	for (n=0; n<=d; n++)
 	{
-		xm = f.ToXMinus(w);
-		xp = f.ToXPlus(w);
-		printf ("its t=%g w=%g %g %g \n", t, w, xm, xp);
+		t = ((double) n) / ((double) d);
+   	f.SetRatio (n,d);
+
+		for (iw=0; iw<NV; iw++)
+		{
+			w = (double) (iw+1);
+			w /= (double) NV;
+			double xm = f.ToXOdd(w);
+			double xp = f.ToXEven(w);
+			gap[iw] = xp - xm;
+		}
+		printf ("%g	%g	%g	%g	%g	%g	%g\n", 
+			t, gap[0], gap[1], gap[2], gap[3], gap[4], gap[5]);
 	}
 }
