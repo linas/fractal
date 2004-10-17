9a66ed421e693bda5a4f12a4963978db7c6d2b02
linas <>
2004-10-17 18:12:54 +0000

    all new improved

--- misc/gap/sway.C
+++ misc/gap/sway.C
@@ -27,14 +27,69 @@ main (int argc, char *argv[])
 	d = atoi (argv[1]);
 	printf ("#\n# denom=%d\n#\n", d);
   
+#ifdef SHOW_SYMMETRY
 	for (n=1; n<d; n++)
 	{
 		t = ((double) n) / ((double) d);
-   	f.SetRatio (n,d);
+		int gcf = gcf32(n,d);
+		int dd = d/gcf;
+		int nn = n/gcf;
+   	f.SetRatio (nn,dd);
 
 		w = 1.0;
-		double xm = f.ToXOdd(w);
 		double xp = f.ToXEven(w);
-		printf ("%g	%g	%g\n", t, xm, xp);
+
+		xp -= t;
+		xp *= dd*dd;
+		xp -= w - w*w + 0.5*w*w*w;
+		xp += t;
+		
+   	f.SetRatio ((dd-nn),dd);
+		double xm = f.ToXOdd(w);
+		xm -= (1.0-t);
+		xm *= dd*dd;
+		xm -= w - w*w + 0.5*w*w*w;
+		xm += (1.0-t);
+		
+		printf ("%8.6g	%4d	%8.6g	%8.6g	%g\n", t, dd, xm, xp, xm+xp);
 	}
+#endif
+
+	int i;
+	for (w=0.01; w<1.0; w+= 0.01)
+	{
+		n = 1; d=200;
+		t = ((double) n) / ((double) d);
+		int gcf = gcf32(n,d);
+		int dd = d/gcf;
+		int nn = n/gcf;
+   	f.SetRatio (nn,dd);
+
+		double xp = f.ToXEven(w);
+
+		xp -= t;
+		xp *= dd*dd;
+		xp -= w - w*w + 0.5*w*w*w;
+		xp += t;
+
+		double a = xp-t;
+		
+		n = 1; d=100;
+		t = ((double) n) / ((double) d);
+		gcf = gcf32(n,d);
+		dd = d/gcf;
+		nn = n/gcf;
+   	f.SetRatio (nn,dd);
+
+		xp = f.ToXEven(w);
+
+		xp -= t;
+		xp *= dd*dd;
+		xp -= w - w*w + 0.5*w*w*w;
+		xp += t;
+
+		double b = xp-t;
+		printf ("%8.6g	%8.6g	%8.6g	%g\n", w, a, b, b-a);
+	}
+
 }
