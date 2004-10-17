fd0712b2f71539d8d1aa054b943c03a340321053
linas <>
2004-10-17 17:53:14 +0000

    build the tree

--- tools/src/libs/farey/FareyTree.C
+++ tools/src/libs/farey/FareyTree.C
@@ -1,6 +1,6 @@
 
 /*
- * FareyTree.h
+ * FareyTree.C
  *
  * Walk the Farey Tree, returning the next Farey number in the tree.
  * This walk is a breadth-first walk, of course, alternating branches
@@ -11,18 +11,10 @@
  *
  * Created by Linas Vepstas October 2004
  */
-class FareyIterator
-{
-	public:
-		FareyIterator (void);
-		~FareyIterator ();
-		void GetNextFarey (int *num, int *denom);
-	protected:
-		int Recur (int np1, int dp1, int np2, int dp2, int *n, int *d, int depth);
-	private:
-		char *state;
-		int state_sz;
-};
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "FareyTree.h"
 
 int
 FareyIterator :: Recur (int np1, int dp1, int np2, int dp2, int *n, int *d, int depth)
@@ -30,22 +22,30 @@ FareyIterator :: Recur (int np1, int dp1, int np2, int dp2, int *n, int *d, int 
 	int nn, dd;
 	nn = np1 + np2;
 	dd = dp1 + dp2;
-	if (0 == state[depth])
+
+	/* Need to pack the state so that we don't waste memory too much */
+	int pong = depth/4;
+	int ping = depth%4;
+	int pack = state[pong];
+	pack >>= 2*ping;
+	pack &= 0x3;
+#define SET_STATE(ns) { int mask = 0x3 << (2*ping); state[pong] &= ~mask; state[pong] |= ns<<(2*ping); }
+
+	if (0 == pack)
 	{
 		*n = nn;
 		*d = dd;
-		state[depth] = 1;
+		SET_STATE (1);
 		return depth;
 	}
-	if (1 == state[depth])
+	if (1 == pack)
 	{
-		
-		state[depth] = 2;
+		SET_STATE (2);
 		return Recur (np1, dp1, nn, dd, n, d, 3*depth+1);
 	}
-	if (2 == state[depth])
+	if (2 == pack)
 	{
-		state[depth] = 1;
+		SET_STATE (1);
 		return Recur (nn, dd, np2, dp2, n, d, 3*depth+2);
 	}
 	return depth;
@@ -57,8 +57,8 @@ FareyIterator :: GetNextFarey (int *n, int *d)
 	int lvl = Recur (0,1, 1,1, n,d, 0);
 	if (3*lvl+3>state_sz)
 	{
-		int newsz = 3*state_sz+3;
-		state = (int *) realloc (state, newsz*sizeof(int));
+		int newsz = (3*state_sz+7)/4;
+		state = (char *) realloc (state, newsz*sizeof(char));
 		if (!state)
 		{
 			fprintf (stderr, "going down, out of memory newsz=%d\n", newsz);
@@ -70,21 +70,21 @@ FareyIterator :: GetNextFarey (int *n, int *d)
 			state[i] = 0;
 		}
 		// printf ("duude realloc old=%d new=%d\n", state_sz, newsz);
-		state_sz = newsz;
+		state_sz = 4*newsz;
 	}
 }
 
 FareyIterator :: FareyIterator (void)
 {
-	#define FAREY_INITIAL_SIZE 3
-	state = (int *) malloc (FAREY_INITIAL_SIZE *sizeof(int));
+	#define FAREY_INITIAL_SIZE 11
+	state = (char *) malloc (FAREY_INITIAL_SIZE *sizeof(char));
 
 	int i;
 	for (i=0; i<FAREY_INITIAL_SIZE; i++) 
 	{
 		state[i] = 0;
 	}
-	state_sz = FAREY_INITIAL_SIZE;
+	state_sz = 4*FAREY_INITIAL_SIZE;
 }
 
 FareyIterator :: ~FareyIterator ()
