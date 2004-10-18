fb7688aa99b70e4fb462f51f35d048828d897710
linas <>
2004-10-18 23:59:40 +0000

    add a bunch of new stuff

---
+++ misc/gap/feigen.C
@@ -0,0 +1,51 @@
+
+/* feigen.C
+ *
+ * eigen-functions:
+ * bernoulli-map eigenfunctions concated with Minkowski map.
+ *
+ * Linas Vepstas October 2004
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+#include "Farey.h"
+
+main (int argc, char *argv[])
+{
+	int i;
+	ContinuedFraction f;
+	f.SetEvenize();
+
+
+#define NP 6
+	double arr[NP];
+
+	int nmax = 400;
+	for (i=0; i<nmax; i++)
+	{
+		double x = ((double) 2*i+1)/ ((double) (2*nmax));
+
+		f.SetRatio (2*i+1, 2*nmax);
+		double xb = f.ToFarey();
+
+		int j;
+		double yb = xb;
+		for (j=0; j<NP; j++)
+		{
+			yb *= 0.5;
+			double y = InvFarey (yb);
+			arr[j] = y;
+		}
+		printf ("%5d	%8.6g", i, x);
+		for (j=0; j<NP; j++)
+		{
+			printf ("	%8.6g", arr[j]);
+		}
+		printf ("\n");
+
+		fflush (stdout);
+	}
+}
+
---
+++ misc/gap/sym.C
@@ -0,0 +1,60 @@
+
+/* sym.C
+ *
+ * Explore symmetries
+ *
+ * Linas Vepstas October 2004
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+#include "Farey.h"
+
+main (int argc, char *argv[])
+{
+	double z;
+	int i;
+	ContinuedFraction f;
+	f.SetEvenize();
+
+	int m = 2;
+	int n = 5;
+	f.SetRatio (m,n);
+	double fact=f.ToFarey();
+	printf ("# norm=%g\n", fact);
+	
+
+	int nmax = 23;
+	for (i=0; i<nmax; i++)
+	{
+
+		int p = i;
+		int q = nmax;
+		double x = ((double) p)/ ((double) q);
+		
+		f.SetRatio (p,q);
+		
+		double y = f.ToFarey();
+		
+		// f.SetRatio (p+q,p+2*q);
+		// z = 4.0* f.ToFarey() -2.0;
+		
+		// f.SetRatio (p+2*q,p+3*q);
+		// z = 8.0* f.ToFarey()-6.0;
+		
+		// f.SetRatio (p+3*q,p+4*q);
+		// z = 16.0* f.ToFarey()-14.0;
+		
+		f.SetRatio (q,(n+1)*q-p);
+
+		z = f.ToFarey();
+		z *= (double) (1<<n);
+		z -= 1.0;
+
+		
+		printf("%5d	%8.6g	%8.6g	%8.6g	%8.6g\n", i,x,y,z,y-z);
+
+	}
+}
+
