5a54d309fe12530b87601a9faa33ada62868f23b
linas <>
2004-10-18 23:58:31 +0000

    whatever

--- misc/gap/fdist.C
+++ misc/gap/fdist.C
@@ -6,10 +6,13 @@
  *
  * Linas October 2004
  */
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "Farey.h"
 #include "FareyTree.h"
+
 
 main(int argc, char *argv[])
 {
@@ -17,23 +20,22 @@ main(int argc, char *argv[])
 
 	if (argc <2)
 	{
-		fprintf (stderr, "Usage: %s <num>\n", argv[0]);
+		fprintf (stderr, "Usage: %s <nbins> <maxiter>\n", argv[0]);
 		exit (1);
 	}
-	int max = atoi (argv[1]);
+	int nbins = atoi (argv[1]);
+	int max = atoi (argv[2]);
 
 	FareyIterator fi;
 
-	int numbins;
-
-#define BINSZ 400
-	double bin[BINSZ];
+#define BINSZ 45400
+	int bin[BINSZ];
 	for (i=0; i<BINSZ; i++)
 	{
-		bin[i] = 0.0;
+		bin[i] = 0;
 	}
-	bin[0] = 1.0;
-	bin[BINSZ-1] = 1.0;
+	bin[0] = 1;
+	bin[nbins-1] = 1;
 
 	int cnt =2;
 	for (i=0; i<max; i++)
@@ -42,18 +44,25 @@ main(int argc, char *argv[])
 		fi.GetNextFarey (&n, &d);
 
 		double x = ((double) n)/ ((double) d);
-		x *= BINSZ;
+		x *= nbins;
 		int ib = (int) x;
-		bin [ib] += 1.0;
+		bin [ib] ++;
 		cnt ++;
 	}
 
-	for (i=0; i<BINSZ; i++)
+   ContinuedFraction f;
+	double gral = 0.0;
+	for (i=0; i<nbins; i++)
 	{
-		bin[i] /= (double) cnt;
-		bin[i] *= BINSZ;
-		double x = ((double) i) / ((double) BINSZ);
-		printf ("%8.6g	%g\n", x, bin[i]);
-	}
+		double bcnt = bin[i];
+		bcnt /= (double) cnt;
+		gral += bcnt;
+		bcnt *= nbins;
+		double x = ((double) i) / ((double) nbins);
 
+   	f.SetRatio (2*i+1, 2*nbins);
+   	double far = f.ToFarey (); 
+
+		printf ("%6d	%8.6g	%8.6g	%8.6g	%8.6g\n", i, x, bcnt, gral, far);
+	}
 }
