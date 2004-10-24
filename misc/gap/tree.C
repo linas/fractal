726dc7e41b7e5c17f6af8ea504596289101e0da9
linas <>
2004-10-24 00:39:09 +0000

    fiddle faddle

--- misc/gap/tree.C
+++ misc/gap/tree.C
@@ -13,7 +13,7 @@
 
 // per driebe chapter 3
 double 
-e_re (int n, int l, double x)
+enl_re (int n, int l, double x)
 {
 	int i;
 	x *=  2*l+1;
@@ -27,7 +27,7 @@ e_re (int n, int l, double x)
 }
 
 double 
-e_im (int n, int l, double x)
+enl_im (int n, int l, double x)
 {
 	int i;
 	x *=  2*l+1;
@@ -40,6 +40,23 @@ e_im (int n, int l, double x)
 	return enl;
 }
 
+double 
+e_zl_re (double z_re, double z_im, int l, double x)
+{
+	int i;
+	double znre = 1.0;
+	double znim = 0.0;
+	double acc = 0.0;
+	for (i=0; i<20; i++)
+	{
+		acc += znre*enl_re(i,l,x) - znim * enl_im (i,l,x);
+		double tmp = znre * z_re - znim * z_im;
+		znim = z_re *znim + z_im * znre;
+		znre = tmp;
+	}
+	return acc;
+}
+
 main (int argc, char *argv[])
 {
 	double z;
@@ -47,8 +64,7 @@ main (int argc, char *argv[])
 	ContinuedFraction f;
 	f.SetEvenize();
 
-	int m = 1;
-	int n = 1;
+	int ell = 1;
 	
 	int nmax = 523;
 	for (i=0; i<nmax; i++)
@@ -58,32 +74,14 @@ main (int argc, char *argv[])
 		int q = nmax;
 		double x = ((double) p)/ ((double) q);
 		
-#ifdef QUADRATIC_PARABOLA
-		// quadratic parabiola y=4(x-1/2)^2
-		p += nmax;
-		q *= 2;
-		int pp =(2*p-q)*(2*p-q);
-		int qq = q*q;
-		f.SetRatio (pp,qq);
-#endif
+		// double e = e_zl_re (0.35,0.0, ell, x);
 
-		double z = x-0.5;
-		z *= 2.0;
-		z = 2*z*z*z-z;
-		z += 1.0;
-		z *= 0.5;
-
-		// z = (2x-1)^3 -x +1
-		// z(0) = 0  and z(1) = 1
-
-		int pp = 2*p-q;
-		pp = pp*pp*pp + (q-p)*q*q;
-		int qq = q*q*q;
-		f.SetRatio (pp,qq);
+		// f.SetReal (e);
+		// double y = f.ToFarey();
+		double y = e_zl_re (x,0.0, ell, 0.33);
+		double e = y;
 		
-		double y = f.ToFarey();
-		
-		printf("%5d	%8.6g	%8.6g	%8.6g\n", i,x,y, z);
+		printf("%5d	%8.6g	%8.6g	%8.6g\n", i,x,y,e);
 
 	}
 }
