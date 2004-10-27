ac6da3182f06b5ff9f0cc6b5ab5ade927a518878
linas <>
2004-10-27 03:39:33 +0000

    fixes

--- misc/gap/takagi.C
+++ misc/gap/takagi.C
@@ -9,18 +9,19 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 double triangle (double x)
 {
 	double t = x - floor(x);
-	if (0.5 < t) return t;
+	if (0.5 > t) return t;
 	return 1.0-t;
 }
 
 double takagi (double w, double x)
 {
 	int k;
-	double acc = 1.0;
+	double acc = 0.0;
 	double tw = w;
 	double tp = 1.0;
 	for (k=0; k<1000; k++)
@@ -34,16 +35,23 @@ double takagi (double w, double x)
 	return acc;
 }
 
-main ()
+main (int argc, char *argv[])
 {
 	int i;
 
 	int nmax = 531;
 
+	if (argc <2)
+	{
+		printf ("Usage: %s <w-value>\n", argv[0]);
+		exit (1);
+	}
+	double w = atof(argv[1]);
+
 	for (i=0; i<nmax; i++)
 	{
 		double x = i/((double)nmax);
-		double tw = takagi (0.6, x);
+		double tw = takagi (w, x);
 
 		printf ("%d	%8.6g	%8.6g\n", i, x, tw);
 	}
