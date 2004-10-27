80597b85c3b62cab987a265731fab9219fc48209
linas <>
2004-10-27 03:25:24 +0000

    add takagi curves

---
+++ misc/gap/takagi.C
@@ -0,0 +1,50 @@
+
+/* 
+ * takagi.C
+ *
+ * draw the Takagi curve
+ *
+ * Linas October 2004
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+double triangle (double x)
+{
+	double t = x - floor(x);
+	if (0.5 < t) return t;
+	return 1.0-t;
+}
+
+double takagi (double w, double x)
+{
+	int k;
+	double acc = 1.0;
+	double tw = w;
+	double tp = 1.0;
+	for (k=0; k<1000; k++)
+	{
+		acc += tw* triangle (tp*x);
+		tp *= 2.0;
+		tw *= w;
+		if (1.0e-14 > tw) break;
+	}
+
+	return acc;
+}
+
+main ()
+{
+	int i;
+
+	int nmax = 531;
+
+	for (i=0; i<nmax; i++)
+	{
+		double x = i/((double)nmax);
+		double tw = takagi (0.6, x);
+
+		printf ("%d	%8.6g	%8.6g\n", i, x, tw);
+	}
+}
