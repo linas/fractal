8b43c1d1188d7a70a361b55ee545bad613c89be0
linas <>
2004-11-05 04:04:53 +0000

    multi-precision stuff

--- misc/gap/rdist.C
+++ misc/gap/rdist.C
@@ -1,27 +1,24 @@
-
 /*
  * rdist.C
  *
  * Distribution of the rationals
+ * converted to use gnu multiple precision libs
  *
  * Linas October 2004
  */
+
+#include <gmp.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "Farey.h"
-
-main(int argc, char *argv[])
+void
+bincount (int nbins, int max)
 {
 	int i;
 
-	if (argc <2)
-	{
-		fprintf (stderr, "Usage: %s <nbins> <maxiter>\n", argv[0]);
-		exit (1);
-	}
-	int nbins = atoi (argv[1]);
-	int max = atoi (argv[2]);
+	printf ("#\n# bincount of rationals using plain math\n#\n");
+	printf ("#\n# nbins=%d   maxiter=%d\n#\n",nbins,max);
 
 #define BINSZ 45720
 	int bin[BINSZ];
@@ -36,12 +33,15 @@ main(int argc, char *argv[])
 	{
 		for (n=0; n<=d; n++)
 		{
+// #define DO_GCF_ELIM
+#ifdef DO_GCF_ELIM
 			int gcf = gcf32 (n,d);
 			int nn = n/gcf;
 			int dd = d/gcf;
-#define DO_GCF_ELIM
-#ifdef DO_GCF_ELIM
 			if (gcf != 1) continue;
+#else 
+			int nn = n;
+			int dd = d;
 #endif
 
 			double x = ((double) (nn*nbins))/ ((double) dd);
@@ -71,3 +71,89 @@ main(int argc, char *argv[])
 		printf ("%5d	%8.6g	%g\n", i, x, bcnt);
 	}
 }
+
+void
+gmp_bincount (int nbins, int max)
+{
+	int i;
+
+	printf ("#\n# bincount of rationals using gnu multi-precison\n#\n");
+	printf ("#\n# nbins=%d   maxiter=%d\n#\n",nbins,max);
+
+#define BINSZ 45720
+	int bin[BINSZ];
+	for (i=0; i<BINSZ; i++)
+	{
+		bin[i] = 0;
+	}
+
+	mpz_t gib, gn, gnbins;
+	mpz_init (gib);
+	mpz_init (gn);
+	mpz_init (gnbins);
+	mpz_set_ui (gnbins, nbins);
+
+	unsigned int n, d;
+	int cnt = 0;
+	for (d=1; d<max; d++)
+	{
+		for (n=0; n<=d; n++)
+		{
+// #define DO_GCF_ELIM
+#ifdef DO_GCF_ELIM
+			int gcf = gcf32 (n,d);
+			int nn = n/gcf;
+			int dd = d/gcf;
+			if (gcf != 1) continue;
+#endif
+
+			// implement the following bining in gmp:
+			// double x = ((double) n)/ ((double) d);
+			// x *= nbins;
+			// int ib = (int) x;
+	
+			mpz_mul_ui (gn, gnbins, n);
+			mpz_fdiv_q_ui (gib, gn, d);
+	
+			unsigned int ib = mpz_get_ui (gib);
+
+			bin [ib] ++;
+			cnt ++;
+		}
+	}
+#if 0
+	cnt -= bin[0];
+	cnt -= bin[nbins-1];
+	cnt += 2*cnt/(nbins-2);
+	bin[0] = cnt/(nbins-2);
+	bin[nbins-1]= cnt/(nbins-2);
+#endif
+
+	double renorm = ((double) nbins) / ((double) cnt);
+	printf ("# total count=%d    renorm=%g\n", cnt, renorm);
+	for (i=0; i<nbins; i++)
+	{
+		double bcnt = bin[i];
+		bcnt *= renorm;
+		double x = ((double) i) / ((double) nbins);
+		printf ("%5d	%8.6g	%g\n", i, x, bcnt);
+	}
+}
+
+
+main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc <2)
+	{
+		fprintf (stderr, "Usage: %s <nbins> <maxiter>\n", argv[0]);
+		exit (1);
+	}
+	int nbins = atoi (argv[1]);
+	int max = atoi (argv[2]);
+
+	// bincount (nbins, max);
+	gmp_bincount (nbins, max);
+}
+
