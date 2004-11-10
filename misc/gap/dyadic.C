58fe4321a0d1b771ea1f783fe90c42204bd95d26
linas <>
2004-11-10 14:54:11 +0000

    initial checkin

---
+++ misc/gap/dyadic.C
@@ -0,0 +1,73 @@
+
+/*
+ * dyadic.C
+ *
+ * Some exploratinos of polynomials generated from binary expansion
+ *
+ * Linas November 2004
+ */
+
+#include <stdio.h>
+
+class Dyadic
+{
+	public:
+
+		// assumes x = p/2^n 
+		void SetFrac (unsigned int p, unsigned int order);
+
+		// return alternating polynomial sum_n w^n (2b_n -1)
+		double ToAlternatingPoly (double w);
+	private:
+		unsigned int ndigits;
+		short bdigits[32];
+};
+
+Dyadic::Dyadic(void)
+{
+	ndigits = 0;
+	int i;
+	for (i=0; i<32; i++) {
+		bdigits[i] = 0;
+	}
+}
+
+ 
+void 
+Dyadic :: SetFrac (unsigned int p, unsigned int order)
+{
+	ndigits = 0;
+	if (0 == p) return;
+
+	int sorder = (int) order;
+	// make sure p isn't even, no powers of 2 in it
+	while (p%2 == 0)
+	{
+		p /= 2;
+		sorder --;
+	}
+	if (0 >= sorder) return;
+	ndigits = sorder;
+
+	unsigned int deno = 1<<(sorder-1);
+
+	int i=0;
+	while (1)
+	{
+		if (p <deno)
+		{
+			bdigits[i] = 0;
+		}
+		else
+		{
+			bdigits[i] = 1;
+			p -= deno;
+		}
+		deno >>= 1;
+		i++;
+		if (0 == deno) break;
+	}
+}
+
+void 
+Dyadic :: SetFrac (unsigned int p, unsigned int order)
