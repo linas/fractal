eef18708a86cf57c942f81b7c9f19b6c7ba05990
linas <>
2004-11-21 04:45:48 +0000

    misc changes

--- misc/gap/zeta.C
+++ misc/gap/zeta.C
@@ -2,7 +2,7 @@
 /* 
  * zeta.C
  * 
- * explore zeta-like eigenfunctions of Bernoulli map
+ * explore Hurwitz zeta eigenfunctions of Bernoulli map
  *
  * Linas November 2004
  */
@@ -17,10 +17,11 @@ double beta (double x, double s)
 	for (n=1; n<57550; n++)
 	{
 		double term = pow(2.0*M_PI*((double) n), -s);
-		// acc += term * cos (2.0*M_PI*((double) n)*x);
-		acc += term * sin (2.0*M_PI*((double) n)*x);
+		acc += term * cos (2.0*M_PI*((double) n)*x);
+		// acc += term * sin (2.0*M_PI*((double) n)*x);
 		if (1.0e-16>term) break;
 	}
+	acc *= 2.0 * tgamma (s+1.0);
 	return acc;
 }
 
@@ -41,16 +42,18 @@ main (int argc, char * argv[])
 
 	printf ("#\n# ess=%g  eigenvalue lambda=%g\n#\n", s, lambda);
 	
-	int imax = 235;
+	int imax = 23;
 	for (i=0; i<imax; i++) 
 	{
 		double x = i/((double) imax);
 		double y = beta (x,s);
 
-#if CHECK_THAT_ITS_EIGENSTATE 
-		// verify its an eignestate
+// #define CHECK_THAT_ZETA_IS_EIGENSTATE 1
+#if CHECK_THAT_ZETA_IS_EIGENSTATE 
+		// verify its an eigenstate
 		double z = 0.5*(beta (0.5*x, s) + beta (0.5+0.5*x, s));
 		z /= lambda;
+		z -= y;
 #endif
 		double z=1.0;
 
