7885eb655275bb7d010296d7f10437a5a470af8d
linas <>
2004-12-06 01:29:26 +0000

    add complex stuff

--- misc/gap/dyadic.C
+++ misc/gap/dyadic.C
@@ -29,6 +29,7 @@ class Dyadic
 
 		// return sum_n (2b_n -1) n^-s
 		double ToZetaPoly (double s);
+		double ToZetaPolyC (double re_s, double im_s);
 
 	private:
 		unsigned int ndigits;
@@ -122,6 +123,32 @@ Dyadic :: ToZetaPoly (double s)
 	return acc;
 }
 
+double
+Dyadic :: ToZetaPolyC (double s_re, double s_im)
+{
+	int i;
+	double re_acc = 0.0;
+	double im_acc = 0.0;
+
+	for (i=0; i<ndigits; i++)
+	{
+		short alt = 2*bdigits[i] - 1;
+		double term = alt;
+
+		double en = i+1;
+		double z = pow (en, -s_re);
+		term *= z;
+
+		double arg = s_im * log (en);
+		double co = cos (arg);
+		double si = -sin (arg);
+		re_acc += term*co;
+		im_acc += term*si;
+	}
+
+	return re_acc;
+}
+
 
 double
 Dyadic :: ToCantorPoly (double z)
@@ -151,13 +178,15 @@ main (int argc, char * argv[])
 	int order = 12;
 	int nmax = 1<<order;
 
-	double z = 1.0/3.0;
+	double zre = 1.0/3.0;
+	double zim = 1.0/3.0;
 	if (2 > argc) {
 		printf ("Usage: %s <z value>\n", argv[0]); 
 		exit(1);
 	}
-	z = atof (argv[1]);
-	printf ("# z=%g\n", z);
+   zre = 1.3;
+	zim = atof (argv[1]);
+	printf ("# z=%g + i %g\n", zre, zim);
 
 	int p;
 	for (p=1; p<nmax; p+=2)
@@ -168,12 +197,14 @@ main (int argc, char * argv[])
 		dy.SetFrac (p, order);
 		// double y = dy.ToAlternatingPoly (z);
 		// double y = dy.ToCantorPoly (z);
-		double y = dy.ToZetaPoly (z);
+		double y = dy.ToZetaPolyC (zre, zim);
 
+#ifdef COMB_STRUCTURE
 		dy.SetFrac (p+1, order);
 		y = dy.ToZetaPoly (z);
 		dy.SetFrac (p-1, order);
 		y -= dy.ToZetaPoly (z);
+#endif
 
 		printf ("%d	%8.6g	%8.6g\n", p, x, y);
 	}
