a0b8a21abe0d0ce486044e7b9aa4021a08dc33c7
linas <>
2004-12-19 05:34:23 +0000

    remove bogus incorrect function

--- misc/continued-frobenius/zeta-takagi.c
+++ misc/continued-frobenius/zeta-takagi.c
@@ -52,7 +52,7 @@ long double summy (long double ess)
 }
 
 /* Basic sum for integers in the middle of the sum */
-long double symy (int m, int n, long double ess)
+long double sym_integer (int m, int n, long double ess)
 {
 	int i;
 
@@ -79,40 +79,9 @@ long double symy (int m, int n, long double ess)
 	return acc;
 }
 
-/* Basic wacky-square half-integer sum */
-long double sym_half (int m, int n, long double ess)
-{
-	int i;
-
-	long double en = n;
-	long double pnk = 1.0L;
-	long double acc = 0.0L;
-	long double ts = powl (0.5L, ess);
-
-	for (i=0; i<60; i++)
-	{
-		long double eye = i;
-		// long double term = gsl_sf_zeta (eye+ess);
-		// term -= harmonic (m, eye+ess);
-		long double term = zeta_minus_harmonic (m, eye+ess);
-		term *= pnk;
-		term *= fbinomial (ess+eye-1.0L, i);
-		acc += term;
-		pnk *= -en*(1.0 + 0.5*en);
-	}
-	acc *= ts;
-
-	double expect = gsl_sf_zeta (ess);
-	expect *= 1.0 - ts;
-	expect -= 1.0;
-	expect -= ts * harmonic_hurwitz (m+n, 0.5L, ess);
-	acc -=  expect;
-	
-	return acc;
-}
 
 /* Generic half-integer sum i.e. for n+1/2  */
-long double sym_half_generic (int m, int n, long double ess)
+long double sym_half (int m, int n, long double ess)
 {
 	int i;
 
@@ -140,6 +109,14 @@ long double sym_half_generic (int m, int n, long double ess)
 	expect += ts*harmonic (n+m, ess);
 	acc -=  expect;
 	
+/*
+	This is an equivalent to above.
+	double expect = gsl_sf_zeta (ess);
+	expect *= 1.0 - ts;
+	expect -= 1.0;
+	expect -= ts * harmonic_hurwitz (m+n, 0.5L, ess);
+	acc -=  expect;
+*/
 	return acc;
 }
 
@@ -173,6 +150,17 @@ long double sym_fourths (long double ess)
 	return acc;
 }
 
+double check (int n, double ess)
+{
+	double ts = pow (2.0, -ess);
+	double expect = 1.0 + ts * harmonic_hurwitz (n, 0.5L, ess);
+
+	expect -= harmonic (2*n+1, ess);
+	expect += ts*harmonic (n, ess);
+
+	return expect;
+}
+
 int
 main (int argc, char * argv[])
 {
@@ -193,13 +181,12 @@ main (int argc, char * argv[])
 		x *= 6.0;
 		x += 1.3;
 
-		double acc;
+		double acc, a2=0.0;
 		// acc = summy (x);
-		// acc = symy (6, 7, x);
+		// acc = sym_integer (6, 7, x);
 		// acc = sym_half (m, n, x);
-		// acc = sym_half_generic (m, n, x);
-		acc = sym_fourths (x);
-		printf ("%d	%g	%g\n", i, x, acc);
+		// acc = sym_fourths (x);
+		printf ("%d	%g	%g	%g	%g\n", i, x, acc, a2, acc-a2);
 	}
 
 	return 0;
