bcff6714f87364dd8e9deee32f6f6960ceee2a4c
linas <>
2005-01-17 02:55:11 +0000

    updates

--- symbolic/tensor.c
+++ symbolic/tensor.c
@@ -14,7 +14,7 @@ struct eff_s
 	int dy;
 };
 
-typedef struct eff_s Eff;
+typedef struct eff_s Part;
 
 /* one term */
 struct term_s
@@ -33,21 +33,113 @@ struct expr_s
 
 typedef struct expr_s Expr;
 
+/* =============================================== */
+
+void 
+PrintPart (Part *p)
+{
+	int i;
+	if (p->dx != 0 || p->dy != 0) 
+	{
+		printf ("d");
+	}
+	for (i=0; i<p->dx; i++)
+	{
+		printf ("x");
+	}
+	for (i=0; i<p->dy; i++)
+	{
+		printf ("y");
+	}
+	printf ("f ");
+}
+
+void
+PrintTerm (Term *t)
+{
+	printf ("+ %d ", t->coeff);
+	
+	GList *pn;
+	for (pn = t->parts; pn; pn=pn->next)
+	{
+		Part *p = pn->data;
+		PrintPart(p);
+	}
+	printf ("\n");
+}
+
+void 
+PrintExpr (Expr *e)
+{
+	printf ("Print expr: \n");
+
+	GList *tn;
+	for (tn = e->terms; tn; tn=tn->next)
+	{
+		Term *t = tn->data;
+		PrintTerm (t);
+	}
+
+	printf ("The End\n");
+}
+
+/* ======================================================== */
+
+Term *Mult_T_T (Term *t1, Term *t2)
+{
+	Term * t3 = g_new0(Term,1);
+	
+	t3->coeff = t1->coeff * t2->coeff;
+	t3->parts = g_list_copy (t1->parts);
+	GList *p = g_list_copy (t2->parts);
+	t3->parts = g_list_append (t3->parts, p);
+
+	return t3;
+}
+
+/* ======================================================== */
+
+Term *dxf (void)
+{
+	Term *t = g_new (Term,1);
+
+	Part *p = g_new0 (Part,1);
+	p->dx = 1;
+	p->dy = 0;
+
+	t->parts = g_list_append (t->parts, p);
+	t->coeff = 1;
+
+	return t;
+}
+
+Term *dyf (void)
+{
+	Term *t = g_new (Term,1);
+
+	Part *p = g_new0 (Part,1);
+	p->dx = 0;
+	p->dy = 1;
+
+	t->parts = g_list_append (t->parts, p);
+	t->coeff = 1;
+
+	return t;
+}
 
 Expr * setup (void)
 {
-	Expr *e = g_new0 (Expr,0);
+	Expr *e = g_new0 (Expr,1);
+
+	Term * t = dxf();
+	e->terms = g_list_append (e->terms, t);
 
 	return e;
-}
-
-void PrintExpr (Expr *e)
-{
-	printf ("Print expr: \n");
 }
 
 main ()
 {
 
 	Expr *e = setup();
+	PrintExpr (e);
 }
