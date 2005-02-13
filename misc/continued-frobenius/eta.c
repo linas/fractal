e51da9609bb20d8389dc1662d53a7bc57994c81b
linas <>
2005-02-13 15:52:18 +0000

    add stuff

--- misc/continued-frobenius/eta.c
+++ misc/continued-frobenius/eta.c
@@ -23,12 +23,27 @@ double eta (double ex)
 	return prod;
 }
 
+double sum (double ex)
+{
+	int n;
+	double sum = 0.0;
+	for (n=1; ; n++)
+	{
+		double term = ex + (double) n;
+		term = 1.0/ term;
+		term = term*term* eta(term);
+		sum += term;
+	}
+	return sum;
+}
+
 main ()
 {
-	double tee =0.5;
-	for (tee=0.5; ; tee*= 0.5)
+	double ex;
+	for (ex=0.95; ex>0.0; ex -= 0.05)
 	{
-		double s = eta (tee);
-		printf ("its %g %g \n", tee, s);
+		double e = eta (ex);
+		double s = sum (ex);
+		printf ("its x=%g eta=%g sum=%g rat=%g\n", ex, e, s, s/e);
 	}
 }
