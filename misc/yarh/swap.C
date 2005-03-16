59b2bb84f31ab206b9264269917e3c28321a1461
linas <>
2005-03-16 02:37:31 +0000

    whatever

--- misc/yarh/swap.C
+++ misc/yarh/swap.C
@@ -26,12 +26,12 @@ main (int argc, char *argv[])
 	int q = atoi (argv[2]);
 	printf ("# swapping terms %d and %d\n", p,q);
 
-	int nmax = 25;
+	int nmax = 2521;
 	for (i=0; i<nmax; i++)
 	{
 		double x = ((double) i)/ ((double) (nmax));
 
-#if BASIC_GRAPH
+#ifdef BASIC_GRAPH
 		f.SetRatio (i,nmax);
 		f.SwapTerms (p,q);
 		double y = f.ToReal();
@@ -39,7 +39,7 @@ main (int argc, char *argv[])
 		printf ("%5d	%8.6g	%8.6g\n", i, x, y);
 #endif
 
-#define SHOW_SELF_SIM 1
+// #define SHOW_SELF_SIM 1
 #ifdef SHOW_SELF_SIM
 		int n = 5;
 		int j = 2*nmax - i;
@@ -55,6 +55,40 @@ main (int argc, char *argv[])
 		double d = z - y/(n*y+1.0);
 
 		printf ("%5d	%8.6g	%8.6g	%8.6g	%8.6g\n", i, x, y,z, d);
+#endif
+
+#define OTHER_SYM
+#ifdef OTHER_SYM
+		f.SetRatio (i,nmax);
+		f.SwapTerms (p,q);
+		double y = f.ToReal();
+
+		f.SetRatio (i,i+nmax);
+		f.SwapTerms (p,q);
+		double z = f.ToReal();
+		double zo = z;
+
+		// z *= (1.0+x)*(1.0-x);
+		// z *= (1.0+x)*y;
+		// z *= (2.0-y)*y;
+
+		// z = z / (1.0+z);
+		// z *= (2.0-x);
+		// z *= (1.0+y);
+
+#if 1
+		y *= (2.0-x)/ (2.0-2.0*x);
+		double a = 2.0;
+		double c;
+		c = a - 1.0/a;
+		double d = (a-c);
+		z = (a*y) / (c*y+d);
+
+		z *= (2.0-2.0*x)/ (2.0-x);
+#endif
+
+
+		printf ("%5d	%8.6g	%8.6g	%8.6g	%8.6g\n", i, x, y, z, zo);
 #endif
 
 	}
