061e00d9da973f0bdfe9c3ddb18c4696e4cc8ff8
linas <>
2005-06-12 17:24:21 +0000

    move contents to geo-lib.c

--- misc/surface/geodesic.c
+++ misc/surface/geodesic.c
@@ -7,50 +7,8 @@
  */
 
 #include <math.h>
+#include "geo-lib.h"
 
-int get_n_of_rho (double rho)
-{
-	rho *= rho;
-	rho = sqrt (rho-0.75);
-	rho -= 0.5;
-	rho = floor (rho);
-	return (int) rho;
-}
-
-double geo_x (double rho)
-{
-	int n = get_n_of_rho (rho);
-
-	double ex = rho*rho + n*(n+2);
-	ex /= 2.0*(n+1);
-	return ex;
-}
-
-double geo_length (double rho)
-{
-	int n = get_n_of_rho (rho);
-
-	double nu = rho*rho + n*(n+2);
-	double de = -nu;
-	double cr = 2.0*rho*(n+1);
-	nu += cr;
-	de += cr;
-
-	double len = log (nu/de);
-	return len;
-}
-
-double geo_energy (double rho)
-{
-	double ex = geo_x (rho);
-	double len = geo_length (rho);
-
-	double eng = ex;
-	eng /= rho*rho - ex*ex;
-	eng += len / (2.0*rho);
-
-	return eng;
-}
 
 main () 
 {
