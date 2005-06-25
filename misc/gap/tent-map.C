387f8f2db09ae46a87689778a76cbca6b6a3ad1e
linas <>
2005-06-25 16:33:42 +0000

    initial checkin

---
+++ misc/gap/tent-map.C
@@ -0,0 +1,54 @@
+
+/*
+ * tent-map.C
+ *
+ * dyadic symbolic values of the tent map.
+ * iterate the unit-height tent map. The result is a set of
+ * symbolic values, left or right. use these symbolic values 
+ * to construct a binary real number. Graphs this number.
+ *
+ * Linas June 2005
+ */
+
+#include <math.h>
+
+long double tent_dyadic (long double x, long double left, long double right)
+{
+	int i;
+
+	long double b;
+	long double norm = 0.5L / (left-right);
+	x -= lfloor (x);
+	for (i=0; i<30; i++)
+	{
+		if (x <0.5L) 
+		{
+			b += left;
+		}
+		else
+		{
+			b += right;
+		}
+		b *= norm;
+
+		x *= 2.0L;
+		if (x > 1.0L) x -= 1.0L;
+	}
+
+	return b;
+}
+
+main () 
+{
+	int i;
+
+	int nmax = 723;
+	for (i=0; i<nmax; i++)
+	{
+		long double x = ((long double) i) / ((long double) nmax);
+
+		long double y = tent_dyadic (x, 0.0, 1.0);
+
+		printf ("%d	%Lg	%Lg\n", i, x,y);
+	}
+}
