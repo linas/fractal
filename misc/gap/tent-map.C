c7d8d9f3ccb8edc91939da3af2a4d218cb4fb860
linas <>
2005-06-27 23:12:36 +0000

    fixes

--- misc/gap/tent-map.C
+++ misc/gap/tent-map.C
@@ -19,10 +19,14 @@ long double tent_dyadic (long double x, long double left, long double right)
 
 	long double b = 0.0L;
 	long double norm = 0.5L / (right-left);
+	left *= norm;
+	right *= norm;
+
 	x -= floorl (x);
 	for (i=0; i<30; i++)
 	{
-		if (x <0.5L) 
+		/* convert dyadic symbol to binary number */
+		if (x < 0.5L) 
 		{
 			b += left;
 		}
@@ -30,10 +34,13 @@ long double tent_dyadic (long double x, long double left, long double right)
 		{
 			b += right;
 		}
-		b *= norm;
+		left *= norm;
+		right *= norm;
 
+		// printf ("duude i=%d x=%Lg b=%Lg \n", i, x, b);
+		/* iterate on the tent map */
 		x *= 2.0L;
-		if (x > 1.0L) x -= 1.0L;
+		if (x >= 1.0L) x = 2.0L-x;
 	}
 
 	return b;
@@ -49,6 +56,7 @@ main ()
 		long double x = ((long double) i) / ((long double) nmax);
 
 		long double y = tent_dyadic (x, 0.0, 1.0);
+		// long double y = tent_dyadic (x, -1.0, 1.0);
 
 		printf ("%d	%Lg	%Lg\n", i, x,y);
 	}
