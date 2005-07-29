d86134d6557f2e0b28b6bae0a69a40b36988de7c
linas <>
2005-07-29 03:07:08 +0000

    initial checkin

---
+++ misc/bignum/mp-zeta.c
@@ -0,0 +1,97 @@
+/*
+ * mp_zeta.c
+ *
+ * High-precison Riemann zeta function, using the 
+ * Gnu Multiple-precision library.
+ * 
+ * Linas Vepstas July 2005
+ */
+
+#include <gmp.h>
+#include <math.h>
+#include <stdio.h>
+
+void fp_prt (char * str, mpf_t val)
+{
+	printf (str);
+	mpf_out_str (stdout, 10, 0, val);
+	printf ("\n");
+}
+
+/* fp_zeta
+ * Floating-point-valued Riemann zeta for positive integer arguments 
+ * return value placed in the arg "zeta".
+ *
+ * Simple-minded algo, carries out math to prec decimal digits
+ */
+void fp_zeta (mpf_t zeta, unsigned int s, int prec)
+{
+	unsigned long int us = s;
+	mpf_t acc;
+	mpf_t term;
+	mpf_t en;
+	mpf_t inv;
+	
+	mpf_init (acc);
+	mpf_init (term);
+	mpf_init (en);
+	mpf_init (inv);
+	
+	mpf_set_ui (zeta, 1);
+
+	/* Compute number of terms to be carried out 
+	 * However, this estimate is wrong; it stops 
+	 * when next term is "smaller than" rather 
+	 * than when its converged.
+	 */
+	double fprec = prec;
+	fprec /= (double) s;
+	double dig = pow (10.0, fprec);
+	if (1.0e10 < dig)
+	{
+		fprintf (stderr, "Sorry bucko, can't do it\n");
+		return;
+	}
+	int nmax = dig+1.0;
+	printf ("zeta will be computed with %d terms\n", nmax);
+	
+	int n;
+	for (n=2; n<= nmax; n++)
+	{
+		mpf_set_ui (en, n);
+		mpf_ui_div (inv, 1, en);  /* inv = 1/n */
+		mpf_pow_ui (term, inv, us); /* term = 1/n^s */
+		mpf_add (acc, zeta, term);
+		mpf_set (zeta, acc);
+	}
+
+	mpf_clear (acc);
+	mpf_clear (term);
+	mpf_clear (en);
+	mpf_clear (inv);
+}
+
+main ()
+{
+	/* set the precision */
+	mpf_set_default_prec (200);
+
+	mpf_t zeta;
+	mpf_init (zeta);
+	
+	printf ("           000000000011111111112222222222333333333344444444445555555555666666666677777777778\n");
+	printf ("           012345678901234567890123456789012345678901234567890123456789012345678901234567890\n");
+	fp_zeta (zeta, 8, 30);
+	fp_prt ("30 digs= ", zeta);
+	fp_zeta (zeta, 8, 40);
+	fp_prt ("40 digs= ", zeta);
+	fp_zeta (zeta, 8, 50);
+	fp_prt ("50 digs= ", zeta);
+	fp_zeta (zeta, 8, 60);
+	fp_prt ("60 digs= ", zeta);
+	fp_zeta (zeta, 8, 70);
+	fp_prt ("70 digs= ", zeta);
+	fp_zeta (zeta, 8, 80);
+	fp_prt ("0 digs= ", zeta);
+}
+
