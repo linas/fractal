2ce1b6e1f605c5c8d545ff9a72fe33271c00443b
linas <>
2005-07-31 16:09:18 +0000

    new program

---
+++ misc/bignum/zero-cross.c
@@ -0,0 +1,21 @@
+
+/*
+ * zero-cross.c
+ *
+ * get zero crossing fits for the data
+ *
+ * Linas July 2005
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+main ()
+{
+	int c;
+	
+	while( (c=getchar()) != EOF)
+	{
+		printf ("%c", c);
+	}
+}
