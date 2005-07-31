9b62323c8437d40e2fa5ab46d4eb72e1754499f8
linas <>
2005-07-31 16:19:42 +0000

    more

--- misc/bignum/zero-cross.c
+++ misc/bignum/zero-cross.c
@@ -7,15 +7,37 @@
  * Linas July 2005
  */
 
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 main ()
 {
 	int c;
-	
+	char buf[4000];
+	double var[300];
+
+	int disc = 1;	
+	int i =0;
+	int n=0;
 	while( (c=getchar()) != EOF)
 	{
-		printf ("%c", c);
+		if (disc && c != '\t') continue;
+		disc = 0;
+		if (c == '\t') continue;
+
+		buf[i] = c;
+
+		if ( c == '\n')
+		{
+			buf[i] = 0;
+			disc = 1;
+		   i=-1;	
+
+			var[n] = atof(buf);
+			n++;
+			printf ("%g\n", var[n-1]);
+		}
+		i++;
 	}
 }
