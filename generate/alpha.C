3b1e0de7f4fef95323a558e74996909fb744b84a
linas <>
2005-08-28 05:19:30 +0000

    fix errors

--- generate/alpha.C
+++ generate/alpha.C
@@ -19,6 +19,44 @@
 
 #include "brat.h"
 
+static void const_series_c (double re_q, double im_q, double *prep, double *pimp)
+{
+	int i;
+	double tmp;
+	*prep = 0.0;
+	*pimp = 0.0;
+
+	double rep = 0.0;
+	double imp = 0.0;
+
+	double a[8];
+	a[0] = -0.077215664901532851;
+	a[1] = -0.0047486314774196408;
+	a[2] = 0.00036610089349548089;
+	a[3] = 0.00037600730566372326;
+	a[4] = 0.00014301182486231440;
+	a[5] = 3.3997818936021684e-5;
+	a[6] = -4.8324221220657863e-7;
+	a[7] = -6.7778497812918602e-6;
+
+	double re_zn = 1.0;
+	double im_zn = 0.0;
+
+	for (i=0; i<8; i++)
+	{
+		// a[i] += 0.5 / ((double) (i+1));
+		rep += a[i] * re_zn;
+		imp += a[i] * im_zn;
+		
+		tmp = re_q * re_zn - im_q * im_zn;
+		im_zn = re_q * im_zn + im_q * re_zn;
+		re_zn = tmp;
+	}
+	
+	*prep = rep;
+	*pimp = imp;
+}
+			  
 static void alpha_series_c (double re_q, double im_q, double *prep, double *pimp)
 {
 	double tmp;
@@ -51,9 +89,9 @@ static void alpha_series_c (double re_q, double im_q, double *prep, double *pimp
 	double re_lng = mod_lng.val * cos (arg_lng.val);
 	double im_lng = mod_lng.val * sin (arg_lng.val);
 
-	/* (1/z) * ln gamma (1/(1-z)) */
-	rep += re_one_over_z * re_lng - im_one_over_z * im_lng;
-	imp += re_one_over_z * im_lng + im_one_over_z * re_lng;
+	/* z * ln gamma (1/(1-z)) */
+	rep += re_q * re_lng - im_q * im_lng;
+	imp += re_q * im_lng + im_q * re_lng;
 
 	/* log (1-z) */
 	gsl_sf_result m_lnz, t_lnz;
@@ -61,21 +99,23 @@ static void alpha_series_c (double re_q, double im_q, double *prep, double *pimp
 	double re_ln_omz = m_lnz.val;
 	double im_ln_omz = t_lnz.val;
 	
-	/* (1/z) * log (1-z) */
-	double re_log_over_z = re_ln_omz * re_one_over_z - im_ln_omz * im_one_over_z;
-	double im_log_over_z = re_ln_omz * im_one_over_z + im_ln_omz * re_one_over_z;
+	/* (1/z) * (1/(1-z)) */
+	double re_zz = re_one_over_z * re_one_over_one_minus_z - im_one_over_z * im_one_over_one_minus_z;
+	double im_zz = re_one_over_z * im_one_over_one_minus_z + im_one_over_z * re_one_over_one_minus_z;
 
-	/* add (1/z) * log (1-z) * (1/(1-z)) */
-	rep += re_log_over_z * re_one_over_one_minus_z - im_log_over_z * im_one_over_one_minus_z;
-	imp += re_log_over_z * im_one_over_one_minus_z + im_log_over_z * re_one_over_one_minus_z;
+	/* (1+z) / (z(1-z)) */
+	double re_tz = (1.0+re_q) * re_zz - im_q * im_zz;
+	double im_tz = (1.0+re_q) * im_zz + im_q * re_zz;
+
+	/* add log (1-z) * (1+z)/(z(1-z)) */
+	rep += re_ln_omz * re_tz - im_ln_omz * im_tz;
+	imp += re_ln_omz * im_tz + im_ln_omz * re_tz;
 	
-	/* add - 0.5 * (1/z) * log (1-z) */
-	rep += -0.5 * re_log_over_z;
-	imp += -0.5 * im_log_over_z;
-
 	/* add 1/(1-z) */
 	rep += re_one_over_one_minus_z;
 	imp += im_one_over_one_minus_z;
+
+	rep -= 0.577215664901532;
 	
 	*prep = rep;
 	*pimp = imp;
@@ -84,10 +124,31 @@ static void alpha_series_c (double re_q, double im_q, double *prep, double *pimp
 static double alpha_series (double re_q, double im_q)
 {
 	double rep, imp;
+	
+#if AT_INFINITY
+	tmp = 1.0 / (re_q *re_q + im_q * im_q);
+	reo = re_q * tmp;
+	imo = -im_q * tmp;
+	re_q = reo;
+	im_q = imo;
+#endif
+	
 	alpha_series_c (re_q, im_q, &rep, &imp);
-	// return sqrt (rep*rep+imp*imp);
+
+	// const_series_c (re_q, im_q, &rep, &imp);
+	
+#if 0
+	double reo = rep * re_q - imp * im_q;
+	double imo = rep * im_q + imp * re_q;
+
+	rep = reo;
+	imp = imo;
+#endif
+
+	return sqrt (rep*rep+imp*imp);
+	// return log (sqrt (rep*rep+imp*imp));
 	// return imp;
-	return (atan2 (imp, rep) + M_PI) / (2.0*M_PI);
+	// return (atan2 (imp, rep) + M_PI) / (2.0*M_PI);
 }
 
 /*-------------------------------------------------------------------*/
