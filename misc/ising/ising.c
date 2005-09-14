42d33bce6974340558aebb7a497dd98e1aae9b1f
linas <>
2005-09-14 03:22:11 +0000

    flesh out

--- misc/ising/ising.c
+++ misc/ising/ising.c
@@ -1,14 +1,17 @@
 /*
- * potts.c
+ * ising.c
  *
- * Potts model
+ * Ising model
  *
  * Linas September 2005
  *
- * State is encoded as a p-adic string.
+ * State is encoded as a 2-adic string.
  *
- * Actually, 2-adic in the current implementation
  */
+
+#include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 /* Nearest neighbor interaction */
 double nearest_neighbor (double s)
@@ -16,29 +19,64 @@ double nearest_neighbor (double s)
 	double s0,s1;
 
 	s0 = -1.0;
-	if (s> 0.5) {
+	if (s>= 0.5) {
 		s0 = 1.0;
 		s -= 0.5;
 	}
 	s *= 2.0;
 	
 	s1 = -1.0;
-	if (s> 0.5) s1 = 1.0;
+	if (s>= 0.5) s1 = 1.0;
 	
 	return 0.2 * s0*s1;
 }
 
-double energy (double (*func)(double), double s, int n, double temperature)
+/* Return the finite-state energy of string s (length n) */
+double energy (double (*interaction)(double), double s, int n)
 {
 	int i;
 
 	double en = 0.0;
 	for (i=0; i<n; i++)
 	{
-		en += func (s);
+		en += interaction (s);
 		
-		if (s> 0.5) s -= 0.5;
+		/* shift one bit */
+		if (s>= 0.5) s -= 0.5;
 		s *= 2.0;
 	}
+
+	return en;
 }
 
+compute finite state partition
+double partition (double (*interaction)(double), int n)
+{
+	double z = 0.0;
+
+	int m = 1<<n;
+	int i;
+
+	double om = 1.0 / ((double) m);
+	for (i=0; i<m i++)
+	{
+		double s = om * ((double) i);
+
+		double en = energy (interaction, s, n);
+
+		z += exp (en);
+
+		printf ("%d	%g	%g	%g\n", i, s, interaction(s), en);
+	}
+
+	printf ("# partition=%g\n", z);
+
+	return z;
+}
+
+main () 
+{
+	int n = 8;
+
+	partition (nearest_neighbor, n);
+}
