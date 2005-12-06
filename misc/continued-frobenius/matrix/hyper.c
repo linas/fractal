d286035065d04f48f5493b58f098d60d920a38ee
linas <>
2005-12-06 04:42:59 +0000

    paritally worrking

--- misc/continued-frobenius/matrix/hyper.c
+++ misc/continued-frobenius/matrix/hyper.c
@@ -28,10 +28,11 @@ getworkdim (int dim,
         double *matrix,  /* double precision complex */
         double *eigenvalues,
 		  double *left_vec, double *right_vec,
-		  int workdim, double *workspace_a, double *workspace_b)
+		  double *workspace_a, double *workspace_b)
 {
 	char jobvl = 'V';
 	char jobvr = 'V';
+	int workdim;
 	int info;
 
 	workdim = -1;
@@ -67,11 +68,42 @@ get_complex_eigen (int dim,
 
 complex double in (int n)
 {
+	complex double ii = I;
+
+	if (0 > n)
+	{
+		n = -n;
+		ii = -I;
+	}
 	if (n%4 == 0) return 1.0;
-	if (n%4 == 1) return I;
+	if (n%4 == 1) return 0.0 + ii;
 	if (n%4 == 2) return -1.0;
-	if (n%4 == 3) return -I;
+	if (n%4 == 3) return 0.0 -ii;
 	return 0.0;
+}
+
+complex double nin (int n)
+{
+	complex double ii = -I;
+
+	if (0 > n)
+	{
+		n = -n;
+		ii = I;
+	}
+	if (n%4 == 0) return 1.0;
+	if (n%4 == 1) return 0.0 + ii;
+	if (n%4 == 2) return -1.0;
+	if (n%4 == 3) return 0.0 -ii;
+	return 0.0;
+}
+
+complex double nn (int n)
+{
+	if (0 > n) n = -n;
+
+	if (n%2 == 0) return 1.0;
+	return -1.0;
 }
 
 complex double compose (int m, int n)
@@ -81,18 +113,23 @@ complex double compose (int m, int n)
 	if (m == n)
 	{
 		if (0 == m) return (complex double) 1.0;
-
-		ret = 0.25 + (in(n) - 1.0)/ (3.0*M_PI*I*n);
+		ret = 0.25 + (in(n) - 1.0)/ (0.0 + 3.0*M_PI*I*n);
 		return ret;
 	}
 	if (m == 2*n)
 	{
+		ret = 0.25 + (nin(n) - 1.0)/ (0.0 + 6.0*M_PI*I*n);
 		return ret;
 	}
-	if (n == 2*n)
+	if (2*m == n)
 	{
+		ret = 0.5 - (nin(n) - 1.0)/ (0.0 + 3.0*M_PI*I*n);
 		return ret;
 	}
+
+	ret = 3.0 * nn(n+m) * (n-m) - in(m)*(n-2.0*m) - in(n) * (2.0*n-m);
+	ret *= nn(m+n) * n;
+	ret /= 2 * M_PI * I * (2*n-m)*(n-m)*(n-2.0*m);
 
 	return ret;
 }
@@ -110,7 +147,8 @@ int main (int argc, char * argv[])
 	int workdim;
 	int i,j, k;
 	
-	dim = 6;
+	dim = 9;
+	int nc = - (dim-1)/2;
 
 	printf ("#\n#\n");
 	printf ("# Eigenvectors of hyperbolic composition \n");
@@ -130,19 +168,19 @@ int main (int argc, char * argv[])
 		for (j=0; j<dim; j++)
 		{
 			/* Note transposed matrix'ing for FORTRAN */
-			// mat[i+j*dim] = ache_mp(i,j);
-			// mat[i+j*dim] = sst(i,j);
+			complex double v = compose (i+nc,j+nc);
+			mat[i+j*dim] = creal(v);
+			mat[i+j*dim+1] = cimag(v);
+			// printf ("duude m(%d %d) = %g + i %g \n", i+nc, j+nc, creal (v), cimag (v));
 		}
 	}
 
-	complex double v = compose (1,1);
-	printf ("duude %g + i %g \n", creal (v), cimag (v));
 
-#if 0
-	int wd = getworkdim (dim, mat, ere, eim, lev, rev, work);
+	int wd = getworkdim (dim, mat, val, lev, rev, work);
 	printf ("# recommended dim=%d actual dim=%d\n#\n", wd, workdim);
 	// workdim = wd;
 	
+#if 0
 	work = (double *) realloc (work, workdim*sizeof (double));
 	geteigen (dim, mat, ere, eim, lev, rev, workdim, work);
 
