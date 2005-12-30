a856bb1dd129d04c6a48a5511b198bc0b5d9acec
linas <>
2005-12-30 11:37:11 +0000

    initial checkin

---
+++ generate/takagi.C
@@ -0,0 +1,35 @@
+/*
+ * takagi.C
+ *
+ * FUNCTION:
+ * display takagi on the complex unit disk
+ * for a "generic" point.
+ *
+ * HISTORY:
+ * Linas December 2005
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "brat.h"
+
+static void takagi_series_c (double re_q, double im_q, double *prep, double *pimp)
+{
+	*prep = rep;
+	*pimp = imp;
+}
+
+static double takagi_series (double re_q, double im_q, int itermax)
+{
+	double rep, imp;
+	takagi_series_c (re_q, im_q, &rep, &imp);
+	// return sqrt (rep*rep+imp*imp);
+	return rep;
+}
+
+
+DECL_MAKE_HISTO(takagi_series);
+
+/* --------------------------- END OF LIFE ------------------------- */
