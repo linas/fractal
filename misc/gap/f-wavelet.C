84541c51ec1744fe3ea240ca970ea5a18a0dedca
linas <>
2006-01-01 17:21:24 +0000

    whatever

--- misc/gap/f-wavelet.C
+++ misc/gap/f-wavelet.C
@@ -1,9 +1,10 @@
 
 /*
- * f-invert.C
+ * f-wavelet.C
  *
  * Obtain binaryu coefficieints corresponding to 
  * Distribution of the Farey Numbers on the unit interval
+ * This may or may not require wavelet-like analysis.
  *
  * Linas October 2004
  */
@@ -31,23 +32,24 @@ void GetNextDyadic (unsigned int *n, unsigned int *d)
 	*d = last_d;
 }
 
+#define BINSZ 45400
+double bin[BINSZ];
 
-void bincount(int nbins, int max)
+void bincount(int npow, int max)
 {
 	int i;
+	int nbins = 1<<npow;
 
 	printf ("#\n# nbins=%d   maxiter=%d\n#\n",nbins,max);
 
 	FareyIterator fi;
 
-#define BINSZ 45400
-	int bin[BINSZ];
 	for (i=0; i<BINSZ; i++)
 	{
-		bin[i] = 0;
+		bin[i] = 0.0;
 	}
-	bin[0] = 1;
-	bin[nbins-1] = 1;
+	bin[0] = 1.0;
+	bin[nbins-1] = 1.0;
 
 	/* Compute the distribution by bining */
 	int cnt =2;
@@ -60,10 +62,11 @@ void bincount(int nbins, int max)
 		double x = ((double) n)/ ((double) d);
 		x *= nbins;
 		int ib = (int) x;
-		bin [ib] ++;
+		bin [ib] += 1.0;
 		cnt ++;
 	}
 
+#if 0
 	/* Compute the integral of the distribution */
    ContinuedFraction f;
 	double gral = 0.0;
@@ -80,9 +83,44 @@ void bincount(int nbins, int max)
 
 		printf ("%6d	%8.6g	%8.6g	%8.6g	%8.6g\n", i, x, bcnt, gral, far);
 	}
+#endif
 }
 
-void coeffs
+/* perform fourier transform; However, this explcitly omits
+ * non-scaling wavelengths: only power of two are considered.
+ */
+
+void fourier (double *bins, int npow)
+{
+	int i;
+	int p;
+	int nbins = 1<<npow;
+
+	int step = nbins >>1;
+	for (p=0; p<npow; p++)
+	{
+		double aleft = 0.0;
+		double aright =  0.0;
+		int ncnt = 0;
+		while (ncnt < nbins-1)
+		{
+			for (i=0; i< step; i++)
+			{
+				aleft += bins[ncnt+i];
+			}
+			ncnt += step;
+			for (i=0; i< step; i++)
+			{
+				aright += bins[ncnt+i];
+			}
+			ncnt += step;
+		}
+		double lpr = aleft+aright;
+		double lmr = (aleft-aright)/lpr;
+		printf ("duude p=%d step=%d \t%g\t%g\n", p, step, lpr, lmr);
+		step >>= 1;
+	}
+}
 
 main(int argc, char *argv[])
 {
@@ -90,12 +128,13 @@ main(int argc, char *argv[])
 
 	if (argc <2)
 	{
-		fprintf (stderr, "Usage: %s <nbins> <maxiter>\n", argv[0]);
+		fprintf (stderr, "Usage: %s <npow> <maxiter>\n", argv[0]);
 		exit (1);
 	}
-	int nbins = atoi (argv[1]);
+	int npow = atoi (argv[1]);
 	int max = atoi (argv[2]);
 
-	bincount (nbins, max);
+	bincount (npow, max);
+	fourier (bin, npow);
 }
 
