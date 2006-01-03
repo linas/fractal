d391a039d0fe42a8117a078ffdaeba302e4012de
linas <>
2006-01-03 14:07:40 +0000

    updates

--- misc/wavelet/question.c
+++ misc/wavelet/question.c
@@ -7,6 +7,27 @@
  */
 
 
+// return single-integer index corresponding to the
+// discrete wavelet index (j,k)
+#DEFINE HIDX(j,k)  ((1<<((j)-1))+(k))
+
+// Return the minimum value below which the 
+// (j,k)'th Haar wavelet is vanishing
+inline double 
+haar_domain_min (int j, int k)
+{
+	return ((double) k) / ((double) (1<<(j))
+}
+
+// Return the maximum value above which the 
+// (j,k)'th Haar wavelet is vanishing
+inline double 
+haar_domain_max (int j, int k)
+{
+	return ((double) k+1) / ((double) (1<<(j))
+}
+
+
 main ()
 {
 }
