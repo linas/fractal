f774a9268d87fc7d94400ed7a753b7dd08f8fd6b
linas <>
2006-01-03 13:55:35 +0000

    initial checkin

---
+++ misc/wavelet/question.c
@@ -0,0 +1,12 @@
+/*
+ * Set up the matrix elements of the transfer operator 
+ * (composition operator) of the Minkowski question mark function.
+ * The basis are Haar wavelets.
+ *
+ * Linas Vepstas January 2006 
+ */
+
+
+main ()
+{
+}
