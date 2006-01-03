86fe15f0bf6d210dcbebe624f31889a2936b29ad
linas <>
2006-01-03 14:29:27 +0000

    rename again

--- misc/wavelet/question.C
+++ misc/wavelet/question.C
@@ -6,8 +6,6 @@
  * Linas Vepstas January 2006 
  */
 
-#include "Farey.h"
-
 // return single-integer index corresponding to the
 // discrete wavelet index (j,k)
 #DEFINE HIDX(j,k)  ((1<<((j)-1))+(k))
@@ -28,12 +26,44 @@ haar_domain_max (int j, int k)
 	return ((double) k+1) / ((double) (1<<(j))
 }
 
-ContinuedFraction f;
+class SternBrocotTree
+{
+	public:
+		SternBrocotTree (void);
+		double GetFarey (int j, int k);
+
+	private:
+		void Fill (int jlo, int jhi);
+		int maxidx;
+		int max_j;
+		int * numerators;
+		int * denominators;
+};
+
+SternBrocotTree::SternBrocotTree (void);
+{
+	max_j = 10;
+	maxidx = 1<<max_j;
+	numerators = (int *) malloc (maxidx * sizeof (int));
+	denominators = (int *) malloc (maxidx * sizeof (int));
+	Fill (0, max_j);
+}
+
+void SternBrocotTree::Fill (int jlo, int jhi)
+{
+	int j,k;
+
+	for (j=jlo; j<=jhi;  j++)
+	{
+		for (k=0; k<(1<<j); k++)
+		{
+		}
+	}
+}
 
 inline double
 farey_domain_midpoint (int j, int k)
 {
-	f.SetRatio (
 }
 
 main ()
