88a4e2522604036e463eea3eb73f5f000f780d5b
linas <>
2006-01-04 00:21:21 +0000

    more fixes

--- misc/wavelet/question.C
+++ misc/wavelet/question.C
@@ -36,6 +36,7 @@ class SternBrocotTree
 		double GetFarey (int j, int k);
 
 	private:
+		void Enlarge (int j);
 		void Fill (int jlo, int jhi);
 		int maxidx;
 		int max_j;
@@ -46,28 +47,34 @@ class SternBrocotTree
 SternBrocotTree::SternBrocotTree (void)
 {
 	max_j = 4;
-	maxidx = 1<<(max_j+1);
+	maxidx = 1<<max_j;
 	numerators = (int *) malloc (maxidx * sizeof (int));
 	denominators = (int *) malloc (maxidx * sizeof (int));
 	Fill (0, max_j);
+}
+
+void SternBrocotTree::Enlarge (int j)
+{
+	maxidx = 1<<j;
+	numerators = (int *) realloc (numerators, maxidx * sizeof (int));
+	denominators = (int *) realloc (denominators, maxidx * sizeof (int));
+	Fill (max_j, j);
+	max_j = j;
 }
 
 void SternBrocotTree::Fill (int jlo, int jhi)
 {
 	int j,k;
 
-	for (j=jlo; j<=jhi;  j++)
+	for (j=jlo; j<jhi;  j++)
 	{
 		numerators [HIDX(j,0)] = 1;
 		denominators [HIDX(j, 0)] = j+2;
 
-printf ("wtf idx=%d (%d/%d)\n", HIDX(j,0), numerators [HIDX(j,0)], denominators [HIDX(j,0)]);
 		if (0 == j) continue;
 
 		numerators [HIDX(j,(1<<j)-1)] = j+1;
 		denominators [HIDX(j, (1<<j)-1)] = j+2;
-k=(1<<j)-1;
-printf ("other idx=%d (%d/%d)\n", HIDX(j,k), numerators [HIDX(j,k)], denominators [HIDX(j,k)]);
 		for (k=1; k<(1<<j)-1; k++)
 		{
 			int kl, kr, jl, jr; 
@@ -75,33 +82,38 @@ printf ("other idx=%d (%d/%d)\n", HIDX(j,k), numerators [HIDX(j,k)], denominator
 			kr = k+1;
 			jl = j-1;
 			jr = j-1;
-printf ("duude start left idx=%d (jl, kl)=(%d %d) lidx=%d \n", HIDX(j,k), jl, kl,  HIDX(jl,kl));
 			while (kl%2 == 0) {kl>>= 1; jl -= 1; } 
 			while (kr%2 == 0) {kr>>= 1; jr -= 1; } 
 			kl = (kl-1)>>1;
 			kr = (kr-1)>>1;
 			
-printf ("duude left idx=%d (jl, kl)=(%d %d) lidx=%d (%d/%d) \n", HIDX(j,k), jl, kl,  HIDX(jl,kl), numerators [HIDX(jl,kl)], denominators [HIDX(jl,kl)] );
-printf ("duude rigt idx=%d (jr, kr)=(%d %d) ridx=%d (%d/%d) \n", HIDX(j,k), jr, kr,  HIDX(jr,kr), numerators [HIDX(jr,kr)], denominators [HIDX(jr,kr)] );
 			numerators [HIDX(j,k)] = numerators [HIDX(jl,kl)] + numerators [HIDX(jr,kr)];
 			denominators [HIDX(j,k)] = denominators [HIDX(jl,kl)] + denominators [HIDX(jr,kr)];
-
-printf ("dude its idx=%d (%d/%d) == (%d/%d)\n\n", HIDX(j,k), 2*k+1, 1<<(j+1), numerators [HIDX(j,k)], denominators [HIDX(j,k)]);
 		}
 	}
 
-printf ("\n");
-	for (j=jlo; j<=jhi;  j++)
+#if 1
+	for (j=jlo; j<jhi;  j++)
 	{
 		for (k=0; k<(1<<j); k++)
 		{
 			int i = HIDX(j,k);
-printf ("really its idx=%d (%d/%d) == (%d/%d)\n", HIDX(j,k), 2*k+1, 1<<(j+1), numerators [HIDX(j,k)], denominators [HIDX(j,k)]);
+			printf ("really its idx=%d (%d/%d) == (%d/%d)\n", HIDX(j,k), 2*k+1, 1<<(j+1), 
+			        numerators [HIDX(j,k)], denominators [HIDX(j,k)]);
 		}
-printf ("\n");
+		printf ("\n");
 	}
-	
+#endif
 }
+
+double SternBrocotTree::GetFarey (int j, int k)
+{
+	if (j > max_j) Enlarge (j);
+
+	return ((double)  numerators [HIDX(j,k)]) / ((double) denominators [HIDX(j,k)]);
+}
+
+// ==========================================================================
 
 inline double
 farey_domain_midpoint (int j, int k)
@@ -111,4 +123,6 @@ farey_domain_midpoint (int j, int k)
 main ()
 {
 	SternBrocotTree t;
+
+	t.GetFarey (8,0);
 }
