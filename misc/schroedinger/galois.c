edce2c18db55f722dbe9e5e8915736b8d9c8f1d7
linas <>
2006-01-22 20:57:34 +0000

    updates

--- misc/schroedinger/galois.c
+++ misc/schroedinger/galois.c
@@ -25,19 +25,59 @@ zero_ansatz (int n, double a8, double a6, double a4, double a2, double e)
 main(int argc, char* argv[])
 {
 	int n;
-#define NMAX 10000
+#define NMAX 1000
 	double ar[NMAX];
 
+	if (argc != 2)
+	{
+		fprintf (stderr, "Usage: %s <energy>\n", argv[0]);
+		exit (1); 
+	}
 	double e = atof (argv[1]);
 
-	ar[NMAX-2] = 1.0;
-	ar[NMAX-4] = 0.5;
-	ar[NMAX-6] = 3.0/16.0;
-	ar[NMAX-8] = 1.0/16.0;
-	for (n=NMAX-10; n>=0; n-=2)
+	int nlast = 100;
+
+	ar[nlast] = 0.0;
+	ar[nlast-2] = 0.0;
+	ar[nlast-4] = 0.0;
+	ar[nlast-6] = 1.0;
+	for (n=nlast-8; n>=0; n-=2)
 	{
 		ar[n] = zero_ansatz (n, ar[n+8], ar[n+6], ar[n+4], ar[n+2], e);
-		printf ("%d	%g\n", n, ar[n]);
+	}
+
+	int nstart = 0;
+	if (nlast%2)
+	{
+		nstart = 1;
+	}
+
+	/* normalize */
+	double off = 1.0/ar[nstart];
+	for (n=nstart; n<nlast; n+=2)
+	{
+		ar[n] *= off;
+		// printf ("%d	%g\n", n, ar[n]);
+	}
+
+	/* wave function */
+	double x;
+
+	for (x=-10.0; x<10.0; x +=0.4)
+	{
+		double xn = 1.0;
+		if (nlast%2)
+		{
+			xn = x;
+		}
+		double psi = 0.0;
+		for (n=nstart; n<nlast; n+=2)
+		{
+			psi += ar[n] * xn;
+			xn *= x*x;
+		}
+		psi *= x*x-0.25;
+		printf ("%g	%g\n", x, psi);
 	}
 }
 
