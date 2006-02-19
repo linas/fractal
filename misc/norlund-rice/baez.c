137a43801092c0dad318f68d16c8516b5cb79068
linas <>
2006-02-19 21:21:23 +0000

    updates

--- misc/norlund-rice/baez.c
+++ misc/norlund-rice/baez.c
@@ -7,6 +7,7 @@
  * Linas Vepstas, February 2006
  */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <gsl/gsl_sf_gamma.h>
@@ -25,6 +26,36 @@ void integrand (double t, int n, double *reg, double * img)
 	gsl_sf_lngamma_complex_e (res, ims, &lnr, &arg);
 	regr += lnr.val;
 	imgr += arg.val;
+
+	/* Gamma (s+n+1) */
+	gsl_sf_lngamma_complex_e (res+n+1.0, ims, &lnr, &arg);
+	regr -= lnr.val;
+	imgr -= arg.val;
+
+	/* Gamma (2*s+1) */
+	gsl_sf_lngamma_complex_e (2.0*res+1.0, 2.0*ims, &lnr, &arg);
+	regr -= lnr.val;
+	imgr -= arg.val;
+
+	/* (2pi)^(2s-1) */
+	double lp = log (2.0*M_PI);
+	regr += (2.0*res-1.0)*lp;
+	imgr += 2.0*ims*lp;
+
+	/* sin (pi s) */
+	double r = exp (-M_PI*ims);
+	double x = r * cos (M_PI*res);
+	double y = r * sin (M_PI*res);
+
+	r = 1.0/r;
+	x -= r * cos (M_PI*res);
+	y += r * sin (M_PI*res);
+
+	double theta = atan2 (y, x);
+	r = 0.5 * log (x*x+y*y);
+
+	regr -= r;
+	imgr -= theta;
 
 	*reg = regr;
 	*img = imgr;
