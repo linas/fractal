d5e77298a50d16ff81ec8160b69250878cec8175
linas <>
2006-02-19 21:05:54 +0000

    gsl_sf_resul lnr, arg;

--- misc/norlund-rice/baez.c
+++ misc/norlund-rice/baez.c
@@ -7,13 +7,31 @@
  * Linas Vepstas, February 2006
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <gsl/gsl_sf_gamma.h>
 
 void integrand (double t, int n, double *reg, double * img)
 {
+	gsl_sf_result lnr, arg;
+
+	double regr = 0.0;
+	double imgr = 0.0;
+
+	double res = 0.5;
+	double ims = t;
+
+	/* Gamma (s) */
+	gsl_sf_lngamma_complex_e (res, ims, &lnr, &arg);
+	regr += lnr.val;
+	imgr += arg.val;
+
+	*reg = regr;
+	*img = imgr;
 }
 
-main ()
+int
+main (int argc, char * argv[])
 {
 	double t=0.0;
 	int n=3;
@@ -23,6 +41,8 @@ main ()
 		double reg, img;
 		integrand (t, n, &reg, &img);
 
-		printf ("duude its %g  - -%g %g \n", t, reg, img);
+		printf ("duude its %g   %g %g \n", t, reg, img);
 	}
+
+	return 0;
 }
