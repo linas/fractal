5b514ae053f29d5034b4ca378588612c727bd9c5
linas <>
2006-03-11 05:54:48 +0000

    near-final form

--- generate/hermite.C
+++ generate/hermite.C
@@ -37,7 +37,7 @@ double xdelta;
  
 void init_hermite_grid (double exmax, double exdelta, int enmax)
 {
-	nsteps = xmax / xdelta +1;
+	nsteps = (int) floor(xmax / xdelta) +1;
 	nmax = enmax;
 	xmax = exmax;
 	xdelta = exdelta;
@@ -58,22 +58,49 @@ void init_hermite_grid (double exmax, double exdelta, int enmax)
 
 double hermite (double x, int n)
 {
-	int iarr = x/xdelta;
-	return  hermite_grid[iarr][n];
+	double sgn = 1.0;
+	if (x < 0.0)
+	{
+		x = -x;
+		if (n%2) sgn = -1.0;
+	}
+	int iarr = (int) ((x+0.5*xdelta)/xdelta);
+	return  sgn*hermite_grid[iarr][n];
 }
 
 static double hermite_green (double x, double y)
 {
 	double acc = 0.0;
 	int n;
+	double en = 0.5;
+	double tn = 1.0;
+	double nfac = 1.0;
 	for (n=0; n<nmax; n++)
 	{
+		double term =  hermite (x,n) * hermite (y,n);
+		term /= tn * nfac * en;
+		acc += term;
+
+		nfac *= n+1;
+		en = n+0.5;
+		tn *= 2.0;
+		printf ("duude n=%d term=%g\n", n, term);
 	}
+	printf ("\n");
+
+	acc *= exp (-0.5*(x*x+y*y));
+	acc /= sqrt (M_PI);
 	return acc;
 }
 
 static double hermite_green_wrap (double x, double y, int itermax, double param)
 {
+	int is_init = 0;
+	if (!is_init)
+	{
+		is_init = 1;
+		init_hermite_grid (9.0, 0.01 ,100);
+	}
 	return hermite_green (x,y);
 }
 
