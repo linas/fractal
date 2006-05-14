334655979fd80786828d80fa13d2289625057f72
linas <>
2006-05-14 04:59:33 +0000

    revisions

--- generate/plouffe.C
+++ generate/plouffe.C
@@ -17,35 +17,41 @@
 
 #include "brat.h"
 
+static double ess;
 
-static void totient_series_c (double re_q, double im_q, double *prep, double *pimp)
+static void plouffe_series_c (double re_q, double im_q, double *prep, double *pimp)
 {
 	double tmp;
-	int i;
+	int n;
 	*prep = 0.0;
 	*pimp = 0.0;
 
 	double rep = 0.0;
 	double imp = 0.0;
 
-	double qpr = 1.0;
-	double qpi = 0.0;
+	double qpr = re_q;
+	double qpi = im_q;
 
 	double qpmod = re_q*re_q+im_q*im_q;
 	if (1.0 <= qpmod) return;
 
-	for (i=0; i<max_terms; i++)
+	for (n=1; n<max_terms; n++)
 	{
-		double t = totient_phi (i+1);
+		/* compute n^(-s) */
+		double term = pow (n, -s);
 
-		t *= (i+1);
-		t *= (i+1);
-		t *= (i+1);
+		/* compute 1/(q^n-1) */
+		double qmr = qpr-1.0;
+		double qmi = qpi;
+		double qm = qmr*qmr + qmi*qmi;
 
-		rep += qpr *t;
-		imp += qpi *t;
+		double qvr = qmr/qm;
+		double qvi = -qmi/qm;
 
-		/* compute q^k */
+		rep += qvr *term;
+		imp += qvi *term;
+
+		/* compute q^n */
 		tmp = qpr*re_q - qpi * im_q;
 		qpi = qpr*im_q + qpi * re_q;
 		qpr = tmp;
@@ -58,38 +64,20 @@ static void totient_series_c (double re_q, double im_q, double *prep, double *pi
 		// printf ("not converged re=%g im=%g modulus=%g\n", re_q, im_q, qpmod);
 	}
 
-#if NO_NOT_THIS
-	/* multiply by (1-q)^2 */
-	qpr = 1.0 - re_q;
-	qpi = - im_q;
-	tmp = qpr*qpr - qpi * qpi;
-	qpi = 2.0*qpr*qpi;
-	qpr = tmp;
-
-	tmp = qpr*rep - qpi * imp;
-	imp = qpr*imp + qpi * rep;
-	rep = tmp;
-#endif
-	/* multiply by (1-|q|)^2 */
-	tmp = 1.0 - sqrt (re_q*re_q + im_q*im_q);
-	tmp *= tmp;
-
-	rep *= tmp;
-	imp *= tmp;
-
 	*prep = rep;
 	*pimp = imp;
 }
 
-static double totient_series (double re_q, double im_q, int itermax, double param)
+static double plouffe_series (double re_q, double im_q, int itermax, double param)
 {
-	max_terms = itermax;
+	ess = param;
+	ess = 3.0;
 	double rep, imp;
-	totient_series_c (re_q, im_q, &rep, &imp);
+	plouffe_series_c (re_q, im_q, &rep, &imp);
 	// return sqrt (rep*rep+imp*imp);
 	return rep;
 }
 
-DECL_MAKE_HISTO(totient_series);
+DECL_MAKE_HISTO(plouffe_series);
 
 /* --------------------------- END OF LIFE ------------------------- */
