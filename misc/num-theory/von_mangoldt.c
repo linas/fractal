db5a8e7df1c50724f7d746f07e7b9f1be89dc114
linas <>
2006-07-03 18:47:17 +0000

    more hacking

--- misc/num-theory/von_mangoldt.c
+++ misc/num-theory/von_mangoldt.c
@@ -31,10 +31,11 @@ int main ()
 {
 	int i;
 
-	int nmax = 10000000;
+	int nmax = 100;
 	
 	long double sum = 0.0;
-	int scale = 10000;
+	long double mert = 0.0;
+	int scale = 1;
 
 	long double offset = log(M_PI);
 	// long double offset = 0.0;
@@ -42,10 +43,12 @@ int main ()
 	{
 		// sum = chebyshev (i);
 		sum += mangoldt_lambda(i);
+		// mert += moebius_mu(i);
 
 		if (0 == i%scale) 
 		{
 			long double lead = sum - i+offset;
+			// long double lead = sum/mert;
 			printf ("%d	%24.18Lg\n", i, lead);
 			fflush (stdout);
 			// if (i > scale*300) scale *= 2;
