3fe4365e1be0ed49d30f614f6d6f3cc780883cc8
linas <>
2006-07-04 00:53:03 +0000

    more general caching

--- tools/src/libs/farey/cache.c
+++ tools/src/libs/farey/cache.c
@@ -9,50 +9,64 @@
 
 #include "cache.h"
 
-/** ld_one_d_cache_check() -- check if long double value is in the cache
+/* =============================================================== */
+/** TYPE_NAME##_d_cache_check() -- check if long double value is in the cache
  *  Returns true if the value is in the cache, else returns false.
  *  This assumes a 1-dimensional cache layout (simple aray)
  */
-int ld_one_d_cache_check (ld_cache *c, unsigned int n)
-{
-	if (c->disabled) return 0;
-	if ((n > c->nmax) || 0==n )
-	{
-		unsigned int newsize = 1.5*n+1;
-		c->cache = (long double *) realloc (c->cache, newsize * sizeof (long double));
-		c->ticky = (char *) realloc (c->ticky, newsize * sizeof (char));
-
-		unsigned int en;
-		unsigned int nstart = c->nmax+1;
-		if (0 == c->nmax) nstart = 0;
-		for (en=nstart; en <newsize; en++)
-		{
-			c->cache[en] = 0.0L;
-			c->ticky[en] = 0;
-		}
-		c->nmax = newsize-1;
-		return 0;
-	}
-
-	return (c->ticky[n]);
+#define CACHE_CHECK(TYPE_NAME,TYPE) \
+int TYPE_NAME##_d_cache_check (TYPE_NAME##_cache *c, unsigned int n)	\
+{	\
+	if (c->disabled) return 0;	\
+	if ((n > c->nmax) || 0==n )	\
+	{	\
+		unsigned int newsize = 1.5*n+1;	\
+		c->cache = (TYPE *) realloc (c->cache, newsize * sizeof (TYPE));	\
+		c->ticky = (char *) realloc (c->ticky, newsize * sizeof (char));	\
+	\
+		unsigned int en;	\
+		unsigned int nstart = c->nmax+1;	\
+		if (0 == c->nmax) nstart = 0;	\
+		for (en=nstart; en <newsize; en++)	\
+		{	\
+			c->cache[en] = 0.0L;	\
+			c->ticky[en] = 0;	\
+		}	\
+		c->nmax = newsize-1;	\
+		return 0;	\
+	}	\
+	\
+	return (c->ticky[n]);	\
 }
 
 /** 
- * ld_one_d_cache_fetch - fetch value from cache
+ * TYPE_NAME##_d_cache_fetch - fetch value from cache	
  */
-long double ld_one_d_cache_fetch (ld_cache *c, unsigned int n)
-{
-	if (c->disabled) return 0.0L;
-	return c->cache[n];
+#define CACHE_FETCH(TYPE_NAME,TYPE) \
+TYPE TYPE_NAME##_d_cache_fetch (TYPE_NAME##_cache *c, unsigned int n)	\
+{	\
+	if (c->disabled) return 0.0L;	\
+	return c->cache[n];	\
 }
 
 /**
- * ld_one_d_cache_store - store value in cache
+ * TYPE_NAME##_d_cache_store - store value in cache
  */
-void ld_one_d_cache_store (ld_cache *c, long double val, unsigned int n)
-{
-	if (c->disabled) return;
-	c->cache[n] = val;
-	c->ticky[n] = 1;
+#define CACHE_STORE(TYPE_NAME,TYPE) \
+void TYPE_NAME##_d_cache_store (TYPE_NAME##_cache *c, TYPE val, unsigned int n)	\
+{	\
+	if (c->disabled) return;	\
+	c->cache[n] = val;	\
+	c->ticky[n] = 1;	\
 }
 
+/* =============================================================== */
+
+#define DEFINE_CACHE(TYPE_NAME,TYPE) \
+		  CACHE_CHECK(TYPE_NAME,TYPE) \
+		  CACHE_FETCH(TYPE_NAME,TYPE) \
+		  CACHE_STORE(TYPE_NAME,TYPE)
+
+DEFINE_CACHE(ld, long double)
+DEFINE_CACHE(ui, unsigned int)
+		  
--- tools/src/libs/farey/cache.h
+++ tools/src/libs/farey/cache.h
@@ -35,3 +35,32 @@ long double ld_one_d_cache_fetch (ld_cache *c, unsigned int n);
  */
 void ld_one_d_cache_store (ld_cache *c, long double val, unsigned int n);
 
+/* ======================================================================= */
+/* Cache management */
+
+typedef struct {
+	unsigned int nmax;
+	unsigned int *cache;
+	char *ticky;
+	short disabled;
+} ui_cache;
+
+#define DECLARE_UI_CACHE(name)         \
+	static ui_cache name = {.nmax=0, .cache=NULL, .ticky=NULL, .disabled = 0}
+
+/** ui_one_d_cache_check() -- check if long double value is in the cache
+ *  Returns true if the value is in the cache, else returns false.
+ *  This assumes a 1-dimensional cache layout (simple aray)
+ */
+int ui_one_d_cache_check (ui_cache *c, unsigned int n);
+
+/** 
+ * ui_one_d_cache_fetch - fetch value from cache
+ */
+unsigned int ui_one_d_cache_fetch (ui_cache *c, unsigned int n);
+
+/**
+ * ui_one_d_cache_store - store value in cache
+ */
+void ui_one_d_cache_store (ui_cache *c, unsigned int val, unsigned int n);
+
