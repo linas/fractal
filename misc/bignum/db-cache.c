616f242b32330b5bef731bde7c5f5b053c6ac148
linas <>
2006-07-30 02:10:19 +0000

    misc updates

--- misc/bignum/db-cache.c
+++ misc/bignum/db-cache.c
@@ -11,6 +11,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 #include <gmp.h>
@@ -20,7 +21,8 @@ void fp_cache_put (const char * dbname, mpf_t val, int idx, int nprec)
 {
 	DB *db;
 
-	db = dbopen (dbname, O_RDWR, O_CREAT, DB_RECNO, NULL);
+	db = dbopen (dbname, O_RDWR|O_CREAT, 
+	             S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH, DB_HASH, NULL);
 
 	if (!db)
 	{
@@ -28,18 +30,41 @@ void fp_cache_put (const char * dbname, mpf_t val, int idx, int nprec)
 		return;
 	}
 
-	DBT pkey, vkey;
+	/* Save the value data to the file */
+	DBT vkey;
 	char buf[50];
 	sprintf (buf, "val[%d]", idx);
 	vkey.data = &buf;
 	vkey.size = strlen(buf)+1;
 
-	DBT vdat;
-	vdat.data = "asdf";
-	vdat.size = 5;
+	/* the printf is floating point */
+	size_t allo = nprec*sizeof(char)+50;
+	char *vstr= malloc (allo);
+	gmp_snprintf (vstr, allo, "%.*Fg", nprec, val);
 
-	db->put (db, &vkey, &vdat, R_SETCURSOR);
+	DBT vdat;
+	vdat.data = vstr;
+	vdat.size = strlen(vstr)+1;
+
+	db->put (db, &vkey, &vdat, 0);
+	free (vstr);
+
+	/* Save the precision data to the file */
+	DBT pkey;
+	sprintf (buf, "prec[%d]", idx);
+	pkey.data = &buf;
+	pkey.size = strlen(buf)+1;
+
+	DBT pdat;
+	pdat.data = &nprec;
+	pdat.size = sizeof(int);
+
+	db->put (db, &pkey, &pdat, 0);
 
 	db->close (db);
 }
 
+int fp_cache_get (const char * dbname, mpf_t val, int idx, int nprec)
+{
+	return 0;
+}
--- misc/bignum/db-cache.h
+++ misc/bignum/db-cache.h
@@ -8,4 +8,17 @@
 
 #include <gmp.h>
 
+/**
+ * fp_cahce_put -- put mpf_t value into the database.
+ * prec: number of decimal places of precision to store.
+ * idx:  index under which to store the value
+ */
 void fp_cache_put (const char * dbname, mpf_t val, int idx, int nprec);
+
+/**
+ * fp_cache_get -- get mpf_t from database
+ * Returns 0 if no value in the database, or if the value in the
+ * database has fewer than nprec digits. Thus, nprec is a minimum
+ * requirement.
+ */ 
+int fp_cache_get (const char * dbname, mpf_t val, int idx, int nprec);
