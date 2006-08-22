562f00f24671234629bcad398cc0eca470f1ceb7
linas <>
2006-08-22 02:51:01 +0000

    make generic

--- misc/de-rham/de-rham.c
+++ misc/de-rham/de-rham.c
@@ -15,6 +15,8 @@ static double ax = 0.5;
 static double ay = sqrt(3.0)/6.0;
 static double w = 0.6;
 
+static double d,e,f,g;
+
 void koch_0 (double *x, double *y)
 {
 	double re = *x;
@@ -93,6 +95,22 @@ void takagi_1 (double *x, double *y)
 	*y = -xx +w *yy +1.0;
 }
 
+void generic_0 (double *x, double *y)
+{
+	double re = *x;
+	double im = *y;
+	*x = ax * re + d * im;
+	*y = ay * re + e * im;
+}
+
+void generic_1 (double *x, double *y)
+{
+	double re = *x;
+	double im = *y;
+	*x = ax + (1.0-ax) * re + f * im;
+	*y = ay - ay * re + g * im;
+}
+
 void fixpt (double val, double *x, double *y)
 {
 	int i = 0;
@@ -106,7 +124,8 @@ void fixpt (double val, double *x, double *y)
 			// cesaro_1 (x,y);
 			// mink_1 (x,y);
 			// bernoulli_1 (x,y);
-			takagi_1 (x,y);
+			// takagi_1 (x,y);
+			generic_1 (x,y);
 		}
 		else
 		{
@@ -114,7 +133,8 @@ void fixpt (double val, double *x, double *y)
 			// cesaro_0 (x,y);
 			// mink_0 (x,y);
 			// bernoulli_0 (x,y);
-			takagi_0 (x,y);
+			// takagi_0 (x,y);
+			generic_0 (x,y);
 		}
 		nt >>= 1;
 	}
@@ -125,26 +145,30 @@ main (int argc, char *argv[])
 	int i;
 	int p,q;
 
-	if (4 > argc)
+	if (8 > argc)
 	{
 		fprintf (stderr, "Usage: %s <q> <ax> <ay>\n", argv[0]);
 		exit (1);
 	}
 	
-	q  = 43;
+	q  = 243;
 	q = atoi (argv[1]);
 	ax = atof (argv[2]);
 	ay = atof (argv[3]);
-	w = ay;
+	d = atof (argv[4]);
+	e = atof (argv[5]);
+	f = atof (argv[6]);
+	g = atof (argv[7]);
 
+	double x = 0.5;
+	double y = 0.0;
+#if 0
 	double aa = sqrt (ax*ax+ay*ay);
 	double ama = sqrt ((1.0-ax)*(1.0-ax)+ay*ay);
 	double c = sqrt (aa*aa+ama*ama);
 	printf ("#\n# denom=%d\n#\n", q);
 	printf ("# x=%g y=%g |a| = %g |1-a|=%g c=%g\n#\n", ax,ay, aa, ama,c);
 
-	double x = 0.5;
-	double y = 0.0;
 	for (i=0; i<30; i++) cesaro_0 (&x,&y);
 	printf ("# fxpt0 = %g + i %g\n", x ,y );
 	cesaro_1 (&x,&y);
@@ -156,6 +180,7 @@ main (int argc, char *argv[])
 	cesaro_0 (&x,&y);
 	printf ("#\n# 0-fxpt1 = %g + i %g\n", x ,y );
 	printf ("#\n");
+#endif
 
 	for (p=0; p<q; p++) 
 	{
