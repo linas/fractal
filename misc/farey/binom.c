894ce3bcbf2aebf602db46632e209459109fd32c
linas <>
2006-08-25 13:52:45 +0000

    misc updates

--- misc/farey/binom.c
+++ misc/farey/binom.c
@@ -7,16 +7,22 @@
  * August 2006 Linas
  */
 
+#include <stdio.h>
 #include "binomial.h"
 
 main()
 {
-	int n=23;
+	int n=30;
 
 	int k;
-	for (k=0; k<23; k++)
+	for (k=1; k<=n; k++)
 	{
-		double b = binomial (n,k);
+		int g = gcf32 (n,k);
+		int p = n/g;
+		int q = k/g;
+		double b = fbinomial (p,q);
+		double x = ((double)q)/((double)p);
+		printf ("%g	%d	%d	%g\n", x, q,p,b);
 	}
 	
 }
