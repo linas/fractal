af86234352f2fe770d618d057e8faf33a2bc0643
linas <>
2006-08-25 13:33:55 +0000

    initial checkin

---
+++ misc/farey/binom.c
@@ -0,0 +1,22 @@
+
+/*
+ * binom.c
+ *
+ * Experiments with binomial coefficients
+ *
+ * August 2006 Linas
+ */
+
+#include "binomial.h"
+
+main()
+{
+	int n=23;
+
+	int k;
+	for (k=0; k<23; k++)
+	{
+		double b = binomial (n,k);
+	}
+	
+}
