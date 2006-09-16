a96550ca5010f473fdf72dd069d9f2521665c51d
linas <>
2006-09-16 02:14:37 +0000

    initial checkin

---
+++ tools/src/libs/farey/question.c
@@ -0,0 +1,23 @@
+
+/*
+ * question.c
+ *
+ * Minkowski question mark function
+ * (wrapper around the C++ code)
+ * 
+ * Linas Vepstas September 2006
+ */
+
+#include "Farey.h"
+
+static ContinuedFraction work;
+
+double question_mark (int num, int denom)
+{
+	return 0.0;
+}
+
+double fquestion_mark (double x)
+{
+	return 0.0;
+}
---
+++ tools/src/libs/farey/question.h
@@ -0,0 +1,12 @@
+
+/*
+ * question.h
+ *
+ * Minkowski question mark function
+ * (wrapper around the C++ code)
+ * 
+ * Linas Vepstas September 2006
+ */
+
+double question_mark (int num, int denom);
+double fquestion_mark (double);
