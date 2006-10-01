7c8be58e82cf262912bcc1c8274c372b1af8aadb
linas <>
2006-10-01 00:25:53 +0000

    fixes etc

--- generate/baker.C
+++ generate/baker.C
@@ -15,25 +15,43 @@
 
 double ising(double x, double y)
 {
-	if ((x<0.25 || (x>0.75)) return -0.3;
-	return 0.3;
+	// x = y;
+	if ((x<0.25) || (x>=0.75)) return 0.3;
+	return -0.3;
 }
 
 void eigenvec (double *re, double *im, double (*func)(double, double), 
                 double x, double y)
 {
 	int i;
-	double hre = him = 0.0;
+	double hre = 0.0;
+	double him = 0.0;
 	
-	for (i=0; i<40; i++)
+	double xo = x;
+	double yo = y;
+#define NITER 10
+	for (i=0; i<NITER; i++)
 	{
 		hre += func (x,y);
-		int ty = 2.0*y;
+		int ty = (int) floor(2.0*y);
 		x = 0.5*(x+ty);
 		y = 2.0*y-ty;
 	}
 
+	x = yo;
+	y = xo;
+	for (i=0; i<NITER; i++)
+	{
+		int ty = (int) floor(2.0*y);
+		x = 0.5*(x+ty);
+		y = 2.0*y-ty;
+		hre += func (x,y);
+	}
+	// hre /= 80;
+	// him /= 80;
+
 	*re = hre;
+	*im = him;
 }
 
 static double 
@@ -42,6 +60,7 @@ density (double x, double y, int itermax, double param)
 	double re, im;
 
 	eigenvec (&re, &im, ising, x, y);
+	re = exp (-re);
 	return re;
 }
 
