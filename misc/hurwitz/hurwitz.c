cb7bdff991531cb310487ff3b75b32c38d349a63
linas <>
2006-10-06 03:00:09 +0000

    updates

--- misc/hurwitz/hurwitz.c
+++ misc/hurwitz/hurwitz.c
@@ -9,17 +9,21 @@
 #include <math.h>
 #include <stdio.h>
 
+#include "binomial.h"
+
 /* A brute-force summation using Hasse formula, 
  * for complex s, real q. */
 
 void hurwitz_zeta (double *hre, double *him, double sre, double sim, double q)
 {
-	int order = 40;
+	int norder = 40;
 
 	/* arrays storing values to be forward-differenced */
 #define NARR 100
 	double refd[NARR];
 	double imfd[NARR];
+	double rens[NARR];
+	double imns[NARR];
 
 	int k;
 	for (k=0; k<norder; k++)
@@ -29,7 +33,23 @@ void hurwitz_zeta (double *hre, double *him, double sre, double sim, double q)
 		refd[k] = mag * cos (sim*logkq);
 		imfd[k] = mag * sin (sim*logkq);
 
-		printf ("its %d %g %g\n", k, refd[k], imfd[k]);
+		// printf ("its %d \t%g \t%g\n", k, refd[k], imfd[k]);
+	}
+
+	int n;
+	for (n=0; n<norder; n++)
+	{
+		double rs=0.0;
+		double is=0.0;
+		for (k=0; k<=n; k++)
+		{
+			double bin = binomial (n,k);
+			rs += bin * refd[k];
+			is += bin * imfd[k];
+		}
+		rens[n] = rs;
+		imns[n] = is;
+		printf ("its %d \t%g \t%g\n", n, rens[n], imns[n]);
 	}
 } 
 
