7afbb84fc206153cde7ffc904637a2aeb964dc2b
linas <>
2006-11-05 03:27:05 +0000

    updates

--- misc/hurwitz/polylog.c
+++ misc/hurwitz/polylog.c
@@ -25,12 +25,11 @@ static inline cplex cplex_one (void)
 	cplex z; z.re=1.0; z.im=0.0; return z;
 }
 
-static inline cplex cplex_invert (cplex z)
+static inline cplex cplex_minus (cplex z)
 {
 	cplex rv;
-	double mag = 1.0/ (z.re*z.re + z.im*z.im);
-	rv.re = z.re * mag;
-	rv.im = -z.im * mag;
+	rv.re = -z.re;
+	rv.im = -z.im;
 	return rv;
 }
 
@@ -39,6 +38,14 @@ static inline cplex cplex_add (cplex a, cplex b)
 	cplex rv;
 	rv.re = a.re + b.re;
 	rv.im = a.im + b.im;
+	return rv;
+}
+
+static inline cplex cplex_sub (cplex a, cplex b)
+{
+	cplex rv;
+	rv.re = a.re - b.re;
+	rv.im = a.im - b.im;
 	return rv;
 }
 
@@ -58,32 +65,80 @@ static inline cplex cplex_mult (cplex a, cplex b)
 	return rv;
 }
 
+static inline cplex cplex_recip (cplex z)
+{
+	cplex rv;
+	double mag = 1.0/ (z.re*z.re + z.im*z.im);
+	rv.re = z.re * mag;
+	rv.im = -z.im * mag;
+	return rv;
+}
+
+static inline cplex cplex_pow (cplex z, int n)
+{
+	cplex rv;
+	if (20>n)
+	{
+		int k;
+		rv = z;
+		for (k=1; k<n; k++)
+		{
+			rv = cplex_mult (rv, z);
+		}
+	}
+	else
+	{
+		fprintf (stderr, "pow=%d unimplemented\n", n);
+	}
+	return rv;
+}
+
 /* 
  * Return value of sum^k_{j=0} (n j) oz^j
  */
-cplex bee_k (int n, int k, cplex oz)
+static cplex bee_k (int n, int k, cplex oz)
 {
 	int j;
-	cplex pzre = cplex_one();
+	cplex pz = cplex_one();
 	cplex acc = cplex_zero();
 
 	double sgn = 1.0;
 	for (j=0; j<=k; j++)
 	{
-		cplex term = pzre;
+		cplex term = pz;
 		double bin = sgn * binomial (n,j);
 
-printf ("duude j=%d bin=%g\n", j, bin);
 		term = cplex_scale (bin, term);
-printf ("term=%g %g \n", term.re, term.im);
 		acc = cplex_add (acc, term);
-printf ("acc=%g %g \n", acc.re, acc.im);
-		
-		pzre = cplex_mult(pzre, oz);
+		pz = cplex_mult(pz, oz);
 		sgn = -sgn;
 	}
 
 	return acc;
+}
+
+cplex beta(int n, cplex z, cplex s)
+{
+	int k;
+	cplex oz = cplex_recip(z);
+
+	cplex ska = z;
+	ska.re -= 1.0;
+	ska = cplex_mult (ska, cplex_recip(cplex_mult(z,z)));
+	ska = cplex_pow (ska,n);
+	
+	cplex pz = cplex_one();
+	for (k=0; k<=2*n; k++)
+	{
+		cplex bk = bee_k (n,k,oz);
+		bk = cplex_sub (bk, ska);
+		bk = cplex_mult (bk, pz);
+		
+	printf ("duude %d %g %g \n", k, bk.re, bk.im);
+		pz = cplex_mult(pz, oz);
+	}
+
+	return z;
 }
 
 main ()
@@ -92,7 +147,6 @@ main ()
 	z.re = -0.8;
 	z.im = 0.2;
 
-	z = bee_k (6,4, z);
+	beta (6, z,z);
 
-	printf ("duude %g %g \n", z.re, z.im);
 }
