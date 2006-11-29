78e6433917865dd29a8f0670a1c1c50a8cc2df25
linas <>
2006-11-29 14:25:20 +0000

    new file

---
+++ misc/bignum/zmp.h
@@ -0,0 +1,11 @@
+
+#include "db-cache.h"
+#include "mp-binomial.h"
+#include "mp-cache.h"
+#include "mp-complex.h"
+#include "mp-consts.h"
+#include "mp-hyper.h"
+#include "mp-misc.h"
+#include "mp-trig.h"
+#include "mp_zeta.h"
+
