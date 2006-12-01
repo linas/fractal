6fd3f95230f3f208437b04000e2799c48cf21b7c
linas <>
2006-12-01 15:49:40 +0000

    whatever

--- generate/sho.C
+++ generate/sho.C
@@ -96,7 +96,7 @@ static int prec;
 static void psi_init (void)
 {
 	/* the decimal precison (number of decimal places) */
-	prec = 250;
+	prec = 120;
 
 	/* compute number of binary bits this corresponds to. */
 	double v = ((double) prec) *log(10.0) / log(2.0);
@@ -117,12 +117,18 @@ static double psi (double re_q, double im_q, int itermax, double param)
 	static int init = 0;
 	if (!init) {psi_init(); init=1; }
 		  
-	double mag = 1.0 - sqrt (re_q*re_q + im_q*im_q);
+#if 0
+	// double mag = 1.0 - sqrt (re_q*re_q + im_q*im_q);
+	double mag = 1.0 - (re_q*re_q + im_q*im_q);
 
-	if (mag <= 0.02) return 0.0;
+	if (mag <= 0.03) return 0.0;
 
 	double re_z = re_q / mag;
 	double im_z = im_q / mag;
+#endif
+
+	double re_z = re_q;
+	double im_z = im_q;
 
 	// printf ("duude compute %g  %g \n", re_z, im_z);
 	psi_one (re_a, im_a, re_z, im_z, prec, param);
