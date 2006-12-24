76b2a7dc92e30633e53d4a2a6fc7bed0579cb5b7
linas <>
2006-12-24 05:26:00 +0000

    start ploting the hurwitz zeta

--- misc/bignum/exact.c
+++ misc/bignum/exact.c
@@ -15,7 +15,7 @@
 int
 main ()
 {
-	int prec = 30;
+	int prec = 40;
 	double q;
 
 	/* Set the precision (number of binary bits) */
@@ -30,15 +30,27 @@ main ()
 	mpf_init (que);
 			  
 	cpx_set_d (ess, 0.5, 14.1);
-	char * zero = "14.134725141734693790457251983562470270784257115699243175685567460149 \
-	               9634298092567649490103931715610127792029715487974367661426914698822545 \
-	               8250536323944713778041338123720597054962195586586020055556672583601077";
+	char * zero;
+	zero = "14.134725141734693790457251983562470270784257115699243175685567460149 \
+	        9634298092567649490103931715610127792029715487974367661426914698822545 \
+	        8250536323944713778041338123720597054962195586586020055556672583601077";
+
+	zero = "21.022039638771554992628479593896902777334340524902781754629520403587 \
+	        5985860688907997136585141801514195337254736424758913838650686037313212 \
+	        6211882162437574166925654471184407119403130672564622779261488733743555";
+					
 	mpf_set_str (ess[0].im, zero, 10);
 
-	for (q=0.1; q<1.0; q++)
+	// printf ("#\n# graph of Hurwitz zeta as function of q, at the Riemann zero\n#\n");
+	printf ("#\n# graph of periodic zeta as function of q, at the Riemann zero\n#\n");
+	fp_prt ("# at s=0.5+i ", ess[0].im);
+	printf ("\n#\n# prec=%d nbits=%d\n#\n", prec, nbits);
+	fflush (stdout);
+	for (q=0.01; q<1.0; q+=0.01)
 	{
 		mpf_set_d (que, q);
-		cpx_hurwitz_zeta (zeta, ess, que, prec);
+		// cpx_hurwitz_zeta (zeta, ess, que, prec);
+		cpx_periodic_zeta (zeta, ess, que, prec);
 
 		printf ("%g",q);
 		fp_prt ("\t", zeta[0].re);
