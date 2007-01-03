04c202d9dd232ef925c5d00a1f943f85bb2b1e6a
linas <>
2007-01-03 22:11:30 +0000

    ad paramters

--- generate/polylog.C
+++ generate/polylog.C
@@ -20,19 +20,22 @@
 static cpx_t zeta, ess, zee;
 static int prec;
 
-static void psi_init (void)
+static void psi_init (int cmd_prec, double ims)
 {
 	/* the decimal precison (number of decimal places) */
-	// prec = 90;
+	prec = 90;
 	// prec = 60;
-	prec = 45;
-	// prec = 25;
+	// prec = 55;
+	// prec = 45;
+	// prec = 35;
+	prec = 25;
+	// prec = 15;
 
 	/* compute number of binary bits this corresponds to. */
 	double v = ((double) prec) *log(10.0) / log(2.0);
-
+	
 	/* the variable-precision calculations are touchy about this */
-	int bits = (int) (v + 30);
+	int bits = (int) (v + 460);
 	
 	/* Set the precision (number of binary bits) */
 	mpf_set_default_prec (bits);
@@ -41,17 +44,19 @@ static void psi_init (void)
 	cpx_init (ess);
 	cpx_init (zee);
 
-	cpx_set_d (ess, 0.5, 14.134725141734);
-	cpx_set_d (ess, 0.5, 21.022039638771);
-	cpx_set_d (ess, 0.5, 37.5861781588256712);
+	// cpx_set_d (ess, 0.5, 14.134725141734);
+	// cpx_set_d (ess, 0.5, 21.022039638771);
+	// cpx_set_d (ess, 0.5, 37.5861781588256712);
+	// cpx_set_d (ess, 0.5, 240.0);
+	cpx_set_d (ess, 0.5, ims);
 }
 	
 static double plogger (double re_q, double im_q, int itermax, double param)
 {
 	static int init = 0;
-	if (!init) {psi_init(); init=1; }
+	if (!init) {psi_init(itermax, param); init=1; }
 		  
-	// printf ("duude compute %g  %g \n", re_z, im_z);
+	// printf ("duude compute %g  %g \n", re_q, im_q);
 
 	cpx_set_d (zee, re_q, im_q);
 	cpx_polylog (zeta, ess, zee, prec);
@@ -59,8 +64,8 @@ static double plogger (double re_q, double im_q, int itermax, double param)
 	double frea = mpf_get_d (zeta[0].re);
 	double fima = mpf_get_d (zeta[0].im);
 
-	double fmag = sqrt(frea*frea + fima*fima);
-	return fmag;
+	// double fmag = sqrt(frea*frea + fima*fima);
+	// return fmag;
 
 	double phase = atan2 (fima, frea);
 	phase += M_PI;
