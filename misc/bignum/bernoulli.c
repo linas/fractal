9392ebe98034e85f699947650ecd6a9e7f5dfe98
linas <>
2007-02-17 20:20:56 +0000

    cleanup

--- misc/bignum/bernoulli.c
+++ misc/bignum/bernoulli.c
@@ -24,6 +24,7 @@ main (int argc, char * argv[])
 {
 	int prec = 40;
 	prec = 20;
+	int i;
 	double q;
 
 	/* Set the precision (number of binary bits) */
@@ -38,51 +39,76 @@ main (int argc, char * argv[])
 	double sre = atof (argv[1]);
 	double sim = atof (argv[2]);
 	
-	cpx_t ess, zeta, zee, plog, gm;
+	cpx_t ess, zeta, zee, plog;
 	cpx_init (ess);
 	cpx_init (zeta);
 	cpx_init (zee);
 	cpx_init (plog);
-	cpx_init (gm);
 
-	mpf_t que, tp;
+	mpf_t que, l2;
 	mpf_init (que);
-	mpf_init (tp);
+	mpf_init (l2);
+	fp_log2 (l2, prec);
 			  
-	cpx_set_d (ess, sre, sim);
-
-	cpx_add_ui (ess, ess, 1, 0);
-	cpx_gamma (gm, ess, prec);
-	cpx_times_ui (gm, gm, 2);
-	cpx_recip (gm, gm);
-	cpx_sub_ui (ess, ess, 1, 0);
-
 	mpf_t twopi;
 	mpf_init (twopi);
 	fp_two_pi (twopi, prec);
+	mpf_div (twopi, twopi, l2);
+
+	/* compute 1/2 Gamma (s+1+2\pi n) */
+	cpx_t gm[10];
+	mpf_t agm[10];
+	cpx_set_d (ess, sre+1.0, sim);
+	for (i=0; i<8; i++)
+	{
+		cpx_init (gm[i]);
+		mpf_init (agm[i]);
+
+		cpx_gamma (gm[i], ess, prec);
+		cpx_recip (gm[i], gm[i]);
+		cpx_abs (agm[i], gm[i]);
+
+		mpf_add (ess[0].im, ess[0].im, twopi);		
+	}
+
 
 	printf ("#\n# graph of periodic zeta as function of q, at \n#\n");
 	printf ("# at s=%g +i %g\n", sre, sim);
 	printf ("#\n# prec=%d nbits=%d\n#\n", prec, nbits);
 	fflush (stdout);
-	for (q=0.001; q<0.999; q+=0.008)
+	for (q=0.001; q<0.999; q+=0.003)
 	{
 		printf ("%g\t", q);
-		mpf_set_d (que, q);
 
 		// cpx_hurwitz_zeta (zeta, ess, que, prec);
 		// cpx_periodic_zeta (zeta, ess, que, prec);
 
-		cpx_periodic_beta (zeta, ess, que, prec);
+		cpx_set_d (ess, sre, sim);
+		for (i=0; i<6;i++)
+		{
+			mpf_set_d (que, q);
+			cpx_periodic_beta (zeta, ess, que, prec);
 
-		mpf_set_d (que, 1.0-q);
-		cpx_periodic_beta (zee, ess, que, prec);
-		cpx_add (zeta, zeta, zee);
+			// mpf_set_d (que, 1.0-q);
+			// cpx_periodic_beta (zee, ess, que, prec);
+			// cpx_add (zeta, zeta, zee);
 
-		double zetare = mpf_get_d(zeta[0].re);
-		// double zetaim = mpf_get_d(zeta[0].im);
-		// printf ("%g\t%g\t", zetare, zetaim);
-		printf ("%g\t", zetare);
+			// cpx_times_mpf (zeta, zeta, agm[i]);
+			cpx_mul (zeta, zeta, gm[i]);
+			cpx_div_ui (zeta, zeta, 2);
+
+			double zetare = mpf_get_d(zeta[0].re);
+			double zetaim = mpf_get_d(zeta[0].im);
+			double mag = sqrt (zetare*zetare + zetaim*zetaim);
+			double arg = atan2 (zetaim, zetare);
+			// printf ("%g\t%g\t", zetare, zetaim);
+			// printf ("%g\t", zetare);
+			// printf ("%g\t", zetaim);
+			// printf ("%g\t", mag);
+			printf ("%g\t", arg);
+
+			mpf_add (ess[0].im, ess[0].im, twopi);		
+		}
 
 		printf ("\n");
 		fflush (stdout);
