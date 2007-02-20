5773149df5155bc50e26c66772ff54e898d04d14
linas <>
2007-02-20 02:01:27 +0000

    updates

--- misc/bignum/hurwitz-euler.c
+++ misc/bignum/hurwitz-euler.c
@@ -114,12 +114,78 @@ void do_perf (int prec)
 	}
 }
 
+void zeta_euler(cpx_t zeta, cpx_t ess, mpf_t q, int prec, int em, int pee);
+
+/* Compute to high precision, monitor the precision */
+void hiprec(cpx_t zeta, int prec)
+{
+	/* Set the precision (number of binary bits) */
+	int nbits = 3.322*prec+2*3.14*3.32*prec+140;
+	// nbits = 3.322*prec+ 7*prec + 140;
+	nbits = 3.322*prec+ 140;
+	mpf_set_default_prec (nbits);
+	cpx_set_prec (zeta, nbits);
+
+	cpx_t cq, ess, prevzeta;
+	cpx_init (ess);
+	cpx_init (cq);
+	cpx_init (prevzeta);
+	cpx_set (prevzeta, zeta);
+
+	cpx_set_d (ess, 0.5, 14.134725);
+	
+	mpf_t que;
+	mpf_init (que);
+	mpf_set_d (que, 0.2);
+	
+	mpf_set (cq[0].re, que);
+	mpf_set_ui (cq[0].im, 0);
+	
+	// cpx_hurwitz_zeta (zeta, ess, que, prec);
+	cpx_hurwitz_taylor (zeta, ess, cq, prec);
+	cpx_sub (ess, zeta, prevzeta);
+
+	printf ("prec=%d ", prec);
+
+	long rex, imx;
+	mpf_get_d_2exp (&rex, ess[0].re);
+	mpf_get_d_2exp (&imx, ess[0].im);
+	rex = -0.30103 *rex;
+	imx = -0.30103 *imx;
+	if (imx < rex) rex = imx;
+	printf ("prev=%ld ", rex);
+
+	double vre = mpf_get_d (ess[0].re);
+	printf ("vre = %g ", vre);
+
+#if 0
+	double vim = mpf_get_d (ess[0].im);
+	printf ("vim = %g ", vim);
+#endif
+
+#if 0
+	// gmp_printf ("re= %.220Ff ", zeta[0].re);
+	printf ("re= ");
+	mpf_out_str (stdout, 10, prec, zeta[0].re);
+	
+	printf (" im= ");
+	mpf_out_str (stdout, 10, 120, zeta[0].im);
+#endif
+	printf ("\n");
+	fflush (stdout);
+
+	cpx_clear (ess);
+	cpx_clear (cq);
+	cpx_clear (prevzeta);
+}
+
 int main ()
 {
 	int prec = 40;
 
 	/* Set the precision (number of binary bits) */
 	mpf_set_default_prec (3.3*prec+140);
+	mpf_set_default_prec (3000);
 
 	cpx_t ess, zeta, z2, z3;
 	cpx_init (ess);
@@ -129,16 +195,25 @@ int main ()
 
 	cpx_set_d (ess, 0.5, 4.0);
 	cpx_set_d (ess, 2.0, 0.1);
+	cpx_set_d (ess, 0.5, 14.134725);
 	
 	mpf_t que;
 	mpf_init (que);
-	mpf_set_d (que, 0.555);
+	mpf_set_d (que, 0.2);
 	
 	cpx_t cq;
 	cpx_init (cq);
 	mpf_set (cq[0].re, que);
 	mpf_set_ui (cq[0].im, 0);
 	
+	int i;
+	for (i=20; i < 10000; i*=1.5)
+	// for (i=100; i > 10; i/=1.3)
+	{
+		hiprec (zeta, i);
+	}
+	
+#if 0
 	cpx_hurwitz_zeta (z2, ess, que, prec);
 	fp_prt ("poly   ", z2[0].re);
 	printf ("\n");
@@ -150,6 +225,7 @@ int main ()
 	cpx_hurwitz_euler (zeta, ess, que, prec);
 	fp_prt ("its    ", zeta[0].re);
 	printf ("\n");
+#endif
 
 #if 0
 	int pee, em;
@@ -162,7 +238,7 @@ int main ()
 		printf ("\n");
 	}
 #endif
-	do_perf (prec);
+	// do_perf (prec);
 	return 0;
 }
 
