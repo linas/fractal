7968fe8dd9d87af785b2d9629991adb0e2f25085
linas <>
2007-03-01 02:56:26 +0000

    more stuff

--- misc/presentation/present.c
+++ misc/presentation/present.c
@@ -9,6 +9,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct _matrix {
 	int dim;
@@ -20,6 +21,7 @@ Matrix * matrix_new (int dim)
 	Matrix *mat = (Matrix *)malloc (sizeof (Matrix));
 	mat->dim = dim;
 	mat->elts = (int *)malloc (dim*dim*sizeof(int));
+	return mat;
 }
 
 /* element acccessor */
@@ -67,13 +69,15 @@ typedef struct _mlist MatList;
 struct _mlist {
 	MatList *next;
 	Matrix *mat;
+	char *str;
 };
 
-MatList *matlist_prepend (MatList *ml, Matrix *mat)
+MatList *matlist_prepend (MatList *ml, Matrix *mat, char *str)
 {
 	MatList *node = (MatList *)malloc(sizeof (MatList));
 	node->mat = mat;
 	node->next = ml;
+	node->str = strdup(str);
 
 	return node;
 }
@@ -86,8 +90,72 @@ MatList *matlist_find (MatList *ptr, Matrix *mat)
 		if (matrix_equal (ptr->mat, mat)) return ptr;
 		ptr = ptr->next;
 	}
+	return NULL;
+}
+
+/* ---------------------------------------------------- */
+/* master struct */
+
+typedef struct _present {
+	MatList *generators;
+	MatList *words;
+} Present;
+
+/* ---------------------------------------------------- */
+
+Present * setup_b3 (void)
+{
+
+	Present *pr = (Present *) malloc (sizeof (Present));
+	pr->generators = NULL;
+	pr->words = NULL;
+
+
+	/* identity matrix */
+	Matrix *e = matrix_new (2);
+	MELT(e, 0,0) = 1;
+	MELT(e, 0,1) = 0;
+	MELT(e, 1,0) = 0;
+	MELT(e, 1,1) = 1;
+	pr->words = matlist_prepend (NULL, e, "E");
+	
+	MatList *ml = NULL;
+
+	/* Set up sigma 1 and sigma 2 of braid group B_3 */
+	Matrix *s1 = matrix_new (2);
+	MELT(s1, 0,0) = 1;
+	MELT(s1, 0,1) = 1;
+	MELT(s1, 1,0) = 0;
+	MELT(s1, 1,1) = 1;
+	ml = matlist_prepend (ml, s1, "A");
+	
+	Matrix *s2 = matrix_new (2);
+	MELT(s2, 0,0) = 1;
+	MELT(s2, 0,1) = 0;
+	MELT(s2, 1,0) = -1;
+	MELT(s2, 1,1) = 1;
+	ml = matlist_prepend (ml, s2, "B");
+	
+	/* and now thier inverses (by hand) */
+	s1 = matrix_new (2);
+	MELT(s1, 0,0) = 1;
+	MELT(s1, 0,1) = -1;
+	MELT(s1, 1,0) = 0;
+	MELT(s1, 1,1) = 1;
+	ml = matlist_prepend (ml, s1, "a");
+	
+	s2 = matrix_new (2);
+	MELT(s2, 0,0) = 1;
+	MELT(s2, 0,1) = 0;
+	MELT(s2, 1,0) = 1;
+	MELT(s2, 1,1) = 1;
+	ml = matlist_prepend (ml, s2, "b");
+
+	pr->generators = ml;
+	return pr;
 }
 
 main ()
 {
+	setup_b3();
 }
