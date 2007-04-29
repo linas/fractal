e90b9d82539491e431924249683a88e52c9b65ae
linas <>
2007-04-29 15:41:57 +0000

    add code to draw arcs

--- misc/hyperbolic/tree.c
+++ misc/hyperbolic/tree.c
@@ -67,13 +67,13 @@ void eps_setup_disk (void)
 }
 void eps_setup_plane (void)
 {
-	printf ("0.0300000 slw\n");
+	printf ("0.0100000 slw\n");
 	printf ("[] 0 sd\n");
 	printf ("[] 0 sd\n");
 	printf ("0 slc\n");
 	printf ("0.000000 0.000000 0.000000 srgb\n");
 	printf ("0.0 -0.8 translate\n");
-	printf ("0.2 0.8 scale\n");
+	printf ("0.4 1.6 scale\n");
 }
 
 void eps_set_color_red (void)
@@ -115,6 +115,11 @@ void eps_draw_circle(void)
 		si = tmp;
 	}
 	printf (" s\n");
+}
+
+void eps_draw_lineseg (double fx, double fy, double tx, double ty)
+{
+	printf ("n %f %f m %f %f l s\n", fx, fy, tx, ty);
 }
 
 /* ==================================================== */
@@ -257,39 +262,68 @@ void show_mobius(mobius_t m)
 
 /* ==================================================== */
 
+void draw_seg(mobius_t m, cplex zf, cplex zt)
+{
+	cplex za = mobius_xform (m, zf);
+	cplex zb = mobius_xform (m, zt);
+	eps_draw_lineseg (za.re, za.im, zb.re, zb.im);
+}
+
+void draw_arc(mobius_t m, cplex zf, cplex zt)
+{
+	int i;
+	int nseg=20;
+
+	cplex zstart = zf;
+	cplex zdelta = cplex_scale((1.0/(double)nseg), cplex_sub (zt,zf));
+	cplex za = mobius_xform (m, zstart);
+
+	for (i=0; i<nseg; i++)
+	{
+		cplex zend = cplex_add (zstart, zdelta);
+		cplex zb = mobius_xform (m, zend);
+		eps_draw_lineseg (za.re, za.im, zb.re, zb.im);
+		zstart = zend;
+		za = zb;
+	}
+}
+
 /* draw three-pointed stick figure */
 void draw_tristar (mobius_t m)
 {
 	cplex za,zb;
 
 	za = cplex_set (0.0, 0.0);
-	za = mobius_xform (m,za);
 
 	// printf ("n 0.0 0.0 m 0.25 0.433012702 l s\n");
-	zb = cplex_set (0.25, 0.433012702);
-	zb = mobius_xform (m,zb);
-	printf ("n %f %f m %f %f l s\n", za.re, za.im,zb.re, zb.im);
+	zb = cplex_set (0.25, 0.25*sqrt(3.0));
+
+	// za = mobius_xform (m,za);
+	// zb = mobius_xform (m,zb);
+	// eps_draw_lineseg (za.re, za.im,zb.re, zb.im);
+	draw_arc (m, za, zb);
 
 	// printf ("n 0.0 0.0 m 0.25 -0.433012702 l s\n");
-	zb = cplex_set (0.25, -0.433012702);
-	zb = mobius_xform (m,zb);
-	printf ("n %f %f m %f %f l s\n", za.re, za.im,zb.re, zb.im);
+	zb = cplex_set (0.25, -0.25*sqrt(3.0));
+	// zb = mobius_xform (m,zb);
+	// eps_draw_lineseg (za.re, za.im,zb.re, zb.im);
+	draw_arc (m, za, zb);
 
 	// printf ("n 0.0 0.0 m -0.5 0.0 l s\n");
 	zb = cplex_set (-0.5, 0.0);
-	zb = mobius_xform (m,zb);
-	printf ("n %f %f m %f %f l s\n", za.re, za.im,zb.re, zb.im);
+	// zb = mobius_xform (m,zb);
+	// eps_draw_lineseg (za.re, za.im,zb.re, zb.im);
+	draw_arc (m, za, zb);
 
 	// draw a splat 
-	// printf ("0.0600000 slw\n");
-	printf ("0.1600000 slw\n");
+	printf ("0.0600000 slw\n");
 	za = cplex_set (-0.23, 0.0);
 	za = mobius_xform (m,za);
 	zb = cplex_set (-0.27, 0.0);
 	zb = mobius_xform (m,zb);
 	printf ("n %f %f m %f %f l s\n", za.re, za.im,zb.re, zb.im);
 	// printf ("0.0100000 slw\n");
-	printf ("0.0300000 slw\n");
+	printf ("0.010000 slw\n");
 }
 
 mobius_t go_to_fork_tip(double sign)
@@ -309,15 +343,19 @@ void draw_fork(mobius_t m, int level)
 	cplex za, zb;
 
 	za = cplex_set (0.0, 0.0);
-	za = mobius_xform (m,za);
 
-	zb = cplex_set (0.25, 0.433012702);
-	zb = mobius_xform (m,zb);
-	printf ("n %f %f m %f %f l s\n", za.re, za.im,zb.re, zb.im);
+	zb = cplex_set (0.25, 0.25*sqrt(3.0));
+	// za = mobius_xform (m,za);
+	// zb = mobius_xform (m,zb);
+	// printf ("n %f %f m %f %f l s\n", za.re, za.im,zb.re, zb.im);
+	// eps_draw_lineseg (za.re, za.im,zb.re, zb.im);
+	draw_arc (m, za, zb);
 
-	zb = cplex_set (0.25, -0.433012702);
-	zb = mobius_xform (m,zb);
-	printf ("n %f %f m %f %f l s\n", za.re, za.im,zb.re, zb.im);
+	zb = cplex_set (0.25, -0.25*sqrt(3.0));
+	// zb = mobius_xform (m,zb);
+	// printf ("n %f %f m %f %f l s\n", za.re, za.im,zb.re, zb.im);
+	// eps_draw_lineseg (za.re, za.im,zb.re, zb.im);
+	draw_arc (m, za, zb);
 
 	mobius_t tip = go_to_fork_tip(+1.0);
 	tip = mobius_mul (m, tip);
@@ -332,13 +370,13 @@ void draw_fork(mobius_t m, int level)
 void draw(int n)
 {
 	mobius_t m;
-	int level=11;
+	int level=8;
 
-	// cplex c = cplex_set (0.0, -0.25);
-	cplex z = cplex_set (-0.25, 0.0);
+	// cplex z = cplex_set (0.0, -0.25);
+	cplex z = cplex_set (-0.266, 0.0);
 	mobius_t off = disk_center (z);
 
-	mobius_t rot = mobius_rotate (0.5*M_PI);
+	mobius_t rot = mobius_rotate (-0.5*M_PI);
 	off = mobius_mul (rot, off);
 
 #if XLATE
@@ -358,12 +396,12 @@ void draw(int n)
 
 	draw_tristar(off);
 
-eps_set_color_red();
+eps_set_color_green();
 	m = go_to_fork_tip(+1.0);
 	m = mobius_mul(off,m);
 	draw_fork (m, level);
 
-eps_set_color_green();
+eps_set_color_red();
 	m = go_to_fork_tip(-1.0);
 	m = mobius_mul(off,m);
 	draw_fork (m, level);
@@ -385,7 +423,7 @@ main (int argc, char * argv[])
 	eps_print_prolog(400,100);
 	// eps_setup_disk();
 	eps_setup_plane();
-	eps_draw_circle();
+	// eps_draw_circle();
 
 	draw(n);
 
