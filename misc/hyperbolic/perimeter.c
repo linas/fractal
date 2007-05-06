80ee8c51dc099829578cb58871ec4a2d38b6895a
linas <>
2007-05-06 15:48:03 +0000

    initial checkin

---
+++ misc/hyperbolic/perimeter.c
@@ -0,0 +1,17 @@
+/* 
+ * perimeter.c
+ *
+ * Map tree to perimeter of the circle.  Used to sanity check
+ * various results.
+ *
+ * Linas Vepstas May 2007
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main()
+{
+}
