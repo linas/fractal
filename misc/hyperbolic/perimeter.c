46a97d2dd226cee7779f73e2fd50858aafb99b5d
linas <>
2007-05-07 13:49:32 +0000

    misc hacking and cleanup

--- misc/hyperbolic/perimeter.c
+++ misc/hyperbolic/perimeter.c
@@ -14,30 +14,39 @@
 #include "flt.h"
 #include "question.h"
 
+double delta(double x)
+{
+	if (0.0>x) x = 0.0;
+	if (0.25>x) return -1.0/question_inverse (4.0*x);
+	if (0.5>x) return -question_inverse (2.0-4.0*x);
+	if (0.75>x) return question_inverse (4.0*x-2.0);
+	return 1.0/question_inverse (4.0-4.0*x);
+}
+
 main()
 {
 	int i;
 
-	int deno=32;
+	int deno=443;
 	for (i=0; i<deno;i++)
 	{
 		double x = ((double) i)/((double) deno);
 
-		double t = dyadic_to_stern_brocot(x);
+		// double t = dyadic_to_stern_brocot(x);
+		double t = delta(x);
 
-		double u = atan2 (-2.0*t, t*t-1.0);
-		// double u = atan2 (-4.0*t, t*t-4.0);
+		double theta = atan2 (-2.0*t, t*t-1.0);
+		double phi = atan2 (-4.0*t, t*t-4.0);
 		// double u = atan2 (-t, t*t-0.25);
 
-		// if (0.0> u) u += 2.0*M_PI;
-		// u /= 2.0*M_PI;
-		u /= M_PI;
-		u += 1.0;
+		if (0.0> theta) theta += 2.0*M_PI;
+		theta /= 2.0*M_PI;
+		
+		if (0.0> phi) phi += 2.0*M_PI;
+		phi /= 2.0*M_PI;
 		
 		// double q = question_mark (i,deno);
-		// double q = question_inverse (2.0*x);
-		// q = 0.5 + 0.5*q;
-		// x = 0.5 + 0.5*x;
+		double q = question_inverse (x);
 
 #if 0
 		double h = t;
@@ -50,23 +59,6 @@ main()
 		}
 #endif
 	
-		double q=0;
-#if 0
-		if (x<0.5)
-		{
-			q = question_inverse (2.0*x);
-		}
-		else
-		{
-			q = 1.0/question_inverse (2.0*(1.0-x));
-		}
-		q = question_inverse (x);
-#endif
-		t = question_inverse (x);
-
-		t = question_mark (t*1000000,1000000);
-		t -= x;
-
-		printf ("%d	%f	%g	%f	%f\n", i, x, t, u, q);
+		printf ("%d	%f	%f	%f	%f	%f\n", i, x, t, theta, phi, q);
 	}
 }
