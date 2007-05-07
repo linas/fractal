e2613bfb749ef61159ba9e965d51f18fd350a49d
linas <>
2007-05-07 02:11:32 +0000

    the whiz-bang qquestion-inverse function moved off to the utility lib

--- misc/hyperbolic/perimeter.c
+++ misc/hyperbolic/perimeter.c
@@ -14,56 +14,6 @@
 #include "flt.h"
 #include "question.h"
 
-/**
- * dyadic_to_stern_brocot -- convert real number to stern-brocot
- *
- * This function maps the unit interval, expressed as a dyadic
- * tree, to the full stern brocot tree.  The input is 0<x<1.
- * Perform binary expansion of x. Use L,R for binary digits 0,1. 
- * Concatenate the L and R to form a matrix. Padd on the right with
- * zeros. The resulting fractinal linear transform will map the 
- * entire complex plane to b/d. This function returns b/d.
- *
- * Basically, this function maps the dyadic tree to the 
- * Stern-Brocot tree.
- */
-double dyadic_to_stern_brocot (double x)
-{
-	if (0.0>x) x -= (int) x - 1;
-	if (1.0<x) x -= (int) x;
-
-	mobius_t ell = mobius_set (1,0,1,1);
-	mobius_t are = mobius_set (1,1,0,1);
-
-	mobius_t acc = mobius_set (1,0,0,1);
-	int i;
-	for (i=0; i<45; i++)
-	{
-		if (0.5 <= x)
-		{
-			acc = mobius_mul (acc, are);
-			x -= 0.5;
-		}
-		else
-		{
-			acc = mobius_mul (acc, ell);
-		}
-		x *= 2.0;
-	}
-	return acc.b.re / acc.d.re;
-}
-
-/**
- * qinverse - return the inverse of the question mark function
- *
- * This impments a rapid algorithm to compute the inverse of 
- * the question mark function.
- */
-double qinverse (double x)
-{
-	return dyadic_to_stern_brocot (0.5*x);
-}
-
 main()
 {
 	int i;
@@ -112,7 +62,7 @@ main()
 		}
 		q = question_inverse (x);
 #endif
-		t = qinverse (x);
+		t = question_inverse (x);
 
 		t = question_mark (t*1000000,1000000);
 		t -= x;
