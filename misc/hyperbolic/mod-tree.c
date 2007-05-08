b81c7de0f7342c79dc338d47a551bcf65c770996
linas <>
2007-05-08 03:03:06 +0000

    misc hacks

--- misc/hyperbolic/mod-tree.c
+++ misc/hyperbolic/mod-tree.c
@@ -1,6 +1,6 @@
 /*
  * tree.c 
- * draw a binary tree on the hyperbolic plane
+ * draw a binary tree on the hyperbolic upper half-plane
  * cosntructed from modular xforms
  *
  * Linas Vepstas April 2007
@@ -14,6 +14,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void eps_setup_disk (void)
+{
+	eps_setup_basic_linstyles();
+}
 void eps_setup_plane (void)
 {
 	eps_setup_basic_linstyles();
@@ -21,39 +25,76 @@ void eps_setup_plane (void)
 	printf ("0.4 1.6 scale\n");
 }
 
-void recursive_draw (int depth, mobius_t m)
+/* ========================================================= */
+
+void recursive_draw (int depth, int lr, mobius_t m)
 {
 	if (0 >= depth)
 		return;
 	depth --;
 
-	// cplex tip = cplex_set(0.5,0.5*sqrt(3.0));
-	cplex tip = cplex_set(-0.5,0.5*sqrt(3.0));
+	cplex tip;
+	if (lr) tip = cplex_set(0.5,0.5*sqrt(3.0));
+	else tip = cplex_set(-0.5,0.5*sqrt(3.0));
 
-eps_set_color_red();
-	// mobius_t are = mobius_set (1,1,0,1);
-	mobius_t are = mobius_set (1,-1,0,1);
+// eps_set_color_red();
+	mobius_t are;
+	if (lr) are = mobius_set (1,1,0,1);
+	else are = mobius_set (1,-1,0,1);
+
 	cplex zb = mobius_xform (are, tip);
 	draw_seg (m, tip, zb);
 
 	mobius_t mr = mobius_mul (m,are);
-	recursive_draw (depth, mr);
+	recursive_draw (depth, lr, mr);
 
-eps_set_color_green();
-	// mobius_t ell = mobius_set (1,0,1,1);
-	mobius_t ell = mobius_set (1,0,-1,1);
+// eps_set_color_green();
+	mobius_t ell;
+	if (lr) ell = mobius_set (1,0,1,1);
+	else ell = mobius_set (1,0,-1,1);
+
 	zb = mobius_xform (ell, tip);
 	draw_seg (m, tip, zb);
 
 	mobius_t ml = mobius_mul (m,ell);
-	recursive_draw (depth, ml);
+	recursive_draw (depth, lr, ml);
 }
 
 void draw (int n)
 {
 	mobius_t ident = mobius_ident();
+	mobius_t tee = mobius_set (1,1,0,1);
+	mobius_t ess = mobius_set (0,1,-1,0);
 
-	recursive_draw (n,ident);
+	mobius_t xfm = ident;
+	// xfm = to_disk (xfm);
+	// xfm = disk_center (cplex_set (0,1));
+	xfm.a = cplex_set (1,0);
+	xfm.b = cplex_set (0,-1);
+	xfm.c = cplex_set (1,0);
+	xfm.d = cplex_set (0,1);
+
+	xfm = mobius_mul (xfm, ess);
+	xfm = mobius_mul (xfm, tee);
+	xfm = mobius_mul (xfm, ess);
+#if 0
+	xfm = mobius_mul (tee,xfm);
+	xfm = mobius_mul (ess,xfm);
+	xfm = mobius_mul (tee,xfm);
+	xfm = mobius_mul (tee,xfm);
+	xfm = mobius_mul (ess,xfm);
+#endif 
+
+eps_set_color_green();
+	recursive_draw (n,1, xfm);
+eps_set_color_red();
+	recursive_draw (n,0, xfm);
+
+	cplex ltip = cplex_set(0.5,0.5*sqrt(3.0));
+	cplex rtip = cplex_set(-0.5,0.5*sqrt(3.0));
+
+eps_set_color_blue();
+	draw_seg (xfm, ltip, rtip);
 }
 
 /* ==================================================== */
@@ -69,8 +110,11 @@ main (int argc, char * argv[])
 
 	int n = atoi (argv[1]);
 
-	eps_print_prolog(800,200);
-	eps_setup_plane();
+	eps_print_prolog(220,220);
+	// eps_print_prolog(800,200);
+	eps_setup_disk();
+	// eps_setup_plane();
+	eps_draw_circle();
 
 	draw(n);
 
