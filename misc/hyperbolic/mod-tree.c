1a4fbf55d83fbef89f6062e74633e97abf875aad
linas <>
2007-05-10 14:34:53 +0000

    try to draw domain lines

--- misc/hyperbolic/mod-tree.c
+++ misc/hyperbolic/mod-tree.c
@@ -29,7 +29,12 @@ void eps_setup_plane (void)
 
 /* ========================================================= */
 
-void recursive_draw (int depth, int lr, mobius_t m)
+/**
+ * @depth: recursion depth
+ * @lr: 0 or 1, indicating that the left or rightt tree is being drawn
+ * @draw_fund: 0 or 1, do draw the fundamental domain lines, or not
+ */
+void recursive_draw_binary_tree (int depth, int lr, int draw_fund, mobius_t m)
 {
 	if (0 >= depth)
 		return;
@@ -47,8 +52,20 @@ void recursive_draw (int depth, int lr, mobius_t m)
 	cplex zb = mobius_xform (are, tip);
 	draw_seg (m, tip, zb);
 
+	if (draw_fund)
+	{
+		eps_set_color(240,130,0);
+		printf ("[0.02 0.01 0.005 0.01] 1 setdash\n");
+
+		cplex top = cplex_set(-1,0);
+		draw_seg (m, top, tip);
+
+		eps_set_color(0,70,220);
+		printf ("[] 0 setdash\n");
+	}
+
 	mobius_t mr = mobius_mul (m,are);
-	recursive_draw (depth, lr, mr);
+	recursive_draw_binary_tree (depth, lr, draw_fund, mr);
 
 // eps_set_color_green();
 	mobius_t ell;
@@ -59,8 +76,10 @@ void recursive_draw (int depth, int lr, mobius_t m)
 	draw_seg (m, tip, zb);
 
 	mobius_t ml = mobius_mul (m,ell);
-	recursive_draw (depth, lr, ml);
+	recursive_draw_binary_tree (depth, lr, draw_fund, ml);
 }
+
+/* ========================================================= */
 
 void draw (int n)
 {
@@ -76,16 +95,23 @@ void draw (int n)
 
 
 	mobius_t xfm = ident;
+
+#if DISK_COORDS
+	/* The following are inappropriate for disk coords */
 	// xfm = to_disk (xfm);
 	// xfm = disk_center (cplex_set (0,1));
+
+	/* The following sets up a transform to disk coords */
 	xfm.a = cplex_set (1,0);
 	xfm.b = cplex_set (0,-1);
 	xfm.c = cplex_set (1,0);
 	xfm.d = cplex_set (0,1);
+#endif
 
+#if 0
+	/* Assorted translations and rotations */
 	xfm = mobius_mul (xfm, ell);
 	xfm = mobius_mul (xfm, are);
-#if 0
 	xfm = mobius_mul (xfm, are);
 	xfm = mobius_mul (xfm, pee);
 	xfm = mobius_mul (xfm, pee);
@@ -99,15 +125,16 @@ void draw (int n)
 	xfm = mobius_mul (ess,xfm);
 #endif 
 
-eps_set_color_green();
-	recursive_draw (n,1, xfm);
-eps_set_color_red();
-	recursive_draw (n,0, xfm);
+eps_set_color_blue();
+// eps_set_color_green();
+	recursive_draw_binary_tree (n,1, 1, xfm);
+// eps_set_color_red();
+	recursive_draw_binary_tree (n,0, 1, xfm);
 
 	cplex ltip = cplex_set(0.5,0.5*sqrt(3.0));
 	cplex rtip = cplex_set(-0.5,0.5*sqrt(3.0));
 
-eps_set_color_blue();
+// eps_set_color_blue();
 	draw_seg (xfm, ltip, rtip);
 }
 
@@ -124,11 +151,11 @@ main (int argc, char * argv[])
 
 	int n = atoi (argv[1]);
 
-	eps_print_prolog(220,220);
-	// eps_print_prolog(800,200);
-	eps_setup_disk();
-	// eps_setup_plane();
-	eps_draw_circle();
+	// eps_print_prolog(220,220);
+	eps_print_prolog(800,200);
+	// eps_setup_disk();
+	eps_setup_plane();
+	// eps_draw_circle();
 
 	draw(n);
 
