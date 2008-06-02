1aad7ae9f0e51ee69ac5fff3d155c487d12b5f1f
linas <>
2008-06-02 15:47:52 +0000

    add quadratic extraploation, which will be a lot better than aitken

--- misc/bignum/entropy.c
+++ misc/bignum/entropy.c
@@ -53,8 +53,23 @@ main (int argc, char * argv[])
 	mpf_init (dprev);
 	mpf_init (tmp);
 
+	// quadratic extrapolation
+	mpf_t tee, ent, x_0, x_1, x_2, y_0, y_1, y_2, d01, d02, d12;
+	mpf_init(tee);
+	mpf_init(x_0);
+	mpf_init(x_1);
+	mpf_init(x_2);
+	mpf_init(y_0);
+	mpf_init(y_1);
+	mpf_init(y_2);
+	mpf_init(d01);
+	mpf_init(d02);
+	mpf_init(d12);
+	mpf_set_ui(x_1, 1);
+	mpf_set_ui(x_2, 2);
 
-	unsigned short pcnt = 1;
+
+	unsigned long int pcnt = 1;
 	
 	mpf_set_ui(k, 2);
 	while(1)
@@ -72,7 +87,7 @@ main (int argc, char * argv[])
 		mpf_mul(term, p_k, term);
 		mpf_add (acc, acc, term);
 
-		if (pcnt == 0)
+		if (pcnt%640123 == 0)
 		{
 			fp_prt("k = ", k);
 			printf ("\n");
@@ -80,14 +95,14 @@ main (int argc, char * argv[])
 			// total probability is tending to 1.0,
 			// so print difference from 1.0
 			mpf_div(term, prob, lg2);
-			mpf_ui_sub (term, 1, term);
-			fp_prt("prob = ", term);
+			mpf_ui_sub (tee, 1, term);
+			fp_prt("prob = ", tee);
 			printf ("\n");
 
 			// print the regular entropy
 			mpf_div(term, acc, lg2);
-			mpf_sub(term, lglg2, term);
-			fp_prt("H = ", term);
+			mpf_sub(ent, lglg2, term);
+			fp_prt("H = ", ent);
 			printf ("\n");
 
 			// Aitken method
@@ -111,6 +126,45 @@ main (int argc, char * argv[])
 			// which differs by a log2.
 			mpf_div(term, term, lg2);
 			fp_prt("H_2 (aitken) = ", term);
+			printf ("\n");
+
+			// quadratic exprapolation
+			mpf_set(x_2, x_1);
+			mpf_set(x_1, x_0);
+			mpf_set(x_0, tee);
+
+			mpf_set(y_2, y_1);
+			mpf_set(y_1, y_0);
+			mpf_set(y_0, ent);
+
+			mpf_sub(d01, x_0, x_1);
+			mpf_sub(d02, x_0, x_2);
+			mpf_sub(d12, x_1, x_2);
+
+			mpf_mul(term, x_1, x_2);
+			mpf_mul(term, term, y_0);
+			mpf_div(term, term, d01);
+			mpf_div(term, term, d02);
+
+			mpf_mul(tmp, x_0, x_2);
+			mpf_mul(tmp, tmp, y_1);
+			mpf_div(tmp, tmp, d01);
+			mpf_div(tmp, tmp, d12);
+			mpf_sub(term, term, tmp);
+
+			mpf_mul(tmp, x_0, x_1);
+			mpf_mul(tmp, tmp, y_2);
+			mpf_div(tmp, tmp, d02);
+			mpf_div(tmp, tmp, d12);
+			mpf_add(term, term, tmp);
+
+			fp_prt("H quad= ", term);
+			printf ("\n");
+
+			// print the information-theoretic entropy
+			// which differs by a log2.
+			mpf_div(term, term, lg2);
+			fp_prt("H_2 (quad) = ", term);
 			printf ("\n");
 
 			printf ("\n");
