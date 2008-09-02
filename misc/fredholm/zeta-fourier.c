6abd623178f47fa95558bc38efc6851fffaf8572
linas <>
2008-09-02 19:34:00 +0000

    initial checkin

--- misc/fredholm/zeta-fourier.c
+++ misc/fredholm/zeta-fourier.c
@@ -7,7 +7,30 @@
  * Linas September 2008
  */
 
+double *zeros;
+
+void coeffs(int nmax)
+{
+	int n;
+
+	double *bn = (double *) malloc (nmax * sizeof(double));
+	for (n=0; n<nmax; n++)
+	{
+		bn[n] = 0.0;
+		for (i=0; i<nzeros; i++)
+		{
+			b[n] += 1.0 / (n*n - zeros[i]*zeros[i]);
+		}
+
+		b[n] *= 2.0*n;
+
+		printf ("%d\t%8.6g\n", n, b[n]);
+	}
+}
+
+
 main()
 {
 
+	coeffs();
 }
