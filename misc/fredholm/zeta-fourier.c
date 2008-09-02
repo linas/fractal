05da49fead631f55d7a0113bd226dc3d4cea2d96
linas <>
2008-09-02 20:26:57 +0000

    misc stuff

--- misc/fredholm/zeta-fourier.c
+++ misc/fredholm/zeta-fourier.c
@@ -9,33 +9,39 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 double *zeros;
 
+/**
+ * read in the zeros of the zeta function from a file.
+ */
 void read_zeros (int nzeros)
 {
 	int i;
 	zeros = (double *) malloc (nzeros * sizeof(double));
-	FILE * fh = fopen ("zeros1");
+	FILE * fh = fopen ("zeros1", "r");
 	for (i=0; i<nzeros; i++)
 	{
 		char buff[100];
 		fgets(buff, 100, fh);
 		zeros[i] = atof(buff);
-printf ("its %g\n", zeros[i]);
 	}
 	fclose (fh);
 }
 
-void coeffs(int nmax)
+/**
+ * Compute sum 1/(rho+n^2)
+ */
+void coeffs(int nzeros, int nmax)
 {
-	int n;
+	int i, n;
 
-	double *bn = (double *) malloc (nmax * sizeof(double));
-	for (n=0; n<nmax; n++)
+	double *b = (double *) malloc (nmax * sizeof(double));
+	for (n = 0; n<nmax; n++)
 	{
-		bn[n] = 0.0;
-		for (i=0; i<nzeros; i++)
+		b[n] = 0.0;
+		for (i = 0; i<nzeros; i++)
 		{
 			b[n] += 1.0 / (n*n - zeros[i]*zeros[i]);
 		}
@@ -47,9 +53,20 @@ void coeffs(int nmax)
 }
 
 
-main()
+int main(int argc, char * argv[])
 {
 
-	read_zeros(30);
-	coeffs();
+	if (argc <3)
+	{
+		fprintf (stderr, "Usage: %s <nzeros> <ncoeffs\n", argv[0]);
+		exit(1);
+	}
+
+	int nzeros = atoi(argv[1]);
+	int ncoeffs = atoi(argv[2]);
+
+	read_zeros(nzeros);
+	coeffs(nzeros, ncoeffs);
+
+	return 0;
 }
