84f556527d5d18626511bdd550dd1abdbceca7ff
linas <>
2008-09-04 12:48:53 +0000

    finish implmentation of the ftag thing.

--- misc/fdist/ftag.c
+++ misc/fdist/ftag.c
@@ -29,7 +29,7 @@ double stupa (double x)
 	return n;
 }
 
-double taga(double x, double weight)
+double taga(double x, double veight)
 {
 	int k;
 
@@ -38,7 +38,49 @@ double taga(double x, double weight)
 	double tw = 1.0;
 	for (k=0; k<20; k++)
 	{
-		tak
+		double f = stupa(tw*x);
+		acc += tw * f;
+		tw *= veight;
+		tk *= 2.0;
 	}
 
+	return acc;
+}
+
+double product(double x, double veight, double weight)
+{
+	double lg = taga(x, veight);
+	lg *= log (weight);
+	lg = exp(lg);
+	return lg;
+}
+
+void prt_graph(int npts, double veight, double weight)
+{
+	int i;
+
+	double gral = 0.0;
+	double delta = 1.0 / ((double) npts);
+	for (i=0; i<npts; i++)
+	{
+		double x = ((double) i) / ((double) npts);
+		double p = product (x, veight, weight);
+		gral += p * delta;
+
+		printf ("%d	%8.6g	%8.6g	%8.6g\n", i, x, p, gral);
+	}
+}
+
+int main (int argc, char * argv[])
+{
+	if (argc < 4)
+	{
+		fprintf (stderr, "Usage: %s <nbins> <veight> <weight>\n", argv[0]);
+		exit (1);
+	}
+	int nbins = atoi (argv[1]);
+	double veight = atof (argv[2]);
+	double weight = atof (argv[3]);
+
+	prt_graph (nbins, veight, weight);
 }
