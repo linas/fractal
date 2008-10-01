d6ae759ff5737847443b50a66139e9b7340326c8
linas <>
2008-10-01 18:58:58 +0000

    compile the new code

--- misc/fdist/takagi-dist.C
+++ misc/fdist/takagi-dist.C
@@ -5,6 +5,8 @@
  * Linas Vepstas October 2008
  */
 
+#include <stdio.h>
+
 double triangle (double x)
 {
 	x -= (int) x;
@@ -14,5 +16,32 @@ double triangle (double x)
 
 double prod (double x)
 {
+	int n;
+	double prod = 1.0;
+	double tk = 1.0;
+	for (n=0; n< 20; n++)
+	{
+		prod *= 0.5 * (1.0 + triangle(tk*x));
+		tk *=- 2.0;
+	}
 }
 
+void graph(int npts)
+{
+	int i;
+
+	double acc = 0.0;
+	for (i=0; i<npts; i++)
+	{
+		double x = (double) i / ((double) npts);
+		double y = prod(x);
+		acc += y;
+
+		printf ("%d	%g	%g %g\n", i, x, y, acc);
+	}
+}
+
+main ()
+{
+	graph (567);
+}
