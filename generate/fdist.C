00bc07ade114a336bbb0d1d10e6e639bba3b6492
linas <>
2008-10-01 20:02:02 +0000

    start hacking it all together

--- generate/fdist.C
+++ generate/fdist.C
@@ -13,6 +13,7 @@
 
 #include "Farey.h"
 #include "FareyTree.h"
+#include "brat.h"
 
 void GetNextDyadic (unsigned int *n, unsigned int *d)
 {
@@ -30,10 +31,14 @@ void GetNextDyadic (unsigned int *n, unsigned int *d)
 	*d = last_d;
 }
 
+int nbins = 0;
+double * bin = NULL;
 
-void bincount(int nbins, int depth)
+void make_bins(int _nbins, int depth)
 {
 	int i;
+
+	nbins = _nbins;
 
 	int max = 1 << depth;
 	printf ("#\n# nbins=%d   tree depth=%d\n#\n",nbins,depth);
@@ -43,16 +48,16 @@ void bincount(int nbins, int depth)
 
 	FareyIterator fi;
 
-	int *bin = (int *) malloc (nbins * sizeof (int));
+	double *bin = (double *) malloc (nbins * sizeof (double));
 	for (i=0; i<nbins; i++)
 	{
-		bin[i] = 0;
+		bin[i] = 0.0;
 	}
-	bin[0] = 1;
-	bin[nbins-1] = 1;
+	bin[0] = 1.0;
+	bin[nbins-1] = 1.0;
 
 	/* Compute the distribution by bining */
-	int cnt =2;
+	int cnt = 2;
 	for (i=0; i<max; i++)
 	{
 		int n,d;
@@ -62,136 +67,50 @@ void bincount(int nbins, int depth)
 		double x = ((double) n)/ ((double) d);
 		x *= nbins;
 		int ib = (int) x;
-		bin [ib] ++;
+		bin [ib] += 1.0;
 		cnt ++;
 	}
 
+	/* now, renormalize */
+	for (i=0; i<nbins; i++)
+	{
+		bin[i] /= ((double) cnt);
+	}
+}
+
+
+static void bincount_c (double re_q, double im_q, double *prep, double *pimp)
+{
+	double tmp;
+
+	double rcz = cos (re_q) * cosh (im_q);
+	double icz = sin (re_q) * sinh (im_q);
+
 	/* Compute the integral of the distribution */
-   ContinuedFraction f;
 	double gral = 0.0;
-	double egral = 0.0;
-	double ejgral = 0.0;
-	double dgral = 0.0;
-	double fprev = 0.0;
 	for (i=0; i<nbins; i++)
 	{
 		/* gral is the ordinary integral of the bin count */
-		double rect = bin[i] / ((double) cnt);
-		gral += rect;
-
-		/* Be careful to bin-count the entropy 
-		 * (use discrete not continuous formula) 
-		 * This is the entropy of just ?'(x) and not of the jacobian */
-		double entropy = - rect * log(rect);
-		if (bin[i] == 0) entropy = 0;
-		egral += entropy;
-
-		double bcnt = rect * nbins;
+		gral += bin[i];
 
 		double x = ((double) i) / ((double) nbins);
 
-   	f.SetRatio (i+1, nbins);
-   	double far = f.ToFarey (); 
-
-		/* Integral of the jacobian */
-		double delt = (far - fprev) * nbins;
-		// if (0.0 != delt)
-		if (1.0e-8 < delt)
-		{
-			dgral += rect / delt;
-			ejgral += entropy / delt;
-		}
-
-#if 1
-		printf ("%6d	%8.6g	%8.6g	%8.6g	%8.6g	%8.6g	%8.6g	%8.6g %8.6g\n", 
-			i, x, bcnt, gral, far, entropy, dgral, egral, ejgral);
-		fflush (stdout);
-#endif
-		fprev = far;
 	}
-
-	printf ("#Total entropy for ?' =  %18.16g (log2 = %18.16g)\n", egral, egral * log(2.0));
-	printf ("#Total entropy for ?'(?^{-1}) =  %18.16g (log2=%18.16g)\n", ejgral, ejgral*log(2.0));
 }
 
-void 
-gmp_bincount(int nbins, int depth)
+	*prep = rcz;
+	*pimp = icz;
+}
+
+static double bincount_series (double re_q, double im_q, int itermax, double param)
 {
-	int i;
-
-	int max = 1 << depth;
-	printf ("#\n# nbins=%d   tree depth=%d\n#\n",nbins,depth);
-
-	int *bin = (int *) malloc (nbins * sizeof (int));
-	for (i=0; i<nbins; i++)
-	{
-		bin[i] = 0;
-	}
-	bin[0] = 1;
-	bin[nbins-1] = 1;
-
-	mpz_t gib, gn, gnbins;
-	mpz_init (gib);
-	mpz_init (gn);
-	mpz_init (gnbins);
-	mpz_set_ui (gnbins, nbins);
-
-	/* Compute the distribution by bining */
-	unsigned int cnt =2;
-	for (i=0; i<max; i++)
-	{
-		unsigned int n,d;
-		GetNextDyadic (&n, &d);
-
-		// implement the following bining in gmp:
-		// double x = ((double) n)/ ((double) d);
-		// x *= nbins;
-		// int ib = (int) x;
-
-		mpz_mul_ui (gn, gnbins, n);
-		mpz_fdiv_q_ui (gib, gn, d);
-
-		unsigned int ib = mpz_get_ui (gib);
-
-		bin [ib] ++;
-		cnt ++;
-	}
-
-	/* Compute the integral of the distribution */
-   ContinuedFraction f;
-	double gral = 0.0;
-	for (i=0; i<nbins; i++)
-	{
-		double bcnt = bin[i];
-		bcnt /= (double) cnt;
-		gral += bcnt;
-		bcnt *= nbins;
-		double x = ((double) i) / ((double) nbins);
-
-   	f.SetRatio (2*i+1, 2*nbins);
-   	double far = f.ToFarey (); 
-
-		printf ("%6d	%8.6g	%8.6g	%8.6g	%8.6g\n", i, x, bcnt, gral, far);
-	}
+	double rep, imp;
+	bincount_c (re_q, im_q, &rep, &imp);
+	// return sqrt (rep*rep+imp*imp);
+	// return rep;
+	return (atan2 (imp,rep)+M_PI)/(2.0*M_PI);
 }
 
-main(int argc, char *argv[])
-{
-	int i;
+DECL_MAKE_HEIGHT(bincount_series);
 
-	if (argc <3)
-	{
-		fprintf (stderr, "Usage: %s <nbins> <tree-depth> [<arg>]\n", argv[0]);
-		exit (1);
-	}
-	int nbins = atoi (argv[1]);
-	int depth = atoi (argv[2]);
-	if (4 == argc)
-	{
-		double misc_arg = atof (argv[3]);
-	}
-
-	bincount (nbins, depth);
-	// gmp_bincount (nbins, depth);
-}
-
+/* --------------------------- END OF LIFE ------------------------- */
