607c82b39438755ccfe1aed586dffa7072a51d56
linas <>
2008-10-05 16:34:31 +0000

    misc changes

--- misc/fdist/takagi-dist.C
+++ misc/fdist/takagi-dist.C
@@ -18,7 +18,7 @@ double triangle (double x)
 	return 1.0-x;
 }
 
-double olde_a_k (double x, int k)
+double a_k (double x, int k)
 {
 	int i;
 	for (i=0; i<k; i++)
@@ -26,66 +26,40 @@ double olde_a_k (double x, int k)
 		if (x < 0.5) x = x/(1.0-x);
 		else x = (2.0*x-1)/x; 
 		// else x = (1.0-x)/x; 
-	}
-	return triangle (x);
-}
-
-double almost_dedekind_zeta (double x)
-{
-	int n;
-	double prod = 1.0;
-	double tk = 1.0;
-	for (n=0; n< 20; n++)
-	{
-		double mand = 1.0 + olde_a_k(x,n+1);
-		prod /= 0.5 * mand*mand;
-	}
-
-	return prod;
-}
-
-double a_k (double x, int k, double lambda)
-{
-	int i;
-	for (i=0; i<k; i++)
-	{
-		if (x < 0.5) x = x/(1.0-x);
-		else x = (2.0*x-1)/x; 
-		// else x = (1.0-x)/x; 
-		x *= lambda;
 	}
 	return x;
 }
 
-double eff(double x, double lm)
+double eff(double x, int p)
 {
 	double f;
 	if (x < 0.5) f = 1.0-x;
 	else f = x;
 	f = 1.0/f;
-   f *= f;
-	// f = exp (lm * log(f));
-	f *=0.5;
-	return f;
+
+	double g = 1.0;
+	for (int i=0; i<p; i++) g *= f;
+	return g;
 }
 
-double prod (double x, int depth, double lambda)
+double enn(double x)
+{
+	return 0.5*eff(x, 2) - eff(x,3)/3.0;
+}
+
+double sum (double x, int depth, double lambda)
 {
 	int n;
-	double prod = 1.0;
-	double tk = 1.0;
+	double acc = 0.0;
+	double lmp = 1.0;
 	for (n=0; n< depth; n++)
 	{
-		// tk *= 2.0;
-		// double mand = 1.0 + olde_a_k (x, n+1);
-		// double mand = 1.0 + triangle(tk*x);
-		// double mand =  1.25 + 0.25*sin(M_PI*tk*x);
-		// double mand = 1.0/eff(a_k(x,n), lambda);
-		double mand = a_k(x,n, lambda);
-		prod *= eff(mand, 1.0);
+		double mand = a_k(x,n);
+		acc += lmp * enn(mand);
+		lmp *= lambda;
 	}
 
-	return prod;
+	return acc;
 }
 
 void graph(int npts, int depth, double lambda)
@@ -100,7 +74,7 @@ void graph(int npts, int depth, double lambda)
 	for (i=0; i<npts; i++)
 	{
 		double x = (double) i / ((double) npts);
-		double y = prod(x, depth, lambda);
+		double y = sum(x, depth, lambda);
 		acc += y*delta;
 
    	f.SetRatio(i, npts);
