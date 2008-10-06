49ffdd77e80440dd0ece1264b1ede09c281e4dd4
linas <>
2008-10-06 00:24:11 +0000

    more stuff

--- misc/fdist/mink-meas.C
+++ misc/fdist/mink-meas.C
@@ -72,11 +72,38 @@ double da(double x)
 	double f;
 	if (x < 0.5) f = 1.0-x;
 	else f = x;
-	f = 1.0/f;
    f *= f;
-	f *=0.5;
+	f = 1.0/f;
+	f *= 0.5;
 	return f;
 }
+double ca(double x)
+{
+	double f;
+	if (x < 0.5) f = 1.0-x;
+	else f = x;
+   f *= f*f;
+	f = 1.0/f;
+	f /= 3.0;
+	return f;
+}
+
+double combo (double x, double alpha)
+{
+	return alpha*da(x) + (1.0-alpha)*ca(x);
+}
+
+double generic(double x, double (*f)(double))
+{
+	if (x < 0.5) return f(x);
+	double y = (2.0*x-1.0)/(3.0*x-1.0);
+	y = f(y);
+	y = 1.0 - x*x*y/((3.0*x-1.0)*(3.0*x-1.0));
+	y /= x*x;
+	return y;
+}
+
+double plain_a (double x) { return 0.5/((1.0-x)*(1.0-x)); }
 
 double da3(double x)
 {
@@ -111,12 +138,19 @@ double prod (double x, int depth)
 		// double mand =  1.25 + 0.25*sin(M_PI*tk*x);
 		// double mand = 1.0/eff(a_k(x,n), lambda);
 		//
+#define TWO_ADIC
 #ifdef TWO_ADIC
 		double mand = a_k(x,n);
-		prod *= da(mand);
+		// prod *= da(mand);
+		// prod *= ca(mand);
+		// prod *= combo(mand, 0.5);
+		// prod *= generic(mand, plain_a);
+		prod *= generic(mand, exp);
 #endif
+#ifdef THREE_ADIC
 		double mand = a3_k(x,n);
 		prod *= da3(mand);
+#endif
 	}
 
 	return prod;
