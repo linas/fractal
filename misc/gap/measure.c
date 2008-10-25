7455ad1b0b25c757b348dd0c437da2398bc44577
linas <>
2008-10-25 05:00:18 +0000

    initial checkin

---
+++ misc/gap/measure.c
@@ -0,0 +1,24 @@
+/*
+ * measure.c
+ *
+ * Get Bernoulli
+ *
+ * Linas Oct 2008
+ */
+i#include <stdio.h>
+
+void beig(int np)
+{
+	int npts = 1<<np;
+	int i;
+
+	for(i=0; i<npts; i++)
+	{
+	}
+}
+
+main()
+{
+	beig(10);
+
+}
