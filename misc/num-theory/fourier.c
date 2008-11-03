8d2cfef040c7870aa98740d2bff757cd7184ffca
linas <>
2008-11-03 16:31:42 +0000

    undo the roueri

--- misc/num-theory/fourier.c
+++ misc/num-theory/fourier.c
@@ -28,6 +28,69 @@ int mertens (int n)
 	return acc;
 }
 
+void rebin (int nbins, int kmax)
+{
+	int i, k;
+
+	double * rebin = (double*) malloc (nbins*sizeof(double));
+	double * imbin = (double*) malloc (nbins*sizeof(double));
+
+	// First compute the fourier xform.
+	double resum = 0.0;
+	double imsum = 0.0;
+	for (i=0; i<nbins; i++)
+	{
+		/* Midpoint of the bin */
+		double x = ((double) 2*i+1) / ((double) 2*nbins);
+
+		double rev = 0.0;
+		double imv = 0.0;
+		for (k=0; k<kmax; k++)
+		{
+			double theta = 2.0*M_PI*x*k;
+			double si = sin(theta);
+			double co = cos(theta);
+
+			// double fk = mangoldt_lambda (k);
+			// double fk = totient_phi(k);
+			// double fk = liouville_lambda (k);
+			// double fk = liouville_omega (k);
+			// double fk = mertens (k);
+			double fk = moebius_mu(k);
+			rev += co * fk;
+			imv += si * fk;
+		}
+
+		resum += rev;
+		imsum += imv;
+
+		// printf ("%d	%g	%g	%g	%g	%g\n", i, x, rev, imv, resum, imsum);
+
+		rebin[i] = rev;
+		imbin[i] = imv;
+	}
+
+	// Next, undo the Fourier.
+	for (k=0; k<kmax; k++)
+	{
+		double res = 0.0;
+		double ims = 0.0;
+		for (i=0; i<nbins; i++)
+		{
+			/* Midpoint of the bin */
+			double x = ((double) 2*i+1) / ((double) 2*nbins);
+			double theta = 2.0*M_PI*x*k;
+			double si = sin(theta);
+			double co = cos(theta);
+
+			res += co * rebin[i];
+			ims += si * rebin[i];
+		}
+		double sane = moebius_mu(k);
+		printf ("%d	%g	%g	%g\n", k, res, ims, sane);
+	}
+}
+
 int main (int argc, char * argv[])
 {
 	int i, k;
@@ -40,33 +103,5 @@ int main (int argc, char * argv[])
 	int nbins = atoi (argv[1]);
 	int kmax = atoi(argv[2]);
 
-	double resum = 0.0;
-	double imsum = 0.0;
-	for (i=0; i<nbins; i++)
-	{
-		double x = ((double) 2*i+1) / ((double) 2*nbins);
-
-		double rev = 0.0;
-		double imv = 0.0;
-		for (k=0; k<kmax; k++)
-		{
-			double theta = 2.0*M_PI*x*k;
-			double si = sin(theta);
-			double co = cos(theta);
-
-			// double fk = mangoldt_lambda (k);
-			double fk = totient_phi(k);
-			// double fk = liouville_lambda (k);
-			// double fk = liouville_omega (k);
-			// double fk = mertens (k);
-			// double fk = moebius_mu(k);
-			rev += co * fk;
-			imv += si * fk;
-		}
-
-		resum += rev;
-		imsum += imv;
-
-		printf ("%d	%g	%g	%g	%g	%g\n", i, x, rev, imv, resum, imsum);
-	}
+	rebin (nbins, kmax);
 }
