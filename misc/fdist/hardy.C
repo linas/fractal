73e0f6b5ba4cda6546944db375fde6ee540902bd
linas <>
2008-11-03 00:55:17 +0000

    re-edit and get rid of crap

--- misc/fdist/hardy.C
+++ misc/fdist/hardy.C
@@ -1,14 +1,14 @@
 /*
- * inner.C
+ * hardy.C
  *
  * Poison integral of the Minkowski measure (i.e. of the
  * distribution of the Farey Numbers on the unit interval)
  * The Poison ingtegral takes the form of a so-called 
  * "singular inner function" in the theory of Hardy spaces.
  *
+ * Linas October 2004
  * Linas October 2008
  */
-#include <gmp.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,45 +16,25 @@
 #include "Farey.h"
 #include "FareyTree.h"
 
-void GetNextDyadic (unsigned int *n, unsigned int *d)
-{
-	static unsigned int last_d = 1;
-	static unsigned int last_n = 1;
-
-	last_n += 2;
-	if (last_n > last_d)
-	{
-		last_d *= 2;
-		last_n = 1;
-	}
-
-	*n = last_n;
-	*d = last_d;
-}
-
+double * bin;
 
 void bincount(int nbins, int depth)
 {
 	int i;
-
 	int max = 1 << depth;
-	printf ("#\n# nbins=%d   tree depth=%d\n#\n",nbins,depth);
-	printf ("# Legend:\n");
-	printf ("# i, x, bin_cnt, bin_cnt_sum, exact_farey\n");
-	fflush (stdout);
 
 	FareyIterator fi;
 
-	int *bin = (int *) malloc (nbins * sizeof (int));
+	bin = (double *) malloc (nbins * sizeof (double));
 	for (i=0; i<nbins; i++)
 	{
-		bin[i] = 0;
+		bin[i] = 0.0;
 	}
-	bin[0] = 1;
-	bin[nbins-1] = 1;
+	bin[0] = 1.0;
+	bin[nbins-1] = 1.0;
 
 	/* Compute the distribution by bining */
-	int cnt =2;
+	int cnt = 2;
 	for (i=0; i<max; i++)
 	{
 		int n,d;
@@ -64,136 +44,129 @@ void bincount(int nbins, int depth)
 		double x = ((double) n)/ ((double) d);
 		x *= nbins;
 		int ib = (int) x;
-		bin [ib] ++;
+		bin [ib] += 1.0;
 		cnt ++;
 	}
 
-	/* Compute the integral of the distribution */
-   ContinuedFraction f;
-	double gral = 0.0;
-	double egral = 0.0;
-	double ejgral = 0.0;
-	double dgral = 0.0;
-	double fprev = 0.0;
+	/* renormalize */
 	for (i=0; i<nbins; i++)
 	{
-		/* gral is the ordinary integral of the bin count */
-		double rect = bin[i] / ((double) cnt);
-		gral += rect;
-
-		/* Be careful to bin-count the entropy 
-		 * (use discrete not continuous formula) 
-		 * This is the entropy of just ?'(x) and not of the jacobian */
-		double entropy = - rect * log(rect);
-		if (bin[i] == 0) entropy = 0;
-		egral += entropy;
-
-		double bcnt = rect * nbins;
-
-		double x = ((double) i) / ((double) nbins);
-
-   	f.SetRatio (i+1, nbins);
-   	double far = f.ToFarey (); 
-
-		/* Integral of the jacobian */
-		double delt = (far - fprev) * nbins;
-		// if (0.0 != delt)
-		if (1.0e-8 < delt)
-		{
-			dgral += rect / delt;
-			ejgral += entropy / delt;
-		}
-
-#if 1
-		printf ("%6d	%8.6g	%8.6g	%8.6g	%8.6g	%8.6g	%8.6g	%8.6g %8.6g\n", 
-			i, x, bcnt, gral, far, entropy, dgral, egral, ejgral);
-		fflush (stdout);
-#endif
-		fprev = far;
+		bin[i] *= ((double) nbins) / ((double) cnt);
 	}
 
-	printf ("#Total entropy for ?' =  %18.16g (log2 = %18.16g)\n", egral, egral * log(2.0));
-	printf ("#Total entropy for ?'(?^{-1}) =  %18.16g (log2=%18.16g)\n", ejgral, ejgral*log(2.0));
 }
 
-void 
-gmp_bincount(int nbins, int depth)
+void fourier (int nbins, int freq_max)
 {
-	int i;
+	int i, n;
 
-	int max = 1 << depth;
-	printf ("#\n# nbins=%d   tree depth=%d\n#\n",nbins,depth);
+	double *fre = (double *) malloc (freq_max*sizeof(double));
+	double *fim = (double *) malloc (freq_max*sizeof(double));
 
-	int *bin = (int *) malloc (nbins * sizeof (int));
-	for (i=0; i<nbins; i++)
+	for (n=0; n<freq_max; n++)
 	{
-		bin[i] = 0;
-	}
-	bin[0] = 1;
-	bin[nbins-1] = 1;
-
-	mpz_t gib, gn, gnbins;
-	mpz_init (gib);
-	mpz_init (gn);
-	mpz_init (gnbins);
-	mpz_set_ui (gnbins, nbins);
-
-	/* Compute the distribution by bining */
-	unsigned int cnt =2;
-	for (i=0; i<max; i++)
-	{
-		unsigned int n,d;
-		GetNextDyadic (&n, &d);
-
-		// implement the following bining in gmp:
-		// double x = ((double) n)/ ((double) d);
-		// x *= nbins;
-		// int ib = (int) x;
-
-		mpz_mul_ui (gn, gnbins, n);
-		mpz_fdiv_q_ui (gib, gn, d);
-
-		unsigned int ib = mpz_get_ui (gib);
-
-		bin [ib] ++;
-		cnt ++;
+		fre[n] = 0.0;
+		fim[n] = 0.0;
 	}
 
 	/* Compute the integral of the distribution */
    ContinuedFraction f;
-	double gral = 0.0;
+
 	for (i=0; i<nbins; i++)
 	{
-		double bcnt = bin[i];
-		bcnt /= (double) cnt;
-		gral += bcnt;
-		bcnt *= nbins;
-		double x = ((double) i) / ((double) nbins);
+		/* x is the midpoint of the bin */
+		double x = ((double) 2*i+1) / ((double) 2*nbins);
 
+		/* Likewise, the midpoint */
    	f.SetRatio (2*i+1, 2*nbins);
    	double far = f.ToFarey (); 
 
-		printf ("%6d	%8.6g	%8.6g	%8.6g	%8.6g\n", i, x, bcnt, gral, far);
+		for (n=0; n<freq_max; n++)
+		{
+// #define JACOB
+#ifdef JACOB
+			double re = cos(2.0*M_PI*n*far);
+			double im = sin(2.0*M_PI*n*far);
+			fre[n] += re;
+			fim[n] += im;
+#endif
+#ifdef INVJACOB
+			double re = cos(2.0*M_PI*n*far);
+			double im = sin(2.0*M_PI*n*far);
+			fre[n] += bin[i] * bin[i] * re;
+			fim[n] += bin[i] * bin[i] * im;
+#endif
+#ifdef STRIAGHT
+			double re = cos(2.0*M_PI*n*x);
+			double im = sin(2.0*M_PI*n*x);
+			fre[n] += bin[i] * re;
+			fim[n] += bin[i] * im;
+#endif
+			if (bin[i] != 0.0)
+			{
+				double re = cos(2.0*M_PI*n*x);
+				double im = sin(2.0*M_PI*n*x);
+				fre[n] += re / bin[i];
+				fim[n] += im / bin[i];
+			}
+		}
 	}
+
+	/* renormalize */
+	for (n=0; n<freq_max; n++)
+	{
+		fre[n] /= (double) nbins;
+		fim[n] /= (double) nbins;
+	}
+
+#if 0
+	for (n=0; n<freq_max; n++)
+	{
+		printf ("%d	%8.6g	%8.6g\n", n, fre[n], fim[n]);
+	}
+#endif
+
+#if 1
+	/* rebin */
+	int npts = 2048;
+	double gral = 0.0;
+	for(i=0; i<npts; i++)
+	{
+		double x = ((double) i) / ((double) npts);
+
+   	f.SetRatio (2*i+1, 2*npts);
+   	double far = f.ToFarey (); 
+
+		double fx = fre[0];
+		for (n=1; n<freq_max; n++)
+		{
+			fx += 2.0* fre[n] * cos(2.0*M_PI*n*x);
+			// fx += 2.0* fre[n] * cos(2.0*M_PI*n*far);
+		}
+
+		// hack alert .. !!??
+		// fx /= fre[0];
+		gral += fx / ((double) npts);
+		printf ("%8.6g	%8.6g	%8.6g	%8.6g\n", x, fx, gral, far);
+	}
+#endif
+
 }
 
 main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc <3)
+	if (argc < 4)
 	{
-		fprintf (stderr, "Usage: %s <nbins> <tree-depth> [<arg>]\n", argv[0]);
+		fprintf (stderr, "Usage: %s <nbins> <tree-depth> <freq>\n", argv[0]);
 		exit (1);
 	}
 	int nbins = atoi (argv[1]);
 	int depth = atoi (argv[2]);
-	if (4 == argc)
-	{
-		double misc_arg = atof (argv[3]);
-	}
+	int max_freq = atoi (argv[3]);
 
 	bincount (nbins, depth);
-	// gmp_bincount (nbins, depth);
+	fourier (nbins, max_freq);
 }
 
