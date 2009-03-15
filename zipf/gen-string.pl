905680151c5f64d21a325f9efc7116fb5270dca4
linas <>
2009-03-15 03:23:43 +0000

    more stuff

--- zipf/gen-string.pl
+++ zipf/gen-string.pl
@@ -7,6 +7,7 @@
 # Linas Vepstas March 2009
 
 use strict;
+use warnings;
 
 
 my $Nletters = 29;
@@ -18,6 +19,18 @@ my @letters = ('e', 'e', 'e', 'e',
 'n', 'n', 's', 's', 'h', 'h', 'r', 'r', 
 'd', 'd', 'l', 'l', 'u');
 
+my $num_sentences = 0;
+my $num_words = 0;
+my $num_pairs = 0;
+
+my %word_freq = ();
+my %left_word_freq = ();
+my %right_word_freq = ();
+my %word_pair_freq = ();
+
+my @word_id = ();  # word to index map
+
+
 sub make_random_word
 {
 	my $word = "";
@@ -25,9 +38,9 @@ sub make_random_word
 	for ($i=0; $i<12; $i++)
 	{
 		my $rn = rand();
-		$rn *= $Nletters + 3;
-		if (($rn < 3) && ($i > 0)) { last;}
-		$rn -= 3;
+		$rn *= $Nletters + 5;
+		if (($rn < 5) && ($i > 0)) { last;}
+		$rn -= 5;
 		my $l = $letters[$rn];
 		
 		$word = $word . $l;
@@ -35,5 +48,44 @@ sub make_random_word
 	$word;
 }
 
-my $w = make_random_word();
-print "its $w\n";
+while ($num_sentences < 501123)
+{
+	my $i;
+
+	# put 13 words into a sentence.
+	@word_id = ();
+	for ($i=0; $i<13; $i++)
+	{
+		my $word = make_random_word();
+		$word_freq{$word} += 1;
+		$num_words ++;
+		
+		my $right_word = $word;
+		my $li = 0;
+		for ($li=0; $li<$i; $li++)
+		{
+			my $left_word = $word_id[$li];
+			$left_word_freq{$left_word} += 1;
+			$right_word_freq{$right_word} += 1;
+
+			# Count word pairs.
+			my $pair = $left_word . "+\@\@\@+" . $right_word;
+			$word_pair_freq{$pair} += 1;
+			$num_pairs ++;
+		}
+
+		$word_id[$i] = $word;
+	}
+
+	$num_sentences ++;
+	if ($num_sentences %1000 == 0)
+	{
+		print "done $num_sentences sentences\n";
+	}
+}
+
+print "Final sentence count = $num_sentences\n";
+print "Final num word = $num_words\n";
+print "Final num pairs = $num_pairs\n";
+
+
