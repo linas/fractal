bd5a95dc6318d661a18e27ee9551ef50a3809055
linas <>
2009-07-29 15:31:43 +0000

    initial checkin

---
+++ misc/walsh/walsh.c
@@ -0,0 +1,17 @@
+/*
+ * walsh.c
+ *
+ * Walsh functions provide a basis for square-integrable functions.
+ * Walsh functions can be enumerated using integers that have non-zero
+ * Mobius mu values. Walsh functions are even or odd, depending on 
+ * whether mu is positive or negative.
+ *
+ * The code here creates the Walsh functions, indexing them with integers.
+ * This allows various series to be explored.
+ *
+ * The 'prime factors' of the walsh functions are the Rademacher functions.
+ *
+ * Linas Vepstas July 2009
+ */
+
+rade
