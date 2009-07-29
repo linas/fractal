e8f3fee8c849ce36c9a7643074a60ca7212a695e
linas <>
2009-07-29 15:45:19 +0000

    start filling in

--- misc/walsh/walsh.c
+++ misc/walsh/walsh.c
@@ -14,4 +14,38 @@
  * Linas Vepstas July 2009
  */
 
-rade
+/**
+ * Generate the Rademacher functions r_n(x).
+ * The counting used here is such that r_0(x) = 1 
+ * r_1(x) = -1 for x<0.5 and 1 for x>0.5
+ * r_2(x) = r_1(2*x)  or r_{n+1}(x) = r_n(2*x)
+ *
+double rademacher (double x, int n)
+{
+	if (0 == n) return 1.0;
+	if (1 == n)
+	{
+		if (x < 0.5) return -1.0;
+		return 1.0;
+	}
+	
+	int shift = 1 << (n-1);
+	x *= shift;
+	x -= floor(x);
+	return rademacher(x, 1);
+}
+
+main (int argc, char * argv[])
+{
+	
+	printf("#\n# Rademacher/Waalsh functions\n#\n");
+	int nsteps = 300;
+	int i;
+	for (i=0; i<= nsteps; i++)
+	{
+		double x = i / ((double) nsteps);
+		double y = rademacher (x, 2);
+		
+		printf(%d	%f	%f\n", i, x, y);
+	}
+}
