510541983a00de24e40f75905491d3f85d12288d
linas <>
2009-08-17 02:00:57 +0000

    more code

--- misc/walsh/walsh-mink.C
+++ misc/walsh/walsh-mink.C
@@ -7,11 +7,36 @@
  * Linas Vepstas August 2009
  */
 
+#include <math.h>
+#include <stdio.h>
+
+/* Reverse the bit order of i, assuming its of length n. */
+int reverse(int p, int n)
+{
+	int r = 0;
+	for (int s=0; s<n; s++)
+	{
+		r <<= 1;
+		if (0x1 == (p & 0x1)) r |= 0x1;
+		p >>= 1;
+	}
+	return r;
+}
+
 /*
  * argument is interpreted as p/2^n
  */
 double eval (int p, int n)
 {
+	int m = 1<<n;
+	int r = reverse (p, n);
+
+printf("duude p=%x  r=%x\n", p,r);
+	for (int i=0; i<m; i++)
+	{
+		
+	}
+	
 	return 0.0;
 }
 
@@ -19,11 +44,11 @@ int main(int argc, char * argv[])
 {
 
 	int n = 4;
-	int m = 1<<n:
+	int m = 1<<n;
 	for (int p=0; p<m; p++)
 	{
 		double x = ((double) p) / ((double) m);
-		double y = eval(q, n);
-		printf("%d	%f	%f\n", p, x, y);
+		double y = eval(p, n);
+		// printf("%d	%f	%f\n", p, x, y);
 	} 
 }
