867f80f726df51d85fabbc17e136ab38e36061d7
linas <>
2010-01-09 22:17:39 +0000

    misc fixes

--- misc/gkw/hurwitz.c
+++ misc/gkw/hurwitz.c
@@ -10,16 +10,20 @@
 #include <gsl/gsl_sf_zeta.h>
 
 
-main ()
+main (int argc, char * argv[])
 {
-	double s=2.o;
+	int i;
+	double s=2.0;
+
+	s = atof (argv[1]);
 
 	int npts = 300;
-	for (int i=0; i< npts; i++)
+	double norm = 1.0 / gsl_sf_hzeta(s, 1.0);
+	for (i=0; i< npts; i++)
 	{
 		double x = ((double) i) / ((double) npts);
-		double y = gsl_sf_hzeta(2.0, x)
+		double y = norm * gsl_sf_hzeta(s, 1.0+x);
 
-		print ("%d	%g	%g\n", i, x, y);
+		printf("%d	%g	%g\n", i, x, y);
 	}
 }
