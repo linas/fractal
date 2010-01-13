6ab9b7ad7eaa8aa1992f1872cec204a1d503331e
linas <>
2010-01-13 00:50:35 +0000

    create an actual gkw binary

--- generate/gkw.C
+++ generate/gkw.C
@@ -5,15 +5,38 @@
  * Display GKW operator, one matrix element per pixel.
  *
  * HISTORY:
- * New Jan 2010
+ * December 2003
+ * Pixelize Jan 2010
  */
 
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../misc/continued-frobenius/ache.h"
 #include "brat.h"
+#include "binomial.h"
+#include "harmonic.h"
+
+// Return the matrix element for H_mp aka the matrix element of GKW.
+//
+long double
+ache_mp(int m, int p)
+{
+	int k;
+
+	long double acc = 0.0L;
+	long double sign = 1.0L;
+	for (k=0; k<=p; k++)
+	{
+		long double term = zetam1 (k+m+2);
+		term *= binomial (m+k+1,m);
+		term *= binomial (p,k);
+		term *= sign;
+		acc += term;
+		sign = -sign;
+	}
+	return acc;
+}
 
 
 static double gkw_operator (double x, double y, int itermax, double param)
