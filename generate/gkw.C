d6e8ebe06b9525da2450d4994fdd811485fa308e
linas <>
2010-01-13 00:18:24 +0000

    nitial checkin

---
+++ generate/gkw.C
@@ -0,0 +1,27 @@
+/*
+ * gkw.C
+ *
+ * FUNCTION:
+ * Display GKW operator, one matrix element per pixel.
+ *
+ * HISTORY:
+ * New Jan 2010
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ache.h"
+
+static double gkw_operator (double x, double y, int itermax, double param)
+{
+	int m = 100.0 * x;
+	int p = 100.0 * y;
+	double gkw = ache_mp(m,p);
+	return gkw;
+}
+
+DECL_MAKE_HEIGHT(gkw_operator);
+
+/* --------------------------- END OF LIFE ------------------------- */
