19371b5718c9979e85204464f2a946329d00f3b5
linas <>
2010-01-13 03:00:26 +0000

    add a bignum implementation

--- generate/gkw.C
+++ generate/gkw.C
@@ -16,11 +16,13 @@
 #include "brat.h"
 #include "binomial.h"
 #include "harmonic.h"
+#include "../misc/bignum/mp-zeta.h"
+#include "../misc/bignum/mp-binomial.h"
 
 // Return the matrix element for H_mp aka the matrix element of GKW.
 //
 long double
-ache_mp(int m, int p)
+ache_mp_double(int m, int p)
 {
 	int k;
 
@@ -38,17 +40,57 @@ ache_mp(int m, int p)
 	return acc;
 }
 
+long double
+ache_mp_mp(int m, int p)
+{
+	int k;
+
+	int prec = 100;
+
+	mpf_t acc, one, term, fbin;
+	mpf_init (term);
+	mpf_init (acc);
+	mpf_init (one);
+	mpf_init (fbin);
+
+	mpz_t bin;
+	mpz_init (bin);
+
+	// long double acc = 0.0L;
+	mpf_set_ui (acc, 0);
+	mpf_set_ui (one, 1);
+
+	for (k=0; k<=p; k++)
+	{
+		// long double term = zetam1 (k+m+2);
+		fp_zeta (term, k+m+2, prec);
+		mpf_sub(term, term, one);
+
+		// term *= binomial (m+k+1,m);
+		i_binomial (bin, m+k+1, m);
+		mpf_set_z (fbin, bin);
+		mpf_mul (term, term, fbin);
+
+		// term *= binomial (p,k);
+		i_binomial (bin, p, k);
+		mpf_set_z (fbin, bin);
+		mpf_mul (term, term, fbin);
+
+		if (k%2 == 0) mpf_add (acc, acc, term);
+		else mpf_sub (acc, acc, term);
+	}
+
+	return mpf_get_d (acc);
+}
+
 
 static double gkw_operator (double x, double y, int itermax, double param)
 {
 	int p = 100.0 * x + 0.5;
 	int m = 100.0 * y + 0.5;
 	m = 100 - m;
-	double gkw = ache_mp(m,p);
-	gkw = fabs(gkw);
-	gkw = log(gkw);
-	gkw += 15.0;
-	gkw /= 1.0;
+	double gkw = ache_mp_mp(m,p);
+	// gkw = fabs(gkw);
 // printf ("%d %d %f\n", m, p, gkw);
 	return gkw;
 }
