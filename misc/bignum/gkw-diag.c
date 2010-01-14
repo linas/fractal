254a480f8693def44345ab0cdc0c134c2ad66970
linas <>
2010-01-14 14:17:55 +0000

    a compilable version.

--- misc/bignum/gkw-diag.c
+++ misc/bignum/gkw-diag.c
@@ -7,8 +7,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "gkw.h"
+#include "mp-gkw.h"
 
 int main () 
 {
+	int n;
+	mpf_t acc;
+	double g;
+
+	int prec = 400;
+
+	/* Set the precision (number of binary bits) = prec*log(10)/log(2) */
+	mpf_set_default_prec (3.3*prec);
+
+	mpf_init (acc);
+
+	for (n=0; n<100; n++)
+	{
+		gkw(acc, n, n, prec);
+
+		g = mpf_get_d (acc);
+		printf("%d	%g\n", n, g);
+	}
+
+	return 0;
 }
