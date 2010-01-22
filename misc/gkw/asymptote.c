e2f7eb88f04ca1283b4dbdd1fc45004c60b05b59
linas <>
2010-01-22 04:07:32 +0000

    initial checkin

---
+++ misc/gkw/asymptote.c
@@ -0,0 +1,20 @@
+
+/*
+ * asymptote.c
+ * Find saddle point of asympttic expansion
+ *
+ * Linas Vepstas January 2010
+ */
+
+#include <complex.h>
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+	double complex c;
+	c 
+	x = 2.2 + I * 3.3;
+
+	printf("its %f %f \n", creal(x), cimag(x));
+}
