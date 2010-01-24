01c6db1835f11491949f1b522790a8aebd1a770e
linas <>
2010-01-24 23:39:12 +0000

    bug fix -- need to verify that s is unchanged in the caches.

--- misc/bignum/mp-trig.c
+++ misc/bignum/mp-trig.c
@@ -1189,16 +1189,24 @@ void fp_pow_rc (cpx_t powc, int k, const mpf_t q, const cpx_t ess, int prec)
 	DECLARE_CPX_CACHE (powcache_one);
 	DECLARE_CPX_CACHE (powcache_two);
 	static mpf_t cache_q_one, cache_q_two;
+	static mpf_t cache_sre_one, cache_sre_two;
+	static mpf_t cache_sim_one, cache_sim_two;
 	static int precision = 0;
 	static cpx_cache *next_cache;
-	static mpf_t *next_q;
+	static mpf_t *next_q, *next_sre, *next_sim;
 
 	if (!precision)
 	{
 		mpf_init (cache_q_one);
 		mpf_init (cache_q_two);
+		mpf_init (cache_sre_one);
+		mpf_init (cache_sre_two);
+		mpf_init (cache_sim_one);
+		mpf_init (cache_sim_two);
 		next_cache = &powcache_one;
 		next_q = &cache_q_one;
+		next_sre = &cache_sre_one;
+		next_sim = &cache_sim_one;
 	}
 
 	if (precision < prec)
@@ -1211,7 +1219,9 @@ void fp_pow_rc (cpx_t powc, int k, const mpf_t q, const cpx_t ess, int prec)
 	}
 
 	cpx_cache *powcache = NULL;
-	if (mpf_eq(q, cache_q_one, prec*3.322))
+	if (0 == mpf_eq(q, cache_q_one, prec*3.322) &&
+	    0 == mpf_eq(ess[0].re, cache_sre_one, prec*3.322) &&
+	    0 == mpf_eq(ess[0].im, cache_sim_one, prec*3.322))
 	{
 		powcache = &powcache_one;
 		if (prec <= cpx_one_d_cache_check (powcache, k))
@@ -1221,9 +1231,13 @@ void fp_pow_rc (cpx_t powc, int k, const mpf_t q, const cpx_t ess, int prec)
 		}
 		next_cache = &powcache_two;
 		next_q = &cache_q_two;
+		next_sre = &cache_sre_two;
+		next_sim = &cache_sim_two;
 	}
 	
-	if (mpf_eq(q, cache_q_two, prec*3.322))
+	if (0 == mpf_eq(q, cache_q_two, prec*3.322) &&
+	    0 == mpf_eq(ess[0].re, cache_sre_two, prec*3.322) &&
+	    0 == mpf_eq(ess[0].im, cache_sim_two, prec*3.322))
 	{
 		powcache = &powcache_two;
 		if (prec <= cpx_one_d_cache_check (powcache, k))
@@ -1233,6 +1247,8 @@ void fp_pow_rc (cpx_t powc, int k, const mpf_t q, const cpx_t ess, int prec)
 		}
 		next_cache = &powcache_one;
 		next_q = &cache_q_one;
+		next_sre = &cache_sre_one;
+		next_sim = &cache_sim_one;
 	}
 	
 	if (NULL == powcache)
@@ -1241,6 +1257,8 @@ void fp_pow_rc (cpx_t powc, int k, const mpf_t q, const cpx_t ess, int prec)
 		cpx_one_d_cache_clear (powcache);
 		cpx_one_d_cache_check (powcache, 4);
 		mpf_set(*next_q, q);
+		mpf_set(*next_sre, ess[0].re);
+		mpf_set(*next_sim, ess[0].im);
 	}
 
 	mpf_t kq;
