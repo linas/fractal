96c55f52a8ce6f497ac1311a5ee4b29d8fc0f700
linas <>
2010-01-25 00:26:20 +0000

    remove some debugging gorp.

--- misc/bignum/mp_zeta_test.c
+++ misc/bignum/mp_zeta_test.c
@@ -739,7 +739,6 @@ int test_complex_riemann_zeta (int nterms, int prec)
 	int i;
 	int pr = prec;
 
-#if 0
 	/*  Verify that its zero where it should be */
 	int nz = 13;
 	if (nterms < nz) nz = nterms;
@@ -753,21 +752,19 @@ int test_complex_riemann_zeta (int nterms, int prec)
 		nfaults = cpx_check_for_zero (nfaults, zeta, epsi, "complex riemann at non-triv zeroes", i, sre, sim);
 	}
 
-#endif
 	/* compare values to integer zeta routine */
 	for (i=3; i<2*nterms; i++ )
 	{
-		double z_expected, z_got;
+		// double z_expected, z_got;
 		fp_zeta (nzeta, i, pr);
-		z_expected = mpf_get_d (nzeta);
+		// z_expected = mpf_get_d (nzeta);
 		
 		cpx_set_ui (ess, i, 0);
 		cpx_borwein_zeta (zeta, ess, pr);
-		z_got = mpf_get_d(zeta[0].re);
+		// z_got = mpf_get_d(zeta[0].re);
 
 		mpf_sub (zeta[0].re, zeta[0].re, nzeta);
 
-		printf("duuude s=%d expcected=%f got=%f\n", i, z_expected, z_got);
 		nfaults = cpx_check_for_zero (nfaults, zeta, epsi, "complex riemann at pos ints", i, i, 0);
 	}
 
@@ -1896,22 +1893,18 @@ int main (int argc, char * argv[])
 #endif /* CONFLUENT_HYPERGEOMETRIC */
 
 	int nfaults = 0;
-/****
 	nfaults += test_real_sine (nterms, prec);
 	nfaults += test_cpx_sqrt (nterms, prec);
 	nfaults += test_complex_gamma (nterms, prec);
- 	nfaults += test_hurwitz_zeta (nterms, prec);
-****/
+	nfaults += test_complex_pow (nterms, prec);
+	nfaults += test_real_gamma (nterms, prec);
 	nfaults += test_complex_riemann_zeta (nterms, prec);
-/*****
+ 	nfaults += test_hurwitz_zeta (nterms, prec);
 	nfaults += test_polylog (nterms, prec, 0);
 	nfaults += test_polylog (nterms, prec, 1);
 	nfaults += test_polylog_euler (nterms, prec);
 	nfaults += test_polylog_series (nterms, prec);
  	nfaults += test_periodic_zeta (nterms, prec);
-	nfaults += test_complex_pow (nterms, prec);
-	nfaults += test_real_gamma (nterms, prec);
-****/
 
 	if (0 == nfaults)
 	{
