564acfa49754527d538473ef6dc6ec24513b6266
linas <>
2010-02-12 13:36:32 +0000

    extend-a-range

--- misc/fourier-question/counting.c
+++ misc/fourier-question/counting.c
@@ -1,6 +1,7 @@
 /* 
  * counting.c
  * Crude quick-n-dirty prime-couting attempt.
+ * Results are fantastic! Wow!
  * 
  * Linas Vepstas February 2010
  */
@@ -11,11 +12,13 @@
 
 main()
 {
+	double x, term;
 	int k;
-	int logstep = 10;
+	// int logstep = 10;
+	int logstep = 7;
 	// int prtstep = logstep - 3;
-	int prtstep = logstep;
-	int nmax = 1000*1000;
+	int prtstep = logstep+5;
+	int nmax = 10*1000*1000;
 
 	int nsteps = 1 << logstep;
 	int psteps = 1 << prtstep;
@@ -27,8 +30,8 @@ main()
 	double delta = 1.0 / ((double) nsteps);
 	for (k=1; k < nsteps; k++)
 	{
-		double x = k * delta;
-		double term  = lg2 * 1.0  / question_inverse(x);
+		x = k * delta;
+		term  = lg2 * 1.0  / question_inverse(x);
 		term = 0.0;
 		acc += delta*term;
 		li += delta / log(x);
@@ -37,14 +40,17 @@ main()
 	acc = -1.0;
 	for (k=nsteps+1; k <= nmax*nsteps; k++)
 	{
-		double x = k * delta;
+		x = k * delta;
 		double ox = 1.0 / x;
-		double term  = 1.0 / question_inverse(ox);
+		term  = 1.0 / question_inverse(ox);
 		term -= 1.0;
 		term = lg2 / term;
 		acc += delta*term;
 		li += delta / log(x);
 		if (k%psteps == 0) printf("%d	%f %g	%g	%g\n", k, x, term, acc, li);
 	}
+
+	/* Print the last one no matter what */
+	printf("%d	%f %g	%g	%g\n", k, x, term, acc, li);
 }
 
