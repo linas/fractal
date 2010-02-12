c12776015b5a3392a276fcf1ecca1933fb622a1e
linas <>
2010-02-12 04:57:04 +0000

    more stuff.

--- misc/fourier-question/counting.c
+++ misc/fourier-question/counting.c
@@ -12,19 +12,28 @@ main()
 {
 	int k;
 	int logstep = 10;
-	int nmax = 100;
+	int prtstep = logstep - 3;
+	int nmax = 1000;
 
 	int nsteps = 1 << logstep;
+	int psteps = 1 << prtstep;
 
 	double acc = 0.0;
 	double delta = 1.0 / ((double) nsteps);
+	for (k=1; k <= nsteps; k++)
+	{
+		double x = k * delta;
+		double term  = 1.0 / question_inverse(x);
+		acc += delta*term;
+		if (k%psteps == 0) printf("%d	%f %g	%g\n", k, x, term, acc);
+	}
 	for (k=nsteps+1; k <= nmax*nsteps; k++)
 	{
 		double x = k * delta;
 		double ox = 1.0 / x;
-		double term  = 1.0 / question_mark(nsteps, k);
+		double term  = question_inverse(ox);
 		acc += delta*term;
-		printf("%d	%f %g	%g\n", k, x, term, acc);
+		if (k%psteps == 0) printf("%d	%f %g	%g\n", k, x, term, acc);
 	}
 }
 
