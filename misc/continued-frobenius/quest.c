e1a9b9a0d1805b1cf1c075f52ce8a517f5bf0aa3
linas <>
2010-02-18 20:08:51 +0000

    misc fixes

--- misc/continued-frobenius/quest.c
+++ misc/continued-frobenius/quest.c
@@ -7,6 +7,9 @@
  * Linas Vepstas Feb 2010
  */
 
+#include <math.h>
+#include <stdio.h>
+
 #include "question.h"
 
 double gkw (double x)
@@ -25,18 +28,20 @@ double gkw (double x)
 }
 
 
+int
 main ()
 {
 	int i;
-	double x, y;
+	double x, y, w;
 
 	int nmax = 200;
 	for (i=1; i<nmax; i++)
 	{
 		x = ((double) i) / ((double) nmax);
+		w = question_inverse(1.0-x);
 		y = gkw(x);
 
-		printf ("%d	%f 	%f	%f\n", i, x, y, y/x);
+		printf ("%d	%f %f	%f	%f\n", i, x, w, y, y/w);
 	}
-
+	return 0;
 }
