a5f5766f96c75ec0acf7f50279d84b635b579c1d
linas <>
2010-03-16 02:53:03 +0000

    expand the experiment

--- misc/fdist/qdiff.C
+++ misc/fdist/qdiff.C
@@ -16,19 +16,36 @@ main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc <1)
+	if (argc <2)
 	{
-		fprintf (stderr, "Usage: %s\n", argv[0]);
+		fprintf (stderr, "Usage: %s <epsilon>\n", argv[0]);
 		exit (1);
 	}
+	double eps = atof(argv[1]);
 
    ContinuedFraction f;
 
-	double rt = 0.5 * sqrt(2.0);
-  	// f.SetRatio (n, (unsigned long) RAND_MAX);
-	f.set(rt);
-  	double x = f.ToFarey (); 
+	// double rt = 0.5 * sqrt(2.0);
 
-	printf("its %g\n", x);
+	// Golden ratio
+	double rt = 0.5 * (sqrt(5.0) - 1.0);
+	rt = 1.0-rt;
+
+  	// f.SetRatio (n, (unsigned long) RAND_MAX);
+	f.SetReal(rt);
+  	double cent = f.ToFarey (); 
+
+	f.SetReal(rt-eps);
+  	double lo = f.ToFarey (); 
+
+	f.SetReal(rt+eps);
+  	double hi = f.ToFarey (); 
+
+	double dlo = (cent-lo) / eps;
+	double dhi = (hi-cent) / eps;
+
+	double disc = dhi-dlo;
+
+	printf("its %g   %g   %g   disc=%g\n", cent, dlo, dhi, disc);
 }
 
