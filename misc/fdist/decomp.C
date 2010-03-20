26318d552bb23de79122e274d0316bf87ffc2781
linas <>
2010-03-20 03:10:22 +0000

    fill in the details of the implementation

--- misc/fdist/decomp.C
+++ misc/fdist/decomp.C
@@ -6,6 +6,10 @@
  * March 2010
  */
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 double triangle(double x)
 {
 	x -= floor(x);
@@ -17,7 +21,7 @@ double triangle(double x)
 double tagaki(double w, double x)
 {
 	int i;
-	double tn = 1.0
+	double tn = 1.0;
 	double wn = 1.0;
 	double acc = 0.0;
 
@@ -29,4 +33,43 @@ double tagaki(double w, double x)
 	}
 
 	return acc;
+}
+
+double decomp(double w, double x)
+{
+	int l;
+
+	double acc = 0.0;
+	for (l=1; l<2000; l++)
+	{
+		double al = 1.0 / ((double) l);
+		double tlp1 = 2*l+1;
+		acc += al * takagi(w, tlp1 * x);
+	}
+
+	return acc;
+}
+
+int main(int agrc, char * argv[])
+{
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <w>\n", argv[0]);
+		exit(1);
+	}
+	double w = atof(argv[1]);
+
+	int nsteps = 600;
+	int i;
+	for (i=0; i<nsteps)
+	{
+		double x = ((double) i) / ((double) nsteps);
+
+		double y = decomp(x);
+
+		printf("%d	%g	%g\n", i, x, y);
+	}
+
+	return 0;
 }
