cb6d30f9b95979005eb0d01be7f72e6f0b88fd53
linas <>
2010-03-22 15:39:03 +0000

    another attempt

--- misc/fdist/taulim.C
+++ misc/fdist/taulim.C
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 double triangle(double x)
 {
 	x -= floor(x);
@@ -17,6 +18,7 @@ double triangle(double x)
 	return 2.0-2.0*x;
 }
 
+#if MONOSUM
 double sum(int k, int n, int lmax)
 {
 	int l;
@@ -35,21 +37,123 @@ double sum(int k, int n, int lmax)
 	acc *= n+1;
 	return acc;
 }
+#endif
+
+#if ALLSUM
+double delta (int l)
+{
+	// return pow(0.5, l) / ((double) (2*l+1));
+	double d = 1.0 / ((double) (2*l+1));
+	d = pow (d, 1.1);
+	return d;
+}
+
+double delta_sum(int lambda, int p)
+{
+	if (p>30) return delta(lambda);
+
+	int tp = 1<<p;
+	int m = 0;
+	double acc =0.0;
+	while(1)
+	{
+		double term = delta(lambda + m * tp);
+		acc += term;
+		m ++;
+		if (fabs(term/acc) < 1.0e-10) break;
+	}
+	return acc;
+}
+
+double lambda_sum(int p)
+{
+	int tp = 1<<p;
+	double otp = 1.0 / ((double) tp);
+
+	double acc = 0.0;
+	int lambda;
+	int smax = p/2 -1;
+	for (lambda = 0; lambda < smax; lambda++)
+	{
+		double tlp1 = 2*lambda+1;
+		acc += triangle(tlp1*otp) * delta_sum(lambda, p);
+	}
+	return acc;
+}
+
+double w_sum(int n, double w)
+{
+	int k;
+	double acc = 0.0;
+	double wk = 1.0;
+	for (k=0; k<n-1; k++)
+	{
+		acc += wk * lambda_sum(n-k);
+		wk *= 0.5*w;
+	}
+
+	acc *= n+1;
+	acc *= w-1.0;
+	acc *= 0.25;
+
+	return acc;
+}
+#endif
+
+double tw(double w, int l, double x)
+{
+	int k;
+	double tlp1 = 2*l+1;
+	double tn = x;
+	double wh = 1.0;
+	double acc = 0.0;
+	for (k=0; k<n; k++)
+	{
+		acc += wh * triangle(tlp1 * tn);
+
+		tn *= 2.0;
+		wh *= 0.5 * w;
+	}
+
+	return acc;
+}
+
+double dual(double w, int l, int n)
+{
+	int tpn = 1<<n;
+	double x = 1.0 / ((double) tpn);
+
+	double v = tw(w,l,x);
+	v /= 2*l+1;
+	v *= n+1;
+	// v *= 0.25 * (w-1.0);
+	return v;
+}
 
 main(int argc, char * argv[])
 {
-	int n;
+	int n, l;
 	int k = 2;
 
-	k = atoi(argv[1]);
-	int lmax = atoi(argv[2]);
+	// k = atoi(argv[1]);
+	// int lmax = atoi(argv[2]);
+	double w = atof(argv[1]);
 
-	for (n=1; n<40; n++)
+	// for (n=1; n<30; n++)
+	for (l=0; l<600; l++)
 	{
-		double y = sum(k,n,lmax);
+		// double y = sum(k,n,lmax);
+		// double y = w_sum(n,w);
 
-		printf ("its %d sum=%g\n", n, y);
-
+		printf ("%d", l);
+		for (n=2; n<10; n++)
+		{
+			double y = dual(w,l, n);
+			printf ("\t%g", y);
+		}
+		printf("\n");
+		fflush(stdout);
+		// printf ("its %d sum=%g\n", n, y);
 	}
 
 }
