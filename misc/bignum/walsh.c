4be2929aa04703cbbfded4b0765ae3dc7793e247
linas <>
2010-05-01 21:48:45 +0000

    mplement steppy n

--- misc/bignum/walsh.c
+++ misc/bignum/walsh.c
@@ -10,7 +10,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void step(mpf_t result, mpf_t x)
+/**
+ * Step function, returns +1 if x< 1/2 and returns -1 if x> 1/2
+ */
+void step_1(mpf_t result, mpf_t x)
 {
 	static int is_init=0;
 	static mpf_t half;
@@ -23,13 +26,36 @@ void step(mpf_t result, mpf_t x)
 		mpf_div_ui (half, half, 2);
 	}
 
-	if (0 < mpf_cmp(x, half))
+	if (0 < mpf_cmp(half, x))
 	{
 		mpf_set_ui(result, 1);
 		return;
 	}
 	
 	mpf_set_si(result, -1);
+}
+
+/**
+ * returns square wave of frequency 2^(n-1)
+ * n must be less than 32/64
+ * x must be in range of zero, one
+ */
+void step_n(mpf_t result, mpf_t x, int n)
+{
+	unsigned long tp;
+
+	if (1 == n)
+	{
+		step_1(result, x);
+		return;
+	}
+
+	tp = 1<<(n-1);
+	mpf_set_ui(result, tp);
+	mpf_mul(result, result, x); 
+	tp = mpf_get_ui(result);
+	mpf_sub_ui(result, result, tp);
+	step_1(result, result);
 }
 
 int main (int argc, char * argv[])
@@ -64,7 +90,7 @@ int main (int argc, char * argv[])
 	mpf_set_ui(x, 0);
 	for (i=0; i<npts; i++)
 	{
-		step(y, x);
+		step_n(y, x, 3);
 
 		x_f = mpf_get_d(x);
 		y_f = mpf_get_d(y);
@@ -73,4 +99,6 @@ int main (int argc, char * argv[])
 
 		mpf_add(x, x, step);
 	}
+
+	return 0;
 }
