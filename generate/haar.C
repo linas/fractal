2d3962a7c243a7a39828130700e0a20c67d0256e
linas <>
2010-11-15 03:14:20 +0000

    base haar code template

--- generate/haar.C
+++ generate/haar.C
@@ -1,83 +1,22 @@
 /*
- * gamma.C
+ * haar.C
  *
- * High-precison Gamma function, using the 
- * Gnu Multiple-precision library.
+ * Har wavelets on complex plane
  *
- * Linas Vepstas December 2006
+ * Linas Vepstas November 2010
  */
 
-#include <gmp.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "brat.h"
-#include "coord-xforms.h"
-#include "../misc/bignum/zmp.h"
 
 /* ======================================================================= */
 
-static cpx_t gam, z;
-static int prec;
-
-static void psi_init (void)
+	
+static double haar (double re_q, double im_q, int itermax, double param)
 {
-	/* the decimal precison (number of decimal places) */
-	prec = 40;
-
-	/* compute number of binary bits this corresponds to. */
-	double v = ((double) prec) *log(10.0) / log(2.0);
-
-	/* the variable-precision calculations are touchy about this */
-	/* XXX this should be stirling's approx for binomial */ 
-	int bits = (int) (v + 30);
-	
-	/* Set the precision (number of binary bits) */
-	mpf_set_default_prec (bits);
-	
-	cpx_init (gam);
-	cpx_init (z);
-}
-	
-static double cgamma (double re_q, double im_q, int itermax, double param)
-{
-	static int init = 0;
-	if (!init) {psi_init(); init=1; }
-		  
-#if 0
-	// double mag = 1.0 - sqrt (re_q*re_q + im_q*im_q);
-	double mag = 1.0 - (re_q*re_q + im_q*im_q);
-
-	if (mag <= 0.03) return 0.0;
-
-	double re_z = re_q / mag;
-	double im_z = im_q / mag;
-#endif
-
-	double re_z = re_q;
-	double im_z = im_q;
-
-#define USE_POINCARE_DISK
-#ifdef USE_POINCARE_DISK
-	double mag = (re_q*re_q + im_q*im_q);
-	if ((mag >= 0.995) && (mag <=1.005)) return 0.0;
-	poincare_disk_to_plane_coords (re_q, im_q, &re_z, &im_z);
-#endif
-
-	/* avoid hang evaluating near infty */
-	mag = (re_z*re_z + im_z*im_z);
-	if (mag >= 10100.97) return 0.0;
-	
-	cpx_set_d (z, re_z, im_z);
-
-	// printf ("duude compute %g  %g \n", re_z, im_z);
-	cpx_gamma (gam, z, prec);
-	// cpx_exp (gam, z, prec);
-
-	double frea = mpf_get_d (gam[0].re);
-	double fima = mpf_get_d (gam[0].im);
-
 	double phase = atan2 (fima, frea);
 
 	phase += M_PI;
@@ -85,6 +24,6 @@ static double cgamma (double re_q, double im_q, int itermax, double param)
 	return phase;
 }
 
-DECL_MAKE_HEIGHT(cgamma);
+DECL_MAKE_HEIGHT(haar);
 
 /* --------------------------- END OF LIFE ------------------------- */
