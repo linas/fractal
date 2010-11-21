2eb08c8aa8a92f19fe47ca1dd804c0eed5ffab0a
linas <>
2010-11-21 17:07:12 +0000

    fixes

--- misc/entropy/ornstein.pl
+++ misc/entropy/ornstein.pl
@@ -6,16 +6,12 @@
 $gkw = 0.0;
 $dyadic = 0.0;
 
+my $tn = 1.0;
 for ($n=1; $n<40; $n++)
 {
-	$dyadic += $n / pow(2.0, $n);
+	$dyadic += $n / $tn;
+	$tn *= 2.0;
 }
 $dyadic *= log(2.0);
 print "Dyadic entropy is $dyadic\n";
 
-for ($n=1; $n<80000; $n++)
-{
-	$gkw += log($n) / $n;
-}
-
-print "GKW entropy is $gkw\n";
