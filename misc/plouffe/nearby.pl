8187ed53b81af7b21096848af51c67e95b74e926
linas <>
2010-11-23 16:03:53 +0000

    tweaks

--- misc/plouffe/nearby.pl
+++ misc/plouffe/nearby.pl
@@ -2,20 +2,30 @@
 #
 # Linas Novemver 2010
 #
+
+use POSIX qw/floor/;
+
 $fp = 3.14159265358979;
 $fp *= $fp;
 $fp *= 4;
 
 $best = 0.99;
 
-for ($n=2; $n < 1000000; $n++)
+for ($n=5; $n < 2000000; $n++)
 {
 	$m = exp($fp / log($n));
 	$fm = $m - floor($m);
-	if ($fm < $est)
+	if ($fm < $best)
 	{
 		$best = $fm;
-		print "$n  $m  $fm\n";
+
+		$m = floor($m);
+		$x = log($n)/(2*3.14159265358979);
+		$y = (2*3.14159265358979) / log($m);
+		$d = sqrt($x*$y);
+		$en = exp((2*3.14159265358979)*$d);
+		$em = exp((2*3.14159265358979)/$d);
+		print "$n  $m  $fm $d \t$en $em \n";
 	}
 }
 
