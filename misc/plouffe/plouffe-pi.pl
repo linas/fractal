7ec146bcd37b509510acd7d271dca926da47c3b6
linas <>
2010-11-24 04:16:18 +0000

    more checksums

--- misc/plouffe/plouffe-pi.pl
+++ misc/plouffe/plouffe-pi.pl
@@ -6,8 +6,8 @@
 #
 
 $pi = 3.14159265358979;
-$q = exp(2*$pi);
-$qn = $q;
+$q = exp($pi);
+$qn = $q * $q;
 
 $acc = 7*$pi*$pi*$pi / 180;
 
@@ -15,7 +15,27 @@ for($n=1; $n<20; $n++)
 {
 	$term = $n*$n*$n *($qn -1);
 	$acc -= 2/$term;
-	$qn *= $q;
+	$qn *= $q*$q;
 }
 
 print "Its $acc\n";
+
+# -------------------------------------
+$qn = $q;
+$qfn = $q*$q*$q*$q;
+
+$acc = 9*$pi*$pi*$pi / 900;
+
+for($n=1; $n<20; $n++)
+{
+	$term = 4/($qn -1) + 1/($qfn -1);
+	$term /= $n*$n*$n;
+	$acc -= 0.4 * $term;
+	$qn *= $q;
+	$qfn *= $q*$q*$q*$q;
+}
+
+print "Its $acc\n";
+
+
+
