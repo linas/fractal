d31e3b2637c3757eb714d41627956f38c8b42456
linas <>
2010-11-28 20:59:48 +0000

    more implementation

--- misc/num-theory/cat-algebra.c
+++ misc/num-theory/cat-algebra.c
@@ -13,7 +13,52 @@
 
 #include "moebius.h"
 
-	double fk = moebius_mu(k);
+int zee(unsigned int i, unsigned int j)
+{
+	if (j%i == 0) return 1;
+	return 0;
+}
+
+int en(unsigned int i, unsigned int j)
+{
+	if (i==j) return 0;
+	return -zee(i,j);
+}
+
+int en_pow_k(unsinged int k, unsigned int i, unsigned int j)
+{
+	unsgined int r;
+	int dot = 0;
+
+	if (k==0) return (i==j)?1:0;
+	if (k==1) return en(i,j);
+	
+	/* recursive matrix product */
+	dot = 0;
+	for(r=1; r<j; r++)
+	{
+		/* N(i,j) == 0 when j <= i*/
+		/* But this is a very conservative estimate, since N^k
+		 * flattens out even faster */
+		dot += en_pow_k(k-1, i, r) * en(r, j);
+	}
+
+	return dot;
+}
+
+int mu(unsigned int i, unsigned int j)
+{
+	unsigned int k;
+
+	int sum = 0;
+	for(k=0; k<j; k++)
+	{
+		sum += en_pow_k(k,i,j);
+	}
+
+	return sum;
+}
+
 
 int main (int argc, char * argv[])
 {
@@ -25,5 +70,6 @@ int main (int argc, char * argv[])
 	int nbins = atoi (argv[1]);
 	int kmax = atoi(argv[2]);
 
+	double fk = moebius_mu(k);
 	rebin (nbins, kmax);
 }
