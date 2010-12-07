2a36acdf255beb3abaf7554c418cbda543e4baae
linas <>
2010-12-07 03:00:36 +0000

    initial checkin

---
+++ misc/yarh/newton-series.c
@@ -0,0 +1,12 @@
+/*
+ * newton-series.c
+ *
+ * Newton-series summation of the continued fraction
+ * riemann integral. For S_12 only.
+ *
+ * Linas Vepstas December 2010
+ */
+
+#include <math.h>
+
+
