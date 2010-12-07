462cd8d66045a0b2c4c86d48044cce0cff906d8d
linas <>
2010-12-07 04:25:58 +0000

    more stuff

--- misc/yarh/newton-series.c
+++ misc/yarh/newton-series.c
@@ -12,10 +12,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-complex alpha(complex ess, int a1max, int a2max) 
+complex alpha(complex ess, complex *betarr, complex *gamarr,  int kmax, int a1max, int a2max) 
 {
-	unsigned int na1, na2;
-	complex sum = 0.0;
+	unsigned int k, na1, na2;
+	complex alpha_sum = 0.0;
 
 	for (na1=1; na1<a1max; na1++)
 	{
@@ -25,30 +25,69 @@ complex alpha(complex ess, int a1max, int a2max)
 
 			double a1 = na1;
 			double a2 = na2;
-			double c = (a1 * a2 + 1.0) * (a2 - a1);
-			double d = 1.0 + a2 * (a1 - a2);
+			double b = a1 - a2;
+			double c = - (a1 * a2 + 1.0) * b;
+			double d = 1.0 + a2 * b;
 			double xlo = a2 / (1.0 + a1 * a2);
 			double xhi = (1.0 + a2) / (1.0 + a1 + a1 * a2);
-			complex beta = - ((double) d) / ((double) c);
+			double greb = - d / c;
+			complex scale = cpow(greb, ess-1.0) / (c*c);
 
-			complex term = log((xhi+beta) /(xlo+beta));
-			term *= cpow(beta, ess-1.0);
-			term /= c*c ;
-			sum += term;
+			complex term = log((xhi+greb) /(xlo+greb));
+			term *= scale;
+			alpha_sum += term;
+
+			double ced = c/d;
+			double exblo = 1.0 + ced*xlo;
+			double exbhi = 1.0 + ced*xhi;
+
+			double klo = exblo;
+			double khi = exbhi;
+			for (k=1; k<kmax; k++)
+			{
+				term = khi - klo;
+				term *= scale;
+
+				betarr[k] += term;
+				gamarr[k] += term*b*c;
+
+				klo *= exblo;
+				khi *= exbhi;
+			}
 		}
 	}
-	return sum;
+	return alpha_sum;
 }
 
 int main (int argc, char * argv[])
 {
+	int k;
+	int amax = atoi(argv[1]);
+	k = atoi(argv[2]);
 
 	complex ess = 0.5 + I*7.0;
-	complex yo = alpha(ess, 121, 121);
+
+	int kmax = k+1;
+	complex *betarr = (complex *) malloc(kmax * sizeof(complex)); 
+	complex *gamarr = (complex *) malloc(kmax * sizeof(complex)); 
+
+	double last = 0;
+	while (1) {
+	complex yo = alpha(ess, betarr, gamarr, kmax, amax, amax);
 
 	double re = creal (yo);
 	double im = cimag(yo);
 
-	printf("duude %g %g\n", re, im);
+	// printf("duude %d %20.18g %20.18g\n", amax, re, im);
+	//for (k=1; k<kmax; k++)
+	{
+		re = creal (betarr[k]);
+		im = cimag (betarr[k]);
+
+		printf("%d	k=%d %20.18g %20.18g delta=%g\n", amax, k, re, im, re-last);
+		last = re;
+	}
+	amax *= 2;
+	}
 	return 0;
 }
