405099f14a12c4ae8db33070eb4f08519004dfae
linas <>
2010-12-19 20:39:47 +0000

    more hacking

--- misc/yarh/self.c
+++ misc/yarh/self.c
@@ -5,6 +5,8 @@
  * Linas Vepstas Dec 2010
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 void get_mob(int a1, int a2, int *a, int *b, int *c, int *d)
 {
@@ -49,20 +51,25 @@ int det(int M[4][4])
 	return d;
 }
 
-main ()
+main (int argc, char * argv[])
 {
+	int arg = atoi(argv[1]);
+
 	int a,b,c,d;
-	int a2 = 5;
-	get_mob (1,a2, &a, &b, &c, &d);
+	int a1 = 1;
+	int a2 = 3;
+	a2=arg;
+	get_mob (a1, a2, &a, &b, &c, &d);
 
 	int aa,bb,cc,dd;
 
-	int a1;
 	for (a1=a2+1; a1<20; a1++)
 	{
 		get_mob (a1, a2, &aa, &bb, &cc, &dd);
 
 		int m[4][4];
+#define SIMILARITY
+#ifdef SIMILARITY
 		m[0][0] = a-aa;
 		m[0][1] = -cc;
 		m[0][2] = b;
@@ -82,6 +89,28 @@ main ()
 		m[3][1] = c;
 		m[3][2] = -bb;
 		m[3][3] = d-dd;
+#endif
+#ifdef SIMPLE
+		m[0][0] = a;
+		m[0][1] = 0;
+		m[0][2] = b;
+		m[0][3] = 0;
+
+		m[1][0] = 0;
+		m[1][1] = a;
+		m[1][2] = 0;
+		m[1][3] = b;
+
+		m[2][0] = c;
+		m[2][1] = 0;
+		m[2][2] = d;
+		m[2][3] = 0;
+
+		m[3][0] = 0;
+		m[3][1] = c;
+		m[3][2] = 0;
+		m[3][3] = d;
+#endif // SIMPLE
 
 		int dt = det(m);
 
