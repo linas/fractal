7e77f2706fb051f870b0e6baa30bbca1e8b0220b
linas <>
2011-01-22 02:42:15 +0000

    a little bit of modernization

--- tools/src/libs/farey/Prime.h
+++ tools/src/libs/farey/Prime.h
@@ -7,26 +7,18 @@
  * Linas Vepstas 23 January 1994
  */
 
-#include <math.h>
+#ifndef __PRIME_H__
+#define __PRIME_H__
 
-struct Prime {
+struct Prime
+{
    int nprimes;          /* number of primes in table */
    int *primes;          /* pointer to table  */
 
 };
 
-#ifdef ANSI_C
-
 extern struct Prime * CreatePrime (void);
 extern void PrintPrime (struct Prime *self);
 extern int GetPrime (struct Prime *self, int i);
 
-#else
-
-extern struct Prime * CreatePrime ();
-extern void PrintPrime ();
-extern int GetPrime ();
-
-#endif /* ANSI_C */
-
-/* ====================================================== */
+#endif /* __PRIME_H__ */
