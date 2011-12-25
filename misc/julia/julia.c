ba70de54646048fad149fa72a790c9ff2d3b5751
linas <>
2011-12-25 22:15:45 +0000

    add a basic implementation.

--- misc/julia/julia.c
+++ misc/julia/julia.c
@@ -8,12 +8,60 @@
 
 #include <complex.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-complex double julia_point (unsigned int path, complex double c)
+void julia_tree (complex double * tree, int depth, complex double c, complex double w)
 {
-	return c;
+	complex double z = w - c;
+	double r = sqrt(cabs (z));
+	double theta = 0.5 * carg(z);
+
+	complex double v = r * cexp (I*theta);
+
+	int shift = 1<<(depth-1);
+   if (1 == depth)
+	{
+   	tree[0] = v;
+   	tree[1] = -v;
+		return;
+	}
+
+// printf("duude v %g and %g\n", creal(v), cimag(v));
+   julia_tree(tree, depth-1, c, v);
+   julia_tree(&tree[shift], depth-1, c, -v);
+
 }
 
-int main (int agrc, char * argv[])
+complex double * make_tree (int depth, complex double c)
 {
+   // depth is same as width in bits.
+	int tree_width = 1 << depth;
+	complex double *tree = malloc (tree_width * sizeof (complex double));
+
+	julia_tree(tree, depth, c, 0.0);
+
+	return tree;
+}
+
+int main (int argc, char * argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <tree-depth>\n", argv[0]);
+		exit(1);
+	}
+
+	int depth = atoi (argv[1]);
+
+	complex double * tree;
+	tree = make_tree(depth, -0.8+0.6*I);
+
+	int sz = 1<< depth;
+	int i;
+	for (i=0; i<sz; i++)
+	{
+		complex double v = tree[i];
+		printf("%d\t%g\t%g\n", i, creal(v), cimag(v));
+	}
 }
