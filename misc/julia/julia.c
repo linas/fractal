4f95db220a006932f9acde2228d777c8d5ffa5aa
linas <>
2011-12-25 21:29:25 +0000

    initial checkin

---
+++ misc/julia/julia.c
@@ -0,0 +1,19 @@
+
+/*
+ * julia.c
+ * simpel julia set explorer for the mandelbrot bulb
+ *
+ * Linas Vepstas December 2011
+ */
+
+#include <complex.h>
+#include <math.h>
+
+complex double julia_point (unsigned int path, complex double c)
+{
+	return c;
+}
+
+int main (int agrc, char * argv[])
+{
+}
