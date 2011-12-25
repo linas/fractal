c896c313723fd9cae27f781bbc3cbc95739eaa28
linas <>
2011-12-25 23:14:46 +0000

    more games

--- misc/julia/julia.c
+++ misc/julia/julia.c
@@ -11,11 +11,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void julia_tree (complex double * tree, int depth, complex double c, complex double w)
+void julia_tree (complex double * tree, int depth,
+                 complex double c, complex double w,
+                 double rot)
 {
 	complex double z = w - c;
 	double r = sqrt(cabs (z));
 	double theta = 0.5 * carg(z);
+	if (theta > rot) theta -= M_PI;
+	if (theta < rot-M_PI) theta += M_PI;
 
 	complex double v = r * cexp (I*theta);
 
@@ -28,18 +32,18 @@ void julia_tree (complex double * tree, int depth, complex double c, complex dou
 	}
 
 // printf("duude v %g and %g\n", creal(v), cimag(v));
-   julia_tree(tree, depth-1, c, v);
-   julia_tree(&tree[shift], depth-1, c, -v);
+   julia_tree(tree, depth-1, c, v, rot);
+   julia_tree(&tree[shift], depth-1, c, -v, rot);
 
 }
 
-complex double * make_tree (int depth, complex double c)
+complex double * make_tree (int depth, complex double c, double rot)
 {
    // depth is same as width in bits.
 	int tree_width = 1 << depth;
 	complex double *tree = malloc (tree_width * sizeof (complex double));
 
-	julia_tree(tree, depth, c, 0.0);
+	julia_tree(tree, depth, c, 0.0, rot);
 
 	return tree;
 }
@@ -61,16 +65,19 @@ unsigned int bit_reverse (int depth, unsigned int str)
 
 int main (int argc, char * argv[])
 {
-	if (argc != 2)
+	if (argc != 5)
 	{
-		fprintf(stderr, "Usage: %s <tree-depth>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <tree-depth> <re> <im> <rot>\n", argv[0]);
 		exit(1);
 	}
 
 	int depth = atoi (argv[1]);
+	double re = atof (argv[2]);
+	double im = atof (argv[3]);
+	double rot = atof (argv[4]);
 
 	complex double * tree;
-	tree = make_tree(depth, -0.8+0.6*I);
+	tree = make_tree(depth, re+im*I, rot);
 
 	unsigned int sz = 1<< depth;
 	unsigned int i;
@@ -78,8 +85,12 @@ int main (int argc, char * argv[])
 	{
 		unsigned int rev = bit_reverse(depth, i);
 		complex double v = tree[rev];
-		printf("%d\t%d\t%g\t%g\n", i, rev, creal(v), cimag(v));
+		double x = ((double) i) / ((double) sz);
+		printf("%d	%d	%g	%g	%g\n", i, rev, x, creal(v), cimag(v));
 	}
+	double x = 1.0;
+	complex double v = tree[0];
+	printf("%d	%d	%g	%g	%g\n", sz, 0, x, creal(v), cimag(v));
 
 	return 0;
 }
