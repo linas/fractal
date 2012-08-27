1ceea0c75f066bbded6558dda1f470dfb8ff27d6
linas <>
2012-08-27 23:05:51 +0000

    Ue long double for everything

--- misc/geodesic/raytrace.c
+++ misc/geodesic/raytrace.c
@@ -30,10 +30,10 @@ typedef int bool;
 
 typedef struct 
 {
-	double x;   // position
-	double y;
-	double vx;  // velocity
-	double vy;
+	long double x;   // position
+	long double y;
+	long double vx;  // velocity
+	long double vy;
 	char code;
 } ray_t;
 
@@ -52,19 +52,19 @@ typedef struct
  */
 char bounce (const ray_t in, ray_t* out)
 {
-	static double rho = 0.5 * sqrt(3.0);
+	static long double rho = 0.5L * sqrtl(3.0L);
 
 	// Calculate center of circle.  Its at y=0, x=center.
-	double center = in.x + in.y * in.vy / in.vx;
+	long double center = in.x + in.y * in.vy / in.vx;
 	DBG("--------\ncenter at %g\n", center);
 	// Square of the radius of the circle
-	double radius_sq = (in.x - center)*(in.x-center) + in.y*in.y;
+	long double radius_sq = (in.x - center)*(in.x-center) + in.y*in.y;
 
 	// exit_code == 'S' means bottom exit
 	// exit_code == 'T' means right exit
 	// exit_code == 'N' means left exit
 	char exit_code = 'S';
-	double x_exit = (1.0 - radius_sq + center*center) / (2.0 * center);
+	long double x_exit = (1.0L - radius_sq + center*center) / (2.0L * center);
 
 	// intersect is true if the geodesic and the unit circle interest.
 	// If intersect, then can take sqrt to get valid intersection point.
@@ -72,16 +72,16 @@ char bounce (const ray_t in, ray_t* out)
 
 	// bottom_entry is true, if the trajectory came in through the bottom.
 	// bool bottom_entry = (-0.5 < in.x) && (in.x < 0.5);
-	// double rinsq = in.x*in.x + in.y*in.y - 1.0;
+	// long double rinsq = in.x*in.x + in.y*in.y - 1.0;
 	// bool bottom_entry = (rinsq < 1.0e-14);
 	bool bottom_entry = (in.code == 'S');
 
 	// If geodesic and unit circle intersect, then it can still be
 	// a side exit if the intersection point is out of bounds.
-	bool side_exit = (x_exit < -0.5) || (0.5 < x_exit);
+	bool side_exit = (x_exit < -0.5L) || (0.5L < x_exit);
 
 	// First time through is a special case...
-	if (('F' == in.code) && intersect && !side_exit && (0.0 < in.vy))
+	if (('F' == in.code) && intersect && !side_exit && (0.0L < in.vy))
 	{
 #define TRUE 1
 		bottom_entry = TRUE;
@@ -89,37 +89,37 @@ char bounce (const ray_t in, ray_t* out)
 
 	if (!intersect || (intersect && (bottom_entry||side_exit)))
 	{
-		if (in.vx > 0.0)
+		if (in.vx > 0.0L)
 		{
 			// Right side exit, not bottom exit
-			x_exit = 0.5;
+			x_exit = 0.5L;
 			exit_code = 'T';
 		}
 		else 
 		{
 			// Left side exit, not bottom exit
-			x_exit = -0.5;
+			x_exit = -0.5L;
 			exit_code = 'N';
 		}
 	}
 
-	double y_exit = sqrt(radius_sq - (x_exit-center)*(x_exit-center));
+	long double y_exit = sqrtl(radius_sq - (x_exit-center)*(x_exit-center));
 	DBG(" %c exit x=%g y=%g\n", exit_code, x_exit, y_exit);
 	if (('S' != exit_code) && (y_exit < rho))
 	{
-		fprintf(stderr, "Error: bad y value = %g for side exit\n", y_exit);
+		fprintf(stderr, "Error: bad y value = %Lg for side exit\n", y_exit);
 		exit(1);
 	}
 
-	double tan_exit = -(x_exit-center)/y_exit;
-	double vx_exit = sqrt(1.0 / (1.0 + tan_exit*tan_exit));
-	double vy_exit = sqrt (1.0 - vx_exit*vx_exit);
+	long double tan_exit = -(x_exit-center)/y_exit;
+	long double vx_exit = sqrtl (1.0L / (1.0L + tan_exit*tan_exit));
+	long double vy_exit = sqrtl (1.0L - vx_exit*vx_exit);
 	// Copy the sign bit to the velocity.
-	if (tan_exit < 0.0) vy_exit = -vy_exit;
+	if (tan_exit < 0.0L) vy_exit = -vy_exit;
 
 	// If incoming velocity was left-pointing, then so is the outgoing
 	// velocity.  exit code should be N or S.
-	if (in.vx < 0.0)
+	if (in.vx < 0.0L)
 	{
 		vx_exit = -vx_exit;
 		vy_exit = -vy_exit;
@@ -137,14 +137,14 @@ char bounce (const ray_t in, ray_t* out)
 	switch (exit_code)
 	{
 		case 'T':
-			out->x = -0.5;
+			out->x = -0.5L;
 			out->y = y_exit;
 
 			out->vx = vx_exit;
 			out->vy = vy_exit;
 			break;
 		case 'N':
-			out->x = 0.5;
+			out->x = 0.5L;
 			out->y = y_exit;
 
 			out->vx = vx_exit;
@@ -155,7 +155,7 @@ char bounce (const ray_t in, ray_t* out)
 			out->x = -x_exit;
 			out->y = y_exit;
 
-			double deno = 2.0 * center * vx_exit;
+			long double deno = 2.0L * center * vx_exit;
 			out->vx = -vx_exit + x_exit * deno;
 			out->vy = vy_exit - y_exit * deno;
 			break;
@@ -185,7 +185,7 @@ void sequence(ray_t in, geodesic_t* geo)
 	in.code = 'F'; // Sanity check
 	geo->tangent = in;
 
-	DBG("==========\nstart %g %g\n", in.vx, in.vy);
+	DBG("==========\nstart %Lg %Lg\n", in.vx, in.vy);
 	int i;
 	for (i=0; i<SEQLEN; i++)
 	{
