a63a3cb4734795565b9e19a791ede80830d23348
linas <>
2014-06-02 02:45:25 +0000

    initial checkin

--- misc/alternate/alt-binary.C
+++ misc/alternate/alt-binary.C
@@ -7,20 +7,46 @@
  */
 
 #include <math.h>
+#include <stdio.h>
 
 #define BITLEN 56
 typedef char[BITLEN] bitstring;
 
-bitstring long to_bits(double x)
+bitstring to_bits(double x)
 {
 	bitstring bits;
 	x -= floor(x);
 
 	for (int i=0; i<BITLEN; i++)
 	{
-		if (0.5 < x) bits[i] = 1;
+		if (0.5 < x) { bits[i] = 1; x -= 0.5; }
 		else bits[i] = 0;
+		x *= 2.0;
 	}
 
 	return bits;
 }
+
+double from_bits(bitstring bits)
+{
+	double val = 0.0;
+	double bv = 0.5;
+	for (int i=0; i<BITLEN; i++)
+	{
+		if (bits[i]) val += bv;
+		bv *= 0.5;
+	}
+	return val;
+}
+
+main(int argc, char* argv[])
+{
+
+	double x = 0.1233456789;
+	bitstring bs = to_bits(x);
+	double y = from_bits(bs);
+
+	printf("duue its %g\n", y);
+}
+
+
