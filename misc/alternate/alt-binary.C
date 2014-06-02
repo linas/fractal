0a0336dde3cb500e2ee9bfb686f770330fc21c78
linas <>
2014-06-02 03:02:54 +0000

    near-final form

--- misc/alternate/alt-binary.C
+++ misc/alternate/alt-binary.C
@@ -36,15 +36,42 @@ double from_bits(bitstring bits)
 	return val;
 }
 
+void drop_bit(const bitstring &from, bitstring &to, int place)
+{
+	for (int i=0, j=0; i<BITLEN; i++, j++)
+	{
+		if (i == place) i++;
+		to[j] = from[i];
+	}
+}
+
+double alternate(double x)
+{
+	bitstring bs;
+	to_bits(bs, x);
+	double acc = 0.0;
+	double sgn = 1.0;
+
+	for (int i=0; i<BITLEN; i++)
+	{
+		bitstring drop;
+		drop_bit(bs, drop, i);
+		double y = from_bits(drop);
+		acc += sgn * y;
+		sgn = - sgn;
+	}
+
+	return acc;
+}
+
 main(int argc, char* argv[])
 {
 
-	double x = 0.1233456789;
-	bitstring bs;
-	to_bits(bs, x);
-	double y = from_bits(bs);
-
-	printf("duue its %g\n", y);
+	for (double x=0.0; x<1.0; x+=0.00312345678)
+	{
+		double y = alternate(x);
+		printf("%16.14g	%16.14g\n", x, y);
+	}
 }
 
 
