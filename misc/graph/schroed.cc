781b51e860d4f31ff7175f2f910cc257d9ff3913
Linas Vepstas <linasvepstas@gmail.com>
2014-11-23 11:00:04 -0600

    ongoing work on schrodinger

--- misc/graph/schroed.cc
+++ misc/graph/schroed.cc
@@ -5,21 +5,41 @@
 //
 // Linas Vepstas 23 Nov 2014
 //
+#include <complex.h>
+#include <math.h>
+#include <stdio.h>
 
-#define LEN 1000
-double wavefn[LEN];
+#include "moebius.h"
+#include "totient.h"
 
-double pot[LEN];
+#define LEN 130
+long double complex wavefn[LEN];
+
+long double pot[LEN];
 
 void init()
 {
 	int i;
 	for (i=0; i<LEN; i++) wavefn[i] = 0.0;
+	for (i=0; i<LEN; i++) pot[i] = totient_phi(i);
+	for (i=0; i<LEN; i++) printf("potential is %d %Lf\n", i, pot[i]);
+
+   wavefn[LEN-2] = 0.001;
 }
 
+void solve(long double energy)
+{
+	int i;
+	for (i=LEN-2; 0 < i; i--)
+	{
+		long double ct = 2.0 + pot[i] - energy;
+		wavefn[i-1] = ct * wavefn[i] - wavefn[i+1];
+	printf("duude fn= %Lg\n", creall (wavefn[i-1]));
+	}
+}
 
 main()
 {
-
 	init();
+   solve(9.0);
 }
