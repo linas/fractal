cbbfc084340eaeeb52cee02568de334f42ea0631
Linas Vepstas <linasvepstas@gmail.com>
2014-11-23 11:13:24 -0600

    more work

--- misc/graph/schroed.cc
+++ misc/graph/schroed.cc
@@ -8,38 +8,58 @@
 #include <complex.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "moebius.h"
 #include "totient.h"
 
-#define LEN 130
-long double complex wavefn[LEN];
+long double complex *wavefn;
 
-long double pot[LEN];
+long double *pot;
 
-void init()
+void init(size_t len)
 {
-	int i;
-	for (i=0; i<LEN; i++) wavefn[i] = 0.0;
-	for (i=0; i<LEN; i++) pot[i] = totient_phi(i);
-	for (i=0; i<LEN; i++) printf("potential is %d %Lf\n", i, pot[i]);
+	size_t i;
 
-   wavefn[LEN-2] = 0.001;
+	wavefn = (long double complex *) malloc(len * sizeof(long double complex));
+	pot = (long double *) malloc(len * sizeof(long double));
+	for (i=0; i<len; i++) wavefn[i] = 0.0;
+	for (i=0; i<len; i++) pot[i] = totient_phi(i);
+	for (i=0; i<len; i++) printf("potential is %zd %Lf\n", i, pot[i]);
+
+   wavefn[len-2] = 0.001;
 }
 
-void solve(long double energy)
+void solve(size_t len, long double energy)
 {
-	int i;
-	for (i=LEN-2; 0 < i; i--)
+	size_t i;
+	for (i=len-2; 0 < i; i--)
 	{
 		long double ct = 2.0 + pot[i] - energy;
 		wavefn[i-1] = ct * wavefn[i] - wavefn[i+1];
-	printf("duude fn= %Lg\n", creall (wavefn[i-1]));
 	}
+
+	// Now renormalize
+	long double acc = 0.0;
+	for (i=0; i<len; i++) acc += creall(wavefn[i] * conjl(wavefn[i]));
+
+	acc = sqrtl(1.0 / acc);
+
+	for (i=0; i<len; i++) wavefn[i] *= acc;
+	for (i=0; i<len; i++) 
+		printf("wavefn= %zd %Lg\n", i, creall (wavefn[i]));
 }
 
-main()
+main(int argc, char* argv[])
 {
-	init();
-   solve(9.0);
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <len>\n", argv[0]);
+		exit (1);
+	}
+	int len = atoi(argv[1]);
+	printf("Length=%d\n", len);
+
+	init(len);
+   solve(len, 9.0);
 }
