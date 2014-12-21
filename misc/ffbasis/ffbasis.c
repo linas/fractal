143d217900f719004cb982f3086c761a41a55a52
Linas Vepstas <linasvepstas@gmail.com>
2014-12-21 16:08:57 -0600

    misc hacking

--- misc/ffbasis/ffbasis.c
+++ misc/ffbasis/ffbasis.c
@@ -16,9 +16,12 @@ long long int ortho(int p, int k)
 {
 	int n;
 	long long int sum = 0LL;
-	for (n=k; n<=p; n++)
+	// for (n=k; n<=p; n++)
+	// for (n=0; n<=p; n++)
+	for (n=0; n<k; n++)
 	{
 		sum += stirling_first(p, n) * stirling_second(n, k);
+		// sum += stirling_second(p, n) * stirling_first(n, k);
 	}
 	return sum;
 } 
@@ -28,10 +31,11 @@ main (int argc, char * argv[])
 {
 	int p, k;
 
-#define MAX 30
+#define MAX 24
 	for (k=1; k<MAX ; k++)
 	{
-		for (p=k; p<MAX; p++)
+		// for (p=k; p<MAX; p++)
+		for (p=1; p<MAX; p++)
 		{
 			long long int sum = ortho(p, k);
 			if (p != k && 0 != sum) printf("duude k=%d p=%d sum=%lld\n", k, p, sum);
