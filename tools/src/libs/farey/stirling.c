44eabd5993619d2bc8542bb5ec4aa5a8b77d00f8
Linas Vepstas <linasvepstas@gmail.com>
2014-12-20 23:38:06 -0600

    worming stirling number of second kind

--- tools/src/libs/farey/stirling.c
+++ tools/src/libs/farey/stirling.c
@@ -18,16 +18,22 @@ long long int stirling_first (int n, int k)
 	if (0 == n && 0 == k) return 1LL;
 	if (0 == n && 0 < k) return 0LL;
 	if (0 == k && 0 < n) return 0LL;
+	if (n == k) return 1LL;
 
-	assert(0 < k && 0 < n);
+	assert(0 < k && 0 < n && k <= n);
 	return ((long int) (1-n)) * stirling_first(n-1, k) + stirling_first(n-1, k-1);
 }
 
 long long int stirling_second (int n, int k)
 {
-	if (1 == k) return 1;
-	if (1 == n) return 1;
-	return 0;
+	if (0 == n && 0 == k) return 1LL;
+	if (0 == n && 0 < k) return 0LL;
+	if (0 == k && 0 < n) return 0LL;
+	if (1 == k) return 1LL;
+	if (k == n) return 1LL;
+
+	assert(0 < k && 0 < n && k <= n);
+	return ((long int) (k)) * stirling_second(n-1, k) + stirling_second(n-1, k-1);
 }
 
 
@@ -41,7 +47,8 @@ int main()
 	int n,k;
 	for (n=0; n<10; n++) {
 		for (k=0; k<=n; k++) {
-			printf("First: %d %d %lld\n", n, k, stirling_first(n,k));
+			// printf("First: %d %d %lld\n", n, k, stirling_first(n,k));
+			printf("Second: %d %d %lld\n", n, k, stirling_second(n,k));
 		}
 		printf("---\n");
 	}
