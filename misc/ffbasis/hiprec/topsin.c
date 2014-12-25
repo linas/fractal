4f09d924f3c10ee968c5cfb3560f7db3b7f2776b
Linas Vepstas <linasvepstas@gmail.com>
2014-12-25 16:23:42 -0600

    start work on arbitrary-precison version

---
+++ misc/ffbasis/hiprec/topsin.c
@@ -0,0 +1,20 @@
+/**
+ * Arbitrary-precision expansion of the topsin series.
+ *
+ * December 2014
+ */
+
+#include "anant/mp-consts.h"
+
+
+mpf_t topsin_series(unsigned int k)
+{
+	mpf_t sum;
+
+	// mpz_bin_uiui
+}
+
+
+int main()
+{
+}
