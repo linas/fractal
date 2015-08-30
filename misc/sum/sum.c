89e3ca18a3abf6aa16097dc2273e2a99956c07c0
Linas Vepstas <linasvepstas@gmail.com>
2015-08-29 22:20:44 -0500

    Make intesting graphs

--- misc/sum/sum.c
+++ misc/sum/sum.c
@@ -7,6 +7,7 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define LEN 60
 
@@ -15,7 +16,7 @@ void float_to_bitstring(double x, int bits[LEN])
 	x -= floor(x);
 	for (int i=0; i<LEN; i++)
 	{
-		if (0.5 < x) bits[i] = 1;
+		if (0.5 <= x) bits[i] = 1;
 		else bits[i] = 0;
 		x *= 2.0;
 		x -= floor(x);
@@ -37,6 +38,17 @@ double bitstring_to_float(int bits[LEN])
 void add_bitstrings(int sum[LEN], int a[LEN], int b[LEN])
 {
 	int carry = 0;
+	for (int i=LEN-1; i>= 0; i--)
+	{
+		int s = a[i] + b[i] + carry;
+		if (s < 2) { sum[i] = s; carry = 0; }
+		else { sum[i] = s-2; carry = 1; }
+	}
+}
+
+void reverse_add(int sum[LEN], int a[LEN], int b[LEN])
+{
+	int carry = 0;
 	for (int i=0; i<LEN; i++)
 	{
 		int s = a[i] + b[i] + carry;
@@ -47,22 +59,53 @@ void add_bitstrings(int sum[LEN], int a[LEN], int b[LEN])
 
 int main (int argc, char * argv[])
 {
-	for (double x=0.0; x<1.0; x+= 0.123)
+#if 0
+	for (double x=0.0; x<1.0; x+= 0.1)
 	{
 		int xbits[LEN];
 		float_to_bitstring(x, xbits);
 
-		for (double y=0.0; y<1.0; y+= 0.08123)
+		for (double y=0.0; y<1.0; y+= 0.08)
 		{
 			int ybits[LEN];
 			float_to_bitstring(y, ybits);
 
 			int bsum[LEN];
-			add_bitstrings(bsum, xbits, ybits);
+			// add_bitstrings(bsum, xbits, ybits);
+			reverse_add(bsum, xbits, ybits);
 			double sum = bitstring_to_float(bsum);
 
-			printf("duude %g %g %g \n", x, y, sum-x-y);
+			double mod = x+y;
+			if (1.0 < mod) mod -= 1.0;
+			printf("duude %g + %g  = %g \n", x, y, sum);
 		}
+	}
+#endif
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <x>\n", argv[0]);
+		exit (-1);
+	}
+	double x = atof(argv[1]);
+
+	int xbits[LEN];
+	float_to_bitstring(x, xbits);
+
+	double ystep = 1.0 / 3197.0;
+	for (double y=0.0; y<1.0; y+= ystep)
+	{
+		int ybits[LEN];
+		float_to_bitstring(y, ybits);
+
+		int bsum[LEN];
+		// add_bitstrings(bsum, xbits, ybits);
+		reverse_add(bsum, xbits, ybits);
+		double sum = bitstring_to_float(bsum);
+
+		double mod = x+y;
+		if (1.0 < mod) mod -= 1.0;
+		printf("%g\t%g\t%g\n", x, y, sum);
 	}
 
 }
