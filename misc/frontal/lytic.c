184467e94d00498bb25ebc1023208816efccef9a
Linas Vepstas <linasvepstas@gmail.com>
2015-09-07 15:32:36 -0500

    Begin the tomfoolery

---
+++ misc/frontal/lytic.c
@@ -0,0 +1,19 @@
+/*
+ *
+ * Numerical exploration of the analytic continuation
+ * given in frontal.lyx
+ *
+ * September 2015
+ */
+
+#include "../sum/sum.h"
+
+int bitcount(int k, int bits[LEN])
+{
+
+	return 0;
+}
+
+main()
+{
+}
