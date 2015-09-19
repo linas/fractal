85c44e4a5e6a7a37e02e24c67eb92a8f5a067e09
Linas Vepstas <linasvepstas@gmail.com>
2015-09-19 16:14:33 -0500

    typos

--- tools/src/libs/farey/question.h
+++ tools/src/libs/farey/question.h
@@ -5,7 +5,7 @@
  *
  * Minkowski question mark function
  * (wrapper around the C++ code)
- * 
+ *
  * HISTORY:
  * Linas Vepstas September 2006
  */
@@ -22,12 +22,12 @@ double fquestion_mark (double);
  *
  * This function maps the unit interval, expressed as a dyadic
  * tree, to the full stern brocot tree.  The input is 0<x<1.
- * Perform binary expansion of x. Use L,R for binary digits 0,1. 
- * Concatenate the L and R to form a matrix. Padd on the right with
- * zeros. The resulting fractinal linear transform will map the 
+ * Perform binary expansion of x. Use L,R for binary digits 0,1.
+ * Concatenate the L and R to form a matrix. Pad on the right with
+ * zeros. The resulting fractional linear transform will map the
  * entire complex plane to b/d. This function returns b/d.
  *
- * Basically, this function maps the dyadic tree to the 
+ * Basically, this function maps the dyadic tree to the
  * Stern-Brocot tree.
  */
 long double dyadic_to_stern_brocot (long double x);
@@ -35,7 +35,7 @@ long double dyadic_to_stern_brocot (long double x);
 /**
  * question_inverse - return the inverse of the question mark function
  *
- * This implements a rapid algorithm to compute the inverse of 
+ * This implements a rapid algorithm to compute the inverse of
  * the question mark function.
  */
 long double question_inverse (long double x);
