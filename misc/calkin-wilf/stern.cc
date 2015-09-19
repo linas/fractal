57cdf4820a412cc5878b04e14c43ce1237e146dd
Linas Vepstas <linasvepstas@gmail.com>
2015-09-19 16:43:45 -0500

    All new question

---
+++ misc/calkin-wilf/stern.cc
@@ -0,0 +1,50 @@
+
+/*
+ * Stern-Brocot tree, new algo
+ * This is the same old question mark, just a different API.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void stern_brocot_tree(unsigned long pos, int level,
+                       unsigned long &p, unsigned long &q)
+{
+	if (0 == level)
+	{
+		if (0 == pos)
+		{
+			p = 0; q = 1; return;
+		}
+		if (1 == pos)
+		{
+			p = 1; q = 1; return;
+		}
+	}
+	if (0 == pos%2)
+		return stern_brocot_tree(pos>>1, level-1, p, q);
+
+	unsigned long pl, ql, pr, qr;
+	stern_brocot_tree(pos>>1, level-1, pl, ql);
+	stern_brocot_tree((pos>>1) + 1, level-1, pr, qr);
+	
+	p = pl + pr;
+	q = ql + qr;
+}
+
+int main(int argc, char *argv[])
+{
+
+	int level = atoi(argv[1]);
+
+	unsigned long p;
+	unsigned long q;
+	double norm = 1.0 / (double)(1<<level);
+	for (int i=0; i<= (1<<level); i++)
+	{
+		stern_brocot_tree(i, level, p, q);
+		double x = i * norm;
+		double y = ((double) p) / (double) q;
+		printf("%d	%lu	%lu	%g	%g\n", i, p, q, x, y);
+	}
+}
