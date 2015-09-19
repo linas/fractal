27eb7c6fdd3ab9a0469547e2e754dab2c309b2d1
Linas Vepstas <linasvepstas@gmail.com>
2015-09-19 16:52:57 -0500

    Add docs

--- misc/calkin-wilf/stern.cc
+++ misc/calkin-wilf/stern.cc
@@ -7,6 +7,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Computes the Stern-Brocot tree.
+ * It takes as input a dyadic fraction pos / 2^level
+ * assuming 0 <= pos <= 2^level.
+ * It returns the corresponding p/q value in p and q.
+ *
+ * In practical terms, given an input x = pos / 2^level
+ * this returns the inverse question-mark, given by
+ * (as per usual) ?(p/q) = pos / 2^level.
+ */
 void stern_brocot_tree(unsigned long pos, int level,
                        unsigned long &p, unsigned long &q)
 {
