41fef4560f9072112c886239ba5743f4e8d8daab
Linas Vepstas <linasvepstas@gmail.com>
2015-09-19 16:10:45 -0500

    Misc fusc updates

--- misc/calkin-wilf/fusc.cc
+++ misc/calkin-wilf/fusc.cc
@@ -3,7 +3,10 @@
  *
  * September 2015
  */
+
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 unsigned long fusc(unsigned long n)
 {
@@ -14,11 +17,51 @@ unsigned long fusc(unsigned long n)
 	return fusc(m) + fusc(m+1);
 }
 
+unsigned long section_sum(int section)
+{
+	unsigned long bot = 1<<section;
+	unsigned long top = 2*bot;
+
+	unsigned long sum = 0;
+	for (unsigned long i=bot; i<= top; i++)
+	{
+		unsigned long fu = fusc(i);
+		sum += fu;
+	}
+	return sum;
+}
+
+void print_sum(int section)
+{
+	unsigned long bot = 1<<section;
+	unsigned long top = 2*bot;
+
+	unsigned long sum = 0;
+	double norm = 1.0 / (double) section_sum(section);
+
+	for (unsigned long i=bot; i<= top; i++)
+	{
+		unsigned long fu = fusc(i);
+		double x = (i - bot) / ((double) bot);
+		sum += fu;
+		double rsum = sum * norm;
+		printf("%lu	%g	%lu	%lu	%g\n", i, x, fu, sum, rsum);
+	}
+}
+
 int main(int argc, char * argv[])
 {
-	for (int i=0; i< 2048; i++)
-	{
-		printf("%lu	%lu\n", i, fusc(i));
-	}
+	int section = atof(argv[1]);
 
+#if 0
+	unsigned long ssum = 0;
+	for (unsigned long i=0; i<= 30; i++)
+	{
+		unsigned long s = section_sum(i);
+		ssum += s;
+		printf("its %lu %lu	%lu\n",i, s, ssum);
+	}
+#endif
+
+	print_sum(section);
 }
