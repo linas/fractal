ef7abd0d403b7dd6f41d46cf352e33588666a8a0
Linas Vepstas <linasvepstas@gmail.com>
2015-09-19 22:13:06 -0500

    continued fracing

--- misc/calkin-wilf/fusc.cc
+++ misc/calkin-wilf/fusc.cc
@@ -12,6 +12,12 @@
 
 /**
  * The "fusc" fuction from the Calkin-Wilf tree.
+ * This gives the denominator of the stern-brocot tree.
+ *
+ * There is nothing (strictly) analogous to it for the numerator
+ * of the stern-brocot tree, because of problematic values at
+ * exact powers-of-two, which must be either 0 or 1 depending
+ * on where in the sequence we are.
  */
 unsigned long fusc(unsigned long n)
 {
@@ -20,6 +26,16 @@ unsigned long fusc(unsigned long n)
 	unsigned long m = n >> 1;
 	if (0 == n%2) return fusc(m);
 	return fusc(m) + fusc(m+1);
+}
+
+unsigned long nusc(unsigned long n)
+{
+	if (0 == n) return 0;
+	if (1 == n) return 0;
+	if (2 == n) return 1;
+	unsigned long m = n >> 1;
+	if (0 == n%2) return nusc(m);
+	return nusc(m+1) + nusc(m);
 }
 
 unsigned long section_sum(int section)
@@ -34,6 +50,26 @@ unsigned long section_sum(int section)
 		sum += fu;
 	}
 	return sum;
+}
+
+void print_bro(int section)
+{
+	unsigned long bot = 1<<section;
+	unsigned long top = 2*bot;
+
+	unsigned long p,q;
+	for (unsigned long i=bot; i<= top; i++)
+	{
+		unsigned long fu = fusc(i);
+		unsigned long nu = nusc(i);
+		stern_brocot_tree(i-bot, section, &p, &q);
+		printf("%lu	%lu	%lu	%lu	%lu\n", i-bot, fu, nu, p, q);
+		if (0 != fu-q)
+		{
+			printf("oooooooo nooooo!\n");
+			exit(1);
+		}
+	}
 }
 
 void print_sum(int section)
@@ -97,5 +133,6 @@ int main(int argc, char * argv[])
 #endif
 
 	// print_sum(section);
-	print_weighted_sum(section);
+	// print_weighted_sum(section);
+	print_bro(section);
 }
