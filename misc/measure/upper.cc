40bf6511d84a8ae29d0a045ba1649aab815f609b
Linas Vepstas <linasvepstas@gmail.com>
2015-09-21 15:45:58 -0500

    Upper bound

--- misc/measure/upper.cc
+++ misc/measure/upper.cc
@@ -13,7 +13,9 @@
 #include <stdlib.h>
 #include "stern.h"
 
-double upper_bound(int lev, int i)
+int maxlev = 0;
+
+double upper_bound(int lowlev, int uplev, int i)
 {
 	unsigned __int128 p, q, pm, qm;
 
@@ -22,7 +24,7 @@ double upper_bound(int lev, int i)
 
 	double bound = 0.0;
 
-	for (int ill = 0; ill <= lev; ill++)
+	for (int ill = lowlev; ill <= uplev; ill++)
 	{
 		int level = ill;
 
@@ -30,28 +32,32 @@ double upper_bound(int lev, int i)
 		double grow = pow(0.5 * phi *phi, level) * 0.2 * phi * phi * phi;
 		double norm = 1.0 / (double)(1<<level);
 
-		int j = i >> (lev-ill);
+		int j = i >> (uplev-ill);
 		int k = j;
 
 		stern_brocot_tree(k, level, p, q);
 		stern_brocot_tree(k+1, level, pm, qm);
-		double delta = norm * q * qm;
-		delta /= grow;
+		double delta = norm * q * qm / grow;
 
-		if (bound < delta) bound = delta;
+		if (bound < delta)
+		{
+			bound = delta;
+			if (maxlev < level) maxlev = level;
+		}
 	}
 	return bound;
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: %s level\n", argv[0]);
+		fprintf(stderr, "Usage: %s lower-level upper-level\n", argv[0]);
 		exit(1);
 	}
 
 	int lev = atoi(argv[1]);
+	int uplev = atoi(argv[2]);
 
 	unsigned __int128 p, q, pm, qm;
 
@@ -59,20 +65,21 @@ int main(int argc, char *argv[])
 	double phi = 0.5 * (1.0 + sqrt(5.0));
 
 	// Growth rate
-	double grow = pow(0.5 * phi *phi, lev) * 0.2 * phi * phi * phi;
-	double norm = 1.0 / (double)(1<<lev);
+	double grow = pow(0.5 * phi *phi, uplev) * 0.2 * phi * phi * phi;
+	double norm = 1.0 / (double)(1<<uplev);
 
-	for (int i=0; i< (1<<lev); i++)
+	for (int i=0; i< (1<<uplev); i++)
 	{
-		stern_brocot_tree(i, lev, p, q);
-		stern_brocot_tree(i+1, lev, pm, qm);
+		stern_brocot_tree(i, uplev, p, q);
+		stern_brocot_tree(i+1, uplev, pm, qm);
 		double delta = norm * q * qm / grow;
 
 		double y = ((double) p) / (double) q;
-		double bound = upper_bound(lev, i);
+		double bound = upper_bound(lev, uplev, i);
 		printf("%d\t%g\t%g\t%g\n", i, y, bound, delta);
 
 		y = ((double) pm) / (double) qm;
 		printf("%d\t%g\t%g\t%g\n", i, y, bound, delta);
 	}
+	printf("#\n# maxlevel=%d\n#\n", maxlev);
 }
