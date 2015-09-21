e56f3f28e0668d6eaa9ed1ac61c90f0328c52c5e
Linas Vepstas <linasvepstas@gmail.com>
2015-09-20 22:13:32 -0500

    More stuff

--- misc/measure/stern.cc
+++ misc/measure/stern.cc
@@ -52,17 +52,22 @@ int main(int argc, char *argv[])
 	int level = atoi(argv[1]);
 
 	unsigned long p, q, pm, qm, pmid, qmid;
+	double sum = 0.0;
+
 	double norm = 1.0 / (double)(1<<level);
 	for (int i=0; i< (1<<level); i++)
 	{
 		stern_brocot_tree(i, level, p, q);
 		stern_brocot_tree(i+1, level, pm, qm);
 		double x = i * norm;
-		// double y = ((double) p) / (double) q;
+		double a = ((double) p) / (double) q;
+		double b = ((double) pm) / (double) qm;
 		stern_brocot_tree(2*i+1, level+1, pmid, qmid);
 		double y = ((double) pmid) / (double) qmid;
 		// unsigned long det = pm * q - p * qm;
 		double delta = norm * q * qm;
-		printf("%d	%lu	%lu	%g	%g\n", i, p, q, y, delta);
+
+		sum += (b-a)* delta;
+		printf("%d	%g	%lu	%lu	%g	%g	%g\n", i, x, p, q, y, delta, sum);
 	}
 }
