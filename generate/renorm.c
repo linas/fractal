386e9f95e2ed75172c7a653cb8705406e8e0ba4b
Linas Vepstas <linasvepstas@gmail.com>
2015-09-27 14:41:19 -0500

    bugfix to ancient routine

--- generate/renorm.c
+++ generate/renorm.c
@@ -14,9 +14,9 @@
 extern FILE *Fopen();
 extern FILE *Fopenr();
 
-int main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
-   float	*data_in ;	/* my data array */
+   float *data_in ;	/* my data array */
    // float	*data_out;	/* my data array */
    // float		*tmp; 		/* pointer into data array */
    unsigned int	data_width, data_height;/* data array dimensions */
@@ -25,14 +25,14 @@ int main (int argc, char *argv[])
    FILE		*fp_in, *fp_out;
    float 		scale_fact;
    float 		offset;
-   
+
    if (argc < 3) {
-      fprintf (stderr, "usage: %s <input file> <output file> [<scale factor>] [<offset>] \n", argv[0]);
+      fprintf (stderr, "Usage: %s <input file> <output file> [<scale factor>] [<offset>] \n", argv[0]);
       return 1;
    }
 
    fprintf (stderr, "%s %s %s ", argv[0], argv[1], argv[2]);
-   
+
    if (4 <= argc) {
       scale_fact = (float) atof (argv[3]);
       fprintf (stderr, "%g ", scale_fact);
@@ -48,7 +48,7 @@ int main (int argc, char *argv[])
    }
    fprintf (stderr, "\n");
 
-   
+
    /*-----------------------------------------------*/
    /* open input file */
    if ( (fp_in = Fopenr (argv[1], ".flo")) == NULL) {
@@ -63,7 +63,7 @@ int main (int argc, char *argv[])
       str[i] = j;
       i++;
    }
-   
+
 /*
    if ( NULL == fgets (str, 80, fp_in)) {
       fprintf (stderr, " Can't read input file %s \n", argv[1]);
@@ -71,34 +71,44 @@ int main (int argc, char *argv[])
       return;
    }
 */
-   
+
    sscanf (str, "%d %d", &data_width, &data_height);
    fprintf (stderr, "Input file %s has width %d height %d \n", argv[1], data_width, data_height);
-   
+
    data_in = (float *) malloc (data_width * data_height * sizeof(float));
-   
+
    /* read floating point data */
    fread(data_in, sizeof(float), data_width*data_height, fp_in);
    fclose (fp_in);
-   
+
    /*-----------------------------------------------*/
    /* open output file */
    if ( (fp_out = Fopen (argv[2], ".flo")) == NULL) {
       fprintf (stderr, " Can't open output file %s \n", argv[2]);
       return 3;
    }
-   
+
    /* dump the size to output */
    fprintf (fp_out, "%d %d\n", data_width, data_height);
-   
+
    /*-----------------------------------------------*/
-   
-   if (!strcmp ("takelog", argv[0])) takelog (data_in, data_width, data_height);
-   if (!strcmp ("abs", argv[0])) absolval (data_in, data_width, data_height);
-   if (!strcmp ("renorm", argv[0])) expand (data_in, data_width, data_height, scale_fact, offset);
-   if (!strcmp ("reclamp", argv[0])) rescale (data_in, data_width, data_height, scale_fact);
-   if (!strcmp ("clamp", argv[0])) clamp (data_in, data_width, data_height, scale_fact, offset);
-   if (!strcmp ("dump", argv[0])) dump (data_in, data_width, data_height);
+	/* Strip out the path */
+	char * p = strdup(argv[0]);
+	char * name = strrchr(p, '/');
+	if (NULL == name) name = p;
+	else name++;
+
+   if (!strcmp ("takelog", name)) takelog (data_in, data_width, data_height);
+   else if (!strcmp ("abs", name)) absolval (data_in, data_width, data_height);
+   else  if (!strcmp ("renorm", name)) expand (data_in, data_width, data_height, scale_fact, offset);
+   else  if (!strcmp ("reclamp", name)) rescale (data_in, data_width, data_height, scale_fact);
+   else  if (!strcmp ("clamp", name)) clamp (data_in, data_width, data_height, scale_fact, offset);
+   else  if (!strcmp ("dump", name)) dump (data_in, data_width, data_height);
+	else
+	{
+		fprintf(stderr, "Error: unknown transform %s\n", name);
+		exit(1);
+	}
 
 /*
    aver = avg (data_in, data_width, data_height);
@@ -107,9 +117,9 @@ int main (int argc, char *argv[])
    printf ("yo average was %f rms %f \n", aver, msq);
    expand (data_in, data_width, data_height, scale_fact/msq, aver);
 */
-   
+
    fwrite (data_in, sizeof(float), data_width*data_height, fp_out);
-   
+
    fclose (fp_out);
 
 	return 0;
--- misc/lattice/cvt.sh
+++ misc/lattice/cvt.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-cat lat.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > lat.png
+# cat lat.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > lat.png
+../../generate/renorm lat rlat 0.1
+cat rlat.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > lat.png
