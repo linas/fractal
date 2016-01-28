afbc28ba4225d1faf2626590de6e959378aabea5
Linas Vepstas <linasvepstas@gmail.com>
2016-01-28 11:04:33 -0600

    Start work on graphing the eigenfunctins

--- misc/walsh/baire.c
+++ misc/walsh/baire.c
@@ -1,5 +1,5 @@
 /*
- * Rademacher functions for Baire space
+ * Fractal eigenfunctions of GKW.
  *
  * Used to illustrate the gkw.lyx paper.
  * Linas January 2016
@@ -25,15 +25,44 @@ double rade(int n, int b, double x)
 	return 0.0;
 }
 
+double fractal(double lambda, int b, double x)
+{
+	double sum = 0.0;
+	double ln = 1.0;
+	int n = 1;
+	while (1.0e-14 < ln)
+	{
+		sum += ln * rade(n, b, x);
+		ln *= lambda;
+		n++;
+	}
+
+	return sum;
+}
+
 int main(int argc, char* argv[])
 {
 	double x;
+
+#ifdef RADEMACHER_GRAPH
+	/* Rademacher functions for Baire space */
+	/* Used to generate the graphs baire-13.eps for the gkw paper */
 	for (x=0.0; x<1.0; x+=0.0001)
 	{
 		double r1 = rade(1,3,x);
 		double r2 = rade(2,1,x);
 		double r3 = rade(3,1,x);
 		printf("%f	%f	%f	%f\n", x, r1, r2, r3);
+	}
+#endif
+
+	double lambda = 0.5;
+	double bee = 1;
+	for (x=0.0; x<1.0; x+=0.0001)
+	{
+		double r1 = rade(1,bee,x);
+		double fra = fractal(lambda, bee, x);
+		printf("%f	%f	%f\n", x, r1, fra);
 	}
 
 	return 0;
