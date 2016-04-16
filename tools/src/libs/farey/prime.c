ef098a56c39f24b50d6e966d5b1ac2cf9e824b3b
Linas Vepstas <linasvepstas@gmail.com>
2016-04-16 16:45:46 -0500

    copy algo over from mobius

--- tools/src/libs/farey/prime.c
+++ tools/src/libs/farey/prime.c
@@ -1,6 +1,6 @@
 /*
  * FUNCTION:
- * Basic prime-number sieve
+ * (Very) basic prime-number sieve.
  *
  * HISTORY:
  * Linas Vepstas January 2005
@@ -15,8 +15,8 @@
 
 /* An unsigned int32 is sufficient for factoring 64-bit ints. */
 static unsigned int *sieve = NULL;
-static unsigned int sieve_size = 0;
-static unsigned int sieve_max = 0;
+static size_t sieve_size = 0;  /* size, in bytes. */
+static size_t sieve_max = 0;   /* largest correct entry. */
 
 #define INIT_PRIME_SIEVE(N) \
 	if (!sieve || sieve[sieve_max]*sieve[sieve_max] <(N)) {\
@@ -24,37 +24,39 @@ static unsigned int sieve_max = 0;
 	}
 
 /* Initialize and fill in a prime-number sieve.
- * Handles primes up to 4 billion (2^32)
- * long long int should be a 64-bit number 
+ * Handles primes up to 4 billion (2^32), which should be enough
+ * to factor 64-bit numbers.
+ *
+ * Initializes size so that it contains at least np primes in it.
  */
 static void
-init_prime_sieve (long long int prod)
+init_prime_sieve (size_t max)
 {
 	unsigned int n, j;
 	unsigned int nstart;
 	unsigned int pos;
-	
-	if (sieve)
-	{
-		long long int ss = sieve[sieve_max];
-		if (ss*ss > prod) return;
-	}
-		
-	unsigned int max = 1000.0+sqrt (prod);
 
+	if (max < sieve_max) return;
+
+	sieve_size = 8192 * ((max / 8192) + 1);
 	if (!sieve)
 	{
-		sieve = (unsigned int *) malloc (8192*sizeof (unsigned int));
-		sieve_size = 8192;
+		sieve = (unsigned int *) malloc(sieve_size * sizeof(unsigned int));
 		sieve_max = 2;
 		sieve[0] = 2;
 		sieve[1] = 3;
 		sieve[2] = 5;
 	}
+	else
+	{
+		sieve = (unsigned int *) realloc(sieve, sieve_size * sizeof(unsigned int));
+	}
+
 	pos = sieve_max+1;
 	nstart = sieve[sieve_max] + 2;
 
-	/* dumb algo, brute-force test all odd numbers against known primes */
+	/* Really dumb algo, brute-force test all odd numbers against
+	 * known primes */
 	for (n=nstart; n<=max; n+=2)
 	{
 		for (j=1; ; j++)
@@ -69,11 +71,6 @@ init_prime_sieve (long long int prod)
 				/* If we got to here, n must be prime; save it, move on. */
 				sieve[pos] = n;
 				pos ++;
-				if (pos >= sieve_size)
-				{
-					sieve_size += 8192;
-					sieve = (unsigned int *)realloc (sieve, sieve_size * sizeof (unsigned int));
-				}
 				break;
 			}
 		}
@@ -91,7 +88,7 @@ init_prime_sieve (long long int prod)
 unsigned int get_nth_prime(unsigned long n)
 {
 	init_prime_sieve(n);
-	return sieve[n];
+	return sieve[n-1];
 }
 
 /* --------------------------- END OF FILE ------------------------- */
