b0a1f2954eafd766de322a3d724824e93c4f8867
Linas Vepstas <linasvepstas@gmail.com>
2016-04-18 13:03:18 -0500

    More zero-finding

--- misc/gpf/gpf-zero.c
+++ misc/gpf/gpf-zero.c
@@ -5,11 +5,11 @@
  * April 2016
  */
 
-#include <complex.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <mp-zerofind.h>
 #include <gpf.h>
 #include "gpf-gen-bignum.h"
 
@@ -31,18 +31,44 @@ double gpf_bignum_exponential(double r, double theta)
 	return rv;
 }
 
-void survey(double rmax)
+void find_zero(double rguess, double tguess, double cell_size)
 {
-	for (double r=1.0; r<rmax; r += 1.0)
+	cpx_t zero;
+	cpx_init(zero);
+
+	cpx_t guess;
+	cpx_init(guess);
+	tguess *= 2.0 * M_PI;
+	cpx_set_d(guess, rguess*cos(tguess), rguess*sin(tguess));
+
+	cpx_t e1, e2;
+	cpx_init(e1);
+	cpx_init(e2);
+	cpx_set_d(e1, cell_size, 0);
+	cpx_set_d(e2, 0, cell_size);
+
+	cpx_find_zero(zero, cpx_gpf_exponential, guess, e1, e2, 20, 50);
+
+	double re = cpx_get_re(zero);
+	double im = cpx_get_im(zero);
+
+	printf("found one at %18.16g %18.16g\n", re, im);
+}
+
+void survey(double rmax, double cell_size)
+{
+	for (double r=cell_size; r<rmax; r += cell_size)
 	{
-		for (double t=0.0; t < 1.0; t += 1.0/r)
+		for (double t=0.0; t < 0.5; t += cell_size/r)
 		{
 			double sample = gpf_bignum_exponential(r, t);
 			sample /= r;
 
-			if (sample < 1.0)
+			if (sample < 0.5)
 			{
-				printf("duude got one here: %g %g\n", r, t);
+				printf("Candidate zero near r=%g t=%g\n", r, t);
+				fflush(stdout);
+				find_zero(r, t, cell_size);
 			}
 		}
 	}
@@ -50,13 +76,13 @@ void survey(double rmax)
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
-		fprintf(stderr, "Usage: %s <rmax>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <rmax> <cell-size>\n", argv[0]);
 		exit(1);
 	}
 
 	double rmax = atof(argv[1]);
-	survey(rmax);
-	fflush(stdout);
+	double cell_size = atof(argv[2]);
+	survey(rmax, cell_size);
 }
