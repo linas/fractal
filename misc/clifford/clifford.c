cd0896d1a3989372e6a71e1d421f4da792264aa5
Linas Vepstas <linasvepstas@gmail.com>
2016-04-28 11:05:29 -0500

    start work.

--- misc/clifford/clifford.c
+++ misc/clifford/clifford.c
@@ -14,6 +14,48 @@
  * clifford algebra thereby induces multiplication of such trees.
  * The goal here is to explore "what this looks like".
  *
+ * The mapping is the "bitstring mapping".  If b_0 b_1 b_2... is a
+ * bitstring, then the correpsonding clifford-algebra basis function
+ * is given by the indexes of those bits that are not zero.  That is,
+ * if the non-zero bits are b_i b_j b_k ... then the corresponding
+ * clifford alg element is e_i e_j e_k ...
+ *
  * Linas Vepstas April 2016
  */
 
+
+#include <math.h>
+#include <stdio.h>
+
+#define BITLEN 56
+typedef char bitstring[BITLEN];
+
+void to_bits(bitstring bits, double x)
+{
+	x -= floor(x);
+
+	for (int i=0; i<BITLEN; i++)
+	{
+		if (0.5 < x) { bits[i] = 1; x -= 0.5; }
+		else bits[i] = 0;
+		x *= 2.0;
+	}
+}
+
+double from_bits(bitstring bits)
+{
+	double val = 0.0;
+	double bv = 0.5;
+	for (int i=0; i<BITLEN; i++)
+	{
+		if (bits[i]) val += bv;
+		bv *= 0.5;
+	}
+	return val;
+}
+
+
+main(int argc, char* argv[])
+{
+
+}
