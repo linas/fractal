75beb70e3298062d6766a279943e825757a957a5
Linas Vepstas <linasvepstas@gmail.com>
2016-06-26 13:25:09 -0500

    Bug fix

--- misc/gpf/movie.sh
+++ misc/gpf/movie.sh
@@ -3,24 +3,24 @@
 i=0
 while [ $i -lt 100 ];
 do
-	j=$i
+	let j=$i
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200 &
-	j=$i+1
+	let j=$i+1
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200 &
-	j=$i+2
+	let j=$i+2
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200 &
-	j=$i+3
+	let j=$i+3
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200 &
-	j=$i+4
+	let j=$i+4
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200 &
-	j=$i+5
+	let j=$i+5
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200 &
-	j=$i+6
+	let j=$i+6
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200 &
-	j=$i+7
+	let j=$i+7
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200 &
-	j=$i+8
+	let j=$i+8
 	time ./gpf-2d gpf-exp-deriv-$j 600 600 $j 0 0 200
-	i=$i+9
+	let i=$i+9
 	sleep 6
 done
