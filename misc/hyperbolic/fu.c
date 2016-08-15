aa1e400a6135b3ecb80586ec6698dd866662d3bb
Linas Vepstas <linasvepstas@gmail.com>
2016-08-15 12:03:58 -0500

    fuck around

--- misc/hyperbolic/fu.c
+++ misc/hyperbolic/fu.c
@@ -1,5 +1,5 @@
 /*
- * tree.c 
+ * fu.c 
  *
  * Draw a binary tree on the hyperbolic disk, using postscript
  * This is an explictly geometric construction. For an algebraic
@@ -17,17 +17,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void eps_setup_disk (void)
-{
-	eps_setup_basic_linstyles();
-}
-void eps_setup_plane (void)
-{
-	eps_setup_basic_linstyles();
-	printf ("0.0 -0.8 translate\n");
-	printf ("0.4 1.6 scale\n");
-}
 
 /* ==================================================== */
 int drawcusp = 1;
@@ -53,47 +42,6 @@ void draw_cusp (mobius_t m)
 	draw_arc (m, za, zb);
 
 	printf ("[] 0 setdash\n");
-}
-
-/* draw three-pointed stick figure */
-void draw_tristar (mobius_t m)
-{
-	draw_cusp(m);
-	eps_set_color_black();
-
-	cplex za,zb;
-
-	za = cplex_set (0.0, 0.0);
-
-	// printf ("n 0.0 0.0 m 0.25 0.433012702 l s\n");
-	zb = cplex_set (0.25, 0.25*sqrt(3.0));
-
-	// za = mobius_xform (m,za);
-	// zb = mobius_xform (m,zb);
-	// eps_draw_lineseg (za.re, za.im,zb.re, zb.im);
-	draw_arc (m, za, zb);
-
-	// printf ("n 0.0 0.0 m 0.25 -0.433012702 l s\n");
-	zb = cplex_set (0.25, -0.25*sqrt(3.0));
-	// zb = mobius_xform (m,zb);
-	// eps_draw_lineseg (za.re, za.im,zb.re, zb.im);
-	draw_arc (m, za, zb);
-
-	// printf ("n 0.0 0.0 m -0.5 0.0 l s\n");
-	zb = cplex_set (-0.5, 0.0);
-	// zb = mobius_xform (m,zb);
-	// eps_draw_lineseg (za.re, za.im,zb.re, zb.im);
-	draw_arc (m, za, zb);
-
-	// draw a splat 
-	printf ("0.0600000 slw\n");
-	za = cplex_set (-0.23, 0.0);
-	za = mobius_xform (m,za);
-	zb = cplex_set (-0.27, 0.0);
-	zb = mobius_xform (m,zb);
-	printf ("n %Lf %Lf m %Lf %Lf l s\n", za.re, za.im,zb.re, zb.im);
-	// printf ("0.0100000 slw\n");
-	printf ("0.010000 slw\n");
 }
 
 mobius_t go_to_fork_tip(double sign)
@@ -144,7 +92,7 @@ void draw_fork(mobius_t m, int level, int r, int g, int b)
 }
 
 
-void draw(int n)
+void draw()
 {
 	mobius_t m;
 	int level=3;
@@ -162,40 +110,17 @@ level=10;
 	// mobius_t rot = mobius_rotate ((-0.5-0.166666)*M_PI);
 	off = mobius_mul (rot, off);
 
-#define XLATE
-#ifdef XLATE
-	int a,b,c,d;
-	a=1;
-	b=n;
-	c=0;
-	d=1;
-	mobius_t xfm = mobius_set (a,b,c,d);
-	xfm = to_disk (xfm);
-	off = mobius_mul (xfm, off);
-#endif
-
-// #define HALF_PLANE
+#define HALF_PLANE
 #ifdef HALF_PLANE
+	mobius_t xfm = mobius_set (1,0,0,1);
 	xfm = to_half_plane_xform();
 	off = mobius_mul (xfm, off);
 #endif
-
-	draw_tristar(off);
-
-eps_set_color_green();
-	m = go_to_fork_tip(+1.0);
-	m = mobius_mul(off,m);
-	draw_fork (m, level, 0,255,0);
 
 eps_set_color_red();
 	m = go_to_fork_tip(-1.0);
 	m = mobius_mul(off,m);
 	draw_fork (m, level, 255,0,0);
-
-eps_set_color_blue();
-	m = go_to_fork_tip(-3.0);
-	m = mobius_mul(off,m);
-	draw_fork (m, level, 0,0,255);
 }
 
 /* ==================================================== */
@@ -203,22 +128,5 @@ eps_set_color_blue();
 int
 main (int argc, char * argv[]) 
 {
-
-	if (argc < 2)
-	{
-		fprintf (stderr, "Usage: %s <shift>\n", argv[0]);
-		exit(1);
-	}
-
-	int n = atoi (argv[1]);
-
-	eps_print_prolog(220,220);
-	// eps_print_prolog(400,100);
-	eps_setup_disk();
-	// eps_setup_plane();
-	eps_draw_circle();
-
-	draw(n);
-
-	printf ("showpage\n");
+	draw();
 }
