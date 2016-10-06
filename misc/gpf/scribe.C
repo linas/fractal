966c785d0e938174589780de9a919b48c4292449
Linas Vepstas <linasvepstas@gmail.com>
2016-10-06 00:01:54 -0500

    Add an explanation

--- misc/gpf/scribe.C
+++ misc/gpf/scribe.C
@@ -29,27 +29,28 @@ static double ploto(double re_q, double im_q, int itermax, double param)
 	// if (loc/x - locheigh < y and y < loc/x + locheigh) return 1.0;
 #endif
 
+	// Writing z = r exp(i theta) then ...
+	// this curve corresponds to sqrt(r) = sine(theta/2)
 	// double crv = exp(- 5.8*x);
 	double crv = exp(- 5.545*x);
 	if (crv - locheigh < y and y < crv + locheigh) return 1.0;
 
-/*
-	crv = exp(- 10.0*(x-0.5));
-	if (crv - locheigh < y and y < crv + locheigh) return 1.0;
-*/
-
+	// this curve corresponds to r/768 = sine(theta/2)
 	// 768
 	crv = exp(- 11.0903*(x-0.59906));
 	if (crv - locheigh < y and y < crv + locheigh) return 1.0;
 
+	// this curve corresponds to r/1200 = sine(theta/2)
 	// 1200
 	crv = exp(- 11.0903*(x-0.639301));
 	if (crv - locheigh < y and y < crv + locheigh) return 1.0;
 
+	// this curve corresponds to r/1500 = sine(theta/2)
 	// 1500
 	crv = exp(- 11.0903*(x-0.659421));
 	if (crv - locheigh < y and y < crv + locheigh) return 1.0;
 
+	// this curve corresponds to r/1948 = sine(theta/2)
 	// 1948  (or 2000??)
 	crv = exp(- 11.0903*(x-0.682986));
 	if (crv - locheigh < y and y < crv + locheigh) return 1.0;
