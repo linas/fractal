31c0e0f969e258cdf2a0bfae5013bb9f9e11e7d8
Linas Vepstas <linasvepstas@gmail.com>
2016-10-06 00:23:25 -0500

    Draw better lines

--- misc/gpf/scribe.C
+++ misc/gpf/scribe.C
@@ -14,8 +14,13 @@
 static double ploto(double re_q, double im_q, int itermax, double param)
 {
 	double pixwidth = 800.0;
-	double locwidth = 0.5 / pixwidth;
-	double locheigh = 0.95 / pixwidth;
+	// double locwidth = 0.5 / pixwidth;
+	double locheigh = 0.8 / pixwidth;
+
+	// if (0.25 < im_q) locheigh = 2.0 / pixwidth;
+	// if (0.5 < im_q) locheigh = 4.0 / pixwidth;
+	// if (0.75 < im_q) locheigh = 8.0 / pixwidth;
+	locheigh = pow(8.0, im_q) / pixwidth;
 
 	double theta = M_PI * im_q;
 	double x = re_q;
