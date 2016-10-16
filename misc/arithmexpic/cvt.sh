3a9801c48704c6018317b556af1d0d06a51e66fa
Linas Vepstas <linasvepstas@gmail.com>
2016-10-16 14:46:25 -0500

    Log what we are doing

--- misc/arithmexpic/cvt.sh
+++ misc/arithmexpic/cvt.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
+# ./totient_ord_phase tot-ord-phase 400 400 5000 0 0 2
 # cat tot-ord-phase.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > tot-ord-phase.png
+
+# ./totient_exp_phase tot-exp-phase 400 400 5000 0 0 60
 # cat tot-exp-phase.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > tot-exp-phase.png
 
-../../generate/renorm tot-exp-mag rg 0.5
-cat rg.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > tot-exp-mag.png
+# ./totient_exp_mag tot-exp-mag-120 400 400 5000 0 0 120
+#../../generate/renorm tot-exp-mag-120 rg 1
+#cat rg.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > tot-exp-mag-120.png
+
+# ./totient_exp_mag tot-exp-mag-1K 400 400 5000 0 0 1000
+# ../../generate/renorm tot-exp-mag-1K rg 0.5
+# cat rg.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > tot-exp-mag-1K.png
+
+# ../../generate/renorm tot-exp-mag rg 0.5
+# cat rg.flo | ../../image/flo2mtv |mtvtoppm | pnmtopng > tot-exp-mag.png
 
