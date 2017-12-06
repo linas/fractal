96cf541783772147052d3c48a9aef22ec2e267f7
Linas Vepstas <linasvepstas@gmail.com>
2017-12-06 14:12:30 -0600

    The fun continues

---
+++ misc/bitops/bernie.C
@@ -0,0 +1,32 @@
+/*
+ * bernie.C
+ *
+ * Altered simplified algorithmic Bernoulli map
+ * Dec 2017
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bitops.h"
+#include "brat.h"
+
+/*-------------------------------------------------------------------*/
+/*
+ */
+
+static double xprod(double x, double y, int itermax, double param)
+{
+	return mult_xor(x, y);
+}
+
+DECL_MAKE_HEIGHT (xprod);
+
+#if 0
+int main ( int argc, char * argv[])
+{
+	double om = atof(argv[1]);
+	double kb = atof(argv[2]);
+}
+#endif
