c3d485347ebca7ffadab0e957f9cbf0e47ff5fc9
Linas Vepstas <linasvepstas@gmail.com>
2017-12-08 14:39:32 -0600

    Ongoing mashing of stuff

--- misc/bitops/remap.c
+++ misc/bitops/remap.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double bmap(double x, double w)
+double bmap(double x, double Kay)
 {
 	// Decompose x into a bit sequence.
 	char nbits[50];
@@ -29,12 +29,13 @@ double bmap(double x, double w)
 	double acc = 0.1;
 	for (int i=0; i<50; i++)
 	{
+		acc *= 1.0 / (2.0*Kay);
 		if (nbits[50-i-1])
 		{
 			acc += 0.5;
 		}
-		acc *= w;
 	}
+
 	return acc;
 }
 
@@ -50,19 +51,20 @@ double unbmap(double y, double K)
 			nbits[i] = 1;
 		}
 		else nbits[i] = 0;
-		y *= K;
+		y *= 2.0*K;
 	}
 
-	// Reconstruct x in a mashed bernoulli sequence.
+	// Reconstruct x in a ordinary bernoulli sequence.
 	double acc = 0.1;
 	for (int i=0; i<50; i++)
 	{
+		acc *= 0.5;
 		if (nbits[50-i-1])
 		{
 			acc += 0.5;
 		}
-		acc *= 0.5;
 	}
+
 	return acc;
 }
 
@@ -74,8 +76,8 @@ int main (int argc, char* argv[])
 	for (int i=0; i<=npts; i++)
 	{
 		double x = ((double) i)/ ((double) npts);
-		// double y = bmap (x, 1.0/lam);
 		double y = unbmap (x, lam);
-		printf("%d	%g	%g\n", i, x, y);
+		double z = bmap (y, lam);
+		printf("%d	%g	%g	%g	%g\n", i, x, y, z, z-x);
 	}
 }
