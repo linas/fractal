ba3f798d7ccafceda408babdbd167d1a7f033c24
Linas Vepstas <linasvepstas@gmail.com>
2017-12-09 20:36:59 -0600

    More hackery

--- misc/bitops/remap.c
+++ misc/bitops/remap.c
@@ -96,7 +96,7 @@ int main (int argc, char* argv[])
 		double z = pdr (1-x, lam);
 		printf("%d	%g	%g	%g	%g	%g\n", i, x, y, lo, hi, z);
 #endif
-#define CPR_SIM 1
+// #define CPR_SIM 1
 #if CPR_SIM
 		double y = cpr (x, lam);
 		double lo = cpr (0.5*x, lam);
@@ -104,5 +104,9 @@ int main (int argc, char* argv[])
 		double z = cpr (lam*x, lam);
 		printf("%d	%g	%g	%g	%g	%g\n", i, x, y, lo, hi, z);
 #endif
+		double y = cpr (x, lam);
+		double lo = cpr (x/(2.0*lam), lam);
+		double hi = cpr ((lam+x)/(2.0*lam), lam);
+		printf("%d	%g	%g	%g	%g\n", i, x, y, lo, hi);
 	}
 }
