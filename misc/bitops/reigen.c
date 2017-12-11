6cfe8dbeb2539d8247f6ad7941fa7b3055cbc093
Linas Vepstas <linasvepstas@gmail.com>
2017-12-10 23:32:18 -0600

    More fun more games

--- misc/bitops/reigen.c
+++ misc/bitops/reigen.c
@@ -32,13 +32,27 @@ int main (int argc, char* argv[])
 		exit (1);
 	}
 	double K = atof(argv[1]);
-	double w = atof(argv[2]);
+	double lambda = atof(argv[2]);
+
+	printf("#\n# K = %g lambda = %g\n#\n", K, lambda);
 
 #define NPTS 1803
+// #define NRECU 40
+#define NRECU 20
+	double acc = 0.0;
 	for (int i=0; i<NPTS; i++)
 	{
 		double x = ((double) i) / ((double) NPTS);
-		double y = reig(x, K, w, 14);
+		double y = reig(x, K, lambda, NRECU);
+		acc += y;
+	}
+	acc /= NPTS;
+
+	for (int i=0; i<NPTS; i++)
+	{
+		double x = ((double) i) / ((double) NPTS);
+		double y = reig(x, K, lambda, NRECU);
+		y /= acc;
 		printf("%d	%g %g\n", i, x, y);
 	}
 }
