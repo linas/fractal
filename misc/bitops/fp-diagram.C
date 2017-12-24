696873e26ae0e1a5ebd528446d5567eb8fd4e42b
Linas Vepstas <linasvepstas@gmail.com>
2017-12-24 15:34:38 -0600

    Redraw diagram

---
+++ misc/bitops/fp-diagram.C
@@ -0,0 +1,106 @@
+/*
+ * fp-diagram.C
+ *
+ * Feigenbaum-style diagram for the undermap, but computed
+ * "exactly", not statisically, so free of noise. I'm calling
+ * this the Frobenius-Perron diagram.
+ *
+ *
+ * Dec 2017
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bitops.h"
+#include "brat.h"
+
+/*-------------------------------------------------------------------*/
+/*
+ * Non-renomralized 
+ * Recursve frobenius-perron eigenfunction for the undershift
+ */
+double reig(double x, double K, int niter)
+{
+	double tkay = 2.0*K;
+
+	if (niter < 0) return 1.0;
+	if (K < x) return 0.0;
+	if (K*(tkay-1.0) < x)
+	{
+		return reig(x/tkay, K, niter-1) / tkay;
+	}
+	double sum = reig(x/tkay, K, niter-1);
+	sum += reig(0.5 + x/tkay, K, niter-1);
+	sum /= tkay;
+	return sum;
+}
+
+/*
+ * Normalization for 
+ * Recursive frobenius-perron eigenfunction for the undershift
+ * That is, the integral of the unrenomralized function.
+ */
+double fpeig(double K, int niter)
+{
+#define NPTS 2803
+	double acc = 0.0;
+	for (int i=0; i<NPTS; i++)
+	{
+		double x = (((double) i) + 0.5) / ((double) NPTS);
+		double y = reig(x, K, niter);
+		acc += y;
+	}
+	acc /= NPTS;
+	return acc;
+}
+
+static void bifurcation_diagram (float *array,
+                                 int array_size,
+                                 double x_center,
+                                 double x_width,
+                                 double K,
+                                 int itermax,
+                                 double omega)
+{
+#if 0
+	double norm = fpeig (K, itermax);
+
+	for (int j=0; j<array_size; j++)
+	{
+		double x = (((double) j) + 0.5) / ((double) array_size);
+		double eig = reig(x, K, itermax);
+		array[j] = eig / norm;
+	}
+#endif
+
+	for (int j=0; j<array_size; j++)
+	{
+		double x = (((double) j) + 0.5) / ((double) array_size);
+		double eig = reig(x, K, itermax);
+		array[j] = eig;
+	}
+
+	double norm = 0.0;
+	for (int j=0; j<array_size; j++)
+	{
+		norm += array[j];
+	}
+	norm /= array_size;
+
+	for (int j=0; j<array_size; j++)
+	{
+		array[j] /= norm;
+	}
+}
+
+DECL_MAKE_BIFUR(bifurcation_diagram)
+
+#if 0
+int main ( int argc, char * argv[])
+{
+	double om = atof(argv[1]);
+	double kb = atof(argv[2]);
+}
+#endif
