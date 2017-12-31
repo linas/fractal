2e24b872394bc35b231feaa8fdc0c13a8fc09c1b
Linas Vepstas <linasvepstas@gmail.com>
2017-12-31 12:41:56 -0600

    start unit testing

--- misc/bitops/treefn.c
+++ misc/bitops/treefn.c
@@ -10,34 +10,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// The finite-length expander function
+double finite_pdr(char* bitseq, int nbits, double Kay, double y)
+{
+	double acc = y;
+	for (int i=0; i<nbits; i++)
+	{
+		acc *= 1.0 / (2.0*Kay);
+		if (bitseq[nbits-i-1])
+		{
+			acc += 0.5;
+		}
+	}
+	return acc;
+}
+
+// The finite-length expander function
+// Alternate implementation, for bug-checking
+double finite_pdr_alt(char* bitseq, int nbits, double Kay, double y)
+{
+	double acc = 0.0;
+	double tk = 1.0;
+	for (int i=0; i<nbits; i++)
+	{
+		if (bitseq[i]) acc += tk;
+		tk *= 1.0 / (2.0*Kay);
+	}
+	acc = 0.5 * acc + y/tk;
+
+	return acc;
+}
+
+// The step function.
+int step(char* bitseq, int nbits, double Kay, double y)
+{
+	if (0 == nbits)
+	{
+		// if ...
+	}
+	return 0;
+}
+
 // The tree function
 double tree_fun(double x, double Kay, double y)
 {
 	// Decompose x into a bit sequence.
-	char nbits[50];
+	char bitseq[50];
 	for (int i=0; i<50; i++)
 	{
 		if (0.5 <= x)
 		{
 			x -= 0.5;
-			nbits[i] = 1;
+			bitseq[i] = 1;
 		}
-		else nbits[i] = 0;
+		else bitseq[i] = 0;
 		x *= 2.0;
 	}
 
-	// Construct the tree function from this sequence
-	double acc = 0.1;
-	for (int i=0; i<50; i++)
-	{
-		acc *= 1.0 / (2.0*Kay);
-		if (nbits[50-i-1])
-		{
-			acc += 0.5;
-		}
-	}
+	double a = finite_pdr(bitseq, 3, Kay, y);
+	double b = finite_pdr_alt(bitseq, 3, Kay, y);
 
-	return acc;
+	// Construct the tree function from this sequence
+	return a-b;
 }
 
 int main (int argc, char* argv[])
