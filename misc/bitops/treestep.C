ea69551b4a3e541bdd5c3344f7d6f81491b9bcaa
Linas Vepstas <linasvepstas@gmail.com>
2017-12-31 19:38:18 -0600

    More figures.

---
+++ misc/bitops/treestep.C
@@ -0,0 +1,96 @@
+/*
+ * treestep.C
+ *
+ * Tree function visualization
+ * Dec 2017
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "brat.h"
+
+/*-------------------------------------------------------------------*/
+
+// The finite-length expander function
+double finite_pdr(char* bitseq, int nbits, double Kay, double y)
+{
+	double acc = y;
+	for (int i=0; i<nbits; i++)
+	{
+		acc *= 1.0 / (2.0*Kay);
+		if (bitseq[nbits-i-1])
+		{
+			acc += 0.5;
+		}
+	}
+	return acc;
+}
+
+// The step function.
+int step(char* bitseq, int nbits, double Kay, double y)
+{
+	double ve = finite_pdr(bitseq, nbits, Kay, y);
+	if (ve <= Kay) return 1;
+	return 0;
+}
+
+// Compute the bit sequence of x.
+void to_bit_sequence(char* bitseq, double x)
+{
+	// Decompose x into a bit sequence.
+	for (int i=0; i<50; i++)
+	{
+		if (0.5 <= x)
+		{
+			x -= 0.5;
+			bitseq[i] = 1;
+		}
+		else bitseq[i] = 0;
+		x *= 2.0;
+	}
+}
+
+// The tree function
+double tree_fun(double x, double Kay, double y)
+{
+	// Decompose x into a bit sequence.
+	char bitseq[50];
+	to_bit_sequence(bitseq, x);
+
+	// Construct the tree function from this sequence
+	for (int i=0; i<50; i++)
+	{
+		if (0 == step(bitseq, i, Kay, y)) return 0.0;
+	}
+
+	return 1.0;
+}
+
+/*-------------------------------------------------------------------*/
+/*
+ */
+
+static void tree_diagram (float *array,
+                                 int array_size,
+                                 double x_center,
+                                 double x_width,
+                                 double y,
+                                 int itermax,
+                                 double Kay)
+{
+	/* clear out the row */
+	for (int j=0; j<array_size; j++) array[j] = 0.0;
+
+	for (int j=0; j<array_size; j++)
+	{
+		double x = (((double) j) + 0.5)/ ((double) array_size);
+		x -= 0.5;
+		x *= x_width;
+		x += x_center;
+		array[j] = tree_fun(x, Kay, y);
+	}
+}
+
+DECL_MAKE_BIFUR(tree_diagram)
