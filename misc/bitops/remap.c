1264a058022a5861a040ada1f537425420cba914
Linas Vepstas <linasvepstas@gmail.com>
2018-01-03 14:05:39 -0600

    Revise the earlier confusion/brain-fart that I had.

--- misc/bitops/remap.c
+++ misc/bitops/remap.c
@@ -70,6 +70,65 @@ double cpr(double y, double K)
 	return acc;
 }
 
+// deconstruct/reconstruct
+// should be same as pdr(cpr(x))
+double der(double y, double K)
+{
+	// Iterate on y using mashed Bernoulli, and extract symbol dynamics
+	char nbits[50];
+	for (int i=0; i<50; i++)
+	{
+		if (0.5 <= y)
+		{
+			y -= 0.5;
+			nbits[i] = 1;
+		}
+		else nbits[i] = 0;
+		y *= 2.0*K;
+	}
+
+	// Reconstruct x in a mashed bernoulli sequence.
+	double acc = 1.0e-30;
+	for (int i=0; i<50; i++)
+	{
+		acc *= 1.0 / (2.0*K);
+		if (nbits[50-i-1])
+		{
+			acc += 0.5;
+		}
+	}
+
+	return acc;
+}
+
+double len(double y, double K)
+{
+	int longest = 0;
+	int len = 0;
+	// Iterate on y using mashed Bernoulli, and extract symbol dynamics
+	for (int i=0; i<50; i++)
+	{
+		if (0.5 <= y)
+		{
+			y -= 0.5;
+			len ++;
+			if (longest < len) longest = len;
+		}
+		else
+		{
+			len = 0;
+		}
+		y *= 2.0*K;
+	}
+
+	return longest;
+}
+
+double flen(double K)
+{
+	return -log(2.0*(1.0-K)) / log (2.0*K);
+}
+
 // the tent compressor
 double tent_cpr(double y, double K)
 {
@@ -159,6 +218,7 @@ int main (int argc, char* argv[])
 	}
 	double lam = atof(argv[1]);
 
+#if 0
 	int npts = 3803;
 	for (int i=0; i<npts; i++)
 	{
@@ -195,8 +255,22 @@ int main (int argc, char* argv[])
 		printf("%d	%g	%g	%g	%g\n", i, x, y, lo, hi);
 #endif
 
-		double y = tent_cpr (x, lam);
-		double z = gist_cpr (x, lam);
+		// double y = tent_cpr (x, lam);
+		// double z = gist_cpr (x, lam);
+		double y = der (x, lam);
+		double t = cpr(x, lam);
+		double z = pdr(t, lam);
 		printf("%d	%g	%g	%g\n", i, x, y, z);
+	}
+#endif
+
+	int npts = 180;
+	for (int i=0; i<npts; i++)
+	{
+		double lam = 0.5 + 0.5* (((double) i) + 0.5)/ ((double) npts);
+		double w = len(1.0, lam);
+		double p = flen(lam);
+		p = ceil(p);
+		printf("%d	%g	%g	%g %g\n", i, lam, w, p, w-p);
 	}
 }
