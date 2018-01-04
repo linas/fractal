be371b5412ce4ba4d9023098f1781f859e787e90
Linas Vepstas <linasvepstas@gmail.com>
2018-01-04 12:36:27 -0600

    What he sent me today

--- misc/adammaj1/electric.c
+++ misc/adammaj1/electric.c
@@ -4,7 +4,7 @@
 
    http://linas.org/art-gallery/escape/ray.html
    image : 
-   http://linas.org/art-gallery/escape/electric.gif
+  http://linas.org/art-gallery/escape/landing.gif
    
    
    "Gradient of the potential
@@ -48,7 +48,7 @@ Maybe make a chic lightning rod for the barn. " Linas Vepstas
 
  
  
-   gcc electric.c -lm -Wall 
+   gcc p.c -lm -Wall 
  
  
    ./a.out
@@ -61,8 +61,8 @@ Maybe make a chic lightning rod for the barn. " Linas Vepstas
 
 
    ----------------------
-   git add  electric.png electric.c
-   git commit -m " electric"
+   git add  landing.png landing.c
+   git commit -m " landing"
    push -u origin master
 
 
@@ -109,61 +109,84 @@ const int IterationMax=88;
 /* bail-out value , radius of circle ;  */
 const double EscapeRadius=300.0;
 double log_2; // = log(2.0);
+double TwoPi=2.0*M_PI;
+
+
        
 // memmory virtual 1D array 
 unsigned char *data;       
 size_t MemmorySize;       
     
     
-void GiveLinasColor(double position , int k, unsigned char c[])
+void  GiveLinasColor(double position , int k, unsigned char c[])
 {
   /* based on the code by Linas Vepstas January 16 1994 : void make_cmap (void) */
 
    
   int i;
-  int iMax = 240;
-  i=(int)(iMax-1)*position;  
-  c[0] = c[1] = c[2] = 0;
+  int iMax = 256;
+  // normalisation of the position : normal = between 0.0 and 1.0
+  //if (position>1.0)  position =  position - (int) position ; // frac 
+  // if position > 1.0 the gradient wiil be repeating
+  
+  i=((int)(iMax-1)*position) ; // linear funtion
+  // i = i % iMax;  // modulo
+  c[0] = 0; // 
+  c[1] = c[2] = 0;
   /* set up a default look up table */
-  /* ramp up to blue */
+  // 4 segment of the gradient
+  
+  /* from black  ramp up to blue */
   if (i<60) {
     c[k] = 0;
     c[k+1] = 0;
     c[k+2] = (unsigned char) i*3;
+    //return 0;
   }
+  
   /* ramp down from blue, up to green */
   if (i>=60 && i<120) {
     c[k] = 0;
     c[k+1] = (unsigned char) (i-60)*3;
     c[k+2] = (unsigned char) (120-i)*3;
+   // return 0;
   }
+  
   /* ramp from green to yellow */
   if (i>=120 && i<180) {
     /* vlt[i].r = (char) (((i-120)*7) / 2); */
     c[k] = (unsigned char) (210 - (7*(180-i)*(180-i)) / 120);
     c[k+1] = (unsigned char) (210 -i/4);
     c[k+2] = 0;
+   
   }
   /* ramp from yellow to red (pink) */
-  if (i>=180 && i<iMax) {
-    c[k] = (unsigned char) (210 + (3*(i-180))/4);
-    c[k+1] = (unsigned char) (510 - 2*i);
-    c[k+2] = (unsigned char) (i-180)/3;
-  }
-
-  /* Pure red set it to 240 not 255, just like above. */
-  if (iMax <= i) {
-    c[k] = 240;
-    c[k+1] = 0;
-    c[k+2] = 0;
-  }
-   
+ // if (i>=180 && i<iMax) {
+//    c[k] = (unsigned char) (210 + (3*(i-180))/4);
+//    c[k+1] = 0; //(unsigned char) (510 - 2*i);
+ //   c[k+2] = 0; //(unsigned char) (i-180)/3;
+ // }
+ 
+ 
+ // position , here gradient can be > 1.0 
+ // colors from original image extracted with gimp  : 254/32/19, 249/44/17, 240/68/13
+  if (position>1.0) {c[k] =  254;  c[k+1] =  32;     c[k+2] = 19;}
+  	else if (i>=180) {
+    		c[k] = 200 + i/5  ; // from 210  to 254 
+    		c[k+1] = 210 - i/2 ; // from 160 to 32
+    		c[k+2] = 13;}
+  
+    
+  
+  
+ 
 }
 
        
         
 /* 
-   gives position ( index) in 1D virtual array  of 2D point (iX,iY) from ; uses also global variable iWidth 
+   gives  index  in 1D array  of 2D point (iX,iY) from 2D virtual array ; uses also global variable iWidth 
+   maps point from 2D virtual array to 1D memmory array
    without bounds check !!
 */
 int f(int ix, int iy)
@@ -182,10 +205,11 @@ double complex give_c(int iX, int iY){
  
 }
  
-
+// https://en.wikipedia.org/wiki/Gradient
+// http://linas.org/art-gallery/escape/ray.html
 double complex GiveGradient(double complex C ){
 
-  double g; 
+  double g; // gradient
   int i; // iteration
   double complex Z  = 0.0; // initial value for iteration Z0
   double complex dZ = 1.0; // derivative with respect to c
@@ -196,8 +220,8 @@ double complex GiveGradient(double complex C ){
   for(i=0;i<IterationMax;i++)
     {
       
-      dZ = 2 * Z * dZ +1.0; 
-      Z=Z*Z+C; 
+      dZ = 2 * Z * dZ +1.0; // derivative
+      Z=Z*Z+C; // complex quadratic map
       if(cabs(Z)>EscapeRadius) break; // bailout test 
     }
    
@@ -213,39 +237,47 @@ double complex GiveGradient(double complex C ){
   //m = m/IterationMax; // normalize = map to [0,1]
   
   
-  // the Douady-Hubbard potential is just f = exp(-m log2) = 2^-m
+  // the Douady-Hubbard potential is just f = e-m log2 = 2-m
   double potential = pow(2,-m);
-
-  // Thereforre the graident of the potential is ... 
-  // 2Df = 2^-m zn Dzn / |zn|^2 log |zn|
+  // 2Df = m zn Dzn / |zn|2 log |zn|
  
-  // g = potential*Z*dZ/(cabsZ*cabsZ*log(cabsZ));
   g = potential*cabs(Z*dZ)/(cabsZ*cabsZ*log(cabsZ));
   
   return g;
 }
  
 
+double GiveTurn(double complex z)
+{
+  double argument;
+ 
+  argument = carg(z); //   argument in radians from -pi to pi
+  if (argument<0) argument=argument + TwoPi; //   argument in radians from 0 to 2*pi
+  return argument/TwoPi ; // argument in turns from 0.0 to 1.0
+}
 
+
+
+// find color of pixel 
 int ComputeAndSavePixelColor(int iX, int iY){
  
   
   complex double C;
-  double complex g;
-  double gm;
+  double complex g; // gradient
+  double ga; // argument of the gradient 
   int k; // index of the 1D array
    
   C = give_c(iX, iY);
    
   g = GiveGradient(C);
-  gm = cabs(g); 
+  ga = carg(g); 
   
   // index of 1D memory array
   k = f(iX, iY);  
   
    
   //Apply this method to both exterior and interior 
-  GiveLinasColor(gm , k,  data); // 
+  GiveLinasColor(ga , k,  data); // color depends on the  gradients modulus 
       
     
  
@@ -285,7 +317,7 @@ int SaveArray_2_PPM_file (unsigned char A[])
 
   FILE *fp;
   const unsigned int MaxColorComponentValue = 255;	/* color component is coded from 0 to 255 ;  it is 8 bit color file */
-  char *filename = "electric.ppm";
+  char *filename = "p.ppm";
   char *comment = "# ";		/* comment should start with # */
 
   /* save image to the pgm file  */
