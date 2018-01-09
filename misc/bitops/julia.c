e7072668a5c5a985a883214751b2c648d6a2b90b
Linas Vepstas <linasvepstas@gmail.com>
2018-01-08 23:00:16 -0600

    psuedo-working julia set

--- misc/bitops/julia.c
+++ misc/bitops/julia.c
@@ -9,32 +9,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NBITS 11
-#define NPTS (1<<(NBITS-1))
+#define NBITS 10
+#define NPTS (1<<NBITS)
 
 double map[NPTS+1];
 void up(double v, double Kay, int loc, int lvl);
 void down(double v, double Kay, int loc, int lvl)
 {
-	if (lvl < 0) return;
+	lvl --;
 	v *= 2.0 * Kay;
    if (Kay < v) v = Kay;
-	int cent = loc - (1<<(lvl-1));
+	int cent = loc - (1<<lvl);
    map[cent] = v;
-	down(v, Kay, cent, lvl-1);
-	up(v, Kay, cent, lvl-1);
+
+	if (lvl < 1) return;
+	down(v, Kay, cent, lvl);
+	up(v, Kay, cent, lvl);
 }
 
 void up(double v, double Kay, int loc, int lvl)
 {
-	if (lvl < 0) return;
 	lvl --;
-
 	v *= 2.0 * Kay;
 	v -= Kay;
    if (v< 0.0) v = 0.0;
-	int cent = loc + 1<<lvl;
+	int cent = loc + (1<<lvl);
    map[cent] = v;
+
+	if (lvl < 1) return;
 	down(v, Kay, cent, lvl);
 	up(v, Kay, cent, lvl);
 }
@@ -45,13 +47,13 @@ void mkmap(double y, double Kay)
 	{
 		map[i] = 99999;
 	}
-	int lvl == NBITS=2;
+	int lvl = NBITS-1;
 	int cent = 1<<lvl;
 	map[cent] = y;
 	down (y, Kay, cent, lvl);
 	up (y, Kay, cent, lvl);
 
-	for (int i=0; i<NPTS; i++)
+	for (int i=1; i<NPTS; i++)
 	{
 		double x = ((double) i) / ((double) NPTS);
 		printf ("%d	%g	%g\n", i, x, map[i]);
@@ -60,6 +62,11 @@ void mkmap(double y, double Kay)
 
 int main(int argc, char* argv[])
 {
+	if (argc<2)
+	{
+		fprintf(stderr, "Usage: %s K\n", argv[0]);
+		exit (1);
+	}
 	double Kay = atof(argv[1]);
 	mkmap(Kay, Kay);
 }
