82ea13f9ebcc6d8beb9ff79b61f693bc2be4f6af
Linas Vepstas <linasvepstas@gmail.com>
2018-01-14 13:22:09 -0600

    bitops fun and games

---
+++ misc/bitops/bitshow.c
@@ -0,0 +1,31 @@
+
+/*
+ * Simple graphs of the bit-wise operations.
+ *
+ * Jan 2018 Linas Vepstas
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "bitops.h"
+
+
+int main(int argc, char* argv[])
+{
+	double K1 = atof(argv[1]);
+	double K2 = atof(argv[2]);
+	double K3 = atof(argv[3]);
+	double K4 = atof(argv[4]);
+
+#define NBINS 979
+	for (int i=0; i< NBINS; i++)
+	{
+		double x = ((double) i + 0.5) / ((double) NBINS);
+		double k1x = add_xor(K1, x);
+		double k2x = add_xor(K2, x);
+		double k3x = add_xor(K3, x);
+		double k4x = add_xor(K4, x);
+		printf ("%d	%g	%g	%g	%g	%g\n", i, x, k1x, k2x, k3x, k4x);
+	}
+}
