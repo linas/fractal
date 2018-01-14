4d0bbc8138194f2e0b1b86b995311eeffb9cc3ef
Linas Vepstas <linasvepstas@gmail.com>
2018-01-14 13:06:26 -0600

    Add more bitops

--- tools/src/libs/farey/bitops.c
+++ tools/src/libs/farey/bitops.c
@@ -10,6 +10,66 @@
 
 #include "bitops.h"
 
+#define MANTISZ 52
+
+// Convert float to bit string
+static void float_to_bits(double a, char* bits)
+{
+	for (int i=0; i< MANTISZ; i++)
+	{
+		if (0.5 <= a) bits[i] = 1;
+		else bits[i] = 0;
+
+		// shift left one bit
+		a *= 2.0;
+		if (1.0 <= a) a -= 1.0;
+	}
+}
+
+// Convert bits to double.
+static double bits_to_float(char* bits)
+{
+	double p = 0.0;
+	double h = 0.5;
+	for (int i=0; i< MANTISZ; i++)
+	{
+		if (0 != bits[i]) p += h;
+		h *= 0.5;
+	}
+	return p;
+}
+
+/**
+ * Carry-free addition.
+ * This takes the XOR of the binary representation of two floating
+ * point numbers, and returns the result as a float.  Basically, its
+ * just ordinary addition, with a failure to to carry the carry bit.
+ */
+
+double add_xor(double a, double b)
+{
+	a -= floor(a);
+	b -= floor(b);
+
+	// XXX This is the simple sloppy implementation, not the
+	// high-performance one!
+	// First, create a bit-expansion of a
+	char abits[MANTISZ];
+	float_to_bits(a, abits);
+
+	char bbits[MANTISZ];
+	float_to_bits(b, bbits);
+
+	// Take the xor
+	for (int i=0; i< MANTISZ; i++)
+	{
+		abits[i] = abits[i] ^ bbits[i];
+	}
+
+	// Now convert back to double.
+	return bits_to_float(abits);
+}
+
 /**
  * Carry-free multiplication.
  * This "multiplies" two floating point numbers, but then fails
@@ -22,17 +82,8 @@ double mult_xor(double a, double b)
 	b -= floor(b);
 
 	// First, create a bit-expansion of a
-#define MANTISZ 52
 	char abits[MANTISZ];
-	for (int i=0; i< MANTISZ; i++)
-	{
-		if (0.5 <= a) abits[i] = 1;
-		else abits[i] = 0;
-
-		// shift left one bit
-		a *= 2.0;
-		if (1.0 <= a) a -= 1.0;
-	}
+	float_to_bits(a, abits);
 
 	// The carry-free product
 	char prod[MANTISZ];
@@ -46,28 +97,20 @@ double mult_xor(double a, double b)
 	{
 		if (0.5 <= b)
 		{
-			// accumulate (add) a/2^i into the product
+			// Accumulate (add) a/2^i into the product
 			for (int j=i; j<MANTISZ; j++)
 			{
 				prod[j] = prod[j] ^ abits[j-i];
 			}
 		}
 
-		// shift left one bit
+		// Shift left one bit
 		b *= 2.0;
 		if (1.0 <= b) b -= 1.0;
 	}
 
 	// Now convert back to double.
-	double p = 0.0;
-	double h = 0.5;
-	for (int i=0; i< MANTISZ; i++)
-	{
-		if (0 != prod[i]) p += h;
-		h *= 0.5;
-	}
-
-	return p;
+	return bits_to_float(prod);
 }
 
 #ifdef UNIT_TEST
--- tools/src/libs/farey/bitops.h
+++ tools/src/libs/farey/bitops.h
@@ -11,6 +11,13 @@ extern "C" {
 #endif
 
 /**
+ * Take the XOR of the binary representatation of two floats.
+ * Return the result, converted back into a float.  This is a lot
+ * like addition, but failing to carry the carry bit.
+ */
+double add_xor(double a, double b);
+
+/**
  * Carry-free multiplication.
  * This "multiplies" two floating point numbers, but then fails
  * to carry the carry bit -- it uses XOR instead of addition.
