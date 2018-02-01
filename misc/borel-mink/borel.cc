6e56cec5f569d887a7d453f3ad3807ccfec177f2
Linas Vepstas <linasvepstas@gmail.com>
2018-01-31 20:59:16 -0600

    Re-write

--- misc/borel-mink/borel.cc
+++ misc/borel-mink/borel.cc
@@ -1,11 +1,10 @@
 
-/*
- * A recursive expression for the Minkowski measure
- * This is the same old question mark, just a different API.
- * Specifically, since the thing is nicely, recursively defined, its more
- * tractable.
+/**
+ * borel.cc
  *
- * Linas Vepstas September 2015
+ * Borel transform, or Riesz–Herglotz transform on the Minkowski measure
+ *
+ * Linas Vepstas February 2018
  */
 
 #include <math.h>
@@ -13,6 +12,63 @@
 #include <stdlib.h>
 #include "question.h"
 
+// Goddamnd fucked up C++ complex numbers
+#define COMPLEX std::complex<double>
+
+/**
+ * The  Riesz–Herglotz transform on the minkowski measure.
+ * `level` is the iteration level to recurse to.
+ */
+COMPLEX riesz_mink(COMPLEX z, in level)
+{
+	unsigned __int128 p, q, pm, qm, pmid, qmid;
+	double sum = 0.0;
+
+	// Golden ratio
+	double phi = 0.5 * (1.0 + sqrt(5.0));
+
+	// Growth rate
+	double grow = pow(0.5 * phi * phi, level) * 0.2 * phi * phi * phi;
+
+	COMPLEX acc = 0.0;
+
+	double norm = 1.0 / (double)(1<<level);
+	for (int i=0; i< (1<<level); i++)
+	{
+		stern_brocot_tree128(i, level, &p, &q);
+		stern_brocot_tree128(i+1, level, &pm, &qm);
+
+		// a and b are the upper and lower endpoints of the interval
+		double a = ((double) p) / (double) q;
+		double b = ((double) pm) / (double) qm;
+		stern_brocot_tree128(2*i+1, level+1, &pmid, &qmid);
+
+		// The midpoint of the interval.
+		double y = ((double) pmid) / (double) qmid;
+
+		// unsigned long det = pm * q - p * qm;
+		double delta = norm * q * qm;
+		delta /= grow;
+
+		sum += (b-a)* delta;
+		// printf("%d	%g	%g	%g\n", i, y, delta, sum);
+
+		// theta ranges -pi to pi
+		double theta = (2.0*y - 1.0) * M_PI;
+		COMPLEX it = I*theta;
+		COMPLEX eit = exp(it);
+
+		// The kernel
+		COMPLEX krn = (eit + z) / (eit - z);
+
+		acc += krn * delta * (b-a);
+	}
+
+	// Normalize
+	acc /= sum;
+
+	acc /= 2.0 * M_PI;
+}
 
 int main(int argc, char *argv[])
 {
@@ -24,33 +80,8 @@ int main(int argc, char *argv[])
 
 	int level = atoi(argv[1]);
 
-	unsigned __int128 p, q, pm, qm, pmid, qmid;
-	double sum = 0.0;
+	COMPLEX z = 0.0;
+	COMPLEX g = reisz_mink(z, level0:
 
-	// Golden ratio
-	double phi = 0.5 * (1.0 + sqrt(5.0));
-
-	// Growth rate
-	double grow = pow(0.5 * phi * phi, level) * 0.2 * phi * phi * phi;
-
-	double norm = 1.0 / (double)(1<<level);
-	for (int i=0; i< (1<<level); i++)
-	{
-		stern_brocot_tree128(i, level, &p, &q);
-		stern_brocot_tree128(i+1, level, &pm, &qm);
-		double x = i * norm;
-		double a = ((double) p) / (double) q;
-		double b = ((double) pm) / (double) qm;
-		stern_brocot_tree128(2*i+1, level+1, &pmid, &qmid);
-		double y = ((double) pmid) / (double) qmid;
-		// unsigned long det = pm * q - p * qm;
-		double delta = norm * q * qm;
-		delta /= grow;
-
-		unsigned long pp = p;
-		unsigned long qq = q;
-
-		sum += (b-a)* delta;
-		printf("%d	%g	%lu	%lu	%g	%g	%g\n", i, x, pp, qq, y, delta, sum);
-	}
+	printf("its %g + i%g\n", real(g), imag(g));
 }
