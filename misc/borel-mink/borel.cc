e59cf81811cbeb6884fa7ceedf78e49fa05a7f74
Linas Vepstas <linasvepstas@gmail.com>
2018-01-31 22:01:14 -0600

    Make it multi-threaded

--- misc/borel-mink/borel.cc
+++ misc/borel-mink/borel.cc
@@ -9,6 +9,7 @@
 
 #include <complex.h>
 #include <math.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "question.h"
@@ -20,8 +21,10 @@
 /**
  * The  Riesz–Herglotz transform on the minkowski measure.
  * `level` is the iteration level to recurse to.
+ *
+ * Raw version, uncahced, slow.
  */
-COMPLEX riesz_mink(COMPLEX z, int level)
+COMPLEX riesz_mink_nocache(COMPLEX z, int level)
 {
 	unsigned __int128 p, q, pm, qm, pmid, qmid;
 
@@ -53,10 +56,11 @@ COMPLEX riesz_mink(COMPLEX z, int level)
 		// measure: the value of the measure at the midpoint.
 		double measure = norm * q * qm;
 		measure /= grow;
+		measure *= (b-a);
 
 		// The integral of the masure: should be the question mark.
 		// up to a scale factor
-		quest += (b-a)* measure;
+		quest += measure;
 
 		// theta ranges -pi to pi
 		double theta = (2.0*y - 1.0) * M_PI;
@@ -66,7 +70,7 @@ COMPLEX riesz_mink(COMPLEX z, int level)
 		// The kernel
 		COMPLEX krn = (eit + z) / (eit - z);
 
-		acc += krn * measure * (b-a);
+		acc += krn * measure;
 
 		// printf("%d	%g	%g	%g	%g	%g\n", i, y, measure, quest, real(acc), imag(acc));
 	}
@@ -74,6 +78,93 @@ COMPLEX riesz_mink(COMPLEX z, int level)
 	// Normalize
 	acc /= quest;
 
+	acc /= 2.0 * M_PI;
+
+	return acc;
+}
+
+/**
+ * The  Riesz–Herglotz transform on the minkowski measure.
+ * `level` is the iteration level to recurse to.
+ */
+COMPLEX riesz_mink(COMPLEX z, int level)
+{
+	int npts = (1<<level);
+	static bool init = false;
+	static double* midpoint = nullptr;
+	static double* measure = nullptr;
+	static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	
+	if (not init)
+	{
+		pthread_mutex_lock(&mutex);
+		if (not init) {
+
+		unsigned __int128 p, q, pm, qm, pmid, qmid;
+
+		// Golden ratio
+		double phi = 0.5 * (1.0 + sqrt(5.0));
+
+		// Growth rate
+		double grow = pow(0.5 * phi * phi, level) * 0.2 * phi * phi * phi;
+
+		double* midpoint = (double*) malloc(npts*sizeof(double));
+		double* measure = (double*) malloc(npts*sizeof(double));
+		double norm = 1.0 / (double) npts;
+		double quest = 0.0;
+		for (int i=0; i<npts; i++)
+		{
+			stern_brocot_tree128(i, level, &p, &q);
+			stern_brocot_tree128(i+1, level, &pm, &qm);
+
+			// a and b are the upper and lower endpoints of the interval
+			double a = ((double) p) / (double) q;
+			double b = ((double) pm) / (double) qm;
+			stern_brocot_tree128(2*i+1, level+1, &pmid, &qmid);
+
+			// The midpoint of the interval.
+			double y = ((double) pmid) / (double) qmid;
+
+			// measure: the value of the measure at the midpoint.
+			double meas = norm * q * qm;
+			meas /= grow;
+			meas *= (b-a);
+
+			// The integral of the masure: should be the question mark.
+			// up to a scale factor
+			quest += meas;
+			midpoint[i] = y;
+			measure[i] = meas;
+		}
+
+		// Normalize
+		for (int i=0; i<npts; i++)
+		{
+			measure[i] /= quest;
+		}
+		init = true;
+		}
+		pthread_mutex_unlock(&mutex);
+	}
+
+	COMPLEX acc = 0.0;
+	for (int i=0; i< (1<<level); i++)
+	{
+		double y = midpoint[i];
+		// theta ranges -pi to pi
+		double theta = (2.0*y - 1.0) * M_PI;
+		COMPLEX it = I*theta;
+		COMPLEX eit = exp(it);
+
+		// The kernel
+		COMPLEX krn = (eit + z) / (eit - z);
+
+		acc += krn * measure[i];
+
+		// printf("%d	%g	%g	%g	%g	%g\n", i, y, measure, quest, real(acc), imag(acc));
+	}
+
+	// Normalize
 	acc /= 2.0 * M_PI;
 
 	return acc;
