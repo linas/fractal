da2112ec1427386984dad22765a0996b1a72e63c
Linas Vepstas <linasvepstas@gmail.com>
2018-02-03 13:11:02 -0600

    done with first draft

--- misc/bitops/psileft.c
+++ misc/bitops/psileft.c
@@ -5,6 +5,7 @@
  *
  * February 2018
  */
+#include <complex.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -13,6 +14,22 @@
 #define NOMAIN
 #include "psi.c"
 #include "psibig.c"
+
+
+void bergman_vect(double Kay, complex z, complex* poly, int maxn)
+{
+	poly[0] = 1.0;
+	for (int n=1; n<maxn; n++)
+	{
+		complex acc = z * poly[n-1];
+		for (int k=0; k<n; k++)
+		{
+			acc += hess(Kay, k, n-1) * poly[k];
+		}
+		acc /= hess(Kay, n, n-1);
+		poly[n] = acc;
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -28,4 +45,13 @@ int main(int argc, char* argv[])
 	// find_midpoints(K);
 	big_midpoints(K, 400, midpoints, MAXN);
 	sequence_midpoints(K, MAXN);
+
+	complex poly[MAXN];
+	complex z = 1;
+	bergman_vect(K, z, poly, MAXN);
+
+	for (int i=0; i<maxn; i++)
+	{
+		printf("%d	%g	%g\n", i, creal(poly[i]), cimag(poly[i]));
+	}
 }
