281001d491ac65b5a7d70512c49418c23ae3d2b4
Linas Vepstas <linasvepstas@gmail.com>
2018-02-05 18:50:41 -0600

    Another limiting formula

--- misc/bitops/cee.sh
+++ misc/bitops/cee.sh
@@ -5,7 +5,8 @@ while [ $i -lt 1000 ];
 do
    let K=$i
 	# echo $K
-   ./psileft $K 450
+   # ./psileft $K 450
+   ./psimom $K 140
 	let i=$i+1
 done
 
--- misc/bitops/psimom.c
+++ misc/bitops/psimom.c
@@ -26,6 +26,8 @@ int main(int argc, char* argv[])
 	double K = atof(argv[1]);
 	int maxn = atoi(argv[2]);
 
+K = 0.81 + K*0.001 * 0.19;
+
 	printf("#\n# K=%g\n#\n", K);
 
 	// find_midpoints(K);
@@ -33,17 +35,19 @@ int main(int argc, char* argv[])
 	sequence_midpoints(K, MAXN);
 
 	// Get a high-accuracy ratio
-	int n = 90;
-	int m = 90;
+	int n = maxn;
+	int m = maxn;
 	double prev = herm(K, n, m-1);
 	double sym = herm(K, n, m);
 	double rat = sym/prev;
 
 	double pnt = pow(rat, n+m+1);
 	double lng = sym/pnt;
-	printf("# %g	%d	%d sym=%g rat=%g f=%g\n#\n", K, n, m, sym, rat, lng);
+	// printf("# %g	%d	%d sym=%g rat=%g f=%g\n#\n", K, n, m, sym, rat, lng);
+	printf("%g	%d	%d %g %g %g\n", K, n, m, sym, rat, lng);
 
-#define PRINT_MATRIX
+
+// #define PRINT_MATRIX
 #ifdef PRINT_MATRIX
 	for (int n=0; n<maxn; n++)
 	{
