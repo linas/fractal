e75081acf212330d349d262860d3abd66d445eb8
Linas Vepstas <linasvepstas@gmail.com>
2018-02-08 15:15:14 -0600

    midpoint hackery

--- misc/bitops/psimid.c
+++ misc/bitops/psimid.c
@@ -15,6 +15,29 @@
 #include "psibig.c"
 #undef NOMAIN
 
+double bisect(double Klo, double Khi, int which)
+{
+	double Kmi = 0.5 * (Klo + Khi);
+	if (Khi - Klo < 1.0e-16) return Kmi;
+
+	big_midpoints(Klo, 400, midpoints, which+4);
+	sequence_midpoints(Klo, which+4);
+	double mlo = midpoints[which];
+
+	big_midpoints(Khi, 400, midpoints, which+4);
+	sequence_midpoints(Khi, which+4);
+	double mhi = midpoints[which];
+
+	big_midpoints(Kmi, 400, midpoints, which+4);
+	sequence_midpoints(Kmi, which+4);
+	double mmi = midpoints[which];
+
+printf("Kmi=%g   mmi=%g\n", Kmi, mmi);
+
+	if (mmi > mhi) return bisect(Kmi, Khi, which);
+	if (mlo < mmi) { printf("Errrror !!!!!!!!!\n"); exit(1); }
+	return bisect(Klo, Kmi, which);
+}
 
 int main(int argc, char* argv[])
 {
@@ -24,8 +47,14 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 	int maxn = atoi(argv[1]);
+	double guess = atof(argv[2]);
 
-#define NPTS 1701
+	double bad = bisect(guess-0.01, guess+0.01, maxn);
+
+	printf("Its %d	%g\n", maxn, bad);
+
+#ifdef PRINT_MIDPOINTS
+#define NPTS 8701
 	for (int i=0; i<NPTS; i++)
 	{
 		double x = ((double) i + 0.5) / ((double) NPTS);
@@ -42,4 +71,6 @@ int main(int argc, char* argv[])
 		}
 		printf("\n");
 	}
+#endif
+
 }
