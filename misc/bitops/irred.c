9b44cb0f9b1000a93b2ced62d31af4593c265590
Linas Vepstas <linasvepstas@gmail.com>
2018-02-11 10:34:21 -0600

    find zero

--- misc/bitops/irred.c
+++ misc/bitops/irred.c
@@ -10,7 +10,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /* Return polynomial for bitstr */
 double beta(int n, double x)
 {
@@ -26,6 +25,15 @@ double beta(int n, double x)
 	return xn - acc;
 }
 
+double find_zero(int n, double lo, double hi)
+{
+	double mid = 0.5 * (lo+hi);
+	if (1.0e-15 > hi-lo) return mid;
+	double fmid = beta(n, mid);
+	if (0.0 < fmid) return find_zero(n, lo, mid);
+	return find_zero(n, mid, hi);
+}
+
 /* Return length of bitstr, length in bits */
 int len(int n)
 {
@@ -39,6 +47,7 @@ int main(int argc, char* argv[])
 {
 	for (int n=0; n<20; n ++)
 	{
-		printf("%d	%d	%g\n", n, len(n), beta(n, 1));
+		double gold = find_zero(n, 1.0, 2.0);
+		printf("%d	%d	%20.18g\n", n, len(n), gold);
 	}
 }
