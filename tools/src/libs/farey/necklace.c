7fae670c9e2c8f9af11eb3baa6467f47115f20d1
Linas Vepstas <linasvepstas@gmail.com>
2018-02-12 11:17:01 -0600

    Work on necklace counting

---
+++ tools/src/libs/farey/necklace.c
@@ -0,0 +1,50 @@
+/*
+ * necklace.c
+ *
+ * Return values for Moreau's necklace-counting function
+ *
+ * Linas Vepstas February 2018
+ */
+
+#include "moebius.h"
+#include "necklace.h"
+#include "cache.h"
+
+/* Moreaus necklace counting function. OEIS A001037 */
+long necklace_raw(int n)
+{
+	long sum = 0;
+	int d = 1;
+	while (d <= n/2)
+	{
+		if (0 == n%d) 
+		{
+			sum += (1<<d) * moebius_mu(n/d);
+		}
+		d += 1;
+	}
+
+	return sum /n;
+}
+
+DECLARE_UL_CACHE (necklace_cache);
+
+long necklace(int n)
+{
+	if (ul_one_d_cache_check (&necklace_cache, n))
+		return ul_one_d_cache_fetch(&necklace_cache, n);
+
+	long val = necklace_raw(n);
+	ul_one_d_cache_store (&necklace_cache, val, n);
+	return val;
+}
+
+
+#include <stdio.h>
+int main (int arc, char* argv[])
+{
+	for (int i=1; i< 12; i++)
+	{
+		printf("%d	%ld\n", i, necklace(i));
+	}
+}
---
+++ tools/src/libs/farey/necklace.h
@@ -0,0 +1,20 @@
+/*
+ * necklace.h
+ *
+ * Return values for Moreau's necklace-counting function
+ *
+ * Linas Vepstas February 2018
+ */
+
+#ifdef   __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Moreau's necklace-counting function
+ */
+long necklace(int);
+
+#ifdef   __cplusplus
+};
+#endif
