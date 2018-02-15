57167f757c7e87d8c62d1f7e52d2bd6b08402884
Linas Vepstas <linasvepstas@gmail.com>
2018-02-15 11:34:52 -0600

    newer improveder

--- misc/bitops/tribo.c
+++ misc/bitops/tribo.c
@@ -5,23 +5,36 @@
  * Linas Vepstas February 2018
  */
 #include <stdio.h>
+#include <stdlib.h>
 
-int main ()
+int main (int argc, char* argv[])
 {
 	int nmax = 50;
-	int seq[nmax];
-
+	unsigned long seq[nmax];
 	seq[0] = 1;
 	seq[1] = 0;
 	seq[2] = 1;
 
-	for (int i=3; i< nmax; i++)
+	if (argc<3)
 	{
-		int sum = 0;
-		for (int j=0; j<3; j++) sum += seq[i-j-1];
+		fprintf(stderr, "Usage: %s len vals...", argv[0]);
+		exit(1);
+	}
+
+	int ilen = atoi(argv[1]);
+	for (int j=0; j<ilen; j++)
+	{
+		seq[j] = atoi(argv[2+j]);
+		printf("# Initial sequence: %d	%lu\n", j, seq[j]);
+	}
+
+	for (int i=ilen; i< nmax; i++)
+	{
+		unsigned long sum = 0;
+		for (int j=0; j<ilen; j++) sum += seq[i-j-1];
 		seq[i] = sum;
 
 		double rat = ((double) seq[i]) / ((double) seq[i-1]);
-		printf("%d	%d	%g\n", i, seq[i], rat);
+		printf("%d	%lu	%20.18g\n", i, seq[i], rat);
 	}
 }
