3778a7ac12ab023062db3ed5622c5deafdb30160
Linas Vepstas <linasvepstas@gmail.com>
2018-02-20 11:19:45 -0600

    Draft that compiles

--- misc/bitops/expan.c
+++ misc/bitops/expan.c
@@ -27,11 +27,11 @@ double reig(double x, double K, int niter)
 	// Short-cut. The other branch has vanishing contribution,
 	if (K*(tkay-1.0) < x)
 	{
-		return reig(x/tkay, K, niter-1) / (lambda * tkay);
+		return reig(x/tkay, K, niter-1) / tkay;
 	}
 	double sum = reig(x/tkay, K, niter-1);
 	sum += reig(0.5 + x/tkay, K, niter-1);
-	sum /= lambda * tkay;
+	sum /= tkay;
 	return sum;
 }
 
@@ -39,7 +39,7 @@ double reig(double x, double K, int niter)
 /* Returns the n'th coefficient in the expansion of the invariant
  * measure. Computed by brute-force integration.
  */
-double alpha_n(double K, int n, int npts)
+double alpha_n(double K, int n, int npts, int niter)
 {
 	/* Get the lower, middle and upper bounds */
 	n++; /* Off-by-one! */
@@ -53,31 +53,66 @@ double alpha_n(double K, int n, int npts)
 	for (int i=0; i< npts; i++)
 	{
 		double x = lower + ((double) i + 0.5) * delta;
-		sumlo +=
+		sumlo += reig(x, K, niter);
 	}
+	sumlo *= delta;
+
+	/* integrate the upper arm */
+	delta = (upper-middle) / ((double) npts);
+	double sumhi = 0.0;
+	for (int i=0; i< npts; i++)
+	{
+		double x = lower + ((double) i + 0.5) * delta;
+		sumhi += reig(x, K, niter);
+	}
+	sumhi *= delta;
+
+	/* Combine the arms, according to the wavelet size. */
+	double sum = sumlo / (middle - lower);
+	sum -= sumhi / (upper - middle);
+
+	/* The wavelet normalization */
+	double norm = 1.0 / (middle - lower);
+	norm += 1.0 / (upper - middle);
+	norm = 1.0 / sqrt(norm);
+
+	sum *= norm;
+
+	return sum;
 }
 
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2)
+	if (argc < 4)
 	{
-		fprintf(stderr, "Usage: %s K\n", argv[0]);
+		fprintf(stderr, "Usage: %s K npts niter\n", argv[0]);
 		exit(1);
 	}
 
 	double K = atof(argv[1]);
+	int npts = atoi(argv[2]);
+	int niter = atoi(argv[3]);
+
+	printf("#\n# npts = %d depth=%d\n#\n", npts, niter);
+
+	// Total integral is unity
+	double norm = 0.0;
+	for (int i = 0; i< npts; i++)
+	{
+		double x = ((double) i + 0.5) / ((double) npts);
+		norm += reig(x, K, niter);
+	}
+	norm /= ((double) npts);
+	printf("# tot integral = %g\n", norm);
+	norm = 1.0 / norm;
 
 	find_midpoints(K, MAXN);
 	sequence_midpoints(K, MAXN);
 
-	int npts = 10;
-	double x = K;
-	for (int i=0; i<npts; i++)
+	for (int i=0; i<10; i++)
 	{
-		int bit = 0;
-		if (0.5 < x) bit = 1;
-		printf ("%d %g	%d	%g\n", i, x, bit, midpoints[i]);
-		x = bmap(K, x);
+		double cf = norm * alpha_n(K, i, npts, niter);
+		printf ("%d %g\n", i, cf);
 	}
 }
