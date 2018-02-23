29d200d337c2931b6d73dfdab11c5ed967664506
Linas Vepstas <linasvepstas@gmail.com>
2018-02-23 15:51:05 -0600

    Ongoing data crunching

--- misc/bitops/fpcoef.c
+++ misc/bitops/fpcoef.c
@@ -30,25 +30,59 @@ int main(int argc, char* argv[])
 
 	printf("#\n# npts = %d depth=%d\n#\n", npts, niter);
 
-	int nkv=600;
+	int nkv=800;
 	for (int i=0; i< nkv; i++)
 	{
-		double K = ((double) i + 0.5) / ((double) nkv);
-		K = 0.5 + 0.5*K;
+		double un = ((double) i + 0.5) / ((double) nkv);
+		// double K = 0.5 + 0.5*un;
+		// double K = 0.62 - 0.12*un;
+		double K = 1.0 - 0.441*un;
 
 		find_midpoints(K, MAXN);
 		// big_midpoints(K, 400, midpoints, MAXN);
 		sequence_midpoints(K, MAXN);
 
+#ifdef OLD_SCHOOL
 		int siter = niter;
 		if (K < 0.8) siter = 1.2*niter;
 		if (K < 0.74) siter = 1.5*niter;
 		if (K < 0.65) siter = 2*niter;
+		if (K < 0.60) siter = 2.5*niter;
 		if (K < 0.57) siter = 3*niter;
 		if (K < 0.55) siter = 5*niter;
+		if (K < 0.53) siter = 8*niter;
+		if (K < 0.51) siter = 12*niter;
 		double cf0 = alpha_n(K, 0, npts, siter);
 		double cf1 = alpha_n(K, 1, npts, siter);
-		printf ("%d %g	%g\n", i, 2.0*K, cf1/cf0);
+		double cf2 = alpha_n(K, 2, npts, siter);
+		double cf3 = alpha_n(K, 3, npts, siter);
+		printf ("%d %d	%g	%g	%g	%g	%g\n", i, siter, 2.0*K, cf0, cf1, cf2, cf3);
+#endif
+		int siter = niter;
+		double cf = alpha_n(K, 1, npts, siter);
+		siter++;
+		double cfn = alpha_n(K, 1, npts, siter);
+		while (1.0e-4 < fabs(cf-cfn))
+		{
+			cf = cfn;
+			siter++;
+			cfn = alpha_n(K, 1, npts, siter);
+		}
+
+		// Save - it will never get easier; only harder.
+		niter = siter-1;
+
+		double cf0 = alpha_n(K, 0, npts, siter);
+		// double cf1 = alpha_n(K, 1, npts, siter);
+		double cf1 = cfn;
+		printf ("%d %d	%g	%g	%g", i, siter, 2.0*K, cf0, cf1);
+		fflush(stdout);
+		for (int ord=2; ord<11; ord++)
+		{
+			double cf = alpha_n(K, ord, npts, siter);
+			printf("	%g", cf);
+		}
+		printf("\n");
 		fflush(stdout);
 	}
 }
