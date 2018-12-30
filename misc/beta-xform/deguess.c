149e3daedff975a01bdd81da51f0712fdc44043d
Linas Vepstas <linasvepstas@gmail.com>
2018-12-30 17:58:10 -0600

    test and retest

--- misc/beta-xform/deguess.c
+++ misc/beta-xform/deguess.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double complex rho(double x, double beta, double lambda)
+double complex rhogu(double x, double beta, double lambda)
 {
 	double co = cos(M_PI*lambda);
 	double si = sin(M_PI*lambda);
@@ -50,8 +50,37 @@ double complex rho(double x, double beta, double lambda)
 
 int main (int argc, char* argv[])
 {
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s K lambda\n", argv[0]);
+		exit(1);
+	}
+
 	double K = atof(argv[1]);
-	double x = atof(argv[2]);
+	double lambda = atof(argv[2]);
 	double beta = 2.0*K;
+
+#define NPTS 801
+	double rerho[NPTS];
+	double imrho[NPTS];
+	double ms = 0.0;
+	for (int i=0; i< NPTS; i++)
+	{
+		double x = ((double)i + 0.5) / ((double) NPTS);
+		double complex rho = rhogu(x, beta, lambda);
+		rerho[i] = creal(rho);
+		imrho[i] = cimag(rho);
+		ms += rerho[i]*rerho[i] + imrho[i]*imrho[i];
+	}
+	double rms = sqrt(ms / (double) NPTS);
+	for (int i=0; i< NPTS; i++)
+	{
+		rerho[i] / = rms;
+		imrho[i] / = rms;
+	}
+	for (int i=0; i< NPTS; i++)
+	{
+		double x = ((double)i + 0.5) / ((double) NPTS);
+		printf("%d	%g	%g	%g\n", i, x, rerho[i], imrho[i]);
+	}
 
 }
