c2ea7c426ef2eea974e0c38793b2d0ac0384f85b
Linas Vepstas <linasvepstas@gmail.com>
2018-12-30 23:27:44 -0600

    More games

--- misc/beta-xform/deguess.c
+++ misc/beta-xform/deguess.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// This performs the sum to get the Renyi-Gelfond-Parry invariant
+// measure, but rotated by exp(i\pi\lambda)
 double complex rhogu(double x, double beta, double lambda)
 {
 	x /= 0.5*beta;
@@ -50,6 +52,41 @@ double complex rhogu(double x, double beta, double lambda)
 	return rho_r + I* rho_i;
 }
 
+// Same as above, but for general z.
+double complex rhoz(double x, double beta, complex double z)
+{
+	x /= 0.5*beta;
+	if (1.0 < x) return 0.0;
+
+	double ob = 1.0/beta;
+	double obn = 1.0;
+	complex double zn = 1.0;
+
+	// accumulated sum
+	complex double rho = 0.0;
+
+	double tn = 1.0;
+
+	while (1.0e-18 < obn)
+	{
+		if (x < tn) {
+			rho += obn * zn;
+		}
+
+		// compute 1/beta^N
+		obn *= ob;
+
+		// compute xform^N(1)
+		tn *= beta;
+		if (1.0 < tn) tn -= 1.0;
+
+		// compute z^n;
+		zn *= z;
+	}
+
+	return rho;
+}
+
 // xfer operator
 void cxfer(double complex* dest, double complex* src, int npts, double beta)
 {
@@ -69,7 +106,7 @@ void cxfer(double complex* dest, double complex* src, int npts, double beta)
 	}
 }
 
-void do_stuff(double beta, double lambda)
+void do_unitary(double beta, double lambda)
 {
 
 #define NPTS 3801
@@ -78,24 +115,23 @@ void do_stuff(double beta, double lambda)
 	for (int i=0; i< NPTS; i++)
 	{
 		double x = ((double)i + 0.5) / ((double) NPTS);
-		double complex rhog = rhogu(x, beta, lambda);
-		rho[i] = rhog;
-#if INSANE
+		rho[i] = rhogu(x, beta, lambda);
+#if MODULUS_PHASE
+		double re = creal(rho[i]);
+		double im = cimag(rho[i]);
 		double rms = sqrt(re*re + im*im);
-		if (1.0e-14 < rms)
-		{
-			rerho[i] = re / rms;
-			imrho[i] = im / rms;
-		}
+		double phase = atan2(im, re);
 #endif
 		sum += creal(rho[i]);
 	}
+
+#ifdef RENORMLIZE
 	double norm = sum / (double) NPTS;
-norm = 1.0;
 	for (int i=0; i< NPTS; i++)
 	{
 		rho[i] /= norm;
 	}
+#endif
 
 	double complex trho[NPTS];
 	cxfer(trho, rho, NPTS, beta);
@@ -103,9 +139,9 @@ norm = 1.0;
 	double co = cos(M_PI*lambda);
 	double si = -sin(M_PI*lambda);
 
-// #define SHOW_DENSITY
+#define SHOW_DENSITY
 #ifdef SHOW_DENSITY
-	// This graphs the since and cosine density
+	// This graphs the sine and cosine density
 	for (int i=0; i< NPTS; i++)
 #else /* SHOW_DENSITY */
 	int i = 0.2*NPTS;
@@ -118,7 +154,42 @@ norm = 1.0;
 		double tim = cimag(trho[i]);
 		double gre = re*co - im*si;
 		double gim = im*co + re*si;
-x=lambda;
+// x=lambda;
+		printf("%d	%g	%g	%g	%g	%g	%g	%g	%g\n", i, x,
+			re, im,
+			tre, tim,
+			tre-gre, tim-gim, tre-gre-(tim-gim)
+			);
+	}
+}
+
+void do_zee(double beta, double complex zee)
+{
+
+// #define NPTS 3801
+	double complex rho[NPTS];
+	double sum = 0.0;
+	for (int i=0; i< NPTS; i++)
+	{
+		double x = ((double)i + 0.5) / ((double) NPTS);
+		rho[i] = rhoz(x, beta, zee);
+		sum += creal(rho[i]);
+	}
+
+	double complex trho[NPTS];
+	cxfer(trho, rho, NPTS, beta);
+
+	// This graphs the sine and cosine density
+	for (int i=0; i< NPTS; i++)
+	{
+		double x = ((double)i + 0.5) / ((double) NPTS);
+		double re = creal(rho[i]);
+		double im = cimag(rho[i]);
+		double tre = creal(trho[i]);
+		double tim = cimag(trho[i]);
+		double complex gee = rho[i] / zee;
+		double gre = creal(gee);
+		double gim = cimag(gee);
 		printf("%d	%g	%g	%g	%g	%g	%g	%g	%g\n", i, x,
 			re, im,
 			tre, tim,
@@ -129,17 +200,24 @@ x=lambda;
 
 int main (int argc, char* argv[])
 {
-	if (argc < 3) {
-		fprintf(stderr, "Usage: %s K lambda\n", argv[0]);
+	if (argc < 4) {
+		fprintf(stderr, "Usage: %s K lambda abszed\n", argv[0]);
 		exit(1);
 	}
 
 	double K = atof(argv[1]);
 	double lambda = atof(argv[2]);
+	double abszed = atof(argv[3]);
 	double beta = 2.0*K;
 
-	// do_stuff(beta, lambda);
+#ifdef UNITARY_ONLY
+	do_unitary(beta, lambda);
+#endif // NITARY_ONLY
 
+	double complex z = abszed * (cos(M_PI*lambda) + I*sin(M_PI*lambda));
+	do_zee(beta, z);
+
+#ifdef EXPLORE_CONST
 #define NLAM 501
 	printf("#\n# Angulare dependence for beta=%g\n#\n", beta);
 	for (int i=0; i< NLAM; i++)
@@ -147,4 +225,5 @@ int main (int argc, char* argv[])
 		double lam = ((double)i + 0.5) / ((double) NLAM);
 		do_stuff(beta, lam);
 	}
+#endif
 }
