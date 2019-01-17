c9f8935c5680a20c6b692a6c9d48c7f5d507beeb
Linas Vepstas <linasvepstas@gmail.com>
2019-01-17 12:38:09 -0600

    Expent range of supported values

--- tools/src/libs/farey/dirichlet.c
+++ tools/src/libs/farey/dirichlet.c
@@ -14,15 +14,52 @@
  * January 2019
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "cache.h"
 #include "moebius.h"
 
+// Manual cache
+DECLARE_UL_CACHE(k2_cache);
+
+
 long unit(long k, long n)
 {
-	if (0 == k) { return n==0; }
+	if (0 == k) { return n==1; }
 	if (1 == k) { return 1; }
 	if (-1 == k) { return moebius_mu(n); }
 
+	if (1 < k)
+	{
+#define SLOW_BUT_SIMPLE
+#ifdef SLOW_BUT_SIMPLE
+		long sum = 0;
+		for (long d=1; 2*d <= n; d++)
+		{
+			if (n%d) continue;
+			sum += unit(k-1, d);
+		}
+		sum += unit(k-1,n);
+		return sum;
+#endif // SLOW_BUT_SIMPLE
+	}
+	if (-1 > k)
+	{
+#define ISLOW_BUT_SIMPLE
+#ifdef ISLOW_BUT_SIMPLE
+		long sum = 0;
+		for (long d=1; 2*d <= n; d++)
+		{
+			if (n%d) continue;
+			sum += unit(k+1, d) * moebius_mu(n/d);
+		}
+		sum += unit(k+1,n);
+		return sum;
+#endif // SLOW_BUT_SIMPLE
+	}
+
+	fprintf(stderr, "Internal error Dirichelt!\n");
+	exit(1);
 	return 0;
 }
 
@@ -52,8 +89,29 @@ long test_unit(void)
 		long convo = convoid(1, -1, n);
 		if ((1 < n && 0 != convo) || (0 == n && 1 != convo))
 		{
-			printf ("ERROR: oh no! n=%ld\n", n);
+			printf ("ERROR: oh no! Moebius error at n=%ld\n", n);
 			have_error ++;
+		}
+	}
+	return have_error;
+}
+
+long test_diff(int d)
+{
+	long have_error=0;
+	long nmax=10000;
+	for (long n=1; n<=nmax; n++)
+	{
+		for (int k=-d; k<= +d; k++)
+		{
+			long convo = convoid(k, d-k, n);
+			long diff = unit(d, n);
+			if (diff != convo)
+			{
+				printf ("ERROR: disadditive at diff=%d %d n=%ld got=%ld expected=%ld\n",
+					k, k+d, n, convo, diff);
+				have_error ++;
+			}
 		}
 	}
 	return have_error;
@@ -62,6 +120,8 @@ long test_unit(void)
 int main()
 {
    long nerr = test_unit();
+   nerr += test_diff(0);
+   nerr += test_diff(1);
 
 	if (0 == nerr) printf("Dirichlet unit test passed\n");
 	return nerr;
