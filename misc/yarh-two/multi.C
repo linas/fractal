7b44169799033c8a704da98104e48befda8afcc4
Linas Vepstas <linasvepstas@gmail.com>
2019-03-07 11:33:29 -0600

    Start work on multiplicative functions

---
+++ misc/yarh-two/multi.C
@@ -0,0 +1,14 @@
+/*
+ * multi.C
+ *
+ * Exploration of zeta series for completely multiplicative functions
+ * Linas Vepstas March 2019
+ */
+
+#include "prime.h"
+
+double plic (int n)
+{
+	// if n is prime, we are done.
+	return 0.0;
+}
