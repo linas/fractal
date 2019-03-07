5b4a1d423082c265f86c0cb25ab8acbd994fa054
Linas Vepstas <linasvepstas@gmail.com>
2019-03-07 10:02:56 -0600

    start work on a new thing

---
+++ tools/src/libs/farey/euler.c
@@ -0,0 +1,14 @@
+/*
+ * euler.c
+ *
+ * Implement Euler summation for some complex-valued function.
+ * viz, given f(n) return
+ *
+ * sum_{n=0}^\infty 2^{-(n+1)} \sum_{k=0}^n {n choose k} f(k)
+ *
+ * See wikipedia for more about Euler summation.
+ *
+ * March 2019  -- Linas Vepstas
+ */
+
+
