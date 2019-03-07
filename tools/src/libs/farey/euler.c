97c128f36ea52aad18bfa6c786c454c3c13c67db
Linas Vepstas <linasvepstas@gmail.com>
2019-03-07 10:32:32 -0600

    Implement

--- tools/src/libs/farey/euler.c
+++ tools/src/libs/farey/euler.c
@@ -33,15 +33,45 @@ complex euler_sum(arithmetic);
 
 #endif /* EULER_H__ */
 
-complex euler_sum(arithmetic f)
+#include "binomial.h"
+
+complex euler_sum(arithmetic fun)
 {
 	complex sum = 0.0;
-	double tn = 2.0;
+	double tn = 0.5;
 	int n = 0;
 	while (1)
 	{
+		complex term = 0.0;
+		for (int k=0; k<=n; k++)
+		{
+			term += binomial(n, k) * fun(k+1);
+		}
+		term *= tn;
+		sum += term;
+		if (cabs(term) < 1.0e-20) return sum;
 		n++;
-		tn *= 2.0;
+		tn *= 0.5;
 	}
 	return 0.0;
 }
+
+#define TEST
+#ifdef TEST
+
+#include <stdio.h>
+
+complex z = 0.0;
+complex zee(int n) { return cpow(z, n-1); }
+
+int main()
+{
+	z = 0.7 + I*0.5;
+	complex rslt = euler_sum(zee);
+	complex xpct = 1.0 / (1.0-z);
+	printf("z=%f + i%f  got %f + i%f expect %f + i%f\n",
+		creal(z), cimag(z), creal(rslt), cimag(rslt), creal(xpct), cimag(xpct));
+	complex diff = rslt - xpct;
+	printf("diff %g + i%g\n", creal(diff), cimag(diff));
+}
+#endif
