8c65ff5997f220196f55b04a5f0601e51f692259
Linas Vepstas <linasvepstas@gmail.com>
2019-03-08 01:46:31 -0600

    Debugging ...

--- misc/yarh-two/plic.c
+++ misc/yarh-two/plic.c
@@ -13,13 +13,13 @@
 
 #include "mp-multiplicative.h"
 #include "mp-euler.h"
+#include "mp-trig.h"
 
 complex ess = 0.0;
 
 // When this is called, p is guaranteed to be prime
 void at_prime(cpx_t f, unsigned long p, int nprec)
 {
-	double pr = (double) p;
 	// return sqrt(pr*pr + pr);
 	// return sqrt(pr*pr - 0.5*pr);
 	// return sqrt(pr*pr - 0.0001*pr);
@@ -28,12 +28,26 @@ void at_prime(cpx_t f, unsigned long p, int nprec)
 	// return sqrt(pr*pr + pr);
 	// return sqrt(pr*pr + 1.0e-6*pr);
 	// return sqrt(pr*pr + 1.0e-4*pr);
-	// return sqrt(pr*pr + 0.01*pr);
 
+#define DBL_PREC
+#ifdef DBL_PREC
+	double pr = (double) p;
+	pr = sqrt(pr*pr + 0.01*pr);
 	complex val = pr; // Riemann zeta
 
 	val = cpow(val, -ess);
 	cpx_set_d(f, creal(val), cimag(val));
+#endif
+
+// #define EXACTITUDE
+#ifdef EXACTITUDE
+	cpx_set_ui(f, p, 0);
+	cpx_recip(f, f);
+	cpx_sqrt(f, f, nprec);
+printf("duuude p=%lu ret=%f+i%f vs val=%f+i%f\n", p,
+cpx_get_re(f), cpx_get_im(f),
+creal(val), cimag(val));
+#endif
 }
 
 // Suitable for graphing with gnuplot
@@ -50,24 +64,20 @@ void mktable(int nprec)
 	cpx_clear(result);
 }
 
-
-#if 0
-complex alter(unsigned int n)
+void alter(cpx_t f, unsigned long n, int nprec)
 {
-// printf("duuude alter n=%d\n", n);
-	complex val = plic_fun(n);
-	val = cpow(val, -ess);
-	if (n%2 == 1) return val;
-	return -val;
+	cpx_multiplicative_cached(f, at_prime, n, nprec);
+	if (n%2 == 1) return;
+	cpx_neg(f, f); // flip the sign, alternating series
 }
-#endif
+
 
 int main()
 {
 	int prec, nbits;
 	prec = 120;
 	nbits = 3.3*prec;
-	mpf_set_default_prec (nbits+200);
+	mpf_set_default_prec (nbits+150);
 
 #ifdef TABLE_TEST
 	ess = 2.0;
@@ -77,8 +87,12 @@ int main()
 	cpx_t result;
 	cpx_init2(result, nbits);
 
-	// ess = 0.5 + I*28;
-	// euler_sum_cut(alter, 2500);
+	ess = 0.5 + I*28;
+	ess = 0.5 + I*0;
+	unsigned int nterms = cpx_euler_sum(result, alter, 40, 300000, prec);
+
+	printf("nterms=%d val=%f+i%f\n", nterms,
+		cpx_get_re(result), cpx_get_im(result));
 exit(0);
 #if 0
 	for (double y = 0.0; y<30.0; y+=0.1)
