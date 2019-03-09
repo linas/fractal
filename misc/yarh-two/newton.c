f29be04fdc66ef32450cc8a0a323db4d11c14fcb
Linas Vepstas <linasvepstas@gmail.com>
2019-03-08 19:17:43 -0600

    A semi-working newton series

--- misc/yarh-two/newton.c
+++ misc/yarh-two/newton.c
@@ -1,8 +1,8 @@
 /*
- * plic.c
+ * newton.c
  *
- * Exploration of zeta series for completely multiplicative functions.
- * This is the GMP version
+ * Exploration of Newton series for multiplicative functions.
+ * This is the GMP version.
  *
  * Linas Vepstas March 2019
  */
@@ -11,8 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "mp-multiplicative.h"
 #include "mp-euler.h"
+#include "mp-multiplicative.h"
 #include "mp-trig.h"
 
 complex ess = 0.0;
@@ -55,7 +55,7 @@ creal(val), cimag(val));
 	// pr = sqrt(pr*pr + 0.01*pr);
 	// pr *= 1.01;
 	// pr += 1.0;
-	pr *= 1.5;
+	// pr *= 1.5;
 	cpx_set_d(f, pr, 0.0);
 	cpx_recip(f, f);
 	cpx_pow(f, f, mpess, nprec);
@@ -63,38 +63,23 @@ creal(val), cimag(val));
 	// printf("duuude p=%lu ret=%f+i%f\n", p, cpx_get_re(f), cpx_get_im(f));
 }
 
-// ------------------------------------
-// Suitable for graphing with gnuplot
-void mktable(int nprec)
+void museq(cpx_t f, unsigned long n, int nprec)
 {
-	int nbits = 3.3*nprec;
-	cpx_t result;
-	cpx_init2(result, nbits);
-	for (int n=1; n<633; n++)
-	{
-		cpx_multiplicative_cached(result, at_prime, n, nprec);
-		printf("%d	%g	%g\n", n, 1.0/cpx_get_re(result), cpx_get_im(result));
-	}
-	cpx_clear(result);
+	cpx_multiplicative_cached(f, at_prime, n, nprec);
 }
 
 // ------------------------------------
-void alter(cpx_t f, unsigned long n, int nprec)
-{
-	cpx_multiplicative_cached(f, at_prime, n, nprec);
-	if (n%2 == 1) return;
-	cpx_neg(f, f); // flip the sign, alternating series
-}
 
 
 int main()
 {
 	int prec, nbits;
-	prec = 320;
+	prec = 120;
 	nbits = 3.3*prec;
-	mpf_set_default_prec (nbits+150);
+	mpf_set_default_prec (nbits+50);
 
-	cpx_t result;
+	cpx_t zee, result;
+	cpx_init2(zee, nbits);
 	cpx_init2(result, nbits);
 	cpx_init2(mpess, nbits);
 
@@ -104,16 +89,15 @@ int main()
 	ess = 1.0;
 	cpx_set_d(mpess, creal(ess), cimag(ess));
 
-// #define TABLE_TEST
-#ifdef TABLE_TEST
-	mktable(prec);
-#endif
+	double zz;
+	for (zz=0.0; zz<20.0; zz += 0.1)
+	{
+		cpx_set_d(zee, zz, 0.0);
 
-#define SINGLE_POINT
-#ifdef SINGLE_POINT
-	unsigned int nterms = cpx_euler_sum(result, alter, 240, 300000, prec);
+		unsigned int nterms =
+			cpx_newton_series(result, museq, zee, 10, 1000, prec);
 
-	printf("nterms=%d val=%f+i%f\n", nterms,
-		cpx_get_re(result), cpx_get_im(result));
-#endif
+		printf("z=%g nterms=%d val=%f+i%f\n", zz, nterms,
+			cpx_get_re(result), cpx_get_im(result));
+	}
 }
