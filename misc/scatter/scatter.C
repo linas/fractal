595fc686cc3be110daabab8cef9f5a5a604adce0
Linas Vepstas <linasvepstas@gmail.com>
2019-07-11 17:48:59 -0500

    Final version for now

--- misc/scatter/scatter.C
+++ misc/scatter/scatter.C
@@ -15,24 +15,43 @@ void MakeHisto (
    float    *glob,
    int      sizex,
    int      sizey,
-   double   re_center,
-   double   im_center,
+   double   x_center,
+   double   y_center,
    double   width,
    double   height,
    int      itermax,
    double   renorm)
 {
 	int cnt = 0;
-	double freq;
-	double fmi;
+	float freq;
+	float fmi;
+
 	while (true)
 	{
-		cnt ++;
-		int rc = fscanf(stdin, "%g	%g\n", &freq, &fmi);
+		int rc = scanf("%f\t%f", &freq, &fmi);
+		// printf("duuude got %d %d EOF=%d %g %g\n", cnt, rc, EOF, freq, fmi);
+		if (0 == rc)
+		{
+			char bug[50];
+			fgets(bug, 50, stdin); // evil
+			continue;
+		}
 		if (EOF == rc)
 		{
-printf("duude fail cnt=%d rc=%d\n", cnt, rc);
+			break;
 		}
-if (10 < cnt)break;
+		cnt ++;
+
+freq = - log(freq) / log(2.0);
+
+		int ni = sizex * (0.5 + (freq - x_center) / width);
+		if (ni < 0) ni = 0;
+		if (sizex <= ni) ni = sizex - 1;
+
+		int nj = sizey * (0.5 + (fmi - y_center) / height);
+		if (nj < 0) nj = 0;
+		if (sizey <= nj) nj = sizey - 1;
+
+		glob [nj*sizex + ni] += 1.0;
 	}
 }
