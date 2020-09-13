27aa012d1245a84cbe98d36d19dce68dbcc6fb43
Linas Vepstas <linasvepstas@gmail.com>
2020-09-13 12:07:17 -0500

    Add a picture of the odometer

--- misc/odometers/bernoulli-odo.c
+++ misc/odometers/bernoulli-odo.c
@@ -6,8 +6,9 @@
  * Linas Vepstas September 2020
  */
 
-#include <complex.h>
+#include <stdbool.h>
 #include <math.h>
+#include <stdio.h>
 
 /* increment the odometer by one.
  * Unpack the double-precision float into a string of bits,
@@ -15,15 +16,45 @@
  */
 double increment(double x)
 {
-	double result;
+	double result = 0.0;
+	double bit = 0.5;
+	bool done = false;
 
 	for (int i=0; i<56; i++)
 	{
-		bool bit = (x
+		if (done)
+		{
+			if (0.5 <= x) result += bit;
+		}
+		else
+		{
+			if (x < 0.5)
+			{
+				result += bit;
+				done = true;
+			}
+			else
+			{
+				/* do nothing here; this rolls over */
+			}
+		}
 
-		x *= 2.0
+		x *= 2.0;
 		if (1.0 <= x) x -= 1.0;
+		bit *= 0.5;
 	}
 
 	return result;
+}
+
+int main (int argc, char* argv[])
+{
+	int nbins=901;
+
+	for (int i=0; i<nbins; i++)
+	{
+		double x = ((double) i) / ((double) nbins);
+		double y = increment(x);
+		printf("%d	%g	%g\n", i, x, y);
+	}
 }
