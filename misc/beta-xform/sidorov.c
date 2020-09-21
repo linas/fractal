ac212807da519489adf87a2127405c4aa4c884b3
Linas Vepstas <linasvepstas@gmail.com>
2020-09-21 16:32:14 -0500

    Chop it up

--- misc/beta-xform/sidorov.c
+++ misc/beta-xform/sidorov.c
@@ -11,14 +11,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// The compressor
-double cpr(double y, double K)
+// Note that beta = 2*K
+double sdr(double y, double K)
 {
 	// Iterate on y using mashed Bernoulli, and extract symbol dynamics
 	char nbits[50];
 	for (int i=0; i<50; i++)
 	{
 		if (0.5 <= y)
+		{
 			y -= 0.5;
 			nbits[i] = 1;
 		}
@@ -40,71 +41,6 @@ double cpr(double y, double K)
 	return acc;
 }
 
-double extcpr(double y, double K)
-{
-	if (y < K) return cpr(y, K);
-	return 2.0 * extcpr(y/(2.0*K), K);
-}
-
-
-// should be same as pdr(cpr(x))
-double der(double y, double K)
-{
-	// Iterate on y using mashed Bernoulli, and extract symbol dynamics
-	char nbits[50];
-	for (int i=0; i<50; i++)
-	{
-		if (0.5 <= y)
-		{
-			y -= 0.5;
-			nbits[i] = 1;
-		}
-		else nbits[i] = 0;
-		y *= 2.0*K;
-	}
-
-	// Reconstruct x in a mashed bernoulli sequence.
-	double acc = 1.0e-30;
-	for (int i=0; i<50; i++)
-	{
-		acc *= 1.0 / (2.0*K);
-		if (nbits[50-i-1])
-		{
-			acc += 0.5;
-		}
-	}
-
-	return acc;
-}
-
-double len(double y, double K)
-{
-	int longest = 0;
-	int len = 0;
-	// Iterate on y using mashed Bernoulli, and extract symbol dynamics
-	for (int i=0; i<50; i++)
-	{
-		if (0.5 <= y)
-		{
-			y -= 0.5;
-			len ++;
-			if (longest < len) longest = len;
-		}
-		else
-		{
-			len = 0;
-		}
-		y *= 2.0*K;
-	}
-
-	return longest;
-}
-
-double flen(double K)
-{
-	return -log(2.0*(1.0-K)) / log (2.0*K);
-}
-
 int main (int argc, char* argv[])
 {
 	if (argc < 2)
@@ -113,14 +49,12 @@ int main (int argc, char* argv[])
 		exit (1);
 	}
 	double lam = atof(argv[1]);
-	double rng = atof(argv[2]);
 
-#if 1
-	int npts = 16803;
+	int npts = 163;
 	for (int i=0; i<npts; i++)
 	{
 		double x = (((double) i) + 0.5)/ ((double) npts);
-		double y = pdr (x, lam);
-		double z = cpr (x, lam);
-		printf("%d	%g	%g	%g\n", i, x, y, z);
+		double z = sdr (x, lam);
+		printf("%d	%g	%g\n", i, x, z);
+	}
 }
