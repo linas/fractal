cb28ea104ae875a2cdcd401e6ac094940b0f1c8f
Linas Vepstas <linasvepstas@gmail.com>
2020-09-21 17:28:18 -0500

    First pass verify ground truth

--- misc/beta-xform/sidorov.c
+++ misc/beta-xform/sidorov.c
@@ -8,37 +8,100 @@
  */
 
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// Note that beta = 2*K
-double sdr(double y, double K)
+// Comp[ute the m from the sidorov paper. This is one more than the
+// run of zeros.  It depends only on beta.  Note that:
+// m=1 for K < 0.25 (1+sqrt(5)) = 0.80902
+// m=2 up until about 0.878
+// m=3 up until about 0.967
+int emrun(double K)
 {
-	// Iterate on y using mashed Bernoulli, and extract symbol dynamics
-	char nbits[50];
+	double beta = 2.0*K;
+	double loga = (beta - 1.0) / (2.0-beta);
+	loga = log(loga) / log(beta);
+	loga = floor(loga) + 1.0;
+	return (int) loga;
+}
+
+// Note that beta = 2*K
+double sdr(double y, double K, int em)
+{
+	// Generate the beta expansion bits in a greedy fashion.
+	char grebits[50];
+	double greedy[50];
 	for (int i=0; i<50; i++)
 	{
+		greedy[i] = y;
 		if (0.5 <= y)
 		{
 			y -= 0.5;
-			nbits[i] = 1;
+			grebits[i] = 1;
 		}
-		else nbits[i] = 0;
+		else grebits[i] = 0;
 		y *= 2.0*K;
 	}
 
-	// Reconstruct x in a ordinary bernoulli sequence.
-	double acc = 1.0e-30;
-	for (int i=0; i<50; i++)
+	// Search for em runs.
+	char lobits[50];
+	for (int i=0; i<50-em; i++)
 	{
-		acc *= 0.5;
-		if (nbits[50-i-1])
+		lobits[i] = grebits[i];
+		if (1 == grebits[i])
 		{
-			acc += 0.5;
+			bool found = true;
+			for (int j=1; j<em; j++)
+			{
+				if (1 == grebits[i+j])
+				{
+					found = false;
+					break;
+				}
+			}
+			if (found)
+			{
+				lobits[i] = 0;
+				y = greedy[i];
+				printf("got one at i=%d y=%g next=%g\n", i, y, y*2*K);
+				y *= 2.0*K;
+
+				for (int j=i+1; j<50; j++)
+				{
+					if (0.5 <= y)
+					{
+						y -= 0.5;
+						lobits[j] = 1;
+					}
+					else lobits[j] = 0;
+					y *= 2.0*K;
+				}
+				break;
+			}
 		}
 	}
 
-	return acc;
+	double Jay = K;
+
+	// Reconstruct both sequences;
+	double hiacc = 1.0e-30;
+	double loacc = 1.0e-30;
+	for (int i=0; i<50; i++)
+	{
+		hiacc *= 1.0 / (2.0*Jay);
+		loacc *= 1.0 / (2.0*Jay);
+		if (grebits[50-i-1])
+		{
+			hiacc += 0.5;
+		}
+		if (lobits[50-i-1])
+		{
+			loacc += 0.5;
+		}
+	}
+
+	return hiacc-loacc;
 }
 
 int main (int argc, char* argv[])
@@ -48,13 +111,18 @@ int main (int argc, char* argv[])
 		fprintf(stderr, "Usage: %s K\n", argv[0]);
 		exit (1);
 	}
-	double lam = atof(argv[1]);
+	double Kay = atof(argv[1]);
 
-	int npts = 163;
+	int em = emrun(Kay);
+	printf("#\n# K=%g m=%d\n#\n", Kay, em);
+
+#if 1
+	int npts = 13;
 	for (int i=0; i<npts; i++)
 	{
 		double x = (((double) i) + 0.5)/ ((double) npts);
-		double z = sdr (x, lam);
+		double z = sdr (x, Kay, em);
 		printf("%d	%g	%g\n", i, x, z);
 	}
+#endif
 }
