7661a3d2f16ac31565a2c2d25bb9b14b7483c41d
Linas Vepstas <linasvepstas@gmail.com>
2020-10-07 21:48:17 -0500

    Verifier works

--- misc/beta-expand/extended-debug.C
+++ misc/beta-expand/extended-debug.C
@@ -31,16 +31,16 @@ int main (int argc, char* argv[])
 	int em = emrun(Kay);
 	double a = 0.5;
 	double b = a * (1.0 + pow(beta, -em));
-	printf("#\n# K=%g m=%d b=%g\n#\n", Kay, em, b);
 
 #define EPS 1e-20
 	int nbits = -log(EPS) / log(beta);
 
 	do_init(nbits);
 
-	mpf_class ebeta;
-	make_random_bitsequence(ebeta, beta, nbits, 1);
+	mpf_class ebeta = beta;
+	double debeta = mpf_get_d(ebeta.get_mpf_t());
 
+	printf("#\n# K=%g em=%d ebeta=%g nbits=%d b=%g\n#\n", Kay, em, debeta, nbits, b);
 
 #define NSAMP 1
 	for (int i=0; i<NSAMP; i++)
@@ -48,14 +48,14 @@ int main (int argc, char* argv[])
 		long int r = random();
 		double x = ((double) r) / ((double) RAND_MAX);
 
-		mpf_class ex;
-		make_random_bitsequence(ex, x, nbits, 1);
+		mpf_class ex=x;
 
 		std::vector<std::vector<mpf_class>> orbit_set;
 		std::vector<std::vector<bool>> bitset;
 		std::vector<std::vector<int>> branch_set;
 		beta_expand(ex, ebeta, em, maxdepth, orbit_set, bitset, branch_set, nbits);
 
+		printf("initial %g ex=%g\n", x, mpf_get_d(ex.get_mpf_t()));
 
 		int ntracks = bitset.size();
 		printf("totoal of %d tracks\n", ntracks);
@@ -65,14 +65,9 @@ int main (int argc, char* argv[])
 			std::vector<int> branch_points = branch_set[j];
 			std::vector<bool> bits = bitset[j];
 
-			size_t nb = branch_points.size();
-			size_t norb = 2*branch_points[nb-1] - branch_points[nb-2];
-			if (orbit.size() <= norb) norb = orbit.size() -1;
-			printf("orbit=%lu\n", norb);
-
 			double y = 0.0;
 			double ob = 0.5;
-			for (int k=0; k<nbits; j++)
+			for (int k=0; k<(int)bits.size(); k++)
 			{
 				y += bits[k] * ob;
 				ob /= beta;
@@ -80,7 +75,7 @@ int main (int argc, char* argv[])
 			// if (9.0*EPS < fabs(y-x))
 			{
 				// prt_bits();
-				printf("i=%d x= %g  expand= %g diff= %g\n", i, x, y, y-x);
+				printf("j=%d x= %g  expand= %g diff= %g\n", j, x, y, y-x);
 				printf("\n");
 			}
 		}
