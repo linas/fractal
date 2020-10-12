2c3c27143b0ee24f8aaa226561dab643d413ee17
Linas Vepstas <linasvepstas@gmail.com>
2020-10-12 13:30:19 -0500

    Also compute the integrals, for normailzation.

--- misc/beta-expand/branching-coeff.C
+++ misc/beta-expand/branching-coeff.C
@@ -12,14 +12,22 @@ int main (int argc, char* argv[])
 {
 	if (argc < 3)
 	{
-		fprintf(stderr, "Usage: %s nbits depth\n", argv[0]);
+		fprintf(stderr, "Usage: %s y nbits depth\n", argv[0]);
 		exit (1);
 	}
-	int base_nbits = atoi(argv[1]);
-	int maxdepth = atoi(argv[2]);
+	double why = atoi(argv[1]);
+	int base_nbits = atoi(argv[2]);
+	int maxdepth = atoi(argv[3]);
 #define MAXDEPTH maxdepth
 
 #define NBINS 503
+
+	printf("#\n# Step function sums at y=%g nbits=%d depth=%d\n#\n",
+	      why, base_nbits, maxdepth);
+
+	printf("# column labels\n"
+	       "# beta, em, theta(beta/2-y), theta(alpha-y), theta(alpha-beta/2-y),\n"
+	       "# integrals for each of the three above.\n#\n");
 
 	for (int i=0; i< NBINS; i++)
 	{
@@ -56,56 +64,68 @@ int main (int argc, char* argv[])
 		// printf("# ntracks=%d etrack=%d\n", ntracks, etracks);
 		if (ntracks < (1<<MAXDEPTH) or etracks < (1<<MAXDEPTH)) continue;
 
-		// printf("# K=%g beta=%g alpha=%g m=%d nbits=%d\n", Kay, dbeta, dalpha, em, nbits);
+		// printf("# K=%g beta=%g alpha=%g\n", Kay, dbeta, dalpha);
 		// printf("# (beta-1)(1-alpha) = %g\n", (dbeta-1.0)*(1.0-dalpha));
 		// printf("# 2(beta-1)(1-alpha) = %g\n", 2*(dbeta-1.0)*(1.0-dalpha));
-		printf("%g	%d", dbeta, em);
 
-		for (int is=0; is<4; is++)
+		double y = why;
+
+		double xacc = 0.0;
+		double uacc = 0.0;
+		double macc = 0.0;
+
+		double Sxacc = 0.0;
+		double Suacc = 0.0;
+		double Smacc = 0.0;
+
+		double bpn = 1.0;
+
+		// Get a reasonable amount of precision for small beta.
+		int kmax=-log(1e-6)/log(dbeta);
+		if ((int) bitset[0].size() < kmax) kmax=bitset[0].size() -1;
+
+		// Sum over powers 1/beta^k
+		for (int k=0; k<kmax; k++)
 		{
-			double y = 0.4*is + 1e-3;
-
-			double xacc = 0.0;
-			double uacc = 0.0;
-			double macc = 0.0;
-			double bpn = 1.0;
-
-int kmax=-log(1e-6)/log(dbeta);
-if (bitset[0].size() < kmax) kmax=bitset[0].size() -1;
-			for (int k=0; k<kmax; k++)
+			// Average over paths gamma
+			for (int j=0; j<ntracks; j++)
 			{
-				for (int j=0; j<ntracks; j++)
-				{
-					// std::vector<bool>& bits = bitset[j];
-					std::vector<mpf_class>& orbit = orbit_set[j];
-					std::vector<int>& branch_points = branch_set[j];
+				// std::vector<bool>& bits = bitset[j];
+				std::vector<mpf_class>& orbit = orbit_set[j];
+				std::vector<int>& branch_points = branch_set[j];
 
-					size_t nb = branch_points.size();
-					size_t norb = 2*branch_points[nb-1] - branch_points[nb-2];
-					if (orbit.size() <= norb) norb = orbit.size() -1;
-					if ((int) norb < k) continue;
+				size_t nb = branch_points.size();
+				size_t norb = 2*branch_points[nb-1] - branch_points[nb-2];
+				if (orbit.size() <= norb) norb = orbit.size() -1;
+				if ((int) norb < k) continue;
 
-					// std::vector<bool>& ebits = ebitset[j];
-					std::vector<mpf_class>& erbit = erbit_set[j];
-					// std::vector<int>& ebranches = branch_set[j];
+				// std::vector<bool>& ebits = ebitset[j];
+				std::vector<mpf_class>& erbit = erbit_set[j];
+				// std::vector<int>& ebranches = branch_set[j];
 
-					double x = mpf_get_d(orbit[k].get_mpf_t());
-					double xu = mpf_get_d(erbit[k].get_mpf_t());
+				double x = mpf_get_d(orbit[k].get_mpf_t());
+				double xu = mpf_get_d(erbit[k].get_mpf_t());
 
-					if (y < x) xacc += bpn;
-					if (y < xu) uacc += bpn;
-					if (y < xu - x) macc -= bpn;
-				}
-				bpn /= dbeta;
+				if (y < x) xacc += bpn;
+				if (y < xu) uacc += bpn;
+				if (y < xu - x) macc += bpn;
+
+				// These are the integrals - the normalization.
+				Sxacc += x*bpn;
+				Suacc += xu*bpn;
+				if (0 < xu-x) Smacc += (xu-x)*bpn;
 			}
-			xacc /= ntracks;
-			uacc /= ntracks;
-			macc /= ntracks;
-			printf("	%g	%g	%g	%g", y, xacc, uacc, macc);
+			bpn /= dbeta;
 		}
-		printf("\n");
+		xacc /= ntracks;
+		uacc /= ntracks;
+		macc /= ntracks;
+		Sxacc /= ntracks;
+		Suacc /= ntracks;
+		Smacc /= ntracks;
+		printf("%g	%d	%g	%g	%g	%g	%g	%g\n",
+		       dbeta, em, xacc, uacc, macc, Sxacc, Suacc, Smacc);
 	}
-
 }
 
 // ================================================================
