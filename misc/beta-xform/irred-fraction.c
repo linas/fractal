fb4545ed60ff949457d558f95aa0fdb9ce4e7505
Linas Vepstas <linasvepstas@gmail.com>
2020-10-18 17:48:43 -0500

    start work on verifying the continued fractions

--- misc/beta-xform/irred-fraction.c
+++ misc/beta-xform/irred-fraction.c
@@ -1,8 +1,8 @@
 /*
- * irred-viz.c
+ * irred-fraction.c
  *
  * Find integer sequence for the golden polynomials.
- * And visualize it.  ... Nothing interesting happened here, dead end.
+ * Relate it to the continued-fraction representation.
  * See irred.c for additional utilities.
  *
  * February 2018, October 2020
@@ -81,6 +81,7 @@ bool zero_is_bracketed(int n, double gold)
 
 	return ork;
 }
+
 /**
  * Find the single, unique real zero of the n'th golden polynomial.
  * If the value of `n` does not really correspond to a golden
@@ -99,59 +100,42 @@ double find_gold(int n)
 	return 0.0;
 }
 
+/*
+ * Given the continue-fraction representation, return the
+ * corresponding sequence number.
+ */
+long sequence_from_cf(int cfrac[], int len)
+{
+	if (1 == len) return 1 << cfrac[0];
+
+	if (3 < len) return -1; // unknown.
+
+	long leader = sequence_from_cf(cfrac, len-1);
+
+	long follower = 2*leader + 1;
+	follower *= 1 << (cfrac[0] + cfrac[len-1]);
+
+	return follower;
+}
+
+/*
+ * Iterate on the continued fraction.
+ * i.e. generate sequences
+ * maxdepth == number of doubling steps
+ * maxlength == max length of fraction.
+ * maxn == cutoff fir highest known n
+ */
+void iterate_cf(int cfrac[], int len, int maxdepth, int maxlength, long maxn)
+{
+}
+
 int main(int argc, char* argv[])
 {
 	int nmax = (1<<21) + 1;
 
 	setup_gold(nmax);
 
-	int cnt = 0;
-	int allcnt = 0;
-	int nsum = 0;
-	int plen = 0;
-	int lyn = 1;
-
-	int tord = 1<<plen;
-
 	for (int n=0; n<nmax; n ++)
-	{
-		double gold = find_gold(n);
+		find_gold(n);
 
-		// printf("---------\ngold=%g\n", gold);
-		if (plen != len(n))
-		{
-			printf("# total for len=%d is %d\n", plen, cnt);
-			plen = len(n);
-			tord = 1<<plen;
-			tord /= 2;
-			lyn = cnt;
-			cnt = 0;
-
-			// advance and count lyndon number
-			lyn = 0;
-			for (int k=n+1; k<nmax; k++)
-			{
-				double g = find_gold(k);
-				if (0.5 < g) lyn++;
-				if (len(k) != plen) break;
-			}
-			printf("# next lyndon is %d\n", lyn);
-		}
-		if (0.5 < gold)
-		{
-			cnt ++;
-			allcnt ++;
-			nsum += n;
-
-			// always the case that tord/2 <= n < tord
-			double frac = ((double) n) / ((double) tord);
-			frac = 2.0*(frac - 0.5); // rescale to run 0 to 1.
-
-			// OK, this should work.
-			double seqfrac = ((double) cnt) / ((double) lyn);
-
-			printf("%d	%d %d %d %g %g	%d	%20.18g\n",
-				allcnt, cnt, n, tord, seqfrac, frac, nsum, gold);
-		}
-	}
 }
