dd6aed1dc90de66e600c1b0a27fd40e85701436e
Linas Vepstas <linasvepstas@gmail.com>
2023-10-25 13:27:05 -0500

    More bulk movie conversion

--- generate/movie-poly-cvt.pl
+++ generate/movie-poly-cvt.pl
@@ -11,6 +11,7 @@ $fps = 30;
 $step = 1/ $fps;
 
 $taumax = 300;  # max tau value but also running time in secs
+$nframes = $taumax * $fps; # $taumax seconds at 30 fps
 
 # $fpref == Filename prefix
 # $deci == decimal prefix for output.
@@ -27,14 +28,30 @@ sub cvt_range($imin, $imax, $deci, $sigma, $fpref) {
 		$t2 = int ($tau * 100 - $itau * 100 - $t1*10);
 		print "$i $itau.$t1$t2 \n";
 
-		# system ("ls $fpref$i-*.flo ");
-		system ("cat $fpref$i-*.flo | /home/linas/src/fractal/image/flo2mtv |mtvtoppm | pnmtopng > tmp.png");
-		system ("convert tmp.png -fill black -draw 'rectangle 5,365,183,393' -pointsize 24 -fill white -gravity SouthWest  -annotate +0+5 ' s = $sigma +i $itau.$t1$t2 ' $fpref$deci$i.png");
+		# If we had the full filename, we could have said
+		# if (-f $filename) but we don't have that.
+		# glob() does the same thing, but with globby filenames.
+		# Its a tad slow, but whatever.
+		if (glob("$fpref$i-*.flo")) {
+			print "$i $itau.$t1$t2 \n";
+
+			system ("ls $fpref$i-*.flo ");
+			# system ("cat $fpref$i-*.flo | /home/linas/src/fractal/image/flo2mtv |mtvtoppm | pnmtopng > tmp.png");
+			# system ("convert tmp.png -fill black -draw 'rectangle 5,365,183,393' -pointsize 24 -fill white -gravity SouthWest  -annotate +0+5 ' s = $sigma +i $itau.$t1$t2 ' $fpref$deci$i.png");
+		}
 	}
 }
 
 sub cvt($sigma, $fpref) {
 	cvt_range(1, 10, "000", $sigma, $fpref);
+	# cvt_range(10, 100, "00", $sigma, $fpref);
+	# cvt_range(100, 1000, "0", $sigma, $fpref);
+	# cvt_range(1000, 10000, "", $sigma, $fpref);
 }
 
-cvt("0.5", "polylog-0.5-");
+cvt("0.3", "polylog-0.3-");
+# cvt("0.5", "polylog-0.5-");
+# cvt("0.7", "polylog-0.7-");
+# cvt("0.3", "polylog-0.3-g1m-");
+# cvt("0.5", "polylog-0.5-g1m-");
+# cvt("0.7", "polylog-0.7-g1m-");
