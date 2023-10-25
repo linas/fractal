d7c2300aaf463ac1e2f2c2be03287e1d5c205477
Linas Vepstas <linasvepstas@gmail.com>
2023-10-25 13:08:56 -0500

    More movie-making

--- generate/movie-poly-cvt.pl
+++ generate/movie-poly-cvt.pl
@@ -3,6 +3,8 @@
 # Run processes to bulk convert polylog movie data files to PNG files.
 #
 
+use feature 'signatures';
+
 # fps == frames per second
 # Must match setting for script that initiall generated the frames.
 $fps = 30;
@@ -12,9 +14,10 @@ $taumax = 300;  # max tau value but also running time in secs
 
 # $fpref == Filename prefix
 # $deci == decimal prefix for output.
-sub cvt_range($imin, $imax, $deci, $fpref) {
+# $sigma == sigma of sigma+i tau
+sub cvt_range($imin, $imax, $deci, $sigma, $fpref) {
 
-	for ($i=$imin; $i<$imax; $i++)
+	for (my $i=$imin; $i<$imax; $i++)
 	{
 		$tau = $step * $i;
 
@@ -24,10 +27,14 @@ sub cvt_range($imin, $imax, $deci, $fpref) {
 		$t2 = int ($tau * 100 - $itau * 100 - $t1*10);
 		print "$i $itau.$t1$t2 \n";
 
-		# system ("ls ptest-$i-*.flo ");
+		# system ("ls $fpref$i-*.flo ");
 		system ("cat $fpref$i-*.flo | /home/linas/src/fractal/image/flo2mtv |mtvtoppm | pnmtopng > tmp.png");
-		system ("convert tmp.png -fill black -draw 'rectangle 5,365,108,393' -pointsize 24 -fill white -gravity SouthWest  -annotate +0+5 ' t=$itau.$t1$t2 ' $fpref$deci$i.png");
+		system ("convert tmp.png -fill black -draw 'rectangle 5,365,183,393' -pointsize 24 -fill white -gravity SouthWest  -annotate +0+5 ' s = $sigma +i $itau.$t1$t2 ' $fpref$deci$i.png");
 	}
 }
 
-cvt_range(0,10, "000", "polylog-0.3-");
+sub cvt($sigma, $fpref) {
+	cvt_range(1, 10, "000", $sigma, $fpref);
+}
+
+cvt("0.5", "polylog-0.5-");
