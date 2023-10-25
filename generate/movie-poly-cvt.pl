c3081633edcb04e606247a485773730c2c727ea6
Linas Vepstas <linasvepstas@gmail.com>
2023-10-25 15:35:08 -0500

    Continue with bulk conversion

--- generate/movie-poly-cvt.pl
+++ generate/movie-poly-cvt.pl
@@ -2,6 +2,13 @@
 #
 # Run processes to bulk convert polylog movie data files to PNG files.
 #
+# After this is done, run this:
+# ffmpeg -i polylog-0.3-%04d.png -framerate 30 polylog-0.3.mp4
+#
+# Paste images side by side (from Imagemagick):
+# montage a.png b.png c.png -tile 3x1 -geometry +0+0 out.png
+# montage a.png b.png c.png d.png -tile 2x2 -geometry +0+0 out.png
+
 
 use feature 'signatures';
 
@@ -37,8 +44,8 @@ sub cvt_range($imin, $imax, $deci, $sigma, $fpref) {
 			# print "$i $itau.$t1$t2 \n";
 
 			system ("ls $fpref$i-*.flo ");
-			# system ("cat $fpref$i-$itau.flo | /home/linas/src/fractal/image/flo2mtv |mtvtoppm | pnmtopng > tmp.png");
-			# system ("convert tmp.png -fill black -draw 'rectangle 5,365,183,393' -pointsize 24 -fill white -gravity SouthWest  -annotate +0+5 ' s = $sigma +i $itau.$t1$t2 ' $fpref$deci$i.png");
+			system ("cat $fpref$i-$itau.flo | /home/linas/src/fractal/image/flo2mtv |mtvtoppm | pnmtopng > tmp.png");
+			system ("convert tmp.png -fill black -draw 'rectangle 5,365,183,393' -pointsize 24 -fill white -gravity SouthWest  -annotate +0+5 ' s = $sigma +i $itau.$t1$t2 ' $fpref$deci$i.png");
 		}
 	}
 }
