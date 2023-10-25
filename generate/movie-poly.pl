21ad806586b7eabe0c7eb1cc7c5b0c3eca25e992
Linas Vepstas <linasvepstas@gmail.com>
2023-10-25 16:46:47 -0500

    More fixes

--- generate/movie-poly.pl
+++ generate/movie-poly.pl
@@ -9,28 +9,35 @@ $taumax = 300;  # max tau value but also running time in secs
 $nframes = $taumax * $fps; # $taumax seconds at 30 fps
 $tau = 0;
 $taudelta = $taumax / $nframes;
+
+# Width, in the complex plane. Since it's centered, this
+# will go from -3.5 to +3.5 obn the real axis.
+$width = 7.0;
+
 for ($i=1; $i<$nframes; )
 {
 	# Manual loop unroll
 	$tau = $i * $taudelta;
+	$itau = int($tau);
 	print ("Start work on $i tau=$tau\n");
-	system ("./polylog-0.3 /home2/linas/tmp/polylog-0.3-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
-	system ("./polylog-0.5 /home2/linas/tmp/polylog-0.5-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
-	system ("./polylog-0.7 /home2/linas/tmp/polylog-0.7-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
+	system ("./polylog-0.3 /home2/linas/tmp/polylog-0.3-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.5 /home2/linas/tmp/polylog-0.5-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.7 /home2/linas/tmp/polylog-0.7-$i-$itau.ext 400 400 1 0 0 $width $tau &");
 
-	system ("./polylog-0.3-g1m /home2/linas/tmp/polylog-0.3-g1m-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
-	system ("./polylog-0.5-g1m /home2/linas/tmp/polylog-0.5-g1m-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
-	system ("./polylog-0.7-g1m /home2/linas/tmp/polylog-0.7-g1m-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
+	system ("./polylog-0.3-g1m /home2/linas/tmp/polylog-0.3-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.5-g1m /home2/linas/tmp/polylog-0.5-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.7-g1m /home2/linas/tmp/polylog-0.7-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
 	$i++;
 
-	$tau.ext = $i * $taudelta;
+	$tau = $i * $taudelta;
+	$itau = int($tau);
 	print ("Start work on $i tau.ext=$tau\n");
-	system ("./polylog-0.3 /home2/linas/tmp/polylog-0.3-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
-	system ("./polylog-0.5 /home2/linas/tmp/polylog-0.5-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
-	system ("./polylog-0.7 /home2/linas/tmp/polylog-0.7-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
+	system ("./polylog-0.3 /home2/linas/tmp/polylog-0.3-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.5 /home2/linas/tmp/polylog-0.5-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.7 /home2/linas/tmp/polylog-0.7-$i-$itau.ext 400 400 1 0 0 $width $tau &");
 
-	system ("./polylog-0.3-g1m /home2/linas/tmp/polylog-0.3-g1m-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
-	system ("./polylog-0.5-g1m /home2/linas/tmp/polylog-0.5-g1m-$i-$tau.ext 400 400 1 0 0 7.0 $tau &");
-	system ("./polylog-0.7-g1m /home2/linas/tmp/polylog-0.7-g1m-$i-$tau.ext 400 400 1 0 0 7.0 $tau");
+	system ("./polylog-0.3-g1m /home2/linas/tmp/polylog-0.3-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.5-g1m /home2/linas/tmp/polylog-0.5-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.7-g1m /home2/linas/tmp/polylog-0.7-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau");
 	$i++;
 }
