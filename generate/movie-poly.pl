4c4dd009f0faf5688400607b5d61044438e14921
Linas Vepstas <linasvepstas@gmail.com>
2023-10-25 21:20:21 -0500

    configurable target dir

--- generate/movie-poly.pl
+++ generate/movie-poly.pl
@@ -14,38 +14,48 @@ $taudelta = $taumax / $nframes;
 # will go from -3.5 to +3.5 obn the real axis.
 $width = 7.0;
 
+$tmpdir="/home2/linas/tmp";
+
 for ($i=1; $i<$nframes; )
 {
 	# Manual loop unroll
 #	$tau = $i * $taudelta;
 #	$itau = int($tau);
 #	print ("Start work on $i tau=$tau\n");
-#	system ("./polylog-0.3 /home2/linas/tmp/polylog-0.3-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-#	system ("./polylog-0.5 /home2/linas/tmp/polylog-0.5-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-#	system ("./polylog-0.7 /home2/linas/tmp/polylog-0.7-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.3 $tmpdir/polylog-0.3-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.5 $tmpdir/polylog-0.5-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.7 $tmpdir/polylog-0.7-$i-$itau.ext 400 400 1 0 0 $width $tau &");
 #
-#	system ("./polylog-0.3-g1m /home2/linas/tmp/polylog-0.3-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-#	system ("./polylog-0.5-g1m /home2/linas/tmp/polylog-0.5-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-#	system ("./polylog-0.7-g1m /home2/linas/tmp/polylog-0.7-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.3-g1m $tmpdir/polylog-0.3-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.5-g1m $tmpdir/polylog-0.5-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.7-g1m $tmpdir/polylog-0.7-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#
+#	system ("./polylog-0.3-g1p $tmpdir/polylog-0.3-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.5-g1p $tmpdir/polylog-0.5-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.7-g1p $tmpdir/polylog-0.7-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#
+#	system ("./polylog-0.3-g10z $tmpdir/polylog-0.3-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.5-g10z $tmpdir/polylog-0.5-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.7-g10z $tmpdir/polylog-0.7-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau &");
 #	$i++;
 
 	$tau = $i * $taudelta;
 	$itau = int($tau);
 	print ("Start work on $i tau.ext=$tau\n");
-#	system ("./polylog-0.3 /home2/linas/tmp/polylog-0.3-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-#	system ("./polylog-0.5 /home2/linas/tmp/polylog-0.5-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-#	system ("./polylog-0.7 /home2/linas/tmp/polylog-0.7-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.3 $tmpdir/polylog-0.3-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.5 $tmpdir/polylog-0.5-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.7 $tmpdir/polylog-0.7-$i-$itau.ext 400 400 1 0 0 $width $tau &");
 #
-#	system ("./polylog-0.3-g1m /home2/linas/tmp/polylog-0.3-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-#	system ("./polylog-0.5-g1m /home2/linas/tmp/polylog-0.5-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-#	system ("./polylog-0.7-g1m /home2/linas/tmp/polylog-0.7-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.3-g1m $tmpdir/polylog-0.3-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.5-g1m $tmpdir/polylog-0.5-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+#	system ("./polylog-0.7-g1m $tmpdir/polylog-0.7-g1m-$i-$itau.ext 400 400 1 0 0 $width $tau &");
 
-	system ("./polylog-0.3-g1p /home2/linas/tmp/polylog-0.3-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-	system ("./polylog-0.5-g1p /home2/linas/tmp/polylog-0.5-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-	system ("./polylog-0.7-g1p /home2/linas/tmp/polylog-0.7-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.3-g1p $tmpdir/polylog-0.3-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.5-g1p $tmpdir/polylog-0.5-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.7-g1p $tmpdir/polylog-0.7-g1p-$i-$itau.ext 400 400 1 0 0 $width $tau &");
 
-	system ("./polylog-0.3-g10z /home2/linas/tmp/polylog-0.3-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-	system ("./polylog-0.5-g10z /home2/linas/tmp/polylog-0.5-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau &");
-	system ("./polylog-0.7-g10z /home2/linas/tmp/polylog-0.7-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau");
+	system ("./polylog-0.3-g10z $tmpdir/polylog-0.3-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.5-g10z $tmpdir/polylog-0.5-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau &");
+	system ("./polylog-0.7-g10z $tmpdir/polylog-0.7-g10z-$i-$itau.ext 400 400 1 0 0 $width $tau");
 	$i++;
 }
