851a5ef996579c17c6a11fa88f06813840afd6fe
Linas Vepstas <linasvepstas@gmail.com>
2023-10-25 21:50:31 -0500

    Ooops, bad code

--- generate/movie-poly-montage.pl
+++ generate/movie-poly-montage.pl
@@ -17,10 +17,6 @@ $SIG{'INT'} = sub { print "bye-bye\n";  die "the end"; };
 # $deci == decimal prefix for output.
 sub monte_range($imin, $imax, $deci) {
 
-	$gp = "polylog-0.3-g1p-";
-	$hp = "polylog-0.5-g1p-";
-	$ip = "polylog-0.7-g1p-";
-
 	$ap = "polylog-0.3-";
 	$bp = "polylog-0.5-";
 	$cp = "polylog-0.7-";
@@ -28,9 +24,13 @@ sub monte_range($imin, $imax, $deci) {
 	$ep = "polylog-0.5-g1m-";
 	$fp = "polylog-0.7-g1m-";
 
-	$jp = "polylog-0.3-g10z-";
-	$kp = "polylog-0.5-g10z-";
-	$lp = "polylog-0.7-g10z-";
+	$gp = "polylog-0.3-g1p-";
+	$hp = "polylog-0.5-g1p-";
+	$jp = "polylog-0.7-g1p-";
+
+	$kp = "polylog-0.3-g10z-";
+	$lp = "polylog-0.5-g10z-";
+	$mp = "polylog-0.7-g10z-";
 
 	for (my $i=$imin; $i<$imax; $i++)
 	{
@@ -42,10 +42,10 @@ sub monte_range($imin, $imax, $deci) {
 		$f = "$fp$deci$i.png";
 		$g = "$gp$deci$i.png";
 		$h = "$hp$deci$i.png";
-		$i = "$ip$deci$i.png";
 		$j = "$jp$deci$i.png";
 		$k = "$kp$deci$i.png";
 		$l = "$lp$deci$i.png";
+		$m = "$mp$deci$i.png";
 
 		$fout = "polylog-montage-$deci$i.png";
 
@@ -58,7 +58,7 @@ sub monte_range($imin, $imax, $deci) {
 
 			system ("ls $a");
 			# system ("montage $a $b $c $d $e $f -tile 3x2 -geometry +0+0 $fout");
-			system ("montage $g $h $i $a $b $c $d $e $f $j $k $l -tile 3x4 -geometry +0+0 $fout");
+			system ("montage $g $h $j $a $b $c $d $e $f $k $l $m -tile 3x4 -geometry +0+0 $fout");
 		}
 	}
 }
