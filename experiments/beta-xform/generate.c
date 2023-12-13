abd4d4e51a29640fbe2651e52b3153324439a304
Linas Vepstas <linasvepstas@gmail.com>
2023-12-12 23:30:13 -0600

    Add first generating result

--- experiments/beta-xform/generate.c
+++ experiments/beta-xform/generate.c
@@ -6,3 +6,28 @@
  *
  * December 2023
  */
+
+#include "irred-gold.c"
+
+// Ordinary generating function
+double beta_ogf(double x)
+{
+	double sum=0.0;
+	double xn = 1.0;
+	for (int i=0; i<50; i++)
+	{
+		double beta = find_gold(i);
+		if (0.5 < beta) sum += xn;
+		printf("%d beta=%g sum=%g\n", i, beta, sum);
+		xn *= x;
+	}
+	return sum;
+}
+
+int main(int argc, char* argv[])
+{
+	long nmax = 513;
+	malloc_gold(nmax);
+
+	printf("hello %20.16g\n", beta_ogf(0.5));
+}
