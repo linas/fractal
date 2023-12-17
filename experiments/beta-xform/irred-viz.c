aa727609eb9f91586311e24489860adb86934ff4
Linas Vepstas <linasvepstas@gmail.com>
2023-12-17 12:05:01 -0600

    Modernize

--- experiments/beta-xform/irred-viz.c
+++ experiments/beta-xform/irred-viz.c
@@ -17,57 +17,57 @@
 
 int main(int argc, char* argv[])
 {
-	int nmax = (1<<21) + 1;
+	int nmax = (1<<21);
 
-	malloc_gold(nmax);
+	malloc_gold(nmax+1);
 
 	int cnt = 0;
 	int allcnt = 0;
-	int nsum = 0;
+	long psisum = 0;
 	int plen = 0;
-	int lyn = 1;
+	int necklace = 1;
 
 	int tord = 1<<plen;
 
-	for (int n=0; n<nmax; n ++)
+	for (int n=0; n<nmax+1; n ++)
 	{
 		double gold = find_gold(n);
 
 		// printf("---------\ngold=%g\n", gold);
 		if (plen != order(n))
 		{
-			printf("# total for len=%d is %d\n", plen, cnt);
+			printf("# Total number for valid orbits of len=%d is %d\n", plen, cnt);
 			plen = order(n);
 			tord = 1<<plen;
 			tord /= 2;
-			lyn = cnt;
 			cnt = 0;
 
-			// advance and count lyndon number
-			lyn = 0;
+			// advance and count necklace number
+			necklace = 0;
 			for (int k=n+1; k<nmax; k++)
 			{
 				double g = find_gold(k);
-				if (0.5 < g) lyn++;
+				if (0.5 < g) necklace++;
 				if (order(k) != plen) break;
 			}
-			printf("# next lyndon is %d\n", lyn);
+			if (0 == necklace) break;
+			printf("# Next necklace is %d\n", necklace);
 		}
 		if (0.5 < gold)
 		{
 			cnt ++;
 			allcnt ++;
-			nsum += n;
+			psisum += n;
 
-			// always the case that tord/2 <= n < tord
+			// Always the case that tord/2 <= n < tord
 			double frac = ((double) n) / ((double) tord);
 			frac = 2.0*(frac - 0.5); // rescale to run 0 to 1.
 
 			// OK, this should work.
-			double seqfrac = ((double) cnt) / ((double) lyn);
+			double seqfrac = ((double) cnt) / ((double) necklace);
 
-			printf("%d	%d %d %d %g %g	%d	%20.18g\n",
-				allcnt, cnt, n, tord, seqfrac, frac, nsum, gold);
+			printf("%d	%d %d %d %g %g	%ld	%20.18g\n",
+				allcnt, cnt, n, tord, seqfrac, frac, psisum, gold);
 		}
 	}
 }
