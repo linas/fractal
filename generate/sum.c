b17e00c92ebf63a5bfe7efc9b63f6907472e7741
Linas Vepstas <linasvepstas@gmail.com>
2024-01-02 17:05:53 -0600

    Modernize another old tool

--- generate/sum.c
+++ generate/sum.c
@@ -12,6 +12,7 @@
 #include <string.h>
 
 #include "opers.h"
+#include "fileio.h"
 
 /*-------------------------------------------------------------------*/
 
@@ -87,21 +88,9 @@ void divergence (float a[], float b[], int nx, int ny) {
 
 /*-------------------------------------------------------------------*/
 
-extern FILE *Fopen();
-extern FILE *Fopenr();
-
-int 
-main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 
 {
-   float	*data_a;		/* my data array */
-   float	*data_b;		/* my data array */
-   unsigned int	data_width, data_height;/* data array dimensions */
-   int		globlen;
-   char		*cmd = NULL;
-   char 	str[80];
-   FILE		*fp_a, *fp_b, *fp_out;
-   
    if (argc < 4) {
       printf ("Usage: %s <input file> <input file> <output file>\n", argv[0]);
       return 1;
@@ -109,50 +98,22 @@ main (int argc, char *argv[])
 
    /*----------------------------------------------------*/
    /* open input file */
-   if ( (fp_a = Fopenr (argv[1], ".flo")) == NULL) {
-      printf (" Can't open input file %s \n", argv[1]);
-      return 1;
-   }
-   if ( NULL == fgets (str, 80, fp_a)) {
-      printf (" Can't read input file %s \n", argv[1]);
-      fclose (fp_a);
-      return 1;
-   }
+	const char* suff;
+   unsigned int	data_width, data_height;/* data array dimensions */
+   float* data_a = read_floats(argv[1], &suff, &data_width, &data_height);
+   printf ("input file %s.%s has width %d height %d\n",
+          argv[1], suff, data_width, data_height);
 
-   sscanf (str, "%d %d", &data_width, &data_height);
-   printf (" input file %s has width %d height %d \n", argv[1], data_width, data_height);
-   
-   /*----------------------------------------------------*/
-   /* open input file */
-   if ( (fp_b = Fopenr (argv[2], ".flo")) == NULL) {
-      printf (" Can't open input file %s \n", argv[2]);
-      return 1;
-   }
-   if ( NULL == fgets (str, 80, fp_b)) {
-      printf (" Can't read input file %s \n", argv[2]);
-      fclose (fp_b);
-      return 1;
-   }
+   float* data_b = read_floats(argv[2], &suff, &data_width, &data_height);
+   printf ("input file %s.%s has width %d height %d\n",
+          argv[2], suff, data_width, data_height);
 
-   sscanf (str, "%d %d", &data_width, &data_height);
-   printf (" input file %s has width %d height %d \n", argv[2], data_width, data_height);
-   
-   /*----------------------------------------------------*/
-   
-   globlen = data_width*data_height;
-   data_a = (float *) malloc (globlen * sizeof (float));
-   data_b = (float *) malloc (globlen * sizeof (float));
-   
-   /* read floating point data */
-   fread(data_a, sizeof(float), globlen, fp_a);
-   fread(data_b, sizeof(float), globlen, fp_b);
-   fclose (fp_a);
-   fclose (fp_b);
+	int globlen = data_width*data_height;
 
    /*----------------------------------------------------*/
    /* strip out leading pathame */
-   cmd = rindex (argv[0], '/');
-   if (!cmd) { 
+   char	*cmd = rindex (argv[0], '/');
+   if (!cmd) {
       cmd = argv[0];
    } else {
       cmd++;
@@ -168,19 +129,8 @@ main (int argc, char *argv[])
 
    /*----------------------------------------------------*/
    /* open output file */
-   if ( (fp_out = Fopen (argv[3], ".flo")) == NULL) {
-      printf (" Can't open output file \n");
-      return 1;
-   }
+	write_floats(argv[3], suff, data_a, data_width, data_height);
 
-   /* dump the size to output */
-   fprintf (fp_out, "%d %d\n", data_width, data_height);
-   fwrite (data_a, sizeof(float), globlen, fp_out);
-   fclose (fp_out);
-
-   free (data_a);
-   free (data_b);
-   
    return 0;
 }
 
