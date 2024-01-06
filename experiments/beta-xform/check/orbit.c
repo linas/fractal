e80a080bf50fbd061c5a0ae0751d45e3ace03ebd
Linas Vepstas <linasvepstas@gmail.com>
2024-01-05 21:07:14 -0600

    Sort the midpoints

--- experiments/beta-xform/check/orbit.c
+++ experiments/beta-xform/check/orbit.c
@@ -5,26 +5,40 @@
  * Jan 2024
  */
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "../periodic/selfie.c"
 
-void orbit(double* mpts, double beta, int order)
+double teen(double beta, int k)
+{
+	double mid = 0.5* beta;
+	for (int i=0; i<k; i++)
+	{
+		if (0.5 < mid) mid -= 0.5;
+		mid *= beta;
+	}
+	return mid;
+}
+
+int cmp(const void* ida, const void* idb, void* pb)
+{
+	double beta = *((double*) pb);
+	double ma = teen(beta, *((int*) ida));
+	double mb = teen(beta, *((int*) idb));
+	return (ma < mb) ? -1: 1;
+}
+
+void orbit(double beta, int order)
 {
 	double mid = 0.5* beta;
 	for (int i=0; i<order; i++)
 	{
 		printf("%d	%g\n", i, mid);
-		mpts[i] = mid;
 		if (0.5 < mid) mid -= 0.5;
 		mid *= beta;
 	}
-}
-
-int cmp(const void *ma, const void* mb)
-{
-	return *((double *) ma) < *((double *) mb);
 }
 
 int main(int argc, char* argv[])
@@ -40,8 +54,14 @@ int main(int argc, char* argv[])
 	printf("idx=%d beta=%g\n", idx, beta);
 
 	int order = bitlen(idx) +1;
-	double mpts[20];
-	orbit(mpts, beta, order);
+	orbit(beta, order);
 
-	qsort(mpts, sizeof(double), order, cmp);
+	int mpts[20];
+	for (int i=0; i< order; i++) mpts[i] = i;
+
+	qsort_r(mpts, order, sizeof(int), cmp, &beta);
+
+	printf("Midpoints:");
+	for (int i=0; i<order; i++) printf(" %d", mpts[i]);
+	printf ("\n");
 }
