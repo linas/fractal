0263764e749ffdbc2d309654d56d4c117275b8ae
Linas Vepstas <linasvepstas@gmail.com>
2024-01-05 20:50:14 -0600

    Work on intervals

--- experiments/beta-xform/check/orbit.c
+++ experiments/beta-xform/check/orbit.c
@@ -8,25 +8,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void orbit(double beta)
+#include "../periodic/selfie.c"
+
+void orbit(double* mpts, double beta, int order)
 {
 	double mid = 0.5* beta;
-	for (int i=0; i<6; i++)
+	for (int i=0; i<order; i++)
 	{
 		printf("%d	%g\n", i, mid);
+		mpts[i] = mid;
 		if (0.5 < mid) mid -= 0.5;
 		mid *= beta;
 	}
+}
+
+int cmp(const void *ma, const void* mb)
+{
+	return *((double *) ma) < *((double *) mb);
 }
 
 int main(int argc, char* argv[])
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s beta\n", argv[0]);
+		fprintf(stderr, "Usage: %s idx\n", argv[0]);
 		exit (1);
 	}
-	double beta = atof(argv[1]);
+	int idx = atoi(argv[1]);
 
-	orbit(beta);
+	double beta = golden_beta(idx);
+	printf("idx=%d beta=%g\n", idx, beta);
+
+	int order = bitlen(idx) +1;
+	double mpts[20];
+	orbit(mpts, beta, order);
+
+	qsort(mpts, sizeof(double), order, cmp);
 }
