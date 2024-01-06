e4b21f3549ae324b20bca5ce1cb6eedd8f2ab20f
Linas Vepstas <linasvepstas@gmail.com>
2024-01-05 21:55:09 -0600

    Fill out the rest of the table.

--- experiments/beta-xform/check/orbit.c
+++ experiments/beta-xform/check/orbit.c
@@ -2,6 +2,7 @@
  * orbit.c
  *
  * Manual exploration of individual orbits.
+ * Print out orbits, and then sort them into order.
  * Jan 2024
  */
 
@@ -13,6 +14,8 @@
 
 double teen(double beta, int k)
 {
+	if (k < 0) return 1.0;
+
 	double mid = 0.5* beta;
 	for (int i=0; i<k; i++)
 	{
@@ -53,15 +56,31 @@ int main(int argc, char* argv[])
 	double beta = golden_beta(idx);
 	printf("idx=%d beta=%g\n", idx, beta);
 
+	// Print the orbit
 	int order = bitlen(idx) +1;
 	orbit(beta, order);
 
+	// Print midpoints in sorted order.
 	int mpts[20];
 	for (int i=0; i< order; i++) mpts[i] = i;
-
 	qsort_r(mpts, order, sizeof(int), cmp, &beta);
 
 	printf("Midpoints:");
 	for (int i=0; i<order; i++) printf(" %d", mpts[i]);
 	printf ("\n");
+
+	double xlo = 0.0;
+	for (int i=0; i< order; i++)
+	{
+		double xhi = teen(beta, mpts[i]);
+
+		double leflo = xlo / beta;
+		double lefhi = xhi / beta;
+		double riglo = leflo + 0.5;
+		double righi = lefhi + 0.5;
+		printf("I %d [%g %g] \tL=[%g %g]  \tR=[%g %g]\n",
+			order-i, xlo, xhi, leflo, lefhi, riglo, righi);
+
+		xlo = xhi;
+	}
 }
