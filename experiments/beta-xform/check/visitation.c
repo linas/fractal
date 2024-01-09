4d560612cd450a5c3bcd0e5c7c6f5becea7cea78
Linas Vepstas <linasvepstas@gmail.com>
2024-01-08 18:42:16 -0600

    Working visitation function

--- experiments/beta-xform/check/visitation.c
+++ experiments/beta-xform/check/visitation.c
@@ -12,35 +12,47 @@
 
 #define DEPTH 300
 unsigned long visit[DEPTH];
+unsigned long rigb[DEPTH];
 double midp[DEPTH];
 
 void iter(double beta)
 {
+	// Setup
+	for (int i=0; i<DEPTH; i++) visit[i] = 0;
+	visit[0] = 1;
+	rigb[0] = 1;
+
 	double midpnt = 0.5*beta;
+	midp[0] = midpnt;
 	double obn = 1.0;
-	for (int i=0; i<DEPTH; i++)
+	for (int i=1; i<DEPTH; i++)
 	{
-		midp[i] = midpnt;
-
-		// Search
-		double least = 42;
-		int rig = -1;
-		for (int j=0; j<i; j++)
-		{
-			if (least < midp[j]) continue;
-			if (midpnt < midp[j] && midp[j] < least)
-			{
-				least = midp[j];
-				rig = j;
-			}
-		}
-printf("i=%d mp=%g least=%g rig=%d\n", i, midpnt, least, rig);
-
 		// Step to next midpoint
 		if (0.5 < midpnt) midpnt -= 0.5;
 		midpnt *= beta;
 		obn /= beta;
 		if (obn < 1e-15) break;
+
+		midp[i] = midpnt;
+
+		// Search
+		double rmost = 4200;
+		int rig = -1;
+		for (int j=0; j<i; j++)
+		{
+			if (rmost < midp[j]) continue;
+			if (midpnt < midp[j] && midp[j] < rmost)
+			{
+				rmost = midp[j];
+				rig = j;
+			}
+		}
+
+		visit[i] = rigb[rig];
+		rigb[i] = 2*rigb[rig];
+		rigb[rig] = 2*rigb[rig]+1;
+		// printf("i=%d mp=%g least=%g rig=%d -- visit= %ld\n",
+		//	i, midpnt, rmost, rig, visit[i]);
 	}
 }
 
@@ -52,9 +64,6 @@ int main(int argc, char* argv[])
 		exit (1);
 	}
 	double beta = atof(argv[1]);
-
-	// Setup
-	for (int i=0; i<DEPTH; i++) visit[i] = 0;
 
 	iter(beta);
 }
