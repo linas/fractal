07b2c368fa3e87dbf229adfa2777b0c1e87f4619
Linas Vepstas <linasvepstas@gmail.com>
2024-01-17 16:39:06 -0600

    Yes, it works right

--- experiments/beta-xform/check/tsum.c
+++ experiments/beta-xform/check/tsum.c
@@ -2,10 +2,12 @@
  * tsum.c
  *
  * Validate that tsum and csum are right.
+ * Yes, it does check out correctly. Yay!
  * January 2024
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 double tsum(double beta, int n)
 {
@@ -24,12 +26,12 @@ double tsum(double beta, int n)
 	return sum;
 }
 
-double csum(double beta, int n)
+double c_sub_n(double beta, int n)
 {
 	double sum=0.0;
 	double tn = 1.0;
 	double bn = beta;
-	for (int i=0; i<n; i++)
+	for (int i=0; i<=n; i++)
 	{
 		if (beta*tn >= 1.0) sum += 1.0 / bn;
 
@@ -41,14 +43,24 @@ double csum(double beta, int n)
 	return 1.0 - sum;
 }
 
+double csum(double beta, int n)
+{
+	double sum=0.0;
+	for (int i=0; i<n; i++)
+		sum += c_sub_n(beta, i);
+
+	return sum;
+}
+
 int main (int argc, char* argv[])
 {
-
-	double beta = 1.6;
+	double beta = atof(argv[1]);
 	for (int n=0; n<20; n++)
 	{
 		double ts = tsum(beta, n);
 		double cs = csum(beta, n);
-		printf("its %d  %g %g\n", n, ts, cs);
+		double cn = c_sub_n(beta, n);
+		double norm = ts - cs;
+		printf("its %d cn=%g	%g %g 	%g\n", n, cn, ts, cs, norm-1.0);
 	}
 }
