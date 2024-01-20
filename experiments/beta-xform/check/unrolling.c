d9eb15a0d302816df7e78679d8a9b1fbaa3d1a79
Linas Vepstas <linasvepstas@gmail.com>
2024-01-20 00:45:50 -0600

    Continue implementation

--- experiments/beta-xform/check/unrolling.c
+++ experiments/beta-xform/check/unrolling.c
@@ -23,11 +23,11 @@ double t_k(double beta, int k)
 }
 
 // Return midpoint iterate digit b_k = d_k(1/2)=theta(beta t_k-1)
-double b_k(double beta, int k)
+int b_k(double beta, int k)
 {
 	double tk = t_k(beta, k);
-	if (beta*tk >= 1.0) return 1.0;
-	return 0.0;
+	if (beta*tk >= 1.0) return 1;
+	return 0;
 }
 
 // Arbitrary function
@@ -36,6 +36,9 @@ double nu(double x)
 	return 1.0;
 }
 
+// Forward decl
+double g_n_1(double beta, int n, double x);
+
 // Return the g_n_k constant from the "generalized stretch-cut-stack"
 // section of paper. This is computed using the recursing formula.
 double g_n_k(double beta, int n, int k, double x)
@@ -43,20 +46,42 @@ double g_n_k(double beta, int n, int k, double x)
 	if (n < k) return 0.0;
 	if (0 == k) return nu(x);
 
-	if (1 == k)
-	{
-		printf("not done yet\n");
-		return 1.0;
-	}
+	if (1 == k) g_n_1(beta, n, x);
+
+	// Recurse
 	double bkm1 = b_k(beta, k-1);
 	double arg = (x + bkm1)/ beta;
 	double guh = g_n_k(beta, n-1, k-1, arg);
 	return guh / beta;
 }
 
+// Return the f_n_k constant from the "generalized stretch-cut-stack"
+// section of paper. This is computed using the recursing formula.
+double f_n_k(double beta, int n, int k, double x)
+{
+	if (n <= k) return 0.0;
+	if (k <= 0) fprintf(stderr, "Error can't have k=0\n");
+	double arg = x / beta;
+	double sum = f_n_k(beta, n-1, k, arg) + g_n_k(beta, n-1, k, arg);
+	return sum / beta;
+}
+
 // Return the g_n_1 constant from the "generalized stretch-cut-stack"
 // section of paper.
-double g_n_1(double beta, int n)
+double g_n_1(double beta, int n, double x)
+{
+	double arg = (x + 1.0) / beta;
+	double sum = nu(arg);
+	for (int k=0; k<n-1; k++)
+	{
+		if (b_k(beta, k))
+			sum += f_n_k(beta, n-1, k, arg);
+	}
+	return sum / beta;
+}
+
+// Iterated density
+double nu_n(double beta, int n, double x)
 {
 	return 0.0;
 }
@@ -70,5 +95,6 @@ int main(int argc, char* argv[])
 	}
 	double beta = atof(argv[1]);
 
-	g_n_k(beta, 3, 3, 0.3);
+	double x = g_n_k(beta, 3, 3, 0.3);
+printf("its %g\n", x);
 }
