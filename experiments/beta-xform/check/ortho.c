3fa02275c046df9ba867ff9bd7098d1b5f805cb0
Linas Vepstas <linasvepstas@gmail.com>
2024-01-24 20:56:00 -0600

    Also integrate

--- experiments/beta-xform/check/ortho.c
+++ experiments/beta-xform/check/ortho.c
@@ -10,6 +10,20 @@
 #include <stdlib.h> 
 
 #include "unref.c"
+#include "unutil.c"
+
+double fnorm(double beta)
+{
+	double sum = 0.0;
+	double bei = 1.0;
+	for (int i=0; i<1000; i++)
+	{
+		sum += t_k(beta, i) / bei;
+		bei *= beta;
+		if (1.0 < bei * 1e-17) break;
+	}
+	return sum;
+}
 
 // Normalization
 double enorm(double beta, int n)
@@ -25,6 +39,23 @@ double enorm(double beta, int n)
 	return sum / ((double) NSAMP);
 }
 
+double fint(double beta)
+{
+#define FSAMP 1513
+	double sum = 0.0;
+	for (int i=0; i< FSAMP; i++)
+	{
+		double x = (((double) i) + 0.5) / ((double) FSAMP);
+		double boo = 1.0+x;
+		if (beta < boo) break;
+
+		double fno = fnorm(boo);
+		if (0.0 < fno)
+			sum += 1.0/fno;
+	}
+	return sum / ((double) FSAMP);
+}
+
 int main(int argc, char* argv[])
 {
 	int imax = 800;
@@ -32,9 +63,16 @@ int main(int argc, char* argv[])
 	{
 		double x = (((double) i) + 0.5) / ((double) imax);
 		double beta = x + 1.0;
-		printf("%d	%f", i, beta);
-		for (int n=0; n<6; n++)
-			printf("	%f", enorm(beta, n));
+		double eff = 1.0 / fnorm(beta);
+		double inf = fint(beta);
+		printf("%d	%f	%f	%f", i, beta, eff, inf);
+		// for (int n=0; n<6; n++)
+		for (int n=0; n<19; n+=3)
+		{
+			double eno = (n+1) * enorm(beta, n);
+			eno = 1.0 / eno;
+			printf("	%f", eno);
+		}
 
 		printf("\n");
 		fflush(stdout);
