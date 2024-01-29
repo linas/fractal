e20cf56f117d2f070826b4ffd824b6582a849b8a
Linas Vepstas <linasvepstas@gmail.com>
2024-01-29 00:02:20 -0600

    Mystery continues unabated

--- experiments/beta-xform/check/lambda.c
+++ experiments/beta-xform/check/lambda.c
@@ -10,13 +10,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "unref.c"
+
 // ==============================================================
+
+int mode = 0;
 
 // Arbitrary function
 double nu(double x)
 {
 	if (x < 0.0) fprintf(stderr, "Error nu fail neg %g\n", x);
 	if (1.0 < x) fprintf(stderr, "Error nu fail pos %g\n", x);
+
+	if (0 == mode) return 1.0;
+	if (1 == mode) return x;
+	return gp_invar(1.6, x);
 
 	// return 1.0;
 	// return x-0.5;
@@ -25,7 +33,7 @@ double nu(double x)
 	// return x - 0.5*0.826154;
 	// return x - 0.5*0.8261542;
 	// return x - 0.5*0.82615419;
-	return x - 0.5*0.826154195;
+	// return x - 0.5*0.826154195;
 
 	// Bernoulli poly B_2
 	// The result is senstive to this being B_2.
@@ -41,11 +49,36 @@ double nu(double x)
 }
 
 // ==============================================================
-#include "unref.c"
 #include "unlambda.c"
 
 int main(int argc, char* argv[])
 {
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s beta n\n", argv[0]);
+		exit (1);
+	}
+	double beta = atof(argv[1]);
+	int n = atoi(argv[2]);
+
+	int imax = 814;
+	for (int i=0; i< imax; i++)
+	{
+		double x = (((double) i) + 0.5) / ((double) imax);
+
+		double y = gp_invar(beta, x);
+		printf("%d	%f	%f", i, x, y);
+
+		double blam = 1.0;
+		mode = 0;
+		double z1 = nul_n(beta, blam, x, n);
+		mode = 22;
+		double z2 = nul_n(beta, blam, x, n);
+		printf("	%f	%f	%f\n", z1, z2, z2/z1);
+		fflush(stdout);
+	}
+
+#ifdef DO_GRAPH
 	if (argc != 4)
 	{
 		fprintf(stderr, "Usage: %s beta lambda n\n", argv[0]);
@@ -64,6 +97,10 @@ int main(int argc, char* argv[])
 	double scale = lambda * beta;
 	// scale = lambda;
 	double scan = pow(scale, n);
+
+	printf("#\n# beta=%f n=%d scale=%f\n#\n", beta, n, scale);
+	fflush(stdout);
+
 	int imax = 814;
 	double delta = 1.0 / ((double) imax);
 	for (int i=0; i< imax; i++)
@@ -92,6 +129,8 @@ int main(int argc, char* argv[])
 	for (int j=0; j<NIT; j++)
 		printf(" %g", sum[j]);
 	printf("\n#\n");
+#endif
+
 }
 
 // ==============================================================
