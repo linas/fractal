9b0c91353169c23ae4b3e60837437128f6985efc
Linas Vepstas <linasvepstas@gmail.com>
2024-02-13 14:52:44 -0600

    Remove the failed crud

--- experiments/beta-xform/coherent/repiece.c
+++ experiments/beta-xform/coherent/repiece.c
@@ -10,15 +10,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Sawtooth for linear g.
-double sgen(double beta, double alpha, double y)
-{
-	double m0 = 0.5*beta;
-	double gen = alpha*y;
-	gen -= m0 * floor (gen / m0);
-	return gen;
-}
-
 double modper(double period, double y)
 {
 	y -= period * floor (y / period);
@@ -31,7 +22,7 @@ double gee(double off, double y)
 }
 
 // gee coherent function.
-double psi(double beta, double omega, double alpha, double period, double y)
+double psi(double beta, double omega, double alpha, double y)
 {
 	double m0 = 0.5*beta;
 	if (m0 < y) return 0.0;
@@ -44,7 +35,7 @@ double psi(double beta, double omega, double alpha, double period, double y)
 	while (1.0e-15 < fabs(wn))
 	{
 		double term = alpha * yn;
-		sum += wn * gee(off, modper(period, term));
+		sum += wn * gee(off, modper(m0, term));
 		yn *= beta;
 		if (m0 < yn) yn -= m0;
 		wn *= omega;
@@ -52,43 +43,25 @@ double psi(double beta, double omega, double alpha, double period, double y)
 	return sum;
 }
 
-double psi_one(double beta, double omega, double alpha, double period, double y)
+double psi_one(double beta, double omega, double alpha, double y)
 {
 	if (y < 0.5) return 0.0;
 	if (0.5*beta < y) return 0.0;
-	return psi(beta, omega, alpha, period, y);
+	return psi(beta, omega, alpha, y);
 }
 
-double psi_two(double beta, double omega, double alpha, double period, double y)
+double psi_two(double beta, double omega, double alpha, double y)
 {
 	if (0.5 < y) return 0.0;
-	return psi(beta, omega, alpha, period, y);
-}
-
-double goldpsi(double omega, int k, double y)
-{
-	double beta = 0.5*(sqrt(5.0) + 1.0);
-	double alpha = beta * k;
-	double period = 0.5*beta;
-
-	// Interval 2
-	if (y < 0.5)
-		return psi(beta, omega, alpha, period, y);
-
-	// Interval 1
-	if (y < 0.5*beta)
-		return -psi(beta, omega, alpha, period, y/beta) / (omega*beta*beta);
-
-	return 0.0;
+	return psi(beta, omega, alpha, y);
 }
 
 double psi_all(double beta, double omega, double y)
 {
 	if (0.5*beta < y) return 0.0;
-	double period = 0.5*beta;
 	double a2 = beta * omega;
-	double f1 = psi_one(beta, omega, 1.0, period, y);
-	double f2 = a2* psi_two(beta, omega, beta, period, y);
+	double f1 = psi_one(beta, omega, 1.0, y);
+	double f2 = a2* psi_two(beta, omega, beta, y);
 	double sum = f1 + f2;
 	return sum;
 }
@@ -107,38 +80,6 @@ int main(int argc, char* argv[])
 
 	printf("#\n# beta=%f k=%d omega=%g\n#\n", beta, k, omega);
 
-#if LATER
-#define NPTS 2019
-	for (int j=0; j< NPTS; j++)
-	{
-		double x = (((double) j) + 0.5) / ((double) NPTS);
-		double y = goldpsi(omega, k, x);
-		double bra1 = goldpsi(omega, k, x/beta);
-		if (0.5*beta < x) bra1 = 0.0;
-		double bra2 = goldpsi(omega, k, x/beta+0.5);
-		if (0.5 < x) bra2 = 0.0;
-		printf("%d	%f	%f	%f	%f\n", j, x, y, bra1, bra2);
-		fflush(stdout);
-	}
-#endif
-
-#ifdef PARTS
-#define NPTS 2019
-	double alpha = k*beta;
-	alpha = 1.0;
-	double period = 0.5*beta;
-	double a2 = beta * omega;
-	for (int j=0; j< NPTS; j++)
-	{
-		double x = (((double) j) + 0.5) / ((double) NPTS);
-		double f1 = psi_one(beta, omega, alpha, period, x);
-		double f2 = a2* psi_two(beta, omega, beta*alpha, period, x/beta);
-		double sum = f1 + f2;
-		printf("%d	%f	%f	%f %g\n", j, x, f1, f2, sum);
-		fflush(stdout);
-	}
-#endif
-
 #define NPTS 2019
 	for (int j=0; j< NPTS; j++)
 	{
@@ -150,143 +91,4 @@ int main(int argc, char* argv[])
 		printf("%d	%f	%f	%f %g\n", j, x, p1, p2, all);
 		fflush(stdout);
 	}
-
-// #define SANITY_CHECK
-#ifdef SANITY_CHECK
-	// Quadruple-check the psierent sum. Yes, it behaves exactly how
-	// it should. So this is not where the problem lies.
-	double alpha = k*beta;
-#define NPTS 19
-	for (int j=0; j< NPTS; j++)
-	{
-		double x = (((double) j) + 0.5) / ((double) NPTS);
-#ifdef LCHECK
-		double y = psi(beta, omega, alpha, x);
-		double bra1 = psi(beta, omega, alpha, x/beta);
-		double sha1 = k*x + omega * y - bra1;
-		printf("%d	%f	%f	%f	%g\n", j, x, y, bra1, sha1);
-#endif
-
-// #define LCHECK_AGAIN
-#ifdef LCHECK_AGAIN
-		// Generic psi identity. passes great.
-		double y = psi(beta, omega, alpha, x);
-		double bra1 = psi(beta, omega, alpha, x/beta);
-		double sha1 = bra1 - omega*y - sgen(beta, alpha, x/beta);
-		printf("%d	%f	%f	%f	%g\n", j, x, y, bra1, sha1);
-#endif
-// #define RCHECK
-#ifdef RCHECK
-		// Generic psi identity. passes great.
-		double y = psi(beta, omega, alpha, x);
-		double bra2 = psi(beta, omega, alpha, x/beta + 0.5);
-		double gen = sgen(beta, alpha, x/beta + 0.5);
-		double sha2 = gen + omega * y - bra2;
-		printf("%d	%f	%f	%f	%g\n", j, x, y, bra2, sha2);
-#endif
-
-#ifdef TERM_COLLECT
-		// Passes great
-		double a2 = -1.0 / beta;
-		double ss = a2*sgen(beta, alpha, x/beta);
-		ss += sgen(beta, alpha, x/beta + 0.5);
-		ss += a2 * sgen(beta, alpha, x/beta) / beta;
-		printf("%d	%f	%g\n", j, x, ss);
-#endif
-
-#ifdef TERM_CONDENSE
-		// Passes great
-		double a2 = -1.0 / beta;
-		double ss = a2*beta*sgen(beta, alpha, x/beta);
-		ss += sgen(beta, alpha, x/beta + 0.5);
-		printf("%d	%f	%g\n", j, x, ss);
-#endif
-
-#ifdef LAST_TERM
-		// Passes great
-		double a2 = -1.0 / beta;
-		double ss = a2*beta*sgen(beta, alpha, x/beta);
-		ss += sgen(beta, alpha, x/beta);
-		printf("wat	%d  %f	%g\n", j, x, ss);
-#endif
-
-#ifdef TOP_ROW_FINAL
-		// works, but only cause its coded to work.
-		double y = goldpsi(omega, k, x);
-		double top = omega*beta*y + goldpsi(omega, k, x/beta)/beta;
-		printf("%d	%f	%f	%g\n", j, x, y, top);
-#endif
-
-#define TOP_SIMP
-#ifdef TOP_SIMP
-// borken. This is the most basic assumption. And it doesn't work.
-		double a2 = -1.0 / beta;
-		double y = psi_one(beta, omega, alpha, x);
-		double f1 = y;
-		double f2 = a2* psi_two(beta, omega, alpha, x/beta);
-		double top = omega*beta*f1 - f2;
-		printf("%d	%f	%f	%f %g\n", j, x, f1, f2, top);
-#endif
-
-// #define BOT_SIMP
-#ifdef BOT_SIMP
-// borken
-		double a2 = -1.0 / beta;
-		double y = psi_two(beta, omega, alpha, x);
-		double bot = omega*beta*a2*y;
-		bot -= a2* psi_two(beta, omega, alpha, x/beta);
-		bot -= psi_one(beta, omega, alpha, x/beta + 0.5);
-		printf("%d	%f	%f	%g\n", j, x, y, bot);
-#endif
-
-#ifdef TOP_PLUG
-// borken
-		double a2 = -1.0 / beta;
-		double y = psi_one(beta, omega, alpha, x);
-		double top = omega*beta*y;
-		top -= a2* omega* psi_two(beta, omega, alpha, x);
-
-		double ss = a2*sgen(beta, alpha, x/beta);
-
-		top -= ss;
-		printf("%d	%f	%f	%g\n", j, x, y, top);
-#endif
-
-// #define BOT_PLUG
-#ifdef BOT_PLUG
-// borken
-		double a2 = -1.0 / beta;
-		double y = psi_two(beta, omega, alpha, x);
-		double bot = omega*beta*a2*y;
-		bot -= a2* omega* psi_two(beta, omega, alpha, x);
-		bot -= omega* psi_one(beta, omega, alpha, x);
-
-		double ss = a2*sgen(beta, alpha, x/beta);
-		ss += sgen(beta, alpha, x/beta + 0.5);
-
-		bot -= ss;
-		printf("%d	%f	%f	%g\n", j, x, y, bot);
-#endif
-
-// #define BOT_ELIM
-#ifdef BOT_ELIM
-		// passes like it should
-		double a2 = -1.0 / beta;
-		double y = psi_two(beta, omega, alpha, x);
-		double bot = omega*beta*a2*y;
-		bot -= a2* omega* psi_two(beta, omega, alpha, x);
-		bot -= omega* a2* psi_two(beta, omega, alpha, x) /beta;
-
-		// ss is zero, so this does nothing.
-		double ss = a2*sgen(beta, alpha, x/beta);
-		ss += sgen(beta, alpha, x/beta + 0.5);
-		ss += a2 * sgen(beta, alpha, x/beta) / beta;
-
-		bot -= ss;
-		printf("%d	%f	%f	%g\n", j, x, y, bot);
-#endif
-
-		fflush(stdout);
-	}
-#endif
 }
