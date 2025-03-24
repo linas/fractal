ebf7d9cc15ef8e3a7da58efb32d81115e92c35e4
Linas Vepštas <linasvepstas@gmail.com>
2025-03-24 17:27:14 -0500

    Implement histogram. Again.

--- experiments/beta-xform/random-walk/random-walk.c
+++ experiments/beta-xform/random-walk/random-walk.c
@@ -5,6 +5,7 @@
  * Linas Vepstas 24 March 2025
  */
 
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -12,7 +13,7 @@
 #define HISTO_NBINS 302
 
 /* Number of samples */
-#defin NSAMP 5000
+#define NSAMP 5000
 
 /* Number of steps to take */
 #define NUM_STEPS 300
@@ -33,11 +34,46 @@ int walk(int num_steps)
 	return pt;
 }
 
+int histogram[HISTO_NBINS];
+
+void histo_init(void)
+{
+	for (int i=0; i< HISTO_NBINS; i++)
+		histogram[i] = 0;
+}
+
+void histo_accum(double x)
+{
+	int bin = 1 + floor(x * (HISTO_NBINS-2));
+	if (bin < 0) bin = 0;
+	if (HISTO_NBINS <= bin) bin = HISTO_NBINS-1;
+	histogram[bin] += 1;
+}
+
+void histo_print(void)
+{
+	int tot_cnt = 0;
+	for (int i=0; i< HISTO_NBINS; i++)
+		tot_cnt += histogram[i];
+
+	for (int i=0; i< HISTO_NBINS; i++)
+	{
+		double x = (i-1) / ((double) (HISTO_NBINS-2));
+		double y = histogram[i] / ((double) tot_cnt);
+		printf("%d	%f	%d	%f\n", i, x, histogram[i], y);
+	}
+	fflush(stdout);
+}
+
 int main(int argc, char* argv[])
 {
+	histo_init();
 	for (int ns=0; ns < NSAMP; ns++)
 	{
 		int pt = walk(NUM_STEPS);
 		double x = pt / sqrt(NUM_STEPS);
+		histo_accum(x);
 	}
+
+	histo_print();
 }
