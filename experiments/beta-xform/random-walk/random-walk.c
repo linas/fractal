4f5fbe3479840bdf3e47ffa09440df5bf3abc66d
Linas Vepštas <linasvepstas@gmail.com>
2025-03-24 18:25:21 -0500

    Assorted fixes and a readme to explain

--- experiments/beta-xform/random-walk/random-walk.c
+++ experiments/beta-xform/random-walk/random-walk.c
@@ -9,12 +9,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* Number of histogram bins. */
-#define HISTO_NBINS 302
-
-/* Number of samples */
-#define NSAMP 5000
-
 int left_or_right(void)
 {
 	int r = rand();
@@ -30,6 +24,9 @@ int walk(int num_steps)
 
 	return pt;
 }
+
+/* Number of histogram bins. */
+#define HISTO_NBINS 302
 
 int histogram[HISTO_NBINS];
 
@@ -53,10 +50,12 @@ void histo_print(void)
 	for (int i=0; i< HISTO_NBINS; i++)
 		tot_cnt += histogram[i];
 
+	double norm = ((double) (HISTO_NBINS-2)) / ((double) tot_cnt);
+
 	for (int i=0; i< HISTO_NBINS; i++)
 	{
 		double x = (i-1) / ((double) (HISTO_NBINS-2));
-		double y = histogram[i] / ((double) tot_cnt);
+		double y = norm * histogram[i];
 		printf("%d	%f	%d	%f\n", i, x, histogram[i], y);
 	}
 	fflush(stdout);
@@ -64,21 +63,26 @@ void histo_print(void)
 
 int main(int argc, char* argv[])
 {
-	if (argc !=2)
+	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: %s nsteps\n", argv[0]);
+		fprintf(stderr, "Usage: %s nsteps nsamp\n", argv[0]);
 		exit(-1);
 	}
+
+	/* Number of samples */
+	int nsamp = atoi(argv[2]);
 
 	/* Number of steps to take */
 	int num_steps = atoi(argv[1]);
 	double norm = 1.0 / sqrt(num_steps);
 
+	/* Also missing sqrt 2pi but whatever. */
+
 	/* six sigma */
 	norm /= 6.0;
 
 	histo_init();
-	for (int ns=0; ns < NSAMP; ns++)
+	for (int ns=0; ns < nsamp; ns++)
 	{
 		int pt = walk(num_steps);
 		double x = norm * pt;
@@ -86,6 +90,6 @@ int main(int argc, char* argv[])
 		histo_accum(x);
 	}
 
-	printf("#\n# num-steps=%d\n#\n", num_steps);
+	printf("#\n# num-steps=%d nsamp=%d\n#\n", num_steps, nsamp);
 	histo_print();
 }
